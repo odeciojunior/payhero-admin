efbe8a1361df777f2873d362eab39a93da0cd432
julio leichtweis <julioleichtweis@gmail.com>
2018-11-30 10:01:40 -0200

    ajustes nos projetos

--- Modules/Projetos/Resources/views/index.blade.php
+++ Modules/Projetos/Resources/views/index.blade.php
@@ -29,7 +29,7 @@
 
         <div class="row">
           @foreach($projetos as $projeto)
-            <div class="col-3" style="height: 300px">
+            <div class="col-3">
               <div class="card" style="border: 1px solid gray">
                 <img class="card-img-top img-fluid w-full" src="{!! '/'.Modules\Core\Helpers\CaminhoArquivosHelper::CAMINHO_FOTO_PROJETO.$projeto->foto !!}" alt="Imagem não encontrada" style="height: 180px; margin: 8px 8px 8px 8px">
                 <div class="card-block">
