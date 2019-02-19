60331f5cfa77db7012c8eee314533cc71878e3f0
julioleichtweis@gmail.com <julioleichtweis@gmail.com>
2019-02-19 15:34:41 -0300

    implementando plugin com desconto shopify

--- Modules/Shopify/Http/Controllers/ShopifyController.php
+++ Modules/Shopify/Http/Controllers/ShopifyController.php
@@ -249,7 +249,6 @@ class ShopifyController extends Controller {
             ]);
         }
         catch(\Exception $e){
-            dd($e);
             return response()->json('Dados do shopify inválidos, revise os dados informados');
         }
 
