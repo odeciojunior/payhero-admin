cc57014969b8ca5d388e3e73eb7aaf44278fe588
julioleichtweis@gmail.com <julioleichtweis@gmail.com>
2019-03-11 15:09:12 -0300

    implementando login

--- Modules/Autenticacao/Http/routes.php
+++ Modules/Autenticacao/Http/routes.php
@@ -1,9 +1,6 @@
 <?php
 
-Route::group(['middleware' => 'web', 'prefix' => 'api/login', 'namespace' => 'Modules\Autenticacao\Http\Controllers'], function()
+Route::group(['middleware' => 'web', 'prefix' => 'autenticacao', 'namespace' => 'Modules\Autenticacao\Http\Controllers'], function()
 {
-    // Route::post('/', 'AutenticacaoController@login');
-    Route::post('/', function(){
-        return 'lalalalalong';
-    });
+    Route::get('/', 'AutenticacaoController@login');
 });
