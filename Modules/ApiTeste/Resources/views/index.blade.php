053ac1da81def6a71d4ef5a5fc40dc08e1e2f2af
julioleichtweis@gmail.com <julioleichtweis@gmail.com>
2019-03-11 15:48:59 -0300

    implementando login

--- Modules/ApiTeste/Resources/views/index.blade.php
+++ Modules/ApiTeste/Resources/views/index.blade.php
@@ -4,10 +4,10 @@
     <h1>Login</h1>
 
     <label>Login</label>
-    <input type="text" id="login" placeholder="login">
+    <input type="text" id="email" placeholder="login">
     <br>
     <label>Senha</label>
-    <input type="text" id="senha" placeholder="senha">
+    <input type="text" id="password" placeholder="senha">
     <br>
     <button id="logar">Sign up</button>
 
@@ -16,6 +16,24 @@
     <script>
 
         $(document).ready(function(){
+
+            $("#logar").on("click", function(){
+
+                $.ajax({
+                    method: "POST",
+                    url: "http://cloudfoxapi.tk/api/login",
+                    headers: {
+                        'Accept': 'application/json'
+                    },
+                    data: { email: $('#email').val(), password: $('#password').val() },
+                    error: function(){
+                        alert('Ocorreu algum erro');
+                    },
+                    success: function(data){
+                        alert(data.toSource());
+                    }
+                });
+            });
 
         });
 
--- Modules/Autenticacao/Http/Controllers/AutenticacaoController.php
+++ Modules/Autenticacao/Http/Controllers/AutenticacaoController.php
@@ -21,7 +21,7 @@ class AutenticacaoController extends Controller {
         $user = User::where('email',$request->email)->first();
 
         if(!$user){
-            return response(['status'=>'error', 'message'=>'Usuário não encontrado']);
+            return response()->json(['status'=>'error', 'message'=>'Usuário não encontrado']);
         }
 
         if(Hash::check($request->password, $user->password)){
@@ -38,10 +38,10 @@ class AutenticacaoController extends Controller {
                 ]
             ]);
 
-            return response(['data'=>json_decode((string) $response->getBody(), true)]);
+            return response()->json(['data'=>json_decode((string) $response->getBody(), true)]);
         }
 
-        return response(['status'=>'error', 'message'=>'Dados inválidos']);
+        return response()->json(['status'=>'error', 'message'=>'Dados inválidos']);
     }
 
 }
