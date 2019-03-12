d0aa06be7567a4286784d5ac3754a2b1527eb15b
julioleichtweis@gmail.com <julioleichtweis@gmail.com>
2019-03-12 11:32:55 -0300

    implementando api - menu lateral

--- Modules/Core/Http/routes.php
+++ Modules/Core/Http/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(['middleware' => 'auth:api', 'prefix' => 'layout', 'namespace' => 'Modules\Core\Http\Controllers'], function()
+Route::group(['middleware' => 'auth:api', 'prefix' => 'api/layout', 'namespace' => 'Modules\Core\Http\Controllers'], function()
 {
     Route::get('/getmenulateral', [
         'uses' => 'LayoutController@getMenuLateral',
