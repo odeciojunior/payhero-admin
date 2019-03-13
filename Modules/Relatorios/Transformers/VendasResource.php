b745009e95690642d57b69a5e718c95c8e7994f8
julioleichtweis@gmail.com <julioleichtweis@gmail.com>
2019-03-13 15:52:22 -0300

    implementando api - vendas

--- Modules/Relatorios/Transformers/VendasResource.php
+++ Modules/Relatorios/Transformers/VendasResource.php
@@ -3,6 +3,8 @@
 namespace Modules\Relatorios\Transformers;
 
 use App\Comprador;
+use Carbon\Carbon;
+use App\PlanoVenda;
 use Illuminate\Http\Resources\Json\Resource;
 
 class VendasResource extends Resource
@@ -15,17 +17,36 @@ class VendasResource extends Resource
      */
     public function toArray($request)
     {
-        // return parent::toArray($request);
-
         $comprador = Comprador::find($this->comprador);
+
+        $planos_venda = PlanoVenda::where('venda',$venda->id)->get()->toArray();
+        $produto = '';
+        if(count($planos_venda) > 1){
+            $produto = "Carrinho";
+        }
+        foreach($planos_venda as $plano_venda){
+            $plano = Plano::find($plano_venda);
+            $produto = $plano['nome'];
+        }
+
+        $status = '';
+        if($venda->pagamento_status == 'paid')
+            $status = 'Aprovada';
+        else if($venda->pagamento_status == 'rejected')
+            $status = 'Rejeitada';
+        else if($venda->pagamento_status == 'pending')
+            $status = 'Pendente';
+        else
+            $status = $this->pagamento_status;
 
         return [
             'id' => $this->id,
+            'produto' => $produto,
             'comprador' => $comprador['nome'],
-            'forma' => $this->forma_pagamento,
-            'status' => $this->pagamento_status,
-            'data_inicio' => $this->data_inicio,
-            'data_finalizada' => $this->data_finalizada,
+            'forma' => $this->forma_pagamento == 'cartao_credito' ? 'cartão' : $this->forma_pagamento,
+            'status' => $status,
+            'data_inicio' => $this->data_inicio ? with(new Carbon($this->data_inicio))->format('d/m/Y H:i:s') : '',
+            'data_finalizada' => $this->data_finalizada ? with(new Carbon($this->data_finalizada))->format('d/m/Y H:i:s') : '',
             'total_pago' => $this->valor_total_pago,
         ];
 
