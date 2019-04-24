f280a8169ee8744a2e7266c1791b734903913c12
Henrique Brites <henriquebrites@live.com>
2019-04-24 17:04:00 -0300

    ajustes nos projetos

--- Modules/Projetos/Resources/views/index.blade.php
+++ Modules/Projetos/Resources/views/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
   <!-- Page -->
-  <div class="page">
+  <div class="page"> 
 
     <div class="page-header">
         <h1 class="page-title">Meus projetos</h1>
@@ -12,70 +12,66 @@
     </div>
 
     <div class="page-content container-fluid">
-        <div class="panel pt-30 p-30" data-plugin="matchHeight">
 
-          @if(count($projetos) == 0)
-              <div class="alert alert-warning" role="alert">
-                  <strong>Ops!</strong> Você ainda não possui projetos cadastrados.
-              </div>
-          @else
-            <div class="row">
-              @foreach($projetos as $projeto)
-                <div class="col-sm-6 col-md-4 col-lg-3 col-xl-3" style="padding: 0 25px 0 25px">
-                  <div class="card" style="border: 1px solid #E6E6FA">
-                      <a href='/projetos/projeto/{!! $projeto['id'] !!}'>
-                        <img class="card-img-top img-fluid w-full" src="{!! '/'.Modules\Core\Helpers\CaminhoArquivosHelper::CAMINHO_FOTO_PROJETO.$projeto['foto'] !!}" alt="Imagem não encontrada" style="height: 180px;width: 90%; margin: 8px 0 8px 0">
+        @if(count($projetos) == 0)
+            <div class="alert alert-warning" role="alert">
+                <strong>Ops!</strong> Você ainda não possui projetos cadastrados.
+            </div>
+        @else
+          <div class="row">
+            @foreach($projetos as $projeto)
+              <div class="col-xl-3 col-md-6 info-panel">
+                  <div class="card card-shadow">
+                    <a href='/projetos/projeto/{!! $projeto['id'] !!}'>
+                      <img class="card-img-top img-fluid w-full" src="{!! '/'.Modules\Core\Helpers\CaminhoArquivosHelper::CAMINHO_FOTO_PROJETO.$projeto['foto'] !!}" alt="Imagem não encontrada" style="height: 180px;width: 90%; margin: 8px 0 8px 0">
+                    </a>
+                    <div class="card-block">
+                      <a href='/projetos/projeto/{!! $projeto['id'] !!}' class="text-center">
+                          <h4 class="card-title">{!! $projeto['nome'] !!}</h4>
+                          <hr>
+                          <p class="card-text">{!! $projeto['descricao'] !!}</p>
                       </a>
-                      <div class="card-block">
-                        <a href='/projetos/projeto/{!! $projeto['id'] !!}' class="text-center">
-                            <h4 class="card-title">{!! $projeto['nome'] !!}</h4>
-                            <hr>
-                            <p class="card-text">{!! $projeto['descricao'] !!}</p>
-                        </a>
-                    </div>
                   </div>
                 </div>
-              @endforeach
-            </div>
-          @endif
+              </div>
+            @endforeach
+          </div>
+        @endif
 
-          <!-- Modal com detalhes do projeto -->
-          <div class="modal fade example-modal-lg modal-3d-flip-vertical" id="modal_detalhes" aria-hidden="true" aria-labelledby="exampleModalTitle" role="dialog" tabindex="-1">
-            <div class="modal-dialog modal-simple">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">×</span>
-                  </button>
-                  <h4 id="modal_detalhes_titulo" class="modal-title" style="width: 100%; text-align:center"></h4>
-                </div>
-                <div id="modal_detalhes_body" class="modal-body">
+        <div class="modal fade example-modal-lg modal-3d-flip-vertical" id="modal_detalhes" aria-hidden="true" aria-labelledby="exampleModalTitle" role="dialog" tabindex="-1">
+          <div class="modal-dialog modal-simple">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">×</span>
+                </button>
+                <h4 id="modal_detalhes_titulo" class="modal-title" style="width: 100%; text-align:center"></h4>
+              </div>
+              <div id="modal_detalhes_body" class="modal-body">
 
-                </div>
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-danger" data-dismiss="modal">Fechar</button>
-                </div>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Fechar</button>
               </div>
             </div>
           </div>
-          <!-- End Modal -->
+        </div>
 
-          <div class="modal fade example-modal-lg modal-3d-flip-vertical" id="modal_excluir" aria-hidden="true" aria-labelledby="exampleModalTitle" role="dialog" tabindex="-1">
-              <div class="modal-dialog modal-simple">
-                <div class="modal-content">
-                    <div class="modal-header">
-                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
-                      </button>
-                      <h4 id="modal_excluir_titulo" class="modal-title" style="width: 100%; text-align:center"></h4>
-                    </div>
-                    <div id="modal_excluir_body" class="modal-body">
-                    </div>
-                    <div class="modal-footer">
-                      <button type="button" class="btn btn-danger" data-dismiss="modal">Fechar</button>
-                      <a id="excluir_projeto" class="btn btn-success">Confirmar</a>
-                    </div>
-                </div>
+        <div class="modal fade example-modal-lg modal-3d-flip-vertical" id="modal_excluir" aria-hidden="true" aria-labelledby="exampleModalTitle" role="dialog" tabindex="-1">
+            <div class="modal-dialog modal-simple">
+              <div class="modal-content">
+                  <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">×</span>
+                    </button>
+                    <h4 id="modal_excluir_titulo" class="modal-title" style="width: 100%; text-align:center"></h4>
+                  </div>
+                  <div id="modal_excluir_body" class="modal-body">
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">Fechar</button>
+                    <a id="excluir_projeto" class="btn btn-success">Confirmar</a>
+                  </div>
               </div>
             </div>
         </div>
