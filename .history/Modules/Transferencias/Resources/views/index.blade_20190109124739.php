a45d279138123bcb970d38369797cc24491babb7
julio leichtweis <julioleichtweis@gmail.com>
2019-06-10 10:25:30 -0300

    removendo .history

--- .history/Modules/Transferencias/Resources/views/index.blade_20190109124739.php
+++
@@ -1,9 +0,0 @@
-@extends('transferencias::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('transferencias.name') !!}
-    </p>
-@stop
