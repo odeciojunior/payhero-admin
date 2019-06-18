e30a6fdf5fb611aec1c9b4a798442be11e5bec24
Gustavo Carvalho <gcarvalho5@gmail.com>
2019-06-18 19:39:17 -0300

    profile 1.2

---
+++ resources/lang/br/definitions.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Database Definitions Language Lines
+    |--------------------------------------------------------------------------
+    | Definicoes de cada campo ao se traduzir para ser utilizado nas views
+    |
+    */
+    'enum' => [
+        "personal_document_status" => [
+            "pending"   => "Pendente",
+            "analyzing" => "Em análise",
+            "approved"  => "Aprovado",
+            "refused"   => "Recusado",
+        ],
+    ],
+
+];
