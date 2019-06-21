fa7088fd24bc03bef22ad8b52be4746dc8927019
giovanitier <giovanitier@gmail.com>
2019-06-21 17:38:13 -0300

    Interface da página perfil e form para criação de projetos

--- Modules/Projects/Resources/views/create.blade.php
+++ Modules/Projects/Resources/views/create.blade.php
@@ -13,17 +13,18 @@
         </div>
         <div class="page-content container">
             @if($companies->count() > 0)
-                <div class="panel pt-30 p-30" data-plugin="matchHeight">
+                <div class="card shadow pt-30 p-30" data-plugin="matchHeight">
                     <form method="POST" action="/projects" enctype="multipart/form-data">
                         @csrf
                         @method('POST')
-                        <h4> Dados gerais </h4>
                         <div class='row'>
                             <div class='col-md-6'>
                                 <input name='project_photo' type='file' class='form-control' id='project-photo' style="display:none">
                                 <div style="margin: 20px 0 0 30px;">
                                     <label for='preview-image-project'>Selecione a foto do projeto</label>
-                                    <img id='preview-image-project' alt='Selecione a foto do projeto' src="{{asset('modules/projects/img/projeto.png')}}" style="max-height: 300px; max-width: 300px;">
+                                    <img id="preview-image-project"  alt='Selecione a foto do projeto' src="{{asset('modules/global/assets/img/projeto.png')}}" style="max-height: 300px; max-width: 300px;">
+                                   <br> <a href="#" id="preview-image-project" class="btn btn-primary mt-15"> <i class="icon fa-cloud-upload" aria-hidden="true"></i> Upload </a>
+
                                 </div>
                                 <input type='hidden' name='photo_x1'/> <input type='hidden' name='photo_y1'/>
                                 <input type='hidden' name='photo_w'/> <input type='hidden' name='photo_h'/>
@@ -40,7 +41,7 @@
                                 </div>
                                 <div class='form-group col-xl-12'>
                                     <label for='company'>Empresa</label>
-                                    <select name='company' class='form-control' id='company' required>
+                                    <select name='company' class='form-control select-pad' id='company' required>
                                         <option value=''>Selecione</option>
                                         @foreach($companies as $company)
                                             <option value='{{Hashids::encode($company->id)}}'>{{$company->fantasy_name}}</option>
@@ -54,7 +55,7 @@
                                 </div>
                                 <div class='form-group col-xl-12'>
                                     <label for='description'>Descrição</label>
-                                    <textarea name='description' class='form-control' id='description' placeholder='Descrição' rows='4' cols='50'></textarea>
+                                    <textarea name='description' class='form-control select-pad' id='description' placeholder='Descrição' rows='4' cols='50'></textarea>
                                     @if ($errors->has('description'))
                                         <div class="invalid-feedback d-block">
                                             <strong>{{ $errors->first('description') }}</strong>
