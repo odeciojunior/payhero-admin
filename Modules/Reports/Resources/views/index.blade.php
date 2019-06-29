be4c799823a974c24975fe6e737da5a8be432093
julioleichtweis@gmail.com <julioleichtweis@gmail.com>
2019-06-29 19:48:41 -0300

    ajuste no layout do relatorio

--- Modules/Reports/Resources/views/index.blade.php
+++ Modules/Reports/Resources/views/index.blade.php
@@ -3,7 +3,6 @@
 
 @section('content')
 
-
     <div class="page">
         <div class="page-header container">
             <div class="row">
@@ -11,47 +10,33 @@
                     <h1 class="page-title">Relatórios</h1>
                     <span type="hidden" class="error-data"></span>
                 </div>
-                <div class="col-lg-12 mt-30">
-                    <div class="row justify-content-between align-items-start">
-                        <div class="col-3">
-                            <div class="input-holder">
-                                <select id='project' class="form-control">
-                                    @foreach($projects as $project)
-                                        <option value='{{$project->id_code}}'>{{$project->name}}</option>
-                                    @endforeach
-                                </select>
-                            </div>
+            </div>
+            <div class="page-content container">
+                <div class="row">
+                    <div class="col-3">
+                        <div class="">
+                            <label for="project">Selecione o projeto:</label>
+                            <select id='project' class="form-control">
+                                @foreach($projects as $project)
+                                    <option value='{{$project->id_code}}'>{{$project->name}}</option>
+                                @endforeach
+                            </select>
                         </div>
-                        <div class="col-lg-4 align-items-baseline">
-                            <div class="col-4">
-
-                            </div>
-                            <div class="row justify-content-end align-items-center">
-                                {{--<div class="col-lg-5 text-right" id='calendar-range'>
-                                    <a id="calendar" class="text-filtros">
-                                        <svg class="icon-filtro" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                            <path d="M24 2v22h-24v-22h3v1c0 1.103.897 2 2 2s2-.897 2-2v-1h10v1c0 1.103.897 2 2 2s2-.897 2-2v-1h3zm-2 6h-20v14h20v-14zm-2-7c0-.552-.447-1-1-1s-1 .448-1 1v2c0 .552.447 1 1 1s1-.448 1-1v-2zm-14 2c0 .552-.447 1-1 1s-1-.448-1-1v-2c0-.552.447-1 1-1s1 .448 1 1v2zm6.687 13.482c0-.802-.418-1.429-1.109-1.695.528-.264.836-.807.836-1.503 0-1.346-1.312-2.149-2.581-2.149-1.477 0-2.591.925-2.659 2.763h1.645c-.014-.761.271-1.315 1.025-1.315.449 0 .933.272.933.869 0 .754-.816.862-1.567.797v1.28c1.067 0 1.704.067 1.704.985 0 .724-.548 1.048-1.091 1.048-.822 0-1.159-.614-1.188-1.452h-1.634c-.032 1.892 1.114 2.89 2.842 2.89 1.543 0 2.844-.943 2.844-2.518zm4.313 2.518v-7.718h-1.392c-.173 1.154-.995 1.491-2.171 1.459v1.346h1.852v4.913h1.711z"></path>
-                                        </svg>
-                                        Personalizado
-                                    </a>
-                                </div>--}}
-                                <div class="input-group-prepend">
-                                    <div class="input-group-text px-1 px-md-2">
-                                        <svg class="icon-filtro" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                            <path d="M24 2v22h-24v-22h3v1c0 1.103.897 2 2 2s2-.897 2-2v-1h10v1c0 1.103.897 2 2 2s2-.897 2-2v-1h3zm-2 6h-20v14h20v-14zm-2-7c0-.552-.447-1-1-1s-1 .448-1 1v2c0 .552.447 1 1 1s1-.448 1-1v-2zm-14 2c0 .552-.447 1-1 1s-1-.448-1-1v-2c0-.552.447-1 1-1s1 .448 1 1v2zm6.687 13.482c0-.802-.418-1.429-1.109-1.695.528-.264.836-.807.836-1.503 0-1.346-1.312-2.149-2.581-2.149-1.477 0-2.591.925-2.659 2.763h1.645c-.014-.761.271-1.315 1.025-1.315.449 0 .933.272.933.869 0 .754-.816.862-1.567.797v1.28c1.067 0 1.704.067 1.704.985 0 .724-.548 1.048-1.091 1.048-.822 0-1.159-.614-1.188-1.452h-1.634c-.032 1.892 1.114 2.89 2.842 2.89 1.543 0 2.844-.943 2.844-2.518zm4.313 2.518v-7.718h-1.392c-.173 1.154-.995 1.491-2.171 1.459v1.346h1.852v4.913h1.711z"></path>
-                                        </svg>
-                                    </div>
-                                    <input type="text" name="daterange" class="form-control pull-right" value="">
-
+                    </div>
+                    <div class="col-9 align-items-baseline">
+                        <div class="row justify-content-end align-items-center">
+                            <div class="input-group-prepend">
+                                <div class="input-group-text px-1 px-md-2">
+                                    <svg class="icon-filtro" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                        <path d="M24 2v22h-24v-22h3v1c0 1.103.897 2 2 2s2-.897 2-2v-1h10v1c0 1.103.897 2 2 2s2-.897 2-2v-1h3zm-2 6h-20v14h20v-14zm-2-7c0-.552-.447-1-1-1s-1 .448-1 1v2c0 .552.447 1 1 1s1-.448 1-1v-2zm-14 2c0 .552-.447 1-1 1s-1-.448-1-1v-2c0-.552.447-1 1-1s1 .448 1 1v2zm6.687 13.482c0-.802-.418-1.429-1.109-1.695.528-.264.836-.807.836-1.503 0-1.346-1.312-2.149-2.581-2.149-1.477 0-2.591.925-2.659 2.763h1.645c-.014-.761.271-1.315 1.025-1.315.449 0 .933.272.933.869 0 .754-.816.862-1.567.797v1.28c1.067 0 1.704.067 1.704.985 0 .724-.548 1.048-1.091 1.048-.822 0-1.159-.614-1.188-1.452h-1.634c-.032 1.892 1.114 2.89 2.842 2.89 1.543 0 2.844-.943 2.844-2.518zm4.313 2.518v-7.718h-1.392c-.173 1.154-.995 1.491-2.171 1.459v1.346h1.852v4.913h1.711z"></path>
+                                    </svg>
                                 </div>
-
-
+                                <input id="date-filter" type="text" name="daterange" class="form-control pull-right" value="">
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="page-content container">
+        
                 <div class="nav-tabs-line">
                     <div class="nav nav-tabs" id="nav-tab" role="tablist">
                         <a class="nav-item nav-link active" id="nav-vendas-tab" data-toggle="tab" href="#nav-vendas"
@@ -90,13 +75,13 @@
                                                 <h6 class="label-price relatorios"> Reembolsos </h6>
                                                 <h4 class="number purple" id='qtd-reembolso'>0</h4>
                                             </div>
-                                            <div class="col-lg-12">
+                                            <!--div class="col-lg-12">
                                                 <div class="grafico">
                                                     <div class="text">
                                                         <h1 class="text-muted op5"> Graph here </h1>
                                                     </div>
                                                 </div>
-                                            </div>
+                                            </div-->
                                         </div>
                                     </div>
                                 </div>
@@ -209,7 +194,9 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="card shadow  gutter_top">
+                            </div>
+                            <div class="col-lg-6 gutter_top">
+                                <div class="card shadow">
                                     <div class="card-header">
                                         <h4> Meios de Pagamento </h4>
                                     </div>
@@ -243,8 +230,8 @@
                                                             0
                                                         </div>
                                                         {{-- <div class="col-lg-2" id='percent-boleto-total'>
-                                                             16%
-                                                         </div>--}}
+                                                                16%
+                                                            </div>--}}
                                                         <div class="col-lg-4">
                                                             <span class="money-td green" id='boleto-value'></span>
                                                         </div>
