d179c830c409473fde7bda256751fd335cfb7a9e
julio leichtweis <julioleichtweis@gmail.com>
2019-07-01 10:17:20 -0300

    ajuste na rotina de transferencia de saldo

--- Modules/Core/Transferencias/Transferencias.php
+++ Modules/Core/Transferencias/Transferencias.php
@@ -4,37 +4,47 @@ namespace Modules\Core\Tranferencias;
 
 use Carbon\Carbon;
 use App\Entities\Company;
+use App\Entities\Transfer;
 use App\Entities\Transaction;
 use Modules\Core\Sms\SmsService;
+use Illuminate\Support\Facades\Log;
 
 class Transferencias {
 
     public static function verify(){
 
-        $transactions = Transaction::where('delivery_date',Carbon::now()->format('Y-m-d'))->get()->toArray();
+        $transactions = Transaction::where([
+            ['release_date','<=',Carbon::now()->format('Y-m-d')],
+            ['status', 'paid']
+        ])->get()->toArray();
+
+        $transfers = [];
 
         foreach($transactions as $t){
 
             $company = Company::find($t['company']);
 
-            Transfer::create([
+            $transfer = Transfer::create([
                 'transaction' => $t['id'],
-                'user'        => $company['user'],
+                'user'        => $company['user_id'],
                 'value'       => $t['value'],
                 'type'        => 'in',
             ]);
 
-            $transacao = Transaction::find($t['id']);
+            $transfers[] = $transfer;
 
-            $transacao->update([
+            $transaction = Transaction::find($t['id']);
+
+            $transaction->update([
                 'status' => 'transfered'
             ]);
 
             $company->update([
-                'balance' => $company['balance'] + substr_replace($t['value'], '.',strlen($t['value']) - 2, 0 ) 
+                'balance' => intval($company->balance) +  intval(preg_replace("/[^0-9]/", "", $t['value']))
             ]);
         }
-    }
 
+        Log::info('transferencias criadas ' . print_r($transfers, true));
+    }
 
 }
