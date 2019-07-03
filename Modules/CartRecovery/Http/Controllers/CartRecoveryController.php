6714bc7137da9ab56b10811a5f0eaf837c5d0071
LuanMaia1234 <luanmaia05@hotmail.com>
2019-07-03 18:46:13 -0300

    filtro carrinho abandonado

--- Modules/CartRecovery/Http/Controllers/CartRecoveryController.php
+++ Modules/CartRecovery/Http/Controllers/CartRecoveryController.php
@@ -2,6 +2,7 @@
 
 namespace Modules\CartRecovery\Http\Controllers;
 
+use App\Entities\Project;
 use Carbon\Carbon;
 use App\Entities\Log;
 use App\Entities\Plan;
@@ -16,27 +17,63 @@ use Yajra\DataTables\Facades\DataTables;
 use Modules\CartRecovery\Transformers\CartRecoveryResource;
 use Modules\CartRecovery\Transformers\CarrinhosAbandonadosResource;
 
-class CartRecoveryController extends Controller {
+class CartRecoveryController extends Controller
+{
+    public function index()
+    {
+        $userProjects = UserProject::where('user', \Auth::user()->id)->get()->toArray();
+        $projects     = [];
 
+        foreach ($userProjects as $userProject) {
+            $project = Project::find($userProject['project']);
+            if ($project['id'] != null) {
+                $projects[] = [
+                    'id'   => $project['id'],
+                    'nome' => $project['name'],
+                ];
+            }
+        }
 
-    public function index() {
-
-        return view('cartrecovery::index');
+        return view('cartrecovery::index', compact('projects'));
     }
- 
-    public function getAbandonatedCarts(Request $request){
 
+    public function getAbandonatedCarts(Request $request)
+    {
+        $client = $request->input('client');
+        //                        dd($request->all());
         $userProjects = UserProject::where([
-            ['user', \Auth::user()->id],
-            ['type','producer']
-        ])->pluck('project')->toArray();
+                                               ['user', \Auth::user()->id],
+                                               ['type', 'producer'],
+                                           ])->pluck('project')->toArray();
 
-        $abandonedCarts = Checkout::whereIn('status',['abandoned cart', 'recovered'])
-                                  ->whereIn('project',$userProjects)
-                                  ->orderBy('id','DESC');
+        $abandonedCarts = Checkout::whereIn('status', ['abandoned cart', 'recovered']);
+
+        //        if ($request->has('client') && $request->input('client') != '') {
+        //
+        //            $abandonedCarts->leftJoin('logs as log', function($join) use ($client) {
+        //                //                $join->where('checkouts.id_log_session', '=', 'log.id_log_session');
+        //                $join->where('log.name', 'like', '%' . $client . '%');
+        //            })->get();
+        //            dd($abandonedCarts[0]->name);
+        //        }
+        if ($request->has('project') && $request->input('project') != '') {
+            $abandonedCarts->where('project', $request->input('project'));
+        } else {
+            $abandonedCarts->whereIn('project', $userProjects)->with(['projectModel']);
+        }
+        if ($request->has('start_date') && $request->input('start_date') != '' && $request->has('end_date') && $request->input('end_date') != '') {
+            $abandonedCarts->whereBetween('created_at', [$request->input('start_date'), date('Y-m-d', strtotime($request->input('end_date') . ' + 1 day'))]);
+        } else {
+            if ($request->input('start_date') != '') {
+                $abandonedCarts->whereDate('created_at', '>=', $request->input('start_date'));
+            }
+
+            if ($request->has('end_date') && $request->input('end_date') != '') {
+                $abandonedCarts->whereDate('created_at', '<', date('Y-m-d', strtotime($request->input('end_date') . ' + 1 day')));
+            }
+        }
+        $abandonedCarts->orderBy('id', 'DESC');
 
         return CartRecoveryResource::collection($abandonedCarts->paginate(10));
-
     }
-
 }
--- Modules/CartRecovery/Transformers/CartRecoveryResource.php
+++ Modules/CartRecovery/Transformers/CartRecoveryResource.php
@@ -2,57 +2,60 @@
 
 namespace Modules\CartRecovery\Transformers;
 
+use App\Entities\Project;
 use Carbon\Carbon;
 use App\Entities\Log;
 use App\Entities\Plan;
 use App\Entities\Domain;
 use App\Entities\CheckoutPlan;
+use Vinkla\Hashids\Facades\Hashids;
 use Illuminate\Http\Resources\Json\Resource;
 
 class CartRecoveryResource extends Resource
 {
     /**
      * Transform the resource into an array.
-     *
      * @param  \Illuminate\Http\Request
      * @return array
      */
     public function toArray($request)
     {
         $client = '';
-        $log = Log::where('id_log_session', $this->id_log_session)->orderBy('id','DESC')->first();
-        if($log){
+        $log    = Log::where('id_log_session', $this->id_log_session)->orderBy('id', 'DESC')->first();
+
+        if ($log) {
             $client = $log->name;
         }
- 
+
         $status = '';
-        if($this->status == 'abandoned cart'){
+        if ($this->status == 'abandoned cart') {
             $status = 'Não recuperado';
-        }
-        else{
+        } else {
             $status = 'Recuperado';
         }
 
-        $value = 0;
-        $plannCheckout = CheckoutPlan::where('checkout',$this->id)->get()->toArray();
-        foreach($plannCheckout as $planCheckout){
-            $plan = Plan::find($planCheckout['plan']);
-            $value += str_replace('.','',$plan['price']) * $planCheckout['amount'];
+        $value         = 0;
+        $plannCheckout = CheckoutPlan::where('checkout', $this->id)->get()->toArray();
+        foreach ($plannCheckout as $planCheckout) {
+            $plan  = Plan::find($planCheckout['plan']);
+            $value += str_replace('.', '', $plan['price']) * $planCheckout['amount'];
         }
-        $value = substr_replace($value, '.',strlen($value) - 2, 0 );
+        $value = substr_replace($value, '.', strlen($value) - 2, 0);
 
-        $domain = Domain::where('project_id',$this->project)->first();
-        $link = "https://checkout.".$domain['name']."/recovery/".$this->id_log_session;
+        $domain = Domain::where('project_id', $this->project)->first();
+        $link   = "https://checkout." . $domain['name'] . "/recovery/" . $this->id_log_session;
 
         return [
+            'id'              => Hashids::encode($this->id),
             'date'            => with(new Carbon($this->created_at))->format('d/m/Y H:i:s'),
+            'project'         => $this->projectModel->name,
             'client'          => $client,
             'email_status'    => 'Não enviado',
             'sms_status'      => 'Não enviado',
             'recovery_status' => $status,
             'value'           => $value,
             'link'            => $link,
-            'whatsapp_link'   => "https://api.whatsapp.com/send?phone=55" . preg_replace('/[^0-9]/', '', $log->telephone)
+            'whatsapp_link'   => "https://api.whatsapp.com/send?phone=55" . preg_replace('/[^0-9]/', '', $log->telephone),
         ];
 
         return parent::toArray($request);
