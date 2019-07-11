e312a60f6956b6de31e1da73317aa8cbfb9a7fa4
Gustavo Carvalho <gcarvalho5@gmail.com>
2019-07-11 11:16:07 -0300

    Retirado validacao do click em domains
    Former-commit-id: 62d6f27cdf7a6cc855649a9e394a40c12251be03

--- Modules/Domains/Transformers/DomainResource.php
+++ Modules/Domains/Transformers/DomainResource.php
@@ -53,35 +53,6 @@ class DomainResource extends Resource
     public function toArray($request)
     {
 
-        if ($this->status != $this->getEnum('status', 'approved')) {
-
-            $activated = null;
-            $responseValidateDomain = null;
-            $responseValidateLink = null;
-/*
-            $activated = $this->getCloudFlareService()->activationCheck($this->name);
-
-            $linkBrandResponse = $this->getSendgridService()->getLinkBrand($this->name);
-            $sendgridResponse  = $this->getSendgridService()->getZone($this->name);
-
-            if (!empty($linkBrandResponse) && !empty($sendgridResponse)) {
-                $responseValidateDomain = $this->getSendgridService()->validateDomain($sendgridResponse->id);
-                $responseValidateLink   = $this->getSendgridService()->validateBrandLink($linkBrandResponse->id);
-            }
-
-            if ($activated && $responseValidateDomain && $responseValidateLink) {
-                $this->update([
-                                  'status' => $this->resource->getEnum('status', 'approved'),
-                              ]);
-            }
-*/
-            if ($this->getCloudFlareService()->checkHtmlMetadata('https://checkout.' . $this->name, 'checkout-cloudfox', '1')) {
-                $this->update([
-                                  'status' => $this->resource->getEnum('status', 'approved'),
-                              ]);
-            }
-        }
-
         return [
             'id'                => Hashids::encode($this->id),
             'domain'            => $this->name,
