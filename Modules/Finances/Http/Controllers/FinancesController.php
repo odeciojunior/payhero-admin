cab81fd591d61b8b54e0a682378fa15dc1c94406
Gustavo Carvalho <gcarvalho5@gmail.com>
2019-07-22 16:55:01 -0300

    Ajustes padrao Finances
    Former-commit-id: 9ed12012ade31000d70f2cb4168e9b01c426eaaf

--- Modules/Finances/Http/Controllers/FinancesController.php
+++ Modules/Finances/Http/Controllers/FinancesController.php
@@ -11,73 +11,69 @@ use Illuminate\Http\Response;
 use Illuminate\Routing\Controller;
 use Vinkla\Hashids\Facades\Hashids;
 
-class FinancesController extends Controller {
-
+class FinancesController extends Controller
+{
     /**
-     * @var Company
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    private $company;
-
-    /**
-     * WithdrawalController constructor.
-     * @param Company $company
-     */
-    public function __construct(Company $company)
+    public function index()
     {
-        $this->company = $company;
-    }
- 
-    public function index() {
+        $companyModel = new Company();
 
-        $userCompanies = $this->company->where('user_id', auth()->user()->id)->get()->toArray();
+        $userCompanies = $companyModel->where('user_id', auth()->user()->id)->get()->toArray();
 
         return view('finances::index', [
             'companies' => $userCompanies,
         ]);
     }
 
-    public function getBalances($company_id){
+    /**
+     * @param $company_id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function getBalances($company_id)
+    {
+        $companyModel     = new Company();
+        $transactionModel = new Transaction();
 
         $antecipableBalance = 0;
         $pendingBalance     = 0;
 
-        $company = $this->company->find(current(Hashids::decode($company_id)));
+        $company = $companyModel->find(current(Hashids::decode($company_id)));
 
-        $pendingTransactions = Transaction::where('company',$company->id)
-            ->where('status','paid')
-            ->whereDate('release_date', '>', Carbon::today()->toDateString())
-            ->get()->toArray();
+        $pendingTransactions = $transactionModel->where('company', $company->id)
+                                                ->where('status', 'paid')
+                                                ->whereDate('release_date', '>', Carbon::today()->toDateString())
+                                                ->get()->toArray();
 
-        if(count($pendingTransactions)){
-            foreach($pendingTransactions as $pendingTransaction){
+        if (count($pendingTransactions)) {
+            foreach ($pendingTransactions as $pendingTransaction) {
                 $pendingBalance += $pendingTransaction['value'];
             }
         }
 
-        $antecipableTransactions = Transaction::where('company',$company->id)
-            ->whereDate('release_date', '>', Carbon::today())
-            ->whereDate('antecipation_date', '<=', Carbon::today())
-            ->get()->toArray();
+        $antecipableTransactions = $transactionModel->where('company', $company->id)
+                                                    ->whereDate('release_date', '>', Carbon::today())
+                                                    ->whereDate('antecipation_date', '<=', Carbon::today())
+                                                    ->get()->toArray();
 
-        if(count($antecipableTransactions)){
-            foreach($antecipableTransactions as $antecipableTransaction){
+        if (count($antecipableTransactions)) {
+            foreach ($antecipableTransactions as $antecipableTransaction) {
                 $antecipableBalance += $antecipableTransaction['antecipable_value'];
             }
         }
 
-        $availableBalance   = $company->balance;
-        $totalBalance       = $availableBalance + $pendingBalance;
+        $availableBalance = $company->balance;
+        $totalBalance     = $availableBalance + $pendingBalance;
 
         return response()->json([
-            'available_balance'   => number_format(intval($availableBalance) / 100, 2, ',', '.'),
-            'antecipable_balance' => number_format(intval($antecipableBalance) / 100, 2, ',', '.'),
-            'total_balance'       => number_format(intval($totalBalance) / 100, 2, ',', '.'),
-            'pending_balance'     => number_format(intval($pendingBalance) / 100, 2, ',', '.'),
-            'currency'            => $company->country == 'usa' ? '$' : 'R$',
-        ]);
-
+                                    'available_balance'   => number_format(intval($availableBalance) / 100, 2, ',', '.'),
+                                    'antecipable_balance' => number_format(intval($antecipableBalance) / 100, 2, ',', '.'),
+                                    'total_balance'       => number_format(intval($totalBalance) / 100, 2, ',', '.'),
+                                    'pending_balance'     => number_format(intval($pendingBalance) / 100, 2, ',', '.'),
+                                    'currency'            => $company->country == 'usa' ? '$' : 'R$',
+                                ]);
     }
-
 }
 
 
