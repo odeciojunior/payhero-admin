93c7692103879165b9279bb1b0bc686982e01165
Gustavo Carvalho <gcarvalho5@gmail.com>
2019-07-22 18:15:00 -0300

    Botao copiar invites
    Former-commit-id: f6e97ab3fac0c7952e02ed02d8f5eb520d100949

--- public/modules/invites/js/invites.js
+++ public/modules/invites/js/invites.js
@@ -7,32 +7,11 @@ $(document).ready(function () {
 
 });
 
-function copyTextToClipboard(text) {
-    var textArea = document.createElement("textarea");
-
-    textArea.style.position = 'fixed';
-    textArea.style.top = 0;
-    textArea.style.left = 0;
-    textArea.style.width = '2em';
-    textArea.style.height = '2em';
-    textArea.style.padding = 0;
-    textArea.style.border = 'none';
-    textArea.style.outline = 'none';
-    textArea.style.boxShadow = 'none';
-    textArea.style.background = 'transparent';
-    textArea.value = text;
-
-    document.body.appendChild(textArea);
-    textArea.select();
-
-    document.execCommand('copy');
-    document.body.removeChild(textArea);
-}
-
 $("#copy-link").on("click", function () {
 
-    var linkText = $("#invite-link").val();
-    copyTextToClipboard(linkText);
+    var copyText = document.getElementById("invite-link");
+    copyText.select();
+    document.execCommand("copy");
 
     alertCustom('success', 'Link copiado!');
 });
