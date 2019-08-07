8ac1acaa8a97676a4800611a15d7492a027fafa5
yanrRocha <contato.yanrocha@gmail.com>
2019-08-07 18:25:41 -0300

    project -> configuração ->
    Former-commit-id: 38b15be4af9cb6c06cbb72cc79a6ced3850444f1

--- Modules/Notifications/Resources/views/UserShopifyIntegrationStoreNotification.blade.php
+++ Modules/Notifications/Resources/views/UserShopifyIntegrationStoreNotification.blade.php
@@ -1,10 +1,10 @@
 <a class="list-group-item dropdown-item" href="/projects/{{$notification->data['link']}}" role="menuitem" id='item-notification'>
     <div class="media">
         <div class="pr-10">
-            <i class="icon wb-shopping-cart bg-green-600 green icon-circle" aria-hidden="true"></i>
+            <i class="fas fa-shopping-bag bg-green-600 green icon-circle" aria-hidden="true"></i>
         </div>
         <div class="media-body">
-            <h6 class="media-heading">
+            <h6 class="media-heading" style='white-space:normal'>
                 {{$notification->data['message']}}
             </h6>
             <time class="media-meta" datetime="2018-06-11T18:29:20+08:00">{{ date('d/m/Y H:m:s', strtotime($notification->updated_at)) }}</time>
