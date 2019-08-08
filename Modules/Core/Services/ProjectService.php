c139c1b4dbea5e408ca471a433a48a3053a0d452
Gustavo Carvalho <gcarvalho5@gmail.com>
2019-08-08 11:16:06 -0300

    ajuste remover projeto
    Former-commit-id: c9f75f69b901a5c8db12d38bfa11a99129f50625

--- Modules/Core/Services/ProjectService.php
+++ Modules/Core/Services/ProjectService.php
@@ -2,6 +2,7 @@
 
 namespace Modules\Core\Services;
 
+use App\Entities\DomainRecord;
 use App\Entities\Project;
 use App\Entities\ShopifyIntegration;
 use Exception;
@@ -22,6 +23,36 @@ class ProjectService
      * @var ShopifyIntegration
      */
     private $shopifyIntegrationModel;
+    /**
+     * @var CloudFlareService
+     */
+    private $cloudFlareService;
+    /**
+     * @var SendgridService
+     */
+    private $sendgridService;
+    /**
+     * @var DomainRecord
+     */
+    private $domainRecordModel;
+    /**
+     * @var ShopifyService
+     */
+    private $shopifyService;
+
+    /**
+     * @param string|null $urlStore
+     * @param string|null $token
+     * @return ShopifyService
+     */
+    private function getShopifyService(string $urlStore = null, string $token = null)
+    {
+        if (!$this->shopifyService) {
+            $this->shopifyService = new ShopifyService($urlStore, $token);
+        }
+
+        return $this->shopifyService;
+    }
 
     /**
      * @return \Illuminate\Contracts\Foundation\Application|mixed
@@ -33,6 +64,42 @@ class ProjectService
         }
 
         return $this->shopifyIntegrationModel;
+    }
+
+    /**
+     * @return \Illuminate\Contracts\Foundation\Application|mixed
+     */
+    private function getCloudFlareService()
+    {
+        if (!$this->cloudFlareService) {
+            $this->cloudFlareService = app(CloudFlareService::class);
+        }
+
+        return $this->cloudFlareService;
+    }
+
+    /**
+     * @return \Illuminate\Contracts\Foundation\Application|mixed|SendgridService
+     */
+    private function getSendgridService()
+    {
+        if (!$this->sendgridService) {
+            $this->sendgridService = app(SendgridService::class);
+        }
+
+        return $this->sendgridService;
+    }
+
+    /**
+     * @return \Illuminate\Contracts\Foundation\Application|mixed
+     */
+    private function getDomainRecordModel()
+    {
+        if (!$this->domainRecordModel) {
+            $this->domainRecordModel = app(DomainRecord::class);
+        }
+
+        return $this->domainRecordModel;
     }
 
     /**
@@ -99,11 +166,6 @@ class ProjectService
                     //n tem vendas
 
                     if (!empty($project->shopify_id)) {
-                        //remover integração do shopify
-
-                        $this->getShopifyIntegration()
-                             ->where('project', $project->id)
-                             ->delete();
 
                         if (!empty($project->plans)) {
                             foreach ($project->plans as $plan) {
@@ -155,37 +217,37 @@ class ProjectService
 
                     foreach ($project->domains as $domain) {
 
-                        if ($this->getCloudFlareService()->deleteZone($domain->name)) {
-                            //zona deletada
-                            $this->getSendgridService()->deleteLinkBrand($domain->name);
-                            $this->getSendgridService()->deleteZone($domain->name);
+                        $this->getCloudFlareService()->deleteZone($domain->name);
+                        //zona deletada
+                        $this->getSendgridService()->deleteLinkBrand($domain->name);
+                        $this->getSendgridService()->deleteZone($domain->name);
 
-                            $recordsDeleted = $this->getDomainRecordModel()->where('domain_id', $domain->id)->delete();
-                            $domainDeleted  = $domain->delete();
+                        $recordsDeleted = $this->getDomainRecordModel()->where('domain_id', $domain->id)->delete();
+                        $domainDeleted  = $domain->delete();
 
-                            if (!empty($project->shopify_id)) {
-                                //se for shopify, voltar as integraçoes ao html padrao
-                                try {
+                        if (!empty($project->shopify_id)) {
+                            //se for shopify, voltar as integraçoes ao html padrao
+                            try {
 
-                                    foreach ($project->shopifyIntegrations as $shopifyIntegration) {
-                                        $shopify = $this->getShopifyService($shopifyIntegration->url_store, $shopifyIntegration->token);
+                                foreach ($project->shopifyIntegrations as $shopifyIntegration) {
+                                    $shopify = $this->getShopifyService($shopifyIntegration->url_store, $shopifyIntegration->token);
 
-                                        $shopify->setThemeByRole('main');
-                                        $shopify->setTemplateHtml($shopifyIntegration->theme_file, $shopifyIntegration->theme_html);
-                                        $shopify->setTemplateHtml('layout/theme.liquid', $shopifyIntegration->layout_theme_html);
-                                        $shopifyIntegration->delete();
-                                    }
-                                } catch (\Exception $e) {
-                                    //throwl
-
-                                    throw new ServiceException('ProjectService - delete - erro ao mudar template ' . $e->getMessage(), $e->getCode(), $e);
+                                    $shopify->setThemeByRole('main');
+                                    $shopify->setTemplateHtml($shopifyIntegration->theme_file, $shopifyIntegration->theme_html);
+                                    $shopify->setTemplateHtml('layout/theme.liquid', $shopifyIntegration->layout_theme_html);
+                                    $shopifyIntegration->delete();
                                 }
+                            } catch (\Exception $e) {
+                                //throwl
+
+                                throw new ServiceException('ProjectService - delete - erro ao mudar template ' . $e->getMessage(), $e->getCode(), $e);
                             }
-                        } else {
-                            //erro ao deletar zona
-                            Log::warning('ProjectController - destroy - Erro ao deletar dominio - ' . $domain->name);
                         }
                     }
+                    //remover integração do shopify
+                    $this->getShopifyIntegration()
+                         ->where('project', $project->id)
+                         ->delete();
 
                     $projectDeleted = $project->delete();
                     if ($projectDeleted) {
