5de1fba74aee7eae84abf75e11f6d46a5e0c2297
joao lucas <luccas_lyber@hotmail.com.br>
2019-08-09 18:00:23 -0300

    fix invites
    Former-commit-id: fefd1f44466d54fc05b9ee851f5f7d38d955c3b3

--- Modules/Invites/Http/Controllers/InvitesApiController.php
+++ Modules/Invites/Http/Controllers/InvitesApiController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Log;
 use Modules\Core\Services\EmailService;
+use Modules\Core\Services\FoxUtils;
 use Modules\Invites\Transformers\InviteResource;
 use Vinkla\Hashids\Facades\Hashids;
 
@@ -35,41 +36,52 @@ class InvitesApiController extends Controller
     public function store(Request $request)
     {
         try {
+            if (!empty($request->input('email')) && !empty($request->input('company'))) {
+                $invitationModel = new Invitation();
+                $inviteSaved     = null;
 
-            $invitationModel = new Invitation();
-            $inviteSaved     = null;
+                $company = current(Hashids::decode($request->input('company')));
+                $invite  = $invitationModel->where([['email_invited', $request->input('email')], ['company', $company]])
+                                           ->first();
 
-            $company = current(Hashids::decode($request->input('company')));
-            $invite  = $invitationModel->where([['email_invited', $request->input('email')], ['company', $company]])
-                                       ->first();
+                if (FoxUtils::validateEmail($request->input('email'))) {
 
-            if (!$invite) {
-                $data   = [
-                    'invite'        => auth()->user()->id,
-                    'status'        => $invitationModel->getEnum('status', 'pending'),
-                    'company'       => current(Hashids::decode($request->input('company'))),
-                    'email_invited' => $request->input('email'),
-                    'parameter'     => $request->input('company'),
-                ];
-                $invite = $invitationModel->create($data);
-            }
-            try {
+                    if (!$invite) {
+                        $data   = [
+                            'invite'        => auth()->user()->id,
+                            'status'        => $invitationModel->getEnum('status', 'pending'),
+                            'company'       => current(Hashids::decode($request->input('company'))),
+                            'email_invited' => $request->input('email'),
+                            'parameter'     => $request->input('company'),
+                        ];
+                        $invite = $invitationModel->create($data);
+                    }
+                    try {
 
-                if (!$invite) {
+                        if (!$invite) {
+                            return response()->json([
+                                                        'message' => 'Erro ao tentar enviar convite',
+                                                    ], 400);
+                        } else {
+
+                            return response()->json([
+                                                        'message' => 'Convite enviado com sucesso!',
+                                                    ]);
+                        }
+                    } catch (Exception $e) {
+                        Log::warning('Erro ao tentar enviar convite (InvitesApiController - store)');
+                        report($e);
+
+                        return response()->json([
+                                                    'message' => 'Erro ao tentar enviar convite',
+                                                ], 400);
+                    }
+                } else {
                     return response()->json([
                                                 'message' => 'Erro ao tentar enviar convite',
                                             ], 400);
-                } else {
-                    EmailService::sendInvite($invite['email_invited'], $invite['parameter']);
-
-                    return response()->json([
-                                                'message' => 'Convite enviado com sucesso!',
-                                            ], 200);
                 }
-            } catch (Exception $e) {
-                Log::warning('Erro ao tentar enviar convite (InvitesApiController - store)');
-                report($e);
-
+            } else {
                 return response()->json([
                                             'message' => 'Erro ao tentar enviar convite',
                                         ], 400);
