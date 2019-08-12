243871bb185bbd7f675e752c6d588143a9a176de
joao lucas <luccas_lyber@hotmail.com.br>
2019-08-12 14:44:21 -0300

    fix bug transferResource
    Former-commit-id: c2bea6e93bfefb90ac7a0d83259e4ba43fe1718e

--- Modules/Finances/Http/routes.php
+++ Modules/Finances/Http/routes.php
@@ -3,17 +3,17 @@
 Route::group(
     [
         'prefix'     => 'finances',
-        'middleware' => ['web','auth'],
-        'namespace' => 'Modules\Finances\Http\Controllers'
+        'middleware' => ['web', 'auth'],
+        'namespace'  => 'Modules\Finances\Http\Controllers',
     ],
     function() {
         // rotas autenticadas
 
         Route::get('/', 'FinancesController@index')->name('finances');
+        Route::get('/testFinances', 'FinancesTestController@index')->name('financesTeste');
 
         Route::get('/getbalances/{company_id}', 'FinancesController@getBalances')->name('finances.balances');
     }
-
 
 );
 
