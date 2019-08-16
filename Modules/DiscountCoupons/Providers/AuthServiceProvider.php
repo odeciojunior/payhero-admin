baa41b93343df22db95ac460f1c9b7f87a050672
Gustavo Carvalho <gcarvalho5@gmail.com>
2019-08-16 10:36:11 -0300

    DiscontCoupomPolicy
    Former-commit-id: 65908d9968dd29b57926c2d359d7a0af9ac94b95

---
+++ Modules/DiscountCoupons/Providers/AuthServiceProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Modules\DiscountCoupons\Providers;
+
+use App\Entities\DiscountCoupon;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Modules\DiscountCoupons\Policies\DiscountCouponsPolicy;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * @var array
+     */
+    protected $policies = [
+        DiscountCoupon::class => DiscountCouponsPolicy::class,
+    ];
+
+    /**
+     * Boot
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->register(AuthServiceProvider::class);
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
+}
