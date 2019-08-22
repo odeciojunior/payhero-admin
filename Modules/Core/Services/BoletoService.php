6d20b294a5e277a36b1ed393aa549455b46375c8
LuanMaia1234 <luanmaia05@hotmail.com>
2019-08-22 14:16:59 -0300

    ajustes email
    Former-commit-id: 17e66a546318d3531de9796a7af2b7d6acc42b13

--- Modules/Core/Services/BoletoService.php
+++ Modules/Core/Services/BoletoService.php
@@ -228,14 +228,12 @@ class BoletoService
     {
         try {
             $saleModel    = new Sale();
-            $planModel    = new Plan();
             $projectModel = new Project();
             $domainModel  = new Domain();
 
             $date    = Carbon::now()->subDay('2')->toDateString();
             $boletos = $saleModel->where([['payment_method', '=', '2'], ['status', '=', '2'], [DB::raw("(DATE_FORMAT(start_date,'%Y-%m-%d'))"), $date]])
                                  ->with('clientModel', 'plansSales.plan.products')->get();
-
             foreach ($boletos as $boleto) {
                 try {
                     $checkoutModel = new Checkout();
@@ -243,11 +241,11 @@ class BoletoService
                     $checkout    = $checkoutModel->where("id", $boleto->checkout)->first();
                     $clientName  = $boleto->clientModel->name;
                     $clientEmail = $boleto->clientModel->email;
-                    $products    = [];
                     $data        = [];
-                    $subTotal    = 0;
-                    $iof         = preg_replace("/[^0-9]/", "", $boleto->iof);
-                    $discount    = preg_replace("/[^0-9]/", "", $boleto->shopify_discount);
+                    $subTotal    = $boleto->present()->getSubTotal();
+
+                    $iof      = preg_replace("/[^0-9]/", "", $boleto->iof);
+                    $discount = preg_replace("/[^0-9]/", "", $boleto->shopify_discount);
 
                     if ($iof == 0) {
                         $iof = '';
@@ -263,49 +261,37 @@ class BoletoService
                     $clientNameExploded       = explode(' ', $clientName);
                     $boleto->total_paid_value = preg_replace("/[^0-9]/", "", $boleto->iof) + preg_replace("/[^0-9]/", "", $boleto->total_paid_value);
                     $boleto->total_paid_value = substr_replace($boleto->total_paid_value, ',', strlen($boleto->total_paid_value) - 2, 0);
-                    foreach ($boleto->plansSales as $plansSale) {
-                        $plan    = $planModel->find($plansSale['plan']);
-                        $project = $projectModel->find($plan['project']);
-                        $domain  = $domainModel->where('project_id', $project->id)->first();
+                    $products                 = $boleto->present()->getProducts();
+                    $project                  = $projectModel->find($boleto->project);
+                    $domain                   = $domainModel->where('project_id', $project->id)->first();
 
-                        $subTotal += str_replace('.', '', $plan['price']) * $plansSale["amount"];
-                        foreach ($plansSale->getRelation('plan')->products as $product) {
-                            $productArray               = [];
-                            $productArray["photo"]      = $product->photo;
-                            $productArray["name"]       = $product->name;
-                            $productArray["name"]       = $product->name;
-                            $productArray["amount"]     = $plansSale->amount;
-                            $productArray["plan_value"] = $plansSale->plan_value;
-                            $products[]                 = $productArray;
-                        }
-                        $subTotal                = substr_replace($subTotal, ',', strlen($subTotal) - 2, 0);
-                        $boleto->shipment_value  = preg_replace("/[^0-9]/", "", $boleto->shipment_value);
-                        $boleto->shipment_value  = substr_replace($boleto->shipment_value, ',', strlen($boleto->shipment_value) - 2, 0);
-                        $boletoDigitableLine     = [];
-                        $boletoDigitableLine[0]  = substr($boleto->boleto_digitable_line, 0, 24);
-                        $boletoDigitableLine[1]  = substr($boleto->boleto_digitable_line, 24, strlen($boleto->boleto_digitable_line) - 1);
-                        $boleto->boleto_due_date = Carbon::parse($boleto->boleto_due_date)->format('d/m/y');
-                        $data                    = [
-                            "name"                  => $clientNameExploded[0],
-                            "boleto_link"           => $boleto->boleto_link,
-                            "boleto_digitable_line" => $boletoDigitableLine,
-                            "boleto_due_date"       => $boleto->boleto_due_date,
-                            "total_paid_value"      => $boleto->total_paid_value,
-                            "shipment_value"        => $boleto->shipment_value,
-                            "subtotal"              => strval($subTotal),
-                            "iof"                   => $iof,
-                            'discount'              => $discount,
-                            "project_logo"          => $project->logo,
-                            "project_contact"       => $project->contact,
-                            "products"              => $products,
-                        ];
-                        $emailValidated          = FoxUtils::validateEmail($clientEmail);
-                        if ($emailValidated) {
-                            $sendEmail = new SendgridService();
+                    $subTotal                = substr_replace($subTotal, ',', strlen($subTotal) - 2, 0);
+                    $boleto->shipment_value  = preg_replace("/[^0-9]/", "", $boleto->shipment_value);
+                    $boleto->shipment_value  = substr_replace($boleto->shipment_value, ',', strlen($boleto->shipment_value) - 2, 0);
+                    $boletoDigitableLine     = [];
+                    $boletoDigitableLine[0]  = substr($boleto->boleto_digitable_line, 0, 24);
+                    $boletoDigitableLine[1]  = substr($boleto->boleto_digitable_line, 24, strlen($boleto->boleto_digitable_line) - 1);
+                    $boleto->boleto_due_date = Carbon::parse($boleto->boleto_due_date)->format('d/m/y');
+                    $data                    = [
+                        "name"                  => $clientNameExploded[0],
+                        "boleto_link"           => $boleto->boleto_link,
+                        "boleto_digitable_line" => $boletoDigitableLine,
+                        "boleto_due_date"       => $boleto->boleto_due_date,
+                        "total_paid_value"      => $boleto->total_paid_value,
+                        "shipment_value"        => $boleto->shipment_value,
+                        "subtotal"              => strval($subTotal),
+                        "iof"                   => $iof,
+                        'discount'              => $discount,
+                        "project_logo"          => $project->logo,
+                        "project_contact"       => $project->contact,
+                        "products"              => $products,
+                    ];
+                    $emailValidated          = FoxUtils::validateEmail($clientEmail);
+                    if ($emailValidated) {
+                        $sendEmail = new SendgridService();
 
-                            $sendEmail->sendEmail('noreply@' . $domain['name'], $project['name'], $clientEmail, $clientNameExploded[0], 'd-690a6140f72643c1af280b079d5e84c5', $data);
-                            $checkout->increment('email_sent_amount');
-                        }
+                        $sendEmail->sendEmail('noreply@' . $domain['name'], $project['name'], $clientEmail, $clientNameExploded[0], 'd-690a6140f72643c1af280b079d5e84c5', $data);
+                        $checkout->increment('email_sent_amount');
                     }
                 } catch (Exception $e) {
                     Log::warning('Erro ao enviar boleto para e-mail no foreach - Vamos ter que liberar sua mercadoria');
