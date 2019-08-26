00e6f9e6f17ef7df497d040069d043a0f8bd5b8a
Gustavo Carvalho <gcarvalho5@gmail.com>
2019-08-26 15:21:00 -0300

    removendo modificador de prioridade do mx
    Former-commit-id: 5d4d8f89072e88dc38142768dae0339a2d747da0

--- app/Http/Controllers/Dev/TesteController.php
+++ app/Http/Controllers/Dev/TesteController.php
@@ -40,38 +40,6 @@ class TesteController extends Controller
         ///
     }
 
-    public function updateAllMxPriority()
-    {
-        //NAO REMOVER ATE SER RODADO EM PRODUCAO
-        $cf = new CloudFlareService();
-
-        $domains = Domain::with(['records'])->whereHas('records', function($query) {
-            $query->where('type', 'MX');
-        })->get();
-
-        foreach ($domains as $domain) {
-            try {
-                $cf->setZone($domain->name);
-            } catch (Exception $ex) {
-                continue;
-            }
-
-            $records = $cf->getRecords();
-            foreach ($records as $record) {
-                if ($record->type = 'MX') {
-                    if (empty($record->priority)) {
-                        $priority = 0;
-                    } else {
-                        $priority = $record->priority;
-                    }
-                    DomainRecord::where('cloudflare_record_id', $record->id)
-                                ->update([
-                                             'priority' => $priority,
-                                         ]);
-                }
-            }
-        }
-    }
 
     /**
      * @param Request $request
@@ -80,8 +48,6 @@ class TesteController extends Controller
      */
     public function indexx()
     {
-        $this->updateAllMxPriority();
-        dd('aaa');
 
         $this->mp = new MP(getenv('MERCADO_PAGO_ACCESS_TOKEN_PRODUCTION'));
 
@@ -301,6 +267,15 @@ class TesteController extends Controller
         }
 
         dd('heyy');
+    }
+
+    /**
+     * Funcao utilizada pelo tg
+     */
+    public function tgFunction()
+    {
+        //nada
+
     }
 }
 
