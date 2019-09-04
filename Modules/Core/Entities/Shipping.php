d2ca2b01bfb1c63ae44fcd5ac45a9d7b33a69f1e
LuanMaia1234 <luanmaia05@hotmail.com>
2019-09-04 17:16:15 -0300

    migration rename
    Former-commit-id: 7f456a97f6c897b0042fb30d48a95e0dbde67614

--- Modules/Core/Entities/Shipping.php
+++ Modules/Core/Entities/Shipping.php
@@ -37,7 +37,7 @@ class Shipping extends Model
      * @var array
      */
     protected $fillable = [
-        'project', 
+        'project_id',
         'name', 
         'information', 
         'value', 
