276db67d3477dfcdf72fd6972ea837cc10f56fe9
LuanMaia1234 <luanmaia05@hotmail.com>
2019-09-05 18:04:31 -0300

    presenters
    Former-commit-id: 7945d9bc2362ae18b7fa2944a6c57eb6752f2ccd

---
+++ Modules/Core/Presenters/CompanyDocumentPresenter.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Modules\Core\Presenters;
+
+use Laracasts\Presenter\Presenter;
+
+class CompanyDocumentPresenter extends Presenter
+{
+    public function getTypeEnum($status)
+    {
+
+        if (is_numeric($status)) {
+            switch ($status) {
+                case 1:
+                    return 'pending';
+                case 2:
+                    return 'analyzing';
+                case 3:
+                    return 'approved';
+                case 4:
+                    return 'refused';
+            }
+
+            return '';
+        } else {
+            switch ($status) {
+                case 'pending':
+                    return 1;
+                case 'analyzing':
+                    return 2;
+                case 'approved':
+                    return 3;
+                case 'refused':
+                    return 4;
+            }
+
+            return '';
+        }
+    }
+}
