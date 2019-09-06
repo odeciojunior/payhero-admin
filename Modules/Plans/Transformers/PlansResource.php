0e8cd45d8ce1d40b557242582fab36aef6f76fc7
LuanMaia1234 <luanmaia05@hotmail.com>
2019-09-06 12:11:51 -0300

    ajustes resource planos
    Former-commit-id: 3384df83f6a6606423037aaa1bb24bc7edc1ab8a

--- Modules/Plans/Transformers/PlansResource.php
+++ Modules/Plans/Transformers/PlansResource.php
@@ -14,11 +14,11 @@ class PlansResource extends Resource
             'id'                => Hashids::encode($this->id),
             'name'              => $this->name,
             'description'       => $this->description,
-            'code'              => isset($plan->project->domains[0]->name) ? 'https://checkout.' . $this->projectId->domains[0]->name . '/' . $this->code : 'Dominio não configurado',
+            'code'              => isset($this->project->domains[0]->name) ? 'https://checkout.' . $this->project->domains[0]->name . '/' . $this->code : 'Dominio não configurado',
             'price'             => 'R$ ' . number_format(intval(preg_replace("/[^0-9]/", "", $this->price)) / 100, 2, ',', '.'),
-            'status'            => isset($plan->project->domains[0]->name) ? 1 : 0,
+            'status'            => isset($this->project->domains[0]->name) ? 1 : 0,
             'status_code'       => $this->status,
-            'status_translated' => isset($plan->project->domains[0]->name) ? 'ativo' : 'desativo',
+            'status_translated' => isset($this->project->domains[0]->name) ? 'ativo' : 'desativo',
         ];
     }
 }
