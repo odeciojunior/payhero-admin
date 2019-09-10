5512cea0545cf05e2c1ad9adf9118d94f680b2e6
joao lucas <luccas_lyber@hotmail.com.br>
2019-09-10 10:51:36 -0300

    fix Dashboard
    Former-commit-id: 3bc01ee4f63e8af922ca88717521ffdea5e1fc50

--- Modules/Dashboard/Http/Controllers/DashboardController.php
+++ Modules/Dashboard/Http/Controllers/DashboardController.php
@@ -3,8 +3,12 @@
 namespace Modules\Dashboard\Http\Controllers;
 
 use Carbon\Carbon;
+use Exception;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\View\View;
 use Modules\Core\Entities\Plan;
 use Modules\Core\Entities\Sale;
 use Illuminate\Routing\Controller;
@@ -13,6 +17,7 @@ use Modules\Core\Entities\Project;
 use Illuminate\Support\Facades\Log;
 use Modules\Core\Entities\PlanSale;
 use Modules\Core\Entities\Transaction;
+use Vinkla\Hashids\Facades\Hashids;
 
 /**
  * Class DashboardController
@@ -21,76 +26,102 @@ use Modules\Core\Entities\Transaction;
 class DashboardController extends Controller
 {
     /**
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @return Factory|View
      */
     public function index()
     {
-        return view('dashboard::dashboard', [
-            'companies' => auth()->user()->companies()->get()->toArray(),
-        ]);
+        try {
+            return view('dashboard::dashboard', [
+                'companies' => auth()->user()->companies()->get(),
+            ]);
+        } catch (Exception $e) {
+            Log::warning('Erro ao buscar empresas do usuario (Dashboard - index)');
+            report($e);
+        }
     }
 
     /**
      * @param Request $request
-     * @return \Illuminate\Http\JsonResponse
+     * @return JsonResponse
      */
     public function getValues(Request $request)
     {
-        $companyModel     = new Company();
-        $transactionModel = new Transaction();
+        try {
+            if ($request->has('company') && !empty($request->input('company'))) {
+                $companyModel     = new Company();
+                $transactionModel = new Transaction();
+                $companyId        = current(Hashids::decode($request->company));
+                $company          = $companyModel->find($companyId);
 
-        $requestData = $request->all();
+                if (!empty($company)) {
+                    $antecipableBalance = 0;
+                    $pendingBalance     = 0;
 
-        $antecipableBalance = 0;
-        $pendingBalance     = 0;
+                    $pendingTransactions = $transactionModel->where('company_id', $company->id)
+                                                            ->where('status', 'paid')
+                                                            ->whereDate('release_date', '>', Carbon::today()
+                                                                                                   ->toDateString())
+                                                            ->get();
 
-        $company = $companyModel->find($request->company);
+                    if (count($pendingTransactions)) {
+                        foreach ($pendingTransactions as $pendingTransaction) {
+                            $pendingBalance += $pendingTransaction->value;
+                        }
+                    }
 
-        $pendingTransactions = $transactionModel->where('company_id', $request->company)
-                                                ->where('status', 'paid')
-                                                ->whereDate('release_date', '>', Carbon::today()->toDateString())
-                                                ->get();
+                    $anticipatedTransactions = $transactionModel->where('company_id', $company->id)
+                                                                ->where('status', 'anticipated')
+                                                                ->whereDate('release_date', '>', Carbon::today()
+                                                                                                       ->toDateString())
+                                                                ->get();
 
-        if (count($pendingTransactions)) {
-            foreach ($pendingTransactions as $pendingTransaction) {
-                $pendingBalance += $pendingTransaction->value;
+                    if (count($anticipatedTransactions)) {
+                        foreach ($anticipatedTransactions as $anticipatedTransaction) {
+                            $pendingBalance += $anticipatedTransaction->value - $anticipatedTransaction->antecipable_value;
+                        }
+                    }
+
+                    $antecipableTransactions = $transactionModel->where('company_id', $company->id)
+                                                                ->where('status', 'paid')
+                                                                ->whereDate('release_date', '>', Carbon::today())
+                                                                ->whereDate('antecipation_date', '<=', Carbon::today())
+                                                                ->get();
+
+                    if (count($antecipableTransactions)) {
+                        foreach ($antecipableTransactions as $antecipableTransaction) {
+                            $antecipableBalance += $antecipableTransaction->antecipable_value;
+                        }
+                    }
+
+                    $availableBalance = $company->balance;
+                    $totalBalance     = $availableBalance + $pendingBalance;
+
+                    return response()->json([
+                                                'available_balance'   => number_format(intval($availableBalance) / 100, 2, ',', '.'),
+                                                'antecipable_balance' => number_format(intval($antecipableBalance) / 100, 2, ',', '.'),
+                                                'total_balance'       => number_format(intval($totalBalance) / 100, 2, ',', '.'),
+                                                'pending_balance'     => number_format(intval($pendingBalance) / 100, 2, ',', '.'),
+                                                'currency'            => $company->country == 'usa' ? '$' : 'R$',
+                                            ]);
+                } else {
+                    return response()->json([
+                                                'message' => 'Ocorreu um erro, tente novamente mais tarde',
+                                            ], 400);
+                }
+            } else {
+
+                return response()->json([
+                                            'message' => 'Ocorreu um erro, tente novamente mais tarde',
+                                        ], 400);
             }
+        } catch (Exception $e) {
+            Log::warning('Erro ao buscar dados da dashboard (DashboardController - getValues)');
+            report($e);
+
+            return response()->json([
+                                        'message' => 'Ocorreu um erro, tente novamente mais tarde',
+                                    ], 400);
         }
-
-        $anticipatedTransactions = $transactionModel->where('company_id', $request->company)
-                                                    ->where('status', 'anticipated')
-                                                    ->whereDate('release_date', '>', Carbon::today()->toDateString())
-                                                    ->get();
-
-        if (count($anticipatedTransactions)) {
-            foreach ($anticipatedTransactions as $anticipatedTransaction) {
-                $pendingBalance += $anticipatedTransaction->value - $anticipatedTransaction->antecipable_value;
-            }
-        }
-
-        $antecipableTransactions = $transactionModel->where('company_id', $request->company)
-                                                    ->where('status', 'paid')
-                                                    ->whereDate('release_date', '>', Carbon::today())
-                                                    ->whereDate('antecipation_date', '<=', Carbon::today())
-                                                    ->get();
-
-        if (count($antecipableTransactions)) {
-            foreach ($antecipableTransactions as $antecipableTransaction) {
-                $antecipableBalance += $antecipableTransaction->antecipable_value;
-            }
-        }
-
-        $availableBalance = $company->balance;
-        $totalBalance     = $availableBalance + $pendingBalance;
-
-        return response()->json([
-                                    'available_balance'   => number_format(intval($availableBalance) / 100, 2, ',', '.'),
-                                    'antecipable_balance' => number_format(intval($antecipableBalance) / 100, 2, ',', '.'),
-                                    'total_balance'       => number_format(intval($totalBalance) / 100, 2, ',', '.'),
-                                    'pending_balance'     => number_format(intval($pendingBalance) / 100, 2, ',', '.'),
-                                    'currency'            => $company->country == 'usa' ? '$' : 'R$',
-                                ]);
     }
-
 }
 
--- public/modules/dashboard/js/dashboard.js
+++ public/modules/dashboard/js/dashboard.js
@@ -18,9 +18,15 @@ $(document).ready(function () {
             headers: {
                 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
             },
-            data: { company: $('#company').val() },
+            data: {company: $('#company').val()},
             error: function error() {
-                //
+                if (response.status === 422) {
+                    for (error in response.responseJSON.errors) {
+                        alertCustom('error', String(response.responseJSON.errors[error]));
+                    }
+                } else {
+                    alertCustom('error', String(response.responseJSON.message));
+                }
             },
             success: function success(data) {
 
