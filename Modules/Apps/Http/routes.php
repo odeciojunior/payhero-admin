cbd173a1cb53686f7be893b0565bf4b81b359e3b
julio leichtweis <julioleichtweis@gmail.com>
2019-09-18 10:38:56 -0300

    modulo de apps na api
    Former-commit-id: ebd06ff036fa1971a693e125046492fbc3aec970

--- Modules/Apps/Http/routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-Route::group(['middleware' => 'web', 'prefix' => 'apps', 'namespace' => 'Modules\Apps\Http\Controllers'], function()
-{
-    Route::get('/',[
-        'uses' => 'AppsController@index',
-        'as' => 'apps'
-    ]);
-});
