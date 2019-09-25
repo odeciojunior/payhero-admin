1513a72654f32d34ac231a3795c6272bcee03b58
Thales Bitencourt <thalesbitencourt@gmail.com>
2019-09-25 15:26:14 -0300

    merge
    Former-commit-id: 7bdcb33d4b9ac2139be9a80ac9d3026da5555cb2

---
+++ Modules/Core/Entities/ProductPlanSale.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace ModulesCoreEntities;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Modules\Core\Entities\Plan;
+use Modules\Core\Entities\Product;
+use Modules\Core\Entities\Sale;
+
+/**
+ * @property integer $id
+ * @property integer $product_id
+ * @property integer $plan_id
+ * @property integer $sale_id
+ * @property string $name
+ * @property string $description
+ * @property string $guarantee
+ * @property string $format
+ * @property string $cost
+ * @property string $photo
+ * @property string $height
+ * @property string $width
+ * @property string $weight
+ * @property string $shopify
+ * @property string $digital_product_url
+ * @property string $price
+ * @property string $shopify_id
+ * @property string $shopify_variant
+ * @property string $created_at
+ * @property string $updated_at
+ * @property string $deleted_at
+ * @property Plan $plan
+ * @property Product $product
+ * @property Sale $sale
+ * @property Tracking[] $trackings
+ */
+class ProductPlanSale extends Model
+{
+    /**
+     * The "type" of the auto-incrementing ID.
+     * @var string
+     */
+    protected $keyType = 'integer';
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'product_id',
+        'plan_id',
+        'sale_id',
+        'name',
+        'description',
+        'guarantee',
+        'format',
+        'cost',
+        'photo',
+        'height',
+        'width',
+        'weight',
+        'shopify',
+        'digital_product_url',
+        'price',
+        'shopify_id',
+        'shopify_variant',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function plan()
+    {
+        return $this->belongsTo('ModulesCoreEntities\Plan');
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function product()
+    {
+        return $this->belongsTo('ModulesCoreEntities\Product');
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function sale()
+    {
+        return $this->belongsTo('ModulesCoreEntities\Sale');
+    }
+
+    /**
+     * @return HasMany
+     */
+    public function trackings()
+    {
+        return $this->hasMany('ModulesCoreEntities\Tracking');
+    }
+}
--- Modules/Core/Entities/Tracking.php
+++ Modules/Core/Entities/Tracking.php
@@ -1,46 +1,50 @@
 <?php
 
-namespace Modules\Core\Entities;
+namespace ModulesCoreEntities;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Modules\Core\Entities\Delivery;
+use Modules\Core\Entities\PlanSale;
 
 /**
  * @property integer $id
- * @property integer $delivery_id
+ * @property integer $product_plan_sale_id
  * @property integer $plans_sale_id
+ * @property integer $delivery_id
  * @property string $tracking_code
- * @property string $created_at
- * @property string $updated_at
  * @property boolean $tracking_type_enum
  * @property boolean $tracking_status_enum
  * @property string $tracking_date
  * @property string $description
+ * @property string $created_at
+ * @property string $updated_at
  * @property string $deleted_at
  * @property Delivery $delivery
+ * @property PlanSale $planSale
+ * @property ProductPlanSale $productPlanSale
  */
-class TrackingHistory extends Model
+class Tracking extends Model
 {
-
     /**
      * The "type" of the auto-incrementing ID.
      * @var string
      */
     protected $keyType = 'integer';
-
     /**
      * @var array
      */
     protected $fillable = [
-        'delivery_id',
+        'product_plan_sale_id',
         'plans_sale_id',
+        'delivery_id',
         'tracking_code',
-        'created_at',
-        'updated_at',
         'tracking_type_enum',
         'tracking_status_enum',
         'tracking_date',
         'description',
+        'created_at',
+        'updated_at',
         'deleted_at',
     ];
 
@@ -50,5 +54,21 @@ class TrackingHistory extends Model
     public function delivery()
     {
         return $this->belongsTo(Delivery::class);
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function planSale()
+    {
+        return $this->belongsTo(PlanSale::class);
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function productPlanSale()
+    {
+        return $this->belongsTo(ProductPlanSale::class);
     }
 }
