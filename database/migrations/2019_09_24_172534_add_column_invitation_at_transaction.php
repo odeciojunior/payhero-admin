5647464541e48fdb2dc2ff2f1295fd2320b9f351
Thales Bitencourt <thalesbitencourt@gmail.com>
2019-09-25 15:04:26 -0300

    merge
    Former-commit-id: 3e3e6987035159e8dbc4bf78d83a2e99a9f27b48

--- database/migrations/2019_09_24_172534_add_column_invitation_at_transaction.php
+++ database/migrations/2019_09_24_172534_add_column_invitation_at_transaction.php
@@ -1,14 +1,12 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
 
 class AddColumnInvitationAtTransaction extends Migration
 {
     /**
      * Run the migrations.
-     *
      * @return void
      */
     public function up()
@@ -24,7 +22,8 @@ class AddColumnInvitationAtTransaction extends Migration
      * Reverse the migrations.
      * @return void
      */
-    public function down() {
+    public function down()
+    {
 
         Schema::table('transactions', function($table) {
             $table->dropForeign(['invitation_id']);
