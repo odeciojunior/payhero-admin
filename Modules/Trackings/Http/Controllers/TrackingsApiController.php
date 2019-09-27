1f3734586979eb5656b96316182c3c0604753a74
LuanMaia1234 <luanmaia05@hotmail.com>
2019-09-27 18:05:32 -0300

    tracking code
    Former-commit-id: d9c6af678033d045c290e55b38018088a8aea605

--- Modules/Trackings/Http/Controllers/TrackingsApiController.php
+++ Modules/Trackings/Http/Controllers/TrackingsApiController.php
@@ -6,8 +6,10 @@ use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Lang;
 use Illuminate\Support\Facades\Log;
 use Modules\Core\Entities\ProductPlanSale;
+use Modules\Core\Entities\TrackingHistory;
 use Vinkla\Hashids\Facades\Hashids;
 
 class TrackingsApiController extends Controller
@@ -33,12 +35,10 @@ class TrackingsApiController extends Controller
     /**
      * @param Request $request
      * @return \Illuminate\Http\JsonResponse
-     * @throws \Laracasts\Presenter\Exceptions\PresenterException
      */
     public function store(Request $request)
     {
         try {
-            dd('sadasd');
             $data                 = $request->all();
             $productPlanSaleModel = new ProductPlanSale();
             if (!empty($data['tracking_code']) && !empty($data['sale_id']) && !empty($data['product_id'])) {
@@ -48,7 +48,6 @@ class TrackingsApiController extends Controller
                     $productPlanSale = $productPlanSaleModel->where([['sale_id', $saleId], ['product_id', $productId]])
                                                             ->first();
                     if ($productPlanSale && empty($productPlanSale->tracking_code)) {
-                        dd('dsad');
                         $trackingCodeupdated = $productPlanSale->update([
                                                                             'tracking_code'        => $data['tracking_code'],
                                                                             'tracking_status_enum' => $productPlanSaleModel->present()
@@ -57,6 +56,11 @@ class TrackingsApiController extends Controller
                         if ($trackingCodeupdated) {
                             return response()->json([
                                                         'message' => 'Código de rastreio alterado',
+                                                        'data'    => [
+                                                            'tracking_code'   => $productPlanSale->tracking_code,
+                                                            'tracking_status' => Lang::get('definitions.enum.product_plan_sale.tracking_status_enum.' . $productPlanSaleModel->present()
+                                                                                                                                                                             ->getStatusEnum($productPlanSale->tracking_status_enum)),
+                                                        ],
                                                     ], 200);
                         } else {
                             return response()->json([
@@ -64,6 +68,17 @@ class TrackingsApiController extends Controller
                                                     ], 400);
                         }
                     }
+                    //                    else {
+                    //                        $trackingCodeupdated = $productPlanSale->update([
+                    //                                                                            'tracking_code' => $data['tracking_code'],
+                    //                                                                        ]);
+                    //                        if ($trackingCodeupdated) {
+                    //                            $trackingHistoryModel = new TrackingHistory();
+                    //                            $trackingHistoryModel->create([
+                    //
+                    //                                                          ]);
+                    //                        }
+                    //                    }
                 }
             } else {
                 return response()->json([
@@ -71,8 +86,10 @@ class TrackingsApiController extends Controller
                                         ], 400);
             }
         } catch (Exception $e) {
-            Log::warning('Erro ao tentar alterar código de rastreio');
+            Log::warning('Erro ao tentar alterar código de rastreio (TrackingApiController - store)');
             report($e);
+            return response()->json(['message' => 'Erro ao alterar código de rastreio'], 400);
+
         }
     }
 
