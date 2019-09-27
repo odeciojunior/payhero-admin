3876de15af3d7547fd21fc2d37c62d8f4e01733b
joao lucas <luccas_lyber@hotmail.com.br>
2019-09-27 13:22:44 -0300

    Deliveries route api middleware
    Former-commit-id: da5dee81ad40b4a51f3a6b01c408752f837b4d49

--- Modules/Deliveries/Routes/api.php
+++ Modules/Deliveries/Routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(
     [
-        'middleware' => [],
+        'middleware' => ['web', 'auth'],
     ],
     function() {
         Route::apiResource('/sale/{sale}/delivery', 'DeliveryApiController')->only('show')->names('api.client');
