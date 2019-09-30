998d5caeb5d8d93f882119105229cc49b4687d08
jeanvcastro <jean.castro1@outlook.com>
2019-09-30 14:19:35 -0300

    Projects
    Former-commit-id: 763301c861981a89c9d7629754273ee59f6ea1c7

--- Modules/Projects/Http/Controllers/ProjectsApiController.php
+++ Modules/Projects/Http/Controllers/ProjectsApiController.php
@@ -62,6 +62,10 @@ class ProjectsApiController extends Controller
     }
 
 
+    /**
+     * @param ProjectStoreRequest $request
+     * @return JsonResponse
+     */
     public function store(ProjectStoreRequest $request)
     {
         try {
