c2e98a08388c4d1a0e967571393334e4949386c2
jeanvcastro <jean.castro1@outlook.com>
2019-10-01 17:22:40 -0300

    Sales: refactor (detail modal) Finances: use sale detail modal
    Former-commit-id: 74f73b32ec98cc34e770384f719ed5d0493b72d2

--- Modules/Sales/Resources/views/index.blade.php
+++ Modules/Sales/Resources/views/index.blade.php
@@ -112,26 +112,7 @@
                         </table>
                     </div>
                     <!-- Modal detalhes da venda-->
-                    <div class="modal fade example-modal-lg" id="modal_detalhes" aria-hidden="true" aria-labelledby="exampleModalTitle" role="dialog" tabindex="-1">
-                        <div class="modal-dialog modal-simple modal-sidebar modal-lg">
-                            <div id='modal-saleDetails' class="modal-content p-20 " style="width: 500px;">
-                                <div class="header-modal">
-                                    <div class="row justify-content-between align-items-center" style="width: 100%;">
-                                        <div class="col-lg-2"> &nbsp;</div>
-                                        <div class="col-lg-8 text-center"><h4> Detalhes da venda </h4></div>
-                                        <div class="col-lg-2 text-right">
-                                            <a role="button" data-dismiss="modal">
-                                                <i class="material-icons pointer">close</i></a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="modal-body">
-                                    @include('sales::details')
-                                </div>
-                                <div class="clearfix"></div>
-                            </div>
-                        </div>
-                    </div>
+                        @include('sales::details')
                     <!-- End Modal -->
                 </div>
 
@@ -199,7 +180,6 @@
 
     @push('scripts')
         <script src="{{ asset('/modules/sales/js/index.js') }}"></script>
-        <script src="{{ asset('/modules/sales/js/detail.js') }}"></script>
         <script src="{{ asset('modules/global/js-extra/moment.min.js') }}"></script>
     @endpush
 
