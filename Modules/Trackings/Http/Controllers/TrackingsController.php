a5e67454b104a3393c29b2435e7d0e1b74dcb009
jeanvcastro <jean.castro1@outlook.com>
2019-10-02 10:11:42 -0300

    Trackings refactor
    Former-commit-id: a19ca740200f6d724efb2cbbf387d71f97698c77

--- Modules/Trackings/Http/Controllers/TrackingsController.php
+++ Modules/Trackings/Http/Controllers/TrackingsController.php
@@ -2,78 +2,18 @@
 
 namespace Modules\Trackings\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
+use Illuminate\Contracts\View\Factory;
 use Illuminate\Routing\Controller;
+use Illuminate\View\View;
 
 class TrackingsController extends Controller
 {
+
     /**
-     * Display a listing of the resource.
-     * @return Response
+     * @return Factory|View
      */
     public function index()
     {
         return view('trackings::index');
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     * @return Response
-     */
-    public function create()
-    {
-        return view('trackings::create');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     * @param Request $request
-     * @return Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Show the specified resource.
-     * @param int $id
-     * @return Response
-     */
-    public function show($id)
-    {
-        return view('trackings::show');
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     * @param int $id
-     * @return Response
-     */
-    public function edit($id)
-    {
-        return view('trackings::edit');
-    }
-
-    /**
-     * Update the specified resource in storage.
-     * @param Request $request
-     * @param int $id
-     * @return Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     * @param int $id
-     * @return Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
