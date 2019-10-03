e188ad7626bc7f998465e0669852f06fa3342943
julio leichtweis <julioleichtweis@gmail.com>
2019-10-03 16:22:05 -0300

    ajuste reports.js
    Former-commit-id: d0149fbadd07128ff7d7e2e33dd9dd416a40909b

--- public/modules/reports/js/reports.js
+++ public/modules/reports/js/reports.js
@@ -3,7 +3,7 @@ $(function () {
     $.ajax({
         method: "GET",
         url: "/api/projects/?select=true",
-        dataType: "jsonp",
+        dataType: "json",
         headers: {
             'Authorization': $('meta[name="access-token"]').attr('content'),
             'Accept': 'application/json',
@@ -52,7 +52,7 @@ $(function () {
                 endDate: endDate,
                 startDate: startDate
             },
-            dataType: "jsonp",
+            dataType: "json",
             headers: {
                 'Authorization': $('meta[name="access-token"]').attr('content'),
                 'Accept': 'application/json',
@@ -113,7 +113,7 @@ $(function () {
         $.ajax({
             url: link,
             type: 'GET',
-            dataType: "jsonp",
+            dataType: "json",
             headers: {
                 'Authorization': $('meta[name="access-token"]').attr('content'),
                 'Accept': 'application/json',
