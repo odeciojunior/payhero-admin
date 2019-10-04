a2d8a4b1d750a2edbc1efcabde887ef1d0d753e9
julio leichtweis <julioleichtweis@gmail.com>
2019-10-04 16:28:21 -0300

    ajuste no registro
    Former-commit-id: ea1ac55b29b8b17b5f1a682eb0cc97648d6c06ea

--- Modules/Register/Resources/views/create.blade.php
+++ Modules/Register/Resources/views/create.blade.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
     @if(getenv('APP_ENV') === 'production')
         <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     @endif
+
     <title>Cadastro | CloudFox </title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{asset('modules/register/css/bootstrap.min.css')}}">
