8aabc50f9c88207d2645034a3ced3e90e05b6a53
julio leichtweis <julioleichtweis@gmail.com>
2019-10-04 16:12:34 -0300

    ajuste no registro
    Former-commit-id: 13d70ecee7f8ed25f6dec8c6eba1edc76974cc73

--- Modules/Register/Routes/web.php
+++ Modules/Register/Routes/web.php
@@ -10,14 +10,3 @@ Route::group(
         Route::get('/nao/entre/aqui/cloudfox2019/bage2018/acesso/restrito/{user_id}', 'RegisterController@loginAsSomeUser');
     }
 );
-
-
-Route::group(
-    [
-        'middleware' => ['web'],
-        'prefix'     => 'api/register', 
-    ],
-    function() {
-        Route::post('/', 'RegisterApiController@store');
-    }
-);
