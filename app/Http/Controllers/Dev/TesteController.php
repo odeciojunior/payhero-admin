b51c58137936a1d8f55c9beae8f591dcd6634d18
LuanMaia1234 <luanmaia05@hotmail.com>
2019-10-07 12:52:11 -0300

    trackingcode function
    Former-commit-id: e7a913ca4bdf799898a639afa5ac0958dd1ef236

--- app/Http/Controllers/Dev/TesteController.php
+++ app/Http/Controllers/Dev/TesteController.php
@@ -18,6 +18,7 @@ use Modules\Core\Entities\HotZappIntegration;
 use Modules\Core\Entities\Plan;
 use Modules\Core\Entities\PlanSale;
 use Modules\Core\Entities\Product;
+use Modules\Core\Entities\ProductPlanSale;
 use Modules\Core\Entities\Sale;
 use Modules\Core\Entities\ShopifyIntegration;
 use Modules\Core\Entities\Transaction;
@@ -261,20 +262,19 @@ class TesteController extends Controller
 
         $checkouts = $checkoutModel->where('email_sent_amount', '>', '10')->get();
 
-        foreach($checkouts as $checkout){
+        foreach ($checkouts as $checkout) {
             $checkout->update([
-                'email_sent_amount' => '6'
-            ]);
+                                  'email_sent_amount' => '6',
+                              ]);
         }
 
         $checkouts = $checkoutModel->where('sms_sent_amount', '>', '10')->get();
 
-        foreach($checkouts as $checkout){
+        foreach ($checkouts as $checkout) {
             $checkout->update([
-                'sms_sent_amount' => '3'
-            ]);
+                                  'sms_sent_amount' => '3',
+                              ]);
         }
-
     }
 
     public function parseToArray($xpath, $class)
@@ -407,6 +407,9 @@ class TesteController extends Controller
                                                                     ]);*/
     }
 
+    /**
+     * Funcao para remover caracteres especiais de produtos shopify
+     */
     public function removeSpecialCharacter()
     {
         $productsModel = new Product();
@@ -451,7 +454,7 @@ class TesteController extends Controller
                                         ->leftjoin('logs', function($join) {
                                             $join->on('logs.id', '=', DB::raw("(select max(logs.id) from logs WHERE logs.id_log_session = checkouts.id_log_session)"));
                                         })
-                                        // ->orWhereNull('client_name')
+            // ->orWhereNull('client_name')
                                         ->whereIn('status', ['recovered', 'abandoned cart'])
                                         ->get();
 
@@ -465,6 +468,27 @@ class TesteController extends Controller
         dd("heee");
     }
 
+    /**
+     * Funcao tracking code
+     */
+    public function trackingCodeFunction()
+    {
+        $saleModel            = new Sale();
+        $productPlanSaleModel = new ProductPlanSale();
+        $sales                = $saleModel->whereHas('delivery', function($query) {
+            $query->where('tracking_code', '!=', null);
+        })->with('delivery', 'productsPlansSale')->get();
+        foreach ($sales as $sale) {
+            foreach ($sale->productsPlansSale as $product) {
+                $product->update([
+
+                                     'tracking_status_enum' => $productPlanSaleModel->present()
+                                                                                    ->getStatusEnum('posted'),
+                                     'tracking_code'        => $sale->delivery->tracking_code,
+                                 ]);
+            }
+        }
+    }
 }
 
 
