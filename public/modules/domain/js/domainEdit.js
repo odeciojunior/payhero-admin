c1133983d5913bde10939cf234392d87a9486c54
joao lucas <luccas_lyber@hotmail.com.br>
2019-10-07 14:27:34 -0300

    fix bug
    Former-commit-id: f4a8ea8d27bb0c84cb54c9c2048a5a1ad35efd0f

--- public/modules/domain/js/domainEdit.js
+++ public/modules/domain/js/domainEdit.js
@@ -38,7 +38,7 @@ $(document).ready(function () {
             }, success: function (response) {
                 $("#domain-table-body").html('');
                 if (response.data == '') {
-                    $("#domain-table-body").html("<tr class='text-center'><td colspan='4' style='height: 70px; vertical-align: middle;'>Nenhum dominio encontrado</td></tr>")
+                    $("#domain-table-body").html("<tr class='text-center'><td colspan='4' style='height: 70px; vertical-align: middle;'>Nenhum domínio encontrado</td></tr>")
                 } else {
                     $.each(response.data, function (index, value) {
                         tableDomains(value);
