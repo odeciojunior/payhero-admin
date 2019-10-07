589f0f31d41f6bfa1aa4e91a3bb47b40b0ee955a
joao lucas <luccas_lyber@hotmail.com.br>
2019-10-07 13:05:14 -0300

    fix bug modal plan-product empty
    Former-commit-id: 0fcb34350d4e0108b817e6d4e45993f4b987f64a

--- Modules/Plans/Resources/views/index.blade.php
+++ Modules/Plans/Resources/views/index.blade.php
@@ -14,12 +14,12 @@
             <table id='table-plans' class='table text-left table-pixels table-striped unify' style='width:100%'>
                 <thead>
                     <tr>
-                        <td class='table-title' >Nome</td>
-                        <td class='table-title' >Descrição</td>
-                        <td class='table-title' >Link</td>
-                        <td class='table-title' >Preço</td>
-                        <td class='table-title' >Status</td>
-                        <td class='table-title text-center options-column-width' >Opções</td>
+                        <td class='table-title'>Nome</td>
+                        <td class='table-title'>Descrição</td>
+                        <td class='table-title'>Link</td>
+                        <td class='table-title'>Preço</td>
+                        <td class='table-title'>Status</td>
+                        <td class='table-title text-center options-column-width'>Opções</td>
                     </tr>
                 </thead>
                 <tbody id='data-table-plan' class='min-row-height'>
@@ -32,7 +32,6 @@
 <ul id="pagination-plans" class="pagination-sm" style="margin-top:10px;position:relative;float:right">
     {{-- js pagination carrega --}}
 </ul>
-
 <!-- Modal padrão para adicionar Adicionar e Editar -->
 <div class="modal fade example-modal-lg modal-3d-flip-vertical" id="modal_add_plan" role="dialog" tabindex="-1">
     <div id="modal_add_size" class="modal-dialog modal-dialog-centered modal-simple">
@@ -58,7 +57,29 @@
         </div>
     </div>
 </div>
-
+{{-- Modal error --}}}
+<div class="modal fade example-modal-lg modal-3d-flip-vertical" id="modal-error-plan" role="dialog" tabindex="-1">
+    <div id="modal-add-size-plan-error" class="modal-dialog modal-dialog-centered modal-simple">
+        <div class="modal-content p-10" id="content-modal-plan-error">
+            <div class="modal-header simple-border-bottom mb-10">
+                <h4 class="modal-title" id="modal-title-plan-error"></h4>
+                <a id="modal-button-close" class="close-card pointer close btn-close-add-plan" role="button" data-dismiss="modal" aria-label="Close">
+                    <i class="material-icons md-16">close</i>
+                </a>
+            </div>
+            <div id="modal-add-plan-body-error" class="modal-body" style='min-height: 100px'>
+            </div>
+            <div class="modal-footer" id='modal-footer-plan-error'>
+                <a id="btn-mobile-modal-close" class="col-sm-6 btn btn-primary display-sm-none display-m-none display-lg-none display-xlg-none" style='color:white' role="button" data-dismiss="modal" aria-label="Close">
+                    Fechar
+                </a>
+                <button id="btn-modal-plan-error" type="button" class="col-sm-6 col-md-3 col-lg-3 btn btn-success" data-dismiss="modal">
+                    <i class="material-icons btn-fix"> save </i> Salvar
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
 <!-- Modal detalhes do plano -->
 <div class="modal fade example-modal-lg modal-3d-flip-vertical" id="modal_details_plan" role="dialog" tabindex="-1">
     <div id="modal_add_size" class="modal-dialog modal-dialog-centered modal-simple">
@@ -72,15 +93,14 @@
             <div id="modal-details-body" class="modal-body" style='min-height: 100px'>
                 @include('plans::details')
             </div>
-{{--            <div class="modal-footer">--}}
-{{--                <a id="btn-mobile-modal-close" class="col-sm-6 btn btn-primary display-sm-none display-m-none display-lg-none display-xlg-none" style='color:white' role="button" data-dismiss="modal" aria-label="Close">--}}
-{{--                    Fechar--}}
-{{--                </a>--}}
-{{--            </div>--}}
+            {{--            <div class="modal-footer">--}}
+            {{--                <a id="btn-mobile-modal-close" class="col-sm-6 btn btn-primary display-sm-none display-m-none display-lg-none display-xlg-none" style='color:white' role="button" data-dismiss="modal" aria-label="Close">--}}
+            {{--                    Fechar--}}
+            {{--                </a>--}}
+            {{--            </div>--}}
         </div>
     </div>
 </div>
-
 <!-- Modal padrão para excluir -->
 <div class="modal fade example-modal-lg modal-3d-flip-vertical" id="modal-delete-plan" aria-hidden="true" aria-labelledby="exampleModalTitle" role="dialog" tabindex="-1">
     <div class="modal-dialog  modal-dialog-centered  modal-simple">
