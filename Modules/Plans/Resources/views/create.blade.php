00f8df1e2465f4841ce12ff949669ad79866ac9a
yanrRocha <contato.yanrocha@gmail.com>
2019-10-16 15:59:13 -0300

    modificações planos (1/n)
    Former-commit-id: 0ae23c6ce2e0da9d94360cd6106ca3276ae47a93

--- Modules/Plans/Resources/views/create.blade.php
+++ Modules/Plans/Resources/views/create.blade.php
@@ -21,9 +21,9 @@
                     </div>
                 </div>
                 <hr class='display-lg-none display-xlg-none'>
-                <div id="products">
-                    <div id="products_div_1" class="row products_div_1">
-                        <div class='col-sm-8 col-md-7 col-lg-7'>
+                <div id="products" class='card container'>
+                    <div id="products_div_1" class="row products_div_1 mb-4">
+                        <div class='col-sm-12 col-md-12 col-lg-12'>
                             <div class="form-group">
                                 <label>Produtos do plano:</label>
                                 <select id="product_1" name="products[]" class="form-control plan_product">
@@ -37,10 +37,26 @@
                                 <input class="form-control products_amount_create" type="text" name="product_amounts[]" id="products_amount" placeholder="quantidade" data-mask="0#" value="1">
                             </div>
                         </div>
-                        <div class='col-sm-12 col-md-2 col-lg-2'>
-                            <label class="display-xsm-none">Remover:</label>
+                        <div class="form-group col-sm-4 col-md-3 col-lg-3">
+                            <label>Custo (<b>Un</b>):</label>
+                            <input value="" id="product_cost_1" class="form-control products_cost" type="text" data-mask='0#' name="product_cost[]" placeholder="Unitario">
+                        </div>
+                        <div class="form-group col-sm-4 col-md-3 col-lg-3">
+                            <label>Custo Total:</label>
+                            <input value="" id="product_total_1" class="form-control products_total" type="text" data-mask='0#' name="product_total[]" placeholder="Total" readonly>
+                        </div>
+                        <div class="switch-holder col-sm-4 col-md-3 col-lg3">
+                            <label for="token" class='mb-10'>Dólar:</label>
+                            <br>
+                            <label class="switch">
+                                <input type="checkbox" name="status" class='check shipping-status' value='0'>
+                                <span class="slider round"></span>
+                            </label>
+                        </div>
+                        <div class='col-sm-12 offset-md-4 col-md-4 offset-lg-4 col-lg-4'>
+                            {{--                            <label class="display-xsm-none">Remover:</label>--}}
                             <button class='btn btn-outline btn-danger btnDelete form-control'>
-                                <i class='icon wb-trash' aria-hidden='true'></i></button>
+                                <b>Remover </b><i class='icon wb-trash' aria-hidden='true'></i>
                             </button>
                         </div>
                         <hr class='mb-30 display-lg-none display-xlg-none'>
