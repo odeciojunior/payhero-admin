8db826afe2d3b834a260a84a2cb0538cd0226087
joao lucas <luccas_lyber@hotmail.com.br>
2019-10-17 14:45:26 -0300

    fix
    Former-commit-id: 656e422e1ad2dba523cd569744b556f52dd46e27

--- Modules/Core/Services/SaleService.php
+++ Modules/Core/Services/SaleService.php
@@ -196,20 +196,7 @@ class SaleService
         } else {
             $transaction->release_date = null;
         }
-        /*
-                if ($sale->payment_method == 2) {
-                    if ($sale->status == 1) {
-                        $transaction->release_date = Carbon::parse($transaction->release_date);
-                    } else {
-                        $transaction->release_date = null;
-                    }
-                } else {
-                    if ($sale->status != 1) {
-                        $transaction->release_date = null;
-                    } else {
-                        $transaction->release_date = Carbon::parse($transaction->release_date);
-                    }
-                }*/
+
 
         //add details to sale
         $sale->details = (object) [
