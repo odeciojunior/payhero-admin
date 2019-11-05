7bec003533b49c02aec07de42aab65c2f8fe2be2
jeanvcastro <jean.castro1@outlook.com>
2019-11-05 16:07:37 -0300

    fill tracking in migration
    Former-commit-id: 1a4943813d41a790996c542ab14a1463cb8f0135

--- database/migrations/2019_10_29_101054_alter_table_trackings.php
+++ database/migrations/2019_10_29_101054_alter_table_trackings.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -29,6 +30,13 @@ class AlterTableTrackings extends Migration
             $table->string('tracking_code')->after('delivery_id');
             $table->integer('tracking_status_enum')->after('tracking_code');
         });
+
+        DB::statement('insert into trackings (product_plan_sale_id, sale_id, product_id, amount, delivery_id, tracking_code, tracking_status_enum, created_at, updated_at)
+                              select pps.id, pps.sale_id, pps.product_id, ((select amount from products_plans where plan_id = pps.plan_id and product_id = pps.product_id) * ps.amount )as amount, s.delivery_id, pps.tracking_code, if(pps.tracking_status_enum is null,0,pps.tracking_status_enum) as tracking_status_enum, now() as created_at, now() as updated_at
+                              from products_plans_sales pps
+                              join sales s on s.id = pps.sale_id
+                              join plans_sales ps on ps.sale_id = pps.sale_id and ps.plan_id = pps.plan_id
+                              where pps.tracking_code is not null');
     }
 
     /**
