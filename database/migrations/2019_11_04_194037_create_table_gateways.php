f1c2950b99437c1ce2b74231f0165954afcfae5e
Gustavo Carvalho <gcarvalho5@gmail.com>
2019-11-07 15:28:37 -0300

    gateways, insert pagarme zoop
    Former-commit-id: 4134741ccd6eacb181df0c14f942c5c63696f171

--- database/migrations/2019_11_04_194037_create_table_gateways.php
+++ database/migrations/2019_11_04_194037_create_table_gateways.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -16,11 +17,51 @@ class CreateTableGateways extends Migration
             $table->bigIncrements('id');
             $table->unsignedTinyInteger('gateway_enum')->index();
             $table->string('name');
-            $table->json('json_config');
+            $table->text('json_config');
+            $table->tinyInteger('production_flag')->default(0);
+            $table->tinyInteger('enabled_flag')->default(0);
 
             $table->softDeletes();
             $table->timestamps();
         });
+
+        $config     = [
+            'public_key'     => '',
+            'encryption_key' => '',
+        ];
+        $jsonConfig = encrypt(json_encode($config));
+        $sql        = "INSERT INTO gateways (gateway_enum, name, json_config, production_flag, enabled_flag) ";
+        $sql        .= "VALUES(1, 'pagarme_production','" . $jsonConfig . "', 1, 0)";
+        DB::select($sql);
+
+        $config     = [
+            'public_key'     => 'ak_test_O22Fy0HHJt1dh5Mg0vZFtqbJPApQZf',
+            'encryption_key' => 'ek_test_4xkCDbiE5MDMpZmMEnb1ZH9Sm7Ifux',
+        ];
+        $jsonConfig = encrypt(json_encode($config));
+        $sql        = "INSERT INTO gateways (gateway_enum, name, json_config, production_flag, enabled_flag) ";
+        $sql        .= "VALUES(2, 'pagarme_sandbox','" . $jsonConfig . "', 0, 0)";
+        DB::select($sql);
+
+        $config     = [
+            'marketplace_id'  => 'ce02cc0783794e7cb1fc8fdc46ef953b',
+            'publishable_key' => 'zpk_prod_RZx0pdCvK3LKQ55Nj2QGKani',
+            'seller_id'       => 'a1f907cd215040729f9f19b7f2df4ec3',
+        ];
+        $jsonConfig = encrypt(json_encode($config));
+        $sql        = "INSERT INTO gateways (gateway_enum, name, json_config, production_flag, enabled_flag) ";
+        $sql        .= "VALUES(3, 'zoop_production','" . $jsonConfig . "', 1, 0)";
+        DB::select($sql);
+
+        $config     = [
+            'marketplace_id'  => '72664906b4444f0fa900098844baf84b',
+            'publishable_key' => 'zpk_test_4UfSOSzIS895VJOt19zDAI1U',
+            'seller_id'       => '43e2543afd5c45e7b219a26c36551bdf',
+        ];
+        $jsonConfig = encrypt(json_encode($config));
+        $sql        = "INSERT INTO gateways (gateway_enum, name, json_config, production_flag, enabled_flag) ";
+        $sql        .= "VALUES(4, 'zoop_sandbox','" . $jsonConfig . "', 0, 0)";
+        DB::select($sql);
     }
 
     /**
