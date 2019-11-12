17fdce454b2ce4d46bdf2750fdeb204788f267fa
Gustavo Carvalho <gcarvalho5@gmail.com>
2019-11-12 18:00:32 -0300

    ajuste sales gategway_id
    Former-commit-id: 52919591e16d830ed298f66a844549cdc7af5813

--- database/migrations/2019_11_07_160214_alter_sale_table_rename_gateway_id_column.php
+++ database/migrations/2019_11_07_160214_alter_sale_table_rename_gateway_id_column.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -23,6 +24,10 @@ class AlterSaleTableRenameGatewayIdColumn extends Migration
         Schema::table('sales', function(Blueprint $table) {
             $table->foreign('gateway_id')->references('id')->on('gateways');
         });
+
+
+        $sql = "UPDATE sales SET gateway_id = 1 "; // 1 - pagarme_production
+        DB::select($sql);
     }
 
     /**
