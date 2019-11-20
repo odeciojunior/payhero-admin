e250d1f06226a1884641ed3fef10e8474a6b4a46
eliseu85 <eliseu.ferreiradesouza@gmail.com>
2019-11-20 11:52:48 -0300

    ActiveCampaign - Lista Solicitacao de convites pelo site
    Former-commit-id: 1076cc589b4d63071480de2791ea3547e51ab628

--- Modules/Register/Resources/views/create.blade.php
+++ Modules/Register/Resources/views/create.blade.php
@@ -598,7 +598,7 @@
 <script src="{{asset('modules/register/js/bootstrap.min.js')}}"></script>
 <script src="{{asset('modules/register/js/wow.min.js')}}"></script>
 <script src="{{asset('modules/register/js/pesquisaCep.js')}}"></script>
-<script src="{{asset('modules/register/js/register.js?v=2')}}"></script>
+<script src="{{asset('modules/register/js/register.js?v=3')}}"></script>
 <script src="{{asset('modules/register/js/passwordStrength.js')}}"></script>
 {{--<script src="{{ asset('modules/global/js-extra/sweetalert2.all.min.js') }}"></script>--}}
 <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.js'></script>
