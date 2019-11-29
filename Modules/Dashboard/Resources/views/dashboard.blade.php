6c10dcafd494a4dccbf8539eab6d8c9c8f8525f7
julio leichtweis <julioleichtweis@gmail.com>
2019-11-29 15:48:10 -0300

    notificatinos comentada
    Former-commit-id: 4bc51482df086ce1c33ab8d56005fe9702df8fff

--- Modules/Dashboard/Resources/views/dashboard.blade.php
+++ Modules/Dashboard/Resources/views/dashboard.blade.php
@@ -110,7 +110,7 @@
     </div>
 
     @push('scripts')
-        <script src="{{ asset('modules/dashboard/js/dashboard.js?v=4') }}"></script>
+        <script src="{{ asset('modules/dashboard/js/dashboard.js?v=5') }}"></script>
     @endpush
 
 @endsection
