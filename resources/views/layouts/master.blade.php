53b8da52f02e16874b0bd8069dba398e0be71d5b
jeanvcastro <jean.castro1@outlook.com>
2019-11-29 15:16:23 -0300

    master.blade
    Former-commit-id: 4a1ab99d77519c834a0f11ce5cca7dc7c1e2a3ba

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -102,7 +102,7 @@
 <script src="{{ asset('modules/global/adminremark/assets/js/Site.js') }}"></script>
 <script src="{{ asset('modules/global/adminremark/assets/examples/js/dashboard/v1.js') }}"></script>
 <script src="{{ asset('modules/global/jquery-imgareaselect/scripts/jquery.imgareaselect.pack.js') }}"></script>
-<script src="{{ asset('modules/global/js/global.js?v=4') }}"></script>
+{{--<script src="{{ asset('modules/global/js/global.js?v=4') }}"></script>--}}
 <script>
     verifyDocumentPending();
 </script>
