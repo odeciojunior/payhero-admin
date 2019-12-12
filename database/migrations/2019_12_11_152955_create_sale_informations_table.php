5b011fe7d2f3a8830ed210280b348b53ea74a7bb
Fausto Marins <faustogmjunior@hotmail.com>
2019-12-12 12:37:05 -0300

    Criando tabela sale_informations
    Former-commit-id: ac96b1d33c65b8a7d99332dcd087e79aab1da195

--- database/migrations/2019_12_11_152955_create_sale_informations_table.php
+++ database/migrations/2019_12_11_152955_create_sale_informations_table.php
@@ -14,29 +14,37 @@ class CreateSaleInformationsTable extends Migration
     {
         Schema::create('sale_informations', function(Blueprint $table) {
             $table->bigIncrements('id');
-            $table->unsignedInteger("sale_id");
-            $table->string("operational_system")->comment("Sistema operacional do usuário");
-            $table->string("browser")->comment("Browser usado para fazer a compra");
-            $table->string("browser_fingerprint")->comment("Fingerprint do browser utilizado na compra");
-            $table->string("ip")->comment("Ip com comprador");
-            $table->string("customer_name")->comment("Nome do comprador");
-            $table->string("customer_email")->comment("Email do comprador");
-            $table->string("customer_phone")->comment("Telefone do comprador");
-            $table->unsignedInteger("customer_identification_number")->index()->comment("CPF/CNPJ do comprador");
-            $table->string("product_name")->comment("Nome do produto comprado");
-            $table->string("transaction_amount")->comment("Valor total da transação");
-            $table->string("country")->index()->comment("País informado pelo comprador");
-            $table->string("zip_code")->comment("CEP informado pelo comprador");
-            $table->string("state")->index()->comment("Estado informado pelo comprador");
-            $table->string("city")->index()->comment("Cidade informada pelo comprador");
-            $table->string("district")->comment("Bairro informado pelo comprador");
-            $table->string("street_name")->comment("Rua informada pelo comprador");
-            $table->string("street_number")->index()->comment("Número da casa informada pelo comprador");
-            $table->string("card_brand")->index()->comment("Bandeira do cartão");
-            $table->unsignedInteger("installments")->index()->comment("Número de parcelas na compra");
-            $table->unsignedInteger("first_six_digits")->index()->comment("Primeiros 6 dígitos do cartão");
-            $table->unsignedInteger("last_four_digits")->index()->comment("Últimos 4 dígitos do cartoã");
+            $table->unsignedBigInteger("sale_id")->nullable();
+            $table->string("operational_system")->comment("Sistema operacional do usuário")->nullable();
+            $table->string("browser")->comment("Browser usado para fazer a compra")->nullable();
+            $table->string("browser_fingerprint")->comment("Fingerprint do browser utilizado na compra")->nullable();
+            $table->string("browser_token")->comment("Token do browser (salvo no cookie) utilizado na compra")
+                  ->nullable();
+            $table->string("ip")->comment("Ip com comprador")->nullable()->index();
+            $table->string("customer_name")->comment("Nome do comprador")->nullable();
+            $table->string("customer_email")->comment("Email do comprador")->nullable();
+            $table->string("customer_phone")->comment("Telefone do comprador")->nullable();
+            $table->string("customer_identification_number")->comment("CPF/CNPJ do comprador")
+                  ->nullable()->index();
+            $table->string("project_name")->comment("Nome do projeto")->nullable()->index();
+            $table->string("transaction_amount")->comment("Valor total da transação")->nullable()->index();
+            $table->string("country")->comment("País informado pelo comprador")->nullable()->index();
+            $table->string("zip_code")->comment("CEP informado pelo comprador")->nullable();
+            $table->string("state")->comment("Estado informado pelo comprador")->nullable()->index();
+            $table->string("city")->comment("Cidade informada pelo comprador")->nullable()->index();
+            $table->string("district")->comment("Bairro informado pelo comprador")->nullable();
+            $table->string("street_name")->comment("Rua informada pelo comprador")->nullable();
+            $table->string("street_number")->comment("Número da casa informada pelo comprador")->nullable()->index();
+            $table->string("card_token")->comment("Token do cartão")->nullable()->index();
+            $table->string("card_brand")->comment("Bandeira do cartão")->nullable()->index();
+            $table->unsignedInteger("installments")->comment("Número de parcelas na compra")->nullable()->index();
+            $table->unsignedInteger("first_six_digits")->comment("Primeiros 6 dígitos do cartão")->nullable()->index();
+            $table->unsignedInteger("last_four_digits")->comment("Últimos 4 dígitos do cartão")->nullable()->index();
             $table->timestamps();
+        });
+
+        Schema::table('sale_informations', function(Blueprint $table) {
+            $table->foreign('sale_id')->references('id')->on('sales');
         });
     }
 
@@ -46,6 +54,10 @@ class CreateSaleInformationsTable extends Migration
      */
     public function down()
     {
+        Schema::table('sale_informations', function(Blueprint $table) {
+            $table->dropForeign(["sale_id"]);
+        });
+
         Schema::dropIfExists('sale_informations');
     }
 }
