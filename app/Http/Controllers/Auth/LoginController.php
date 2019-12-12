9a7d8560e9a8e2abeb9f64c7ee84f9f555ccc0e3
julio leichtweis <julioleichtweis@gmail.com>
2019-12-12 16:48:28 -0300

    nova funcionalidade - usuarios com conta bloqueada
    Former-commit-id: 666c7d0da5c20ce9d47290ded7112a94a8c37f8b

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Http\Request;
+use Modules\Core\Entities\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
@@ -20,11 +22,57 @@ class LoginController extends Controller
 
     use AuthenticatesUsers;
 
+
     /**
+     * Handle a login request to the application.
+     *
+     * Overwritten from trait AuthenticatesUsers for handle user with account blocked
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Illuminate\Http\JsonResponse
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+
+        $userModel = new User();
+
+        $user = $userModel->where('email', $request->email)->first();
+
+        if(!empty($user) && $user->status == $userModel->present()->getStatus('account blocked')){
+
+            return response()->redirectTo('/')->withErrors(['accountErrors' => 'Blocked account']);
+        }
+
+        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+        // the login attempts for this application. We'll key this by the username and
+        // the IP address of the client making these requests into this application.
+        if (method_exists($this, 'hasTooManyLoginAttempts') &&
+            $this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
+        if ($this->attemptLogin($request)) {
+            return $this->sendLoginResponse($request);
+        }
+
+        // If the login attempt was unsuccessful we will increment the number of attempts
+        // to login and redirect the user back to the login form. Of course, when this
+        // user surpasses their maximum number of attempts they will get locked out.
+        $this->incrementLoginAttempts($request);
+
+        return $this->sendFailedLoginResponse($request);
+    }
+
+
+    /**
+     *  default -> protected $redirectTo = '/dashboard';
      * @return string
      */
-
-    //    protected $redirectTo = '/dashboard';
     protected function redirectTo()
     {
         if (auth()->user()->hasRole('account_owner') || auth()->user()->hasRole('admin')) {
--- database/migrations/2019_12_12_121841_alter_users_table_add_status_column.php
+++ database/migrations/2019_12_12_121841_alter_users_table_add_status_column.php
@@ -4,27 +4,29 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AlterClientCardsTableAddFingerprintColumn extends Migration
+class AlterUsersTableAddStatusColumn extends Migration
 {
     /**
      * Run the migrations.
+     *
      * @return void
      */
     public function up()
     {
-        Schema::table("client_cards", function(Blueprint $table) {
-            $table->string("browser_fingerprint")->nullable()->after("id");
+        Schema::table('users', function(Blueprint $table) {
+            $table->integer('status')->after('email_verified')->default(1);
         });
     }
 
     /**
      * Reverse the migrations.
+     *
      * @return void
      */
     public function down()
     {
-        Schema::table("client_cards", function(Blueprint $table) {
-            $table->dropColumn(["browser_fingerprint"]);
+        Schema::table('users', function(Blueprint $table) {
+            $table->dropColumn('status');
         });
     }
 }
