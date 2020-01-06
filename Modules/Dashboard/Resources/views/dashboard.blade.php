23e8b3b51917699a332dc352f55c7ba3a8841a20
jeanvcastro <jean.castro1@outlook.com>
2020-01-06 16:58:08 -0300

    Dashboard: chargeback
    Former-commit-id: 64db8beb8682e81b8398be72d8178d88ec024ba0

--- Modules/Dashboard/Resources/views/dashboard.blade.php
+++ Modules/Dashboard/Resources/views/dashboard.blade.php
@@ -4,7 +4,7 @@
 @section('content')
 
     @push('css')
-        <link rel="stylesheet" href="{{ asset('modules/global/css/new-dashboard.css') }}">
+        <link rel="stylesheet" href="{{ asset('modules/global/css/new-dashboard.css?v=1') }}">
     @endpush
 
     <div class="page">
@@ -29,24 +29,23 @@
         <div class="page-content container" style="display:none">
             <!-- CARDS EXTRATO -->
             <div class="row" id="card-extrato">
-                <div class="col-sm-12 col-md-6 col-lg-3">
+                <div class="col-6 col-lg-3">
                     <div class="card card-shadow bg-white">
-                        <div class="card-header d-flex justify-content-start align-items-center bg-white p-20">
+                        <div class="card-header d-flex justify-content-start align-items-center bg-white pt-20 pb-0">
                             <div class="font-size-14 gray-600">
                                 <img src="{{ asset('modules/global/img/svg/moeda-vermelha.svg') }}" width="35px">
                                 <span class="card-desc">Hoje</span>
                             </div>
                         </div>
-                        <div class="card-body font-size-24 text-center d-flex align-items-topline justify-content-center">
+                        <div
+                            class="card-body font-size-24 text-center d-flex align-items-topline justify-content-center">
                             <span class="moeda">R$</span>
                             <span id="today_money" class="text-money"></span>
                         </div>
                         <div class="card-bottom orangered"></div>
                     </div>
-                </div>
-                <div class="col-sm-12 col-md-6 col-lg-3">
                     <div class="card card-shadow bg-white">
-                        <div class="card-header d-flex justify-content-start align-items-center bg-white p-20">
+                        <div class="card-header d-flex justify-content-start align-items-center bg-white pt-20 pb-0">
                             <div class="font-size-14 gray-600">
                                 <img src="{{ asset('modules/global/img/svg/moeda-laranja.svg') }}" width="35px">
                                 <span class="card-desc">Pendente</span>
@@ -60,9 +59,9 @@
                         <div class="card-bottom orange"></div>
                     </div>
                 </div>
-                <div class="col-sm-12 col-md-6 col-lg-3">
+                <div class="col-6 col-lg-3">
                     <div class="card card-shadow bg-white">
-                        <div class="card-header d-flex justify-content-start align-items-center bg-white p-20">
+                        <div class="card-header d-flex justify-content-start align-items-center bg-white pt-20 pb-0">
                             <div class="font-size-14 gray-600">
                                 <img src="{{ asset('modules/global/img/svg/moeda.svg') }}" width="35px">
                                 <span class="card-desc">Disponível</span>
@@ -75,10 +74,8 @@
                         </div>
                         <div class="card-bottom green"></div>
                     </div>
-                </div>
-                <div class="col-sm-12 col-md-6 col-lg-3">
                     <div class="card card-shadow bg-white">
-                        <div class="card-header d-flex justify-content-start align-items-center bg-white p-20">
+                        <div class="card-header d-flex justify-content-start align-items-center bg-white pt-20 pb-0">
                             <div class="font-size-14 gray-600">
                                 <img src="{{ asset('modules/global/img/svg/moeda-azul.svg') }}" width="35px">
                                 <span class="card-desc">Total</span>
@@ -90,6 +87,33 @@
                             <span id="total_money" class="text-money"></span>
                         </div>
                         <div class="card-bottom blue"></div>
+                    </div>
+                </div>
+
+                <div class="col-12 col-lg-6">
+                    <div class="card card-shadow bg-white">
+                        <div class="card-header d-flex justify-content-start align-items-center bg-white pt-20 pb-0">
+                            <div class="font-size-14 gray-600 mr-auto">
+                                <img class="orange-gradient" src="{{ asset('modules/global/img/svg/chargeback.svg') }}"
+                                     width="35px">
+                                <span class="card-desc">Taxa de Chargebacks</span>
+                            </div>
+                            <i class="material-icons gray" data-toggle="tooltip" data-placement="bottom" title="Taxa geral de chargeback de sua empresa">help</i>
+                        </div>
+                        <div class="card-body font-size-24 text-center d-flex align-items-topline align-items-center">
+                            <div class="col text-center">
+                                <div class="circle">
+                                    <strong>0.00%</strong>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <span id="total_sales_approved" class="text-money">0</span>
+                                <div class="font-size-14 mb-20">Vendas no Cartão</div>
+                                <span id="total_sales_chargeback" class="text-money">0</span>
+                                <div class="font-size-14">Chargebacks</div>
+                            </div>
+                        </div>
+                        <div class="card-bottom red"></div>
                     </div>
                 </div>
             </div>
@@ -110,7 +134,8 @@
     </div>
 
     @push('scripts')
-        <script src="{{ asset('modules/dashboard/js/dashboard.js?v=10') }}"></script>
+        <script src="{{ asset('modules/global/js/circle-progress.min.js') }}"></script>
+        <script src="{{ asset('modules/dashboard/js/dashboard.js?v=11') }}"></script>
     @endpush
 
 @endsection
