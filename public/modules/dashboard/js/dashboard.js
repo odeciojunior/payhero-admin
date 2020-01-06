0290fe4f88d62344481527b5dd4e525c04adfa04
jeanvcastro <jean.castro1@outlook.com>
2020-01-06 16:59:35 -0300

    Dashboard: chargeback
    Former-commit-id: c0a10017178828eb9e57fdf5754a6a3ed2494cbd

--- public/modules/dashboard/js/dashboard.js
+++ public/modules/dashboard/js/dashboard.js
@@ -73,6 +73,8 @@ $(document).ready(function () {
                 $("#total_money").html(data.total_balance);
                 $("#today_money").html(data.today_balance);
 
+                $('#total_sales_approved').text(data.values.total_sales_approved);
+                $('#total_sales_chargeback').text(data.values.total_sales_chargeback);
                 updateProgressBar(data.values.chargeback_tax);
             }
         });
