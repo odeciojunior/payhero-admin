28a48ec3cb06e9498ac1681f75dd2feadd0dd4f6
jeanvcastro <jean.castro1@outlook.com>
2020-01-07 11:07:48 -0300

    revert
    Former-commit-id: a6c84a5c3913cb3f3389804498f9d81e2b545ac2

--- Modules/Core/Services/TrackingService.php
+++ Modules/Core/Services/TrackingService.php
@@ -191,7 +191,7 @@ class TrackingService
                 'sale.client',
                 'product',
             ])
-            ->whereHas('sale', function ($query) use ($filters, $saleStatus, $userId) {
+            ->whereHas('sale', function ($query) use ($filters, $saleStatus) {
                 //tipo da data e periodo obrigatorio
                 $dateRange = FoxUtils::validateDateRange($filters["date_updated"]);
                 $query->whereBetween('end_date', [$dateRange[0] . ' 00:00:00', $dateRange[1] . ' 23:59:59'])
