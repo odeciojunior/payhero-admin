b16546dcc718a70df4464e5a5084c1d585747f75
jeanvcastro <jean.castro1@outlook.com>
2020-01-13 10:55:40 -0300

    ajustes
    Former-commit-id: 0dc020b92ccb355800e97a8d22a485bbbd8d879b

--- Modules/Sales/Resources/views/index.blade.php
+++ Modules/Sales/Resources/views/index.blade.php
@@ -109,7 +109,7 @@
                         <strong class="font-size-16">Exportando seu relatório</strong>
                         <p class="font-size-14 pr-md-100 mb-0" >Sua exportação será entregue por e-mail para: <strong id="export-email"></strong> e aparecerá nas suas notificações. Pode levar algum tempo, dependendo de quantos registros você estiver exportando.</p>
                     </div>
-                    <i class="material-icons pointer" data-dimiss="alert">close</i>
+                    <i class="material-icons pointer" data-dismiss="alert">close</i>
                 </div>
             </div>
 
