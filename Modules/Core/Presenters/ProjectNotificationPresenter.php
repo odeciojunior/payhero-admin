dbde74d03ea44f109a9e4f9c3c46a9510e828540
LuanMaia1234 <luanmaia05@hotmail.com>
2020-01-21 17:36:23 -0300

    ajuste project notification service
    Former-commit-id: 4d940b29d4bfffb1c0f943ca7b52fd597a9b720f

--- Modules/Core/Presenters/ProjectNotificationPresenter.php
+++ Modules/Core/Presenters/ProjectNotificationPresenter.php
@@ -68,5 +68,27 @@ class ProjectNotificationPresenter extends Presenter
         }
 
     }
+    public function getStatus($status)
+    {
+
+        if (is_numeric($status)) {
+            switch ($status) {
+                case 1:
+                    return 'active';
+                case 2:
+                    return 'disabled';
+            }
+
+            return '';
+        } else {
+            switch ($status) {
+                case 'active':
+                    return 1;
+                case 'disabled':
+                    return 2;
+            }
+            return '';
+        }
+    }
 
 }
