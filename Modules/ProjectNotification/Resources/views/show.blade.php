71ade3c01a4f11ffc2b1e848cc46f3ad3d15aab6
eliseu85 <eliseu.ferreiradesouza@gmail.com>
2020-01-24 08:45:08 -0300

    detalhe notificacao
    Former-commit-id: b3d1448d2f6e06a68a95efeba763c3a321f0908f

--- Modules/ProjectNotification/Resources/views/show.blade.php
+++ Modules/ProjectNotification/Resources/views/show.blade.php
@@ -26,14 +26,20 @@
             <td class='text-left projectn-subject'></td>
         </tr>
         <tr>
-            <td class='table-title'>Título</td>
-            <td style='width: 20px'></td>
-            <td class='text-left projectn-title'></td>
-        </tr>
-        <tr>
             <td class='table-title'>Mensagem</td>
             <td style='width: 20px'></td>
             <td class='text-left projectn-message'></td>
         </tr>
     </tbody>
 </table>
+<style type="text/css">
+    .font-padrao {  font-family: 'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif; }
+    .item-text-padrao { font-size: 12px; line-height: 30px; margin: 0; color: #999999; }
+    .lineh-18 { line-height: 18px; }
+    .font-15 { font-size: 15px; }
+    .color-999 { color: #999999 }
+    .color-333 { color: #333333 }
+</style>
+<div class="row include-templates-email">
+    @include('projectnotification::billetpaid')
+</div>
