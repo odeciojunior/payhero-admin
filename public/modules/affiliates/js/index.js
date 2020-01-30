bde474e8d0665e43eae4c21e7dfb292237fcbddc
LuanMaia1234 <luanmaia05@hotmail.com>
2020-01-30 17:53:56 -0300

    ajustes tela de afiliação
    Former-commit-id: 803b40a8127041823a415139b8cddd3f4e9a279b

--- public/modules/affiliates/js/index.js
+++ public/modules/affiliates/js/index.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     getProjectData();
 
     function getProjectData() {
-        loadOnAny('.page');
+        loadOnAny('.page-content');
         $.ajax({
             method: "GET",
             url: "/api/affiliates/" + projectId,
@@ -13,18 +13,22 @@ $(document).ready(function () {
                 'Accept': 'application/json',
             },
             error: (response) => {
-                loadOnAny('.page', true);
+                loadOnAny('.page-content', true);
                 errorAjaxResponse(response);
             },
             success: (response) => {
-                loadOnAny('.page', true);
+                loadOnAny('.page-content', true);
                 $('.page-content').show();
                 $('.project-header').html(response.data.name);
                 $('.project-image').prop('src', response.data.photo);
-                $('#created_by').html(`Criado por: ${response.data.user_name}`);
+                $('#created_by').html(`Produtor: ${response.data.user_name}`);
                 $('.percentage-affiliate').html(` <strong >Porcentagem de afiliado: <span class='green-gradient'>${response.data.percentage_affiliates}%</span></strong>`);
                 $('.text-terms').html(response.data.terms_affiliates);
                 $('.text-about-project').html(response.data.description);
+                $('.url_page').html(` <strong >URL da página principal: <a href='${response.data.url_page}' target='_blank'>${response.data.url_page}</a></strong>`);
+                $('.contact').html(`<strong>E-mail: ${response.data.contact}</strong>`);
+                $('.support_phone').html(`<strong>Telefone: ${response.data.support_phone}</strong>`);
+                $('.created_at').html(` <strong >Criado em: ${response.data.created_at}</strong>`);
             }
         });
     }
