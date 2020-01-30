187ece15f674e1ac69f329227285144ce97acbbb
eliseu85 <eliseu.ferreiradesouza@gmail.com>
2020-01-30 16:22:50 -0300

    configuracoes afiliacao
    Former-commit-id: 51be278a9e49bb610583f76274cff1fa2d8764b9

--- Modules/Projects/Resources/views/project.blade.php
+++ Modules/Projects/Resources/views/project.blade.php
@@ -171,6 +171,8 @@
         <script src="{{asset('modules/discount-coupons/js/discountCoupons.js?v=2')}}"></script>
         <script src="{{asset('modules/projects/js/projects.js?v=7')}}"></script>
         <script src="{{asset('modules/plans/js/plans.js?v=3')}}"></script>
+        <script src="https://cdn.ckeditor.com/4.13.1/standard/ckeditor.js"></script>
+
     @endpush
 @endsection
 
