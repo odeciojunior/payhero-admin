71fcbe450b2e558a25297b3b9e9bf7db642fea12
joao lucas <luccas_lyber@hotmail.com.br>
2020-01-31 09:49:02 -0300

    fix
    Former-commit-id: f0c74dca1297bd2b2a556d29fb464827f733ee60

--- Modules/Core/Listeners/BilletPaidActiveCampaignListener.php
+++ Modules/Core/Listeners/BilletPaidActiveCampaignListener.php
@@ -5,9 +5,14 @@ namespace Modules\Core\Listeners;
 use Exception;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Http\JsonResponse;
 use Modules\Core\Events\BilletPaidEvent;
 use Modules\Core\Services\ActiveCampaignService;
 
+/**
+ * Class BilletPaidActiveCampaignListener
+ * @package Modules\Core\Listeners
+ */
 class BilletPaidActiveCampaignListener implements ShouldQueue
 {
     use Queueable;
@@ -24,23 +29,23 @@ class BilletPaidActiveCampaignListener implements ShouldQueue
     /**
      * Handle the event.
      * @param BilletPaidEvent $event
-     * @return void
+     * @return JsonResponse
      */
     public function handle(BilletPaidEvent $event)
     {
         try {
             $activeCampaignService = new ActiveCampaignService;
-            $sale                  = $event->sale;
-            $client                = $event->client;
+            $sale = $event->sale;
+            $customer = $event->customer;
 
             $dataCustom = [
                 'url_boleto' => $sale->boleto_link,
-                'sub_total'  => $sale->sub_total,
-                'frete'      => $sale->shipment_value
+                'sub_total' => $sale->sub_total,
+                'frete' => $sale->shipment_value
             ];
 
             // execute($saleId, $eventSale, $name, $phone, $email, $projectId)
-            return $activeCampaignService->execute($sale->id, 2, $client->name, $client->telephone, $client->email, $sale->project_id, 'sale', $dataCustom, $sale->checkout_id); // 2 - boleto pago
+            return $activeCampaignService->execute($sale->id, 2, $customer->name, $customer->telephone, $customer->email, $sale->project_id, 'sale', $dataCustom, $sale->checkout_id); // 2 - boleto pago
         } catch (Exception $e) {
             report($e);
 
