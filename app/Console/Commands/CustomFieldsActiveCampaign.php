15b8a1f24c7960dc4306b4517d4888f7d054cb5a
joao lucas <luccas_lyber@hotmail.com.br>
2020-02-05 11:19:27 -0300

    fix
    Former-commit-id: 7c32c3e96681c89f0b10aadc269680f99ae49e34

--- app/Console/Commands/CustomFieldsActiveCampaign.php
+++ app/Console/Commands/CustomFieldsActiveCampaign.php
@@ -46,7 +46,7 @@ class CustomFieldsActiveCampaign extends Command
      */
     public function handle()
     {
-        $transfers = Transfer::all();
+        /*$transfers = Transfer::all();
 
         foreach ($transfers as $transfer) {
             $transfer->update(
@@ -54,7 +54,70 @@ class CustomFieldsActiveCampaign extends Command
                     'type_enum' => $transfer->present()->getTypeEnum($transfer->type)
                 ]
             );
+        }*/
+    }
+
+    /*public function createCustomFields(){
+        $integrations = ActivecampaignIntegration::with('customFields')->where('id', 10)->get();
+
+        $fieldsDefault = [
+            'url_boleto',
+            'projeto_nome',
+            'link_carrinho_abandonado',
+            'codigo_pedido',
+            'produtos',
+            'sub_total',
+            'frete',
+            'codigo_rastreio',
+            'link_rastreamento',
+        ];
+
+        $service = new ActiveCampaignService();
+        foreach ($integrations as $integration) {
+
+            $service->setAccess($integration->api_url, $integration->api_key, $integration->id);
+
+            $fieldsCreate = $fieldsDefault;
+
+            $customFieldsIntegration = $integration->customFields->pluck('custom_field')->toArray();
+
+            if(isset($customFieldsIntegration[0])) {
+                $fieldsCreate = array_diff($fieldsDefault, $customFieldsIntegration);
+            }
+
+            $customnFieldsActive = $service->getCustomFields();
+            if(isset($customnFieldsActive['fields'])) {
+
+                foreach ($customnFieldsActive['fields'] as $value) {
+                    if(in_array($value['title'], $fieldsCreate)) {
+                        $fieldsCreate = array_diff($fieldsCreate, [$value['title']]);
+                        ActivecampaignCustom::create([
+                            'custom_field'                  => $value['title'],
+                            'custom_field_id'               => $value['id'],
+                            'activecampaign_integration_id' =>$integration->id
+                        ]);
+                    }
+                }
+            }
+
+            foreach ($fieldsCreate as $value) {
+                $type = ($value == 'produtos') ? 'listbox' : 'text';
+                $newField = json_decode($service->createCustomField($value, $type), true);
+                if(isset($newField['field']['id'])) {
+
+                    ActivecampaignCustom::create([
+                        'custom_field'                  => $value,
+                        'custom_field_id'               => $newField['field']['id'],
+                        'activecampaign_integration_id' =>$integration->id
+                    ]);
+
+                    $service->createCustomFieldRelation($newField['field']['id'], 0);
+                }
+            }
         }
+    }*/
+
+
 
         /*$userProjects = UserProject::withTrashed();
 
@@ -64,7 +127,7 @@ class CustomFieldsActiveCampaign extends Command
                 'type_enum' => $value->present()->getTypeEnum($value->type)
             ]);
         }*/
-    }
+
 
 
     /*
