ac6358b4d0d8b816dac6331462e9fdf912c60a66
LuanMaia1234 <luanmaia05@hotmail.com>
2020-03-02 16:36:21 -0300

    upsell rules
    Former-commit-id: 6f118e69d0f1dc3df591afcd7845c4546be1846e

---
+++ Modules/Core/Entities/ProjectUpsellRule.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Modules\Core\Entities;
+
+use App\Traits\FoxModelTrait;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Laracasts\Presenter\PresentableTrait;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ProjectUpsellRule extends Model
+{
+    use PresentableTrait, FoxModelTrait, SoftDeletes;
+    /**
+     * @var string
+     */
+    protected $keyType = 'integer';
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'project_id',
+        'description',
+        'apply_on_plans',
+        'offer_on_plans',
+        'active_flag',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
+}
+
