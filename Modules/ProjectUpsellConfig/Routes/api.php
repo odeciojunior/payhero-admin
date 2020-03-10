e66271a00e98f0d44a0d75102400a384cb9d64d9
LuanMaia1234 <luanmaia05@hotmail.com>
2020-03-10 17:34:18 -0300

    configuracoes upsell
    Former-commit-id: 0ac8a3809e8aaf19762a3f50da815dc12971f403

---
+++ Modules/ProjectUpsellConfig/Routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+Route::group(
+    [
+        'middleware' => ['auth:api', 'setUserAsLogged'],
+    ],
+    function() {
+        Route::apiResource('/projectupsellconfig', 'ProjectUpsellConfigApiController')
+             ->only('index', 'store', 'destroy', 'update', 'show', 'edit')
+             ->middleware('role:account_owner|admin');
+    }
+);
+//Route::middleware('auth:api')->get('/projectupsellconfig', function (Request $request) {
+//    return $request->user();
+//});
