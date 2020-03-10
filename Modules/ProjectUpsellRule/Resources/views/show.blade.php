3023ae60a0744f4d12d08553f6d9157075321cca
julio leichtweis <julioleichtweis@gmail.com>
2020-03-10 12:18:10 -0300

    merge
    Former-commit-id: dd7b71950fed06027cbf28c911c05e661e0632b1

---
+++ Modules/ProjectUpsellRule/Resources/views/show.blade.php
@@ -0,0 +1,25 @@
+<table id="upsell-details-table" class='table table-striped' style='width: 100%'>
+    <tbody>
+        <tr>
+            <td class="table-title">Descrição</td>
+            <td style='width: 20px'></td>
+            <td class='text-left upsell-description'></td>
+        </tr>
+        <tr>
+            <td class="table-title">Ao comprar planos</td>
+            <td style='width: 20px'></td>
+            <td class='text-left upsell-apply-plans'></td>
+        </tr>
+        <tr>
+            <td class="table-title">Oferecer planos</td>
+            <td style='width: 20px'></td>
+            <td class='text-left upsell-offer-plans'></td>
+        </tr>
+        <tr>
+            <td class="table-title">Status</td>
+            <td style='width: 20px'></td>
+            <td class='text-left upsell-status'>
+            </td>
+        </tr>
+    </tbody>
+</table>
