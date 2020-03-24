6990f5871f3e1c5d75052596f46d7588ee34a566
LuanMaia1234 <luanmaia05@hotmail.com>
2020-03-24 15:46:57 -0300

    ajustes link de afiliado
    Former-commit-id: dd600e09afa68c22ed3f6e060731862ba825068d

--- Modules/Projects/Resources/views/editprojectaffiliate.blade.php
+++ Modules/Projects/Resources/views/editprojectaffiliate.blade.php
@@ -5,16 +5,16 @@
             <div class='col-lg-12'>
                 <h3>Configurações Básicas</h3>
             </div>
-            <div class='col-lg-4'>
-                <div class='d-flex flex-column text-center' id='div-img-project' style='position: relative;'>
-                    <label for='photo'>Imagem capa do projeto</label>
-                    <div style="width:100%" class="text-center">
-                        <img id='previewimage' alt='Selecione a foto do projeto'
-                             src="{{asset('modules/global/img/projeto.png')}}"
-                             style="min-width: 250px; max-width: 250px;margin: auto">
-                    </div>
-                </div>
-            </div>
+{{--            <div class='col-lg-4'>--}}
+{{--                <div class='d-flex flex-column text-center' id='div-img-project' style='position: relative;'>--}}
+{{--                    <label for='photo'>Imagem capa do projeto</label>--}}
+{{--                    <div style="width:100%" class="text-center">--}}
+{{--                        <img id='previewimage' alt='Selecione a foto do projeto'--}}
+{{--                             src="{{asset('modules/global/img/projeto.png')}}"--}}
+{{--                             style="min-width: 250px; max-width: 250px;margin: auto">--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
             <div class='row col-md-8 col-lg-8 col-sm-12'>
                 <div class="col-12 row">
 {{--                    <div class="form-group col-12">--}}
@@ -31,7 +31,7 @@
 {{--                            <i class='icon wb-info-circle' aria-hidden='true'></i> Contato da loja informado no checkout e nos emails--}}
 {{--                        </p>--}}
 {{--                    </div>--}}
-                    <div class="form-group col-12">
+                    <div class="form-group col-12 mt-20">
                         <label for="contact">Telefone para suporte</label>
                         <div class="input-group">
                             <div class="input-group-prepend">
@@ -49,20 +49,20 @@
                 </div>
             </div>
         </div>
-        <div class='row'>
-            <div class='col-md-4 col-lg-4 col-sm-12'>
-                <div class="text-center">
-                    <label for='name'>Imagem para página do checkout e emails</label>
-                </div>
-                <div class='row'>
-                    <div class="col-12">
-                        <div class='d-flex flex-column text-center' id='div-img-project' style='position: relative;'>
-                            <img id='image-logo-email' alt='Selecione a foto do projeto' src='{{asset('modules/global/img/projeto.png')}}' style='max-height:250px;max-width:250px;margin:auto'>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+{{--        <div class='row'>--}}
+{{--            <div class='col-md-4 col-lg-4 col-sm-12'>--}}
+{{--                <div class="text-center">--}}
+{{--                    <label for='name'>Imagem para página do checkout e emails</label>--}}
+{{--                </div>--}}
+{{--                <div class='row'>--}}
+{{--                    <div class="col-12">--}}
+{{--                        <div class='d-flex flex-column text-center' id='div-img-project' style='position: relative;'>--}}
+{{--                            <img id='image-logo-email' alt='Selecione a foto do projeto' src='{{asset('modules/global/img/projeto.png')}}' style='max-height:250px;max-width:250px;margin:auto'>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
 
         <div class="mt-30">
             <div class="row">
