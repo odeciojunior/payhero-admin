f7786bc760c42704d210d42e41cea0c7bc339e34
joao lucas <luccas332@gmail.com>
2020-03-25 13:59:43 -0300

    add column affiliate_id table checkouts
    Former-commit-id: 6fba6606743954264fa7eecca349e47fd76f9257

---
+++ database/migrations/2020_03_25_100028_add_column_affiliate_id_on_table_checkouts.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnAffiliateIdOnTableCheckouts extends Migration
+{
+    /**
+     * Run the migrations.
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('checkouts', function(Blueprint $table) {
+            $table->unsignedBigInteger('affiliate_id')->nullable()->after('project_id');
+        });
+        Schema::table('checkouts', function(Blueprint $table) {
+            $table->foreign('affiliate_id')->references('id')->on('affiliates');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('checkouts', function(Blueprint $table) {
+            $table->dropForeign(["affiliate_id"]);
+            $table->dropColumn('affiliate_id');
+        });
+    }
+}
