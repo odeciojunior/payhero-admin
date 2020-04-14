e848b2e82adab679c1e72de8b69845e6bf1875d8
jeanvcastro <jean.castro1@outlook.com>
2020-04-14 12:56:03 -0300

    Frete automático
    Former-commit-id: 9d6bd302b6bc1b0b6d69e38cedea7af3f3294110

--- public/modules/products/js/products.js
+++ public/modules/products/js/products.js
@@ -143,8 +143,7 @@ $(document).ready(function () {
                         event.preventDefault();
 
                         let myForm = document.getElementById('my-form');
-                        console.log(myForm)
-                        alert(1)
+
                         let formData = new FormData(myForm);
 
                         if (verify()) {
