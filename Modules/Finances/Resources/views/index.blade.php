ad62039081acb24d8f043a08b3972010b8c8109c
joao lucas <luccas332@gmail.com>
2020-04-27 12:55:39 -0300

    fix
    Former-commit-id: bfc7f66424c48fe9093c2abc3196769bb040256a

--- Modules/Finances/Resources/views/index.blade.php
+++ Modules/Finances/Resources/views/index.blade.php
@@ -334,7 +334,7 @@
     @push('scripts')
         <script src="{{ asset('modules/global/js-extra/moment.min.js') }}"></script>
         <script src='{{ asset('modules/global/js/daterangepicker.min.js') }}'></script>
-        <script src="{{ asset('modules/finances/js/index.js?v=1') }}"></script>
+        <script src="{{ asset('modules/finances/js/index.js?v=2') }}"></script>
     @endpush
 
 @endsection
