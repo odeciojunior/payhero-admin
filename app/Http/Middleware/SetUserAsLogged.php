d0720b0b8d76f61ea4d1259a25acf5bfaba69f0c
joao lucas <luccas332@gmail.com>
2020-05-06 15:02:56 -0300

    auth user is null
    Former-commit-id: f0112651031e8225d499767e29dfdb7001536ae7

--- app/Http/Middleware/SetUserAsLogged.php
+++ app/Http/Middleware/SetUserAsLogged.php
@@ -5,20 +5,24 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Support\Facades\Redis;
 
+/**
+ * Class SetUserAsLogged
+ * @package App\Http\Middleware
+ */
 class SetUserAsLogged
 {
     /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param $request
+     * @param Closure $next
      * @return mixed
+     * Handle an incoming request.
      */
     public function handle($request, Closure $next)
     {
-        Redis::set('user-logged-'.auth()->user()->id, 'true', 'EX', 300);  // key expire in 300 seconds
+        if (!empty(auth()->user())) {
+            Redis::set('user-logged-' . auth()->user()->id, 'true', 'EX', 300);  // key expire in 300 seconds
+        }
 
         return $next($request);
     }
-
 }
