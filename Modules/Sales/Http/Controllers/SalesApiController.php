082aa37e605e73a07a64f615afe3dfed3df07939
jeanvcastro <jean.castro1@outlook.com>
2020-05-20 11:14:20 -0300

    Profitfy: ajustes
    Former-commit-id: c0087a71e365f8fa7d68d6fa3d8c69824e5c462d

--- Modules/Sales/Http/Controllers/SalesApiController.php
+++ Modules/Sales/Http/Controllers/SalesApiController.php
@@ -375,6 +375,63 @@ class SalesApiController extends Controller
         }
     }
 
+    public function indexExternal()
+    {
+        try {
+            $salesModel = new Sale();
+            $saleService = new SaleService();
+            $companiesModel = new Company();
+
+            //Conta as  requisições diárias da Profitfy
+            $log = settings()->group('profitfy_requests')->get(now()->format('Y-m-d'), true);
+            settings()->group('profitfy_requests')->set(now()->format('Y-m-d'), ($log ?? 0) + 1);
+
+            $user = auth()->user();
+
+            if (!empty($user)) {
+                $userId = $user->account_owner_id;
+
+                $saleStatus = [
+                    $salesModel->present()->getStatus('approved'),
+                    $salesModel->present()->getStatus('pending'),
+                ];
+
+                $sales = $salesModel->with([
+                    'transactions',
+                    'productsPlansSale.product',
+                ])->where('owner_id', $userId)
+                    ->whereDate('start_date', '<=', now()->subDays(30))
+                    ->whereIn('status', $saleStatus)
+                    ->paginate(100);
+
+                $userCompanies = $companiesModel->where('user_id', $userId)->pluck('id');
+
+                foreach ($sales as $sale) {
+                    $saleService->getDetails($sale, $userCompanies);
+
+                    $products = [];
+                    foreach ($sale->productsPlansSale as $productPlanSale) {
+                        $product = $productPlanSale->product;
+                        $products[] = [
+                            'id' => $product->shopify_id,
+                            'variant_id' => $product->shopify_variant_id,
+                            'quantity' => $productPlanSale->amount,
+                        ];
+                    }
+                    $sale->products = $products;
+                }
+                return SalesExternalResource::collection($sales);
+            } else {
+                return response()->json(['error' => 'Usuário não autenticado'], 401);
+            }
+        } catch (Exception $e) {
+            Log::warning('Erro ao obter venda - SalesApiController - indexExternal');
+            report($e);
+
+            return response()->json(['error' => 'Erro ao obter vendas'], 400);
+        }
+    }
+
     public function showExternal($saleId)
     {
         try {
