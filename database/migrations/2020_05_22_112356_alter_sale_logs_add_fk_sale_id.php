b183f43a4b7a27d574bebc075f2d45f151e46280
eliseu85 <eliseu.ferreiradesouza@gmail.com>
2020-05-22 15:08:48 -0300

    ajuste chargeback
    Former-commit-id: bc233434962abdaf2070fd0bb6f91c47abfe9521

---
+++ database/migrations/2020_05_22_112356_alter_sale_logs_add_fk_sale_id.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterSaleLogsAddFkSaleId extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('sale_logs', function(Blueprint $table) {
+            $table->foreign('sale_id')->references('id')->on('sales');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
