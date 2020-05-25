032dbd6ca16d56ac0a59c42a3811deccd013558f
jeanvcastro <jean.castro1@outlook.com>
2020-05-25 12:21:45 -0300

    Fix
    Former-commit-id: 26bb15adc6cd8bb402a2d95511eb4501649cacca

--- app/Jobs/ImportShopifyTrackingCodesJob.php
+++ app/Jobs/ImportShopifyTrackingCodesJob.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 
+use Illuminate\Support\Carbon;
 use Modules\Core\Entities\Tracking;
 use Vinkla\Hashids\Facades\Hashids;
 use Illuminate\Bus\Queueable;
@@ -9,8 +10,6 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
 use Modules\Core\Entities\Project;
 use Modules\Core\Entities\Sale;
 use Modules\Core\Events\TrackingCodeUpdatedEvent;
@@ -44,6 +43,7 @@ class ImportShopifyTrackingCodesJob implements ShouldQueue
         $project = $this->project;
         $productService = new ProductService();
         $trackingService = new TrackingService();
+        $trackingModel = new Tracking();
 
         $integration = $project->shopifyIntegrations->first();
         $shopifyService = new ShopifyService($integration->url_store, $integration->token, false);
@@ -52,19 +52,19 @@ class ImportShopifyTrackingCodesJob implements ShouldQueue
 
         $shopifyService->createShopWebhook([
             "topic" => "products/create",
-            "address" => 'https://app.cloudfox.net/postback/shopify/' . Hashids::encode($project->id),
+            "address" => 'https://app.cloudfox.net/postback/shopify/'.Hashids::encode($project->id),
             "format" => "json",
         ]);
 
         $shopifyService->createShopWebhook([
             "topic" => "products/update",
-            "address" => 'https://app.cloudfox.net/postback/shopify/' . Hashids::encode($project->id),
+            "address" => 'https://app.cloudfox.net/postback/shopify/'.Hashids::encode($project->id),
             "format" => "json",
         ]);
 
         $shopifyService->createShopWebhook([
             "topic" => "orders/updated",
-            "address" => 'https://app.cloudfox.net/postback/shopify/' . Hashids::encode($project->id) . '/tracking',
+            "address" => 'https://app.cloudfox.net/postback/shopify/'.Hashids::encode($project->id).'/tracking',
             "format" => "json",
         ]);
 
@@ -76,36 +76,59 @@ class ImportShopifyTrackingCodesJob implements ShouldQueue
             ])->doesntHave('tracking')
             ->where('status', 1)
             ->whereNotNull('shopify_order')
-            ->chunk(100, function ($sales) use ($productService, $trackingService, $shopifyService) {
+            ->chunk(100, function ($sales) use ($productService, $trackingService, $trackingModel, $shopifyService) {
                 foreach ($sales as $sale) {
                     try {
                         $fulfillments = $shopifyService->findFulfillments($sale->shopify_order);
                         if (!empty($fulfillments)) {
-
                             $saleProducts = $productService->getProductsBySale($sale);
                             foreach ($fulfillments as $fulfillment) {
-                                if (!empty($fulfillment->getTrackingNumber())) {
-
+                                $trackingCode = $fulfillment->getTrackingNumber();
+                                if (!empty($trackingCode)) {
                                     foreach ($fulfillment->getLineItems() as $lineItem) {
-
-                                        $products = $saleProducts->where('shopify_variant_id', $lineItem->getVariantId())->where('amount', $lineItem->getQuantity());
+                                        $products = $saleProducts->where('shopify_variant_id',
+                                            $lineItem->getVariantId())->where('amount', $lineItem->getQuantity());
 
                                         if ($products->count()) {
                                             foreach ($products as &$product) {
-
                                                 $productPlanSale = $sale->productsPlansSale->find($product->product_plan_sale_id);
 
-                                                $tracking = new Tracking();
-                                                $tracking->tracking_code = $fulfillment->getTrackingNumber();
+                                                //verifica se já tem uma venda nessa conta com o mesmo código de rastreio
+                                                $sale = $productPlanSale->sale;
+                                                $exists = $trackingModel->where('trackings.tracking_code',
+                                                    $trackingCode)
+                                                    ->where('sale_id', '!=', $sale->id)
+                                                    ->whereHas('sale', function ($query) use ($sale) {
+                                                        $query->where('owner_id', $sale->owner_id)
+                                                            ->where('upsell_id', '!=', $sale->id);
+                                                    })->exists();
+                                                if ($exists) {
+                                                    continue;
+                                                }
 
-                                                $apiTracking = $trackingService->sendTrackingToApi($tracking);
+                                                $apiResult = $trackingService->sendTrackingToApi($trackingCode);
 
-                                                if (!empty($apiTracking)) {
+                                                if (!empty($apiResult)) {
+                                                    //verifica se a data de postagem na transportadora é menor que a data da venda
+                                                    if (!empty($apiResult->origin_info)) {
+                                                        $postDate = Carbon::parse($apiResult->origin_info->ItemReceived);
+                                                        if ($postDate->lt($productPlanSale->created_at)) {
+                                                            if (!$apiResult->already_exists) { // deleta na api caso seja recém criado
+                                                                $trackingService->deleteTrackingApi($apiResult);
+                                                            }
+                                                            continue;
+                                                        }
+                                                    }
+
+                                                    $statusEnum = $trackingService->parseStatusApi($apiResult->status);
+
                                                     activity()->disableLogging();
-                                                    $tracking = $trackingService->createTracking($fulfillment->getTrackingNumber(), $productPlanSale);
+                                                    $tracking = $trackingService->createTracking($trackingCode,
+                                                        $productPlanSale, $statusEnum);
                                                     activity()->enableLogging();
-                                                    $product->tracking_code = $fulfillment->getTrackingNumber();
-                                                    event(new TrackingCodeUpdatedEvent($sale, $tracking, $saleProducts));
+                                                    $product->tracking_code = $trackingCode;
+                                                    event(new TrackingCodeUpdatedEvent($sale, $tracking,
+                                                        $saleProducts));
                                                 }
                                             }
                                         }
@@ -118,6 +141,5 @@ class ImportShopifyTrackingCodesJob implements ShouldQueue
                     }
                 }
             });
-
     }
 }
