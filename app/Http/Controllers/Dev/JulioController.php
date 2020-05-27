ebd950b54b97d7f0b010fde755e3c8814d5080ae
jeanvcastro <jean.castro1@outlook.com>
2020-05-27 12:20:51 -0300

    Verificar orders shopify
    Former-commit-id: e13ca3006c00b59290c6e77fb89ebbb9984378ed

--- app/Http/Controllers/Dev/JulioController.php
+++ app/Http/Controllers/Dev/JulioController.php
@@ -393,6 +393,63 @@ class JulioController extends Controller
         // }
         // $this->info('ACABOOOOOOOOOOOOOU!');
     }
+
+    public function approveShopifyOrderAntifraud() {
+        //command
+
+        $salesModel = new Sale();
+
+        $sales = $salesModel->with(['project.shopifyIntegrations'])
+            ->where('status', $salesModel->present()->getStatus('approved'))
+            ->where('payment_method', $salesModel->present()->getPaymentType('credit_card'))
+            ->whereNotNull('shopify_order')
+            ->whereHas('saleLogs', function ($query) use ($salesModel) {
+                $query->where('status_enum', $salesModel->present()->getStatus('in_review'));
+            })->get();
+
+        $shopifyStores = [];
+
+        $total = $sales->count();
+
+        $ordersApproved = 0;
+        foreach ($sales as $key => $sale) {
+            $count = $key + 1;
+            $this->line("Verificando venda {$count} de {$total}: {$sale->id}");
+
+            $project = $sale->project;
+            if (!empty($shopifyStores[$project->id])) {
+                $shopifyService = $shopifyStores[$project->id];
+            } else {
+                $integration = $sale->project->shopifyIntegrations->first();
+                if (!empty($integration)) {
+                    $shopifyService = new ShopifyService($integration->url_store, $integration->token, false);
+                    $shopifyStores[$project->id] = $shopifyService;
+                } else {
+                    $this->warn('Nenhuma integração encontrada para este projeto');
+                    continue;
+                }
+            }
+
+            try {
+                $order = $shopifyService->getClient()->getOrderManager()->find($sale->shopify_order);
+                if ($order->getFinancialStatus() == 'pending') {
+                    $data = [
+                        "kind" => "capture",
+                        "gateway" => "cloudfox",
+                        "authorization" => Hashids::connection('sale_id')->encode($sale->id),
+                    ];
+                    $shopifyService->getClient()->getTransactionManager()->create($sale->shopify_order, $data);
+                    $ordersApproved++;
+
+                    $this->info('Order criada no shopify');
+                }
+            } catch (Exception $e) {
+                $this->error($e->getMessage());
+            }
+        }
+
+        $this->info("{$ordersApproved} orders aprovadas no shopify de {$total} vendas verificadas");
+    }
 }
 
 
