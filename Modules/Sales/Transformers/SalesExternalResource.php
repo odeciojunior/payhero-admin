a9f1a6c4153006f105ead191137bec7e18b73013
jeanvcastro <jean.castro1@outlook.com>
2020-06-16 14:56:01 -0300

    Fix
    Former-commit-id: 739cb8c859a13aeb8baac5a0926de18cccda54b8

--- Modules/Sales/Transformers/SalesExternalResource.php
+++ Modules/Sales/Transformers/SalesExternalResource.php
@@ -15,19 +15,23 @@ use Modules\Core\Entities\Affiliate;
 class SalesExternalResource extends JsonResource
 {
     /**
-     * @param Request $request
+     * @param  Request  $request
      * @return array
      */
     public function toArray($request)
     {
-        $fee = preg_replace("/[^0-9]/", "", $this->details->taxaReal)/100;
-        $fee += preg_replace("/[^0-9]/", "", $this->installment_tax_value ?? 0)/100;
+        $amount = preg_replace("/[^0-9]/", "", $this->details->total) / 100;
+
+        $netAmount = preg_replace("/[^0-9]/", "", $this->details->comission) / 100;
+
+        $fee = preg_replace("/[^0-9]/", "", $this->details->taxaReal) / 100;
+        $fee += preg_replace("/[^0-9]/", "", $this->installment_tax_value ?? 0) / 100;
 
         return [
             'id' => Hashids::connection('sale_id')->encode($this->id),
-            'amount' => (float) number_format(preg_replace("/[^0-9]/", "", $this->details->total)/100, 2),
-            'fee' => (float) number_format($fee, 2),
-            'net_amount' => (float) number_format(preg_replace("/[^0-9]/", "", $this->details->comission)/100, 2),
+            'amount' => (float) number_format($amount, 2, '.', ''),
+            'fee' => (float) number_format($fee, 2, '.', ''),
+            'net_amount' => (float) number_format($netAmount, 2, '.', ''),
             'payment_method' => $this->present()->getPaymentType(),
             'status' => $this->present()->getStatus(),
             'created_at' => $this->end_date,
