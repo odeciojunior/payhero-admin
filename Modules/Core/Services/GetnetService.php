031e5bc45b7ca71c0d1c296de6402456f4987eb4
julio leichtweis <julioleichtweis@gmail.com>
2020-06-18 11:13:40 -0300

    geração do token de acesso getnet backoffice
    Former-commit-id: f2b9d13540bee0ccb2ba07234d9d8ae48f27c58e

--- Modules/Core/Services/GetnetService.php
+++ Modules/Core/Services/GetnetService.php
@@ -2,25 +2,62 @@
 
 namespace Modules\Core\Services;
 
+use Exception;
 use Illuminate\Support\Facades\Http;
 
 class GetnetService
 {
-
-    private $clientId;
-    private $clientSecret;
+    const URL_API = 'https://api-homologacao.getnet.com.br/';
+    
+    private $accessToken;
 
     public function __construct()
     {
-        $this->clientId = getenv('Client_ID');
-        $this->clientSecret = getenv('Client_Secret');
-        $this->token();
+        $this->setAccessToken();
     }
 
-    public function token()
+    public function dumpAccessToken()
     {
-        $credentialsBase64 = base64_encode($this->clientSecret . ':' . $this->clientSecret);
+        dd($this->accessToken);
+    }
+
+    public function getAuthorizationToken()
+    {
+        $clientId = getenv('GET_NET_CLIENT_ID');
+        $clientSecret = getenv('GET_NET_CLIENT_SECRET');
+
+        return base64_encode($clientId . ':' . $clientSecret);
+    }
+
+    public function setAccessToken()
+    {
+        try {
+            $headers = [
+                'content-type: application/x-www-form-urlencoded',
+                'authorization: Basic ' . $this->getAuthorizationToken(),
+            ];
+
+            $ch = curl_init(self::URL_API . 'credenciamento/auth/oauth/v2/token');
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, 'scope=mgm&grant_type=client_credentials');
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            $result     = curl_exec($ch);
+            $httpStatus = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            curl_close($ch);
+
+            if($httpStatus == 200){
+                $this->accessToken = json_decode($result)->access_token;
+            }
+            else{
+                throw new Exception('Erro ao gerar token de acesso backoffice getnet');
+            }
+
+        } catch (Exception $ex) {
+            report($ex);
+        }
 
     }
+
 
 }
--- app/Console/Commands/GenericCommand.php
+++ app/Console/Commands/GenericCommand.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Modules\Core\Entities\Sale;
 use Modules\Core\Events\TrackingCodeUpdatedEvent;
+use Modules\Core\Services\GetnetService;
 use Modules\Core\Services\ProductService;
 use Modules\Core\Services\ShopifyService;
 use Modules\Core\Services\SmsService;
@@ -29,85 +30,9 @@ class GenericCommand extends Command
 
     public function handle()
     {
-        $productService = new ProductService();
-        $trackingService = new TrackingService();
-        $smsService = new SmsService();
+        $getNetService = new GetnetService();
 
-        $shopifyStores = [];
-
-        $salesQuery = Sale::with([
-            'project',
-            'productsPlansSale',
-        ])->doesntHave('tracking')
-            ->whereHas('project', function ($query) {
-                $query->whereNotNull('shopify_id');
-            })
-            ->where('status', 1)
-            //->where('owner_id', 557)
-            ->whereNotNull('shopify_order')
-            ->orderByDesc('id');
-
-        $total = $salesQuery->count();
-        $count = 0;
-        $added = 0;
-
-        $salesQuery->chunk(100,
-            function ($sales) use ($productService, $trackingService, $shopifyStores, $total, &$count, &$added) {
-                foreach ($sales as $sale) {
-                    try {
-                        $count += 1;
-                        $this->line("Verificando venda {$count} de {$total}: {$sale->id}");
-
-                        $project = $sale->project;
-
-                        if (empty($shopifyStores[$project->id])) {
-                            $integration = $project->shopifyIntegrations->first();
-                            $shopifyService = new ShopifyService($integration->url_store, $integration->token, false);
-                            $shopifyStores[$project->id] = $shopifyService;
-                        } else {
-                            $shopifyService = $shopifyStores[$project->id];
-                        }
-
-                        $fulfillments = $shopifyService->findFulfillments($sale->shopify_order);
-                        if (!empty($fulfillments)) {
-                            $saleProducts = $productService->getProductsBySale($sale);
-                            foreach ($fulfillments as $fulfillment) {
-                                $trackingCode = $fulfillment->getTrackingNumber();
-                                if (!empty($trackingCode)) {
-                                    foreach ($fulfillment->getLineItems() as $lineItem) {
-                                        $products = $saleProducts->where('shopify_variant_id',
-                                            $lineItem->getVariantId())->where('amount', $lineItem->getQuantity());
-
-                                        if ($products->count()) {
-                                            foreach ($products as &$product) {
-                                                $productPlanSale = $sale->productsPlansSale->find($product->product_plan_sale_id);
-
-                                                $tracking = $trackingService->createOrUpdateTracking($trackingCode,
-                                                    $productPlanSale);
-
-                                                if (!empty($tracking)) {
-                                                    $added++;
-                                                    $this->info("Trackings criado!");
-                                                    $product->tracking_code = $trackingCode;
-                                                    event(new TrackingCodeUpdatedEvent($sale, $tracking,
-                                                        $saleProducts));
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        } else{
-                          $this->warn('Sem fulfillment!');
-                        }
-                    } catch (\Exception $e) {
-                        $this->error($e->getMessage());
-                    }
-                }
-            });
-
-        $this->info("Trackings criados: {$added}");
-        $smsService->sendSms('5524998345779', "ACABOU! Trackings criados: {$added}");
+        $getNetService->dumpAccessToken();
     }
 }
 
