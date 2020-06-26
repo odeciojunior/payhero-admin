9fae05018e843b28bdbe60f36f5579b2fc356e99
jeanvcastro <jean.castro1@outlook.com>
2020-06-26 11:32:00 -0300

    Ajustes
    Former-commit-id: 1a97e3b411d59480b87a98ccdc7cd65daaa724f9

--- Modules/Core/Services/TrackingService.php
+++ Modules/Core/Services/TrackingService.php
@@ -281,7 +281,7 @@ class TrackingService
                     ]);
                 }
             } else { //senao cria o tracking
-                $tracking = $trackingModel->create([
+                $tracking = $trackingModel->firstOrNew([
                     'sale_id' => $productPlanSale->sale_id,
                     'product_id' => $productPlanSale->product_id,
                     'product_plan_sale_id' => $productPlanSale->id,
@@ -291,6 +291,7 @@ class TrackingService
                     'tracking_status_enum' => $statusEnum,
                     'system_status_enum' => $systemStatusEnum,
                 ]);
+                $tracking->save();
             }
 
             return $tracking;
