8bc98de812185e4eacbcfc40e867a00b12ba1cef
LuanMaia1234 <luanmaia05@hotmail.com>
2020-07-23 16:20:47 -0300

    ajustes
    Former-commit-id: 651aafd05825bad2135772833bd03805c00bf421

--- Modules/Affiliates/Routes/api.php
+++ Modules/Affiliates/Routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 Route::group(
     [
-        'middleware' => ['auth:api', 'scopes:admin'],
+        'middleware' => ['auth:api'],
     ],
     function() {
 
