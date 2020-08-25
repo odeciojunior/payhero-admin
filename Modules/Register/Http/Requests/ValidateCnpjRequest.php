8e4c6dbbc0c8eb10f82682e724f47b8b707e1647
Pedro Henrique Rodrigues <pedrohenrique@cloudfox.net>
2020-08-25 15:28:22 -0300

    [ajuste] requests /register

---
+++ Modules/Register/Http/Requests/ValidateCnpjRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Modules\Register\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ValidateCnpjRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'company_document'  => 'nullable',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            // Coloque aqui as mensagens de erro da validação
+        ];
+    }
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+}
---
+++ Modules/Register/Http/Requests/ValidateEmailRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Modules\Register\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ValidateEmailRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'email'             => 'required|unique:users|max:200',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'email.required'        => 'Precisamos do seu email para continuar',
+            'email.unique'          => 'Email informado ja esta sendo utilizado',
+        ];
+    }
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+}
