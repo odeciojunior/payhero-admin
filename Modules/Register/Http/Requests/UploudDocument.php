0617d9861821555f44e87d350fc4670ad891d34a
marcosmarion <marcosmarioncloudfox@cloudfox.net>
2020-09-03 16:52:49 -0300

    Colocando o erro da validação retornando em json

--- Modules/Register/Http/Requests/UploudDocument.php
+++ Modules/Register/Http/Requests/UploudDocument.php
@@ -2,7 +2,9 @@
 
 namespace Modules\Register\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class UploudDocument extends FormRequest
 {
@@ -29,4 +31,10 @@ class UploudDocument extends FormRequest
     {
         return true;
     }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json($validator->errors(), 422));
+    }
+
 }
