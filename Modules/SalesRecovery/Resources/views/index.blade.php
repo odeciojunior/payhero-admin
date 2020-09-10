b33a8efe5b41657c7a187b5378ca568f037047a5
LuanMaia1234 <luanmaia05@hotmai.com>
2020-09-10 12:55:27 -0300

    relatorio recuperacao

--- Modules/SalesRecovery/Resources/views/index.blade.php
+++ Modules/SalesRecovery/Resources/views/index.blade.php
@@ -10,7 +10,26 @@
     <!-- Page -->
     <div class="page">
         <div class="page-header container">
-            <h1 class="page-title">Recuperação de vendas</h1>
+            <div class="row align-items-center justify-content-between" style="min-height:50px">
+                <div class="col-6">
+                    <h1 class="page-title">Recuperação de vendas</h1>
+                </div>
+                @if(auth()->user()->hasRole('account_owner') || auth()->user()->hasRole('admin'))
+                    <div class="col-6 text-right">
+                        <div class="justify-content-end align-items-center" id="export-excel">
+                            <div class="p-2 align-items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon-download" width="20" height="20" viewBox="0 0 24 24">
+                                    <path d="M8 20h3v-5h2v5h3l-4 4-4-4zm11.479-12.908c-.212-3.951-3.473-7.092-7.479-7.092s-7.267 3.141-7.479 7.092c-2.57.463-4.521 2.706-4.521 5.408 0 3.037 2.463 5.5 5.5 5.5h3.5v-2h-3.5c-1.93 0-3.5-1.57-3.5-3.5 0-2.797 2.479-3.833 4.433-3.72-.167-4.218 2.208-6.78 5.567-6.78 3.453 0 5.891 2.797 5.567 6.78 1.745-.046 4.433.751 4.433 3.72 0 1.93-1.57 3.5-3.5 3.5h-3.5v2h3.5c3.037 0 5.5-2.463 5.5-5.5 0-2.702-1.951-4.945-4.521-5.408z"/>
+                                </svg>
+                                <div class="btn-group" role="group">
+                                    <button id="bt_get_xls" type="button" class="btn btn-round btn-default btn-outline btn-pill-left">.XLS</button>
+                                    <button id="bt_get_csv" type="button" class="btn btn-round btn-default btn-outline btn-pill-right">.CSV</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endif
+            </div>
         </div>
         <div class="page-content container">
             <div id='project-not-empty' style='display:none'>
@@ -45,6 +64,19 @@
                     </div>
                     <div class="row mt-15">
                         <div class='col-xl-3'></div>
+                    </div>
+                </div>
+                <!-- Aviso de Exportação -->
+                <div id="alert-export" class="alert alert-info alert-dismissible fade show card py-10 pl-20 pr-10" style="display:none;">
+                    <div class="d-flex">
+                        <i class="material-icons mr-10">info</i>
+                        <div class="w-full">
+                            <strong class="font-size-16">Exportando seu relatório</strong>
+                            <p class="font-size-14 pr-md-100 mb-0">Sua exportação será entregue por e-mail para:
+                                <strong id="export-email"></strong> e aparecerá nas suas notificações. Pode levar algum tempo, dependendo de quantos registros você estiver exportando.
+                            </p>
+                        </div>
+                        <i class="material-icons pointer" data-dismiss="alert">close</i>
                     </div>
                 </div>
                 <div class="card shadow" style="min-height: 300px">
@@ -239,9 +271,30 @@
             {{-- FIM projeto nao existem projetos--}}
         </div>
     </div>
+
+    <!-- Modal exportar relatorio -->
+    <div id="modal-export-sale" class="modal fade example-modal-lg modal-3d-flip-vertical" role="dialog" tabindex="-1">
+        <div class="modal-dialog modal-dialog-centered modal-simple">
+            <div class="modal-content p-10">
+                <div class='my-20 mx-20 text-center'>
+                    <h3 class="black"> Informe o email para receber o relatório </h3>
+                </div>
+                <div class="modal-footer">
+                    <input type="email" id="email_export">
+                    <button type="button" class="btn btn-success btn-confirm-export-sale">
+                        Enviar
+                    </button>
+                    <a id="btn-mobile-modal-close" class="btn btn-primary" style='color:white' role="button" data-dismiss="modal" aria-label="Close">
+                        Fechar
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Modal -->
     @push('scripts')
 
-        <script src="{{ asset('modules/salesrecovery/js/salesrecovery.js?v=4') }}"></script>
+        <script src="{{ asset('modules/salesrecovery/js/salesrecovery.js?v=5') }}"></script>
         <script src="{{ asset('modules/global/js-extra/moment.min.js') }}"></script>
         <script src='{{asset('modules/global/js/daterangepicker.min.js')}}'></script>
 
