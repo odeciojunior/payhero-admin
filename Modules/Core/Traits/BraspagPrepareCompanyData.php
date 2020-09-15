8725822635583c2ba228e26437a9bb30b511b142
Henrique Brites <henriquebrites@live.com>
2020-09-15 17:01:02 -0300

    braspag postback implementation

--- Modules/Core/Traits/BraspagPrepareCompanyData.php
+++ Modules/Core/Traits/BraspagPrepareCompanyData.php
@@ -149,7 +149,9 @@ trait BraspagPrepareCompanyData
     private function getPrepareDataCreatePjCompany(Company $company)
     {
         $user = $company->user;
+        $userInformation = $user->userInformation;
         $telephone = FoxUtils::formatCellPhoneBraspag($user->cellphone);
+
         return [
             'CorporateName' => FoxUtils::removeAccents(FoxUtils::removeSpecialChars($company->fantasy_name)),
             'FancyName' => FoxUtils::removeAccents(FoxUtils::removeSpecialChars($company->fantasy_name)),
@@ -167,7 +169,7 @@ trait BraspagPrepareCompanyData
                 'Operation' => '',
                 'VerifierDigit' => $company->account_digit == 'X' || $company->account_digit == 'x' ? 0 : $company->account_digit,
                 'AgencyNumber' =>$company->agency,
-                'AgencyDigit' => is_null($company->agency_digit) ? 'x' : $company->account_digit,
+                'AgencyDigit' => FoxUtils::isEmpty($company->braspaagency_digitg_merchant_id) ? 'x' : $company->account_digit,
                 'DocumentNumber' => $company->company_document,
                 'DocumentType' => 'CNPJ'
             ],
@@ -182,6 +184,9 @@ trait BraspagPrepareCompanyData
                 'State' => $company->state,
                 'ZipCode' => FoxUtils::onlyNumbers($company->zip_code)
             ],
+            'Agreement' =>  $this->getAgreementListCommissions($company->user),
+
+
 
             // "Notification": {
             //     "Url": "https://site.com.br/api/subordinados",
@@ -194,7 +199,6 @@ trait BraspagPrepareCompanyData
             //         "Value": "value2"
             //     }]
             // },
-
             'Notification' => [
                 'Url' => 'https://app.cloudfox.net/postback/braspag',
                 'Headers' => [
@@ -206,149 +210,92 @@ trait BraspagPrepareCompanyData
                 ]
 
             ]
+
+            // "Attachments": [{
+            //     "AttachmentType": "ProofOfBankDomicile",
+            //     "File": {
+            //         "Name": "comprovante",
+            //         "FileType": "png",
+            //         "Data": "iVBORw0KGgoAAAANSUhEUgAAAH4AAAAsCAMAAACUu/xGAAAAq1BMVEUAAABlZVJlZVKsrJthYU+zs6Grq5ylpZazs6FlZVJfX01lZVJlZVKsrJurq5urq5xlZVKtrZ1lZVJlZVKvr52zs6GysqCoqJeqqpmzs6Grq5xlZVJgYE6zs6Gnp5mrq5yiopRjY1CRkX2rq5yzs6FlZVKRkX2goJKKineRkX2Pj3yrq5yIiHWRkX2RkX2RkX1lZVKRkX2rq5yzs6GoqJdfX02goJKHh3SHh3VrpzVsAAAAMHRSTlMAQIDHx3+Ax0Ag7qBgIA9AEFCPMLOgMO7bYKBQ24+zYNuzkY9wcAXu0oiocPFBMHYlVbK0AAAD3UlEQVRYw6SW7Y6qMBCGB0IkLfKdnB9ocFmjru7HERL03P+VnXY6bdmWjcF9f2inxjydvjMDcHy99zP693oEpTpQYjBR7W4VmzA81GoZCDn/ycrValVmYOJcKBWL1/4HnUEpupLGxOI47iQmDkfc4GEBEFyNQkClzYDKQQs3VmJBufu6G7zRWNMeUzEHUnLVWs/gy9vg4NNB4wUIPOG2h7e8NcV0HRt7QPDxfzTd4ptleB5F6ro3NtsIc7UnjMKKXyuN30ZS+PuLRMW7PN+l2vlhAZ6yqCZmcrm05stfOrwVpvEBaJWStIOpVk/gC8Rb62tjRj25Fx/fEsgqE27cluKB8GR9hDFzeX44CFbmJb9/Cn8w1ldA5tO9VD/gc8FpveTbxfi1LXWOl10Z80c0Yx7/jpyyjRtd9zuxU8ZL8FEYJjZFpg6yIfOpKsf1FJ+EUkzddKkabQ+o0zCcwMN/vZm+uLh4UmW7nptTCBVq5nUF4Y0CgBaNVip18jsPn370909cfX708/gusF3fkQfrKZHXHh45Wi8meRefvfVCfwGOZ9zx8TZ9TjWY2M6vVf4jm8e3WYrDJ1Vj4N3FHwVd6vKFCxefBMFmq7ub6UI7TMZw0SEv8ryPDVaoxPiWufhL/02zY0cm3ZH1VgxIIYa1U/nIibH/EZjjp4M/9w/x9FijbyuqdzOVH+BbWQJxHMupd4pjINhDPKVH1lslBl9g6OKb73j0wmoBHrMj691nsJ0QLn4l0/09nrIm6wv7nGdQqwjGucvPJSWjN4z8aXyBlkfK+i2gmDI/HENGjXA9uPhsUJ22p2OQFg3daaFx0/9qnWBRbOl9hHlvOw3OW/xs4Hf4rcnYzj+OeFOIHj4dtG7/2y+b3IhBGAqjUiQWQ9JI/ErDpop6gcei9z9ZIXHIhLaLSGRW8zYxIuaTZccxqsGfHDXvH4cf37Z4e3ihxVOTp5bf4E8N2u+3PWB2SP7tXsfsFl80rtOeZX/gvz6//7tmnFFzD2mkxnFgL710ToHH1eCcm/LU2aA9m027v+kBH8ipyHbACxAMWaV5I4v2ZgAzIxkUGXIqkn3xrhw4wVe8hoMmOwBmYJMiJy+lHPriNcSyrvgEgUS2h/vl1BcvSqgcZsPbbABrhgdgvhgvS6hIYsPP8MwTVR5SLZA4573xHMpCV7xGZBFmxyProfR64yNCgKh4hygjXIuvpdcbPyEayA2vsEpRHcgl6gtzr8A9ho0RlgQnBPoK4tV45gBfGQZ6KQBDqzRcjdeAqQwHUfYp+SohcQdc1/Ukm4Gw4dV6vqTkM+uQpRv8E2VPF/sPp9xSb2qlGH4AAAAASUVORK5CYII="
+            //     }
+            // }]
+
+            'Attachments' => [
+                [
+                    'AttachmentType' =>
+                ]
         ];
-        //     'Attachments' => [
-        //         [
-        //             'AttachmentType' =>
-        //         ]
 
-        //     'merchant_id' => $this->getMerchantId(),
-        //     'legal_document_number' => $company->company_document,
-        //     'legal_name' => FoxUtils::removeAccents(FoxUtils::removeSpecialChars($company->fantasy_name)),
-        //     'trade_name' => FoxUtils::removeAccents(FoxUtils::removeSpecialChars($company->fantasy_name)),
-        //     'state_fiscal_document_number' => empty($company->state_fiscal_document_number) ? 'ISENTO' : $company->state_fiscal_document_number,
-        //     'email' => $user->email,
-        //     'cellphone' => [
-        //         'area_code' => $telephone['dd'],
-        //         'phone_number' => $telephone['number']
-        //     ],
-        //     'business_address' => [
-        //         'street' => FoxUtils::removeAccents(FoxUtils::removeSpecialChars($company->street)),
-        //         'number' => $company->number ?? '',
-        //         'district' => FoxUtils::removeAccents(FoxUtils::removeSpecialChars($company->neighborhood)),
-        //         'city' => FoxUtils::removeAccents(FoxUtils::removeSpecialChars($company->city)),
-        //         'state' => $company->state,
-        //         'postal_code' => FoxUtils::onlyNumbers($company->zip_code),
-        //         'suite' => empty($company->complement) ? '' : FoxUtils::removeAccents(
-        //             FoxUtils::removeSpecialChars($company->complement)
-        //         ),
-        //         'country' => 'BR'
 
-        //     ],
-        //     'bank_accounts' => [
-        //         'type_accounts' => 'unique',
-        //         'unique_account' => [
-        //             'bank' => $company->bank,
-        //             'agency' => $company->agency,
-        //             'account' => $company->account,
-        //             'account_type' => $company->present()->getAccountType(),
-        //             'account_digit' => $company->account_digit == 'X' || $company->account_digit == 'x' ? 0 : $company->account_digit,
-        //         ]
-        //     ],
-        //     'url_callback' => 'https://app.cloudfox.net/postback/getnet',
-        //     "accepted_contract" => "S",
-        //     "liability_chargeback" => "S",
-        //     'marketplace_store' => "S",
-        //     'payment_plan' => 1005,
-        //     'business_entity_type' => FoxUtils::onlyNumbers($company->business_entity_type),
-        //     'economic_activity_classification_code' => FoxUtils::onlyNumbers(
-        //         $company->economic_activity_classification_code
-        //     ),
-        //     'monthly_gross_income' => FoxUtils::onlyNumbers($company->monthly_gross_income),
-        //     'federal_registration_status' => 'active',
-        //     'founding_date' => $company->founding_date,
-        // ];
     }
 
-    /**
-     * @param Company $company
-     * @return array
-     */
-    private function getPrepareDataComplementPjCompany(Company $company)
+    private function getAgreementListCommissions($user)
     {
-        return [
-            'merchant_id' => $this->getMerchantId(),
-            'subseller_id' => $company->subseller_getnet_id,
-            'legal_document_number' => $company->company_document,
-            "working_hours" => [
-                "start_day" => "mon",
-                "end_day" => "mon",
-                "start_time" => "08:00:00",
-                "end_time" => "18:00:00"
-            ],
-            'identification_document' => [
-                'document_type' => 'nire',
-                'document_number' => '',
-                'document_issue_date' => $company->document_issue_date,
-                'document_issuer' => $company->document_issuer,
-                'document_issuer_state' => $company->document_issuer_state
-            ],
-            'url_callback' => 'https://app.cloudfox.net/postback/getnet',
-            'state_fiscal_document_number' => $company->state_fiscal_document_number,
-            'federal_registration_status_date' => $company->federal_registration_status_date,
-            'social_value' => $company->social_value,
+        $listCommissions = [];
+
+        $brands = [
+            "MASTERCARD",
+            "MAESTRO",
+            "VISA",
+            "VISA ELECTRON",
+            "AMEX",
+            "ELO CRÉDITO",
+            "ELO DÉBITO",
+            "HIPERCARD"
         ];
-    }
 
-    /**
-     * @param Company $company
-     * @return array
-     * @throws PresenterException
-     */
-    private function getPrepareDataUpdatePjCompany(Company $company)
-    {
-        $user = $company->user;
-        $telephone = FoxUtils::formatCellPhoneGetNet($user->cellphone);
+        $products = [
+            "DEBITO A VISTA",
+            "CREDITO A VISTA",
+            "PARCELADO LOJISTA 3X",
+            "PARCELADO LOJISTA 6X",
+            "PARCELADO LOJISTA 9X",
+            "PARCELADO LOJISTA 12X",
+            "PARCELADO EMISSOR",
+            "BOLETO"
+        ];
 
-        $country = 'BR';
-        if ($company->country == 'usa') {
-            $country = 'EUA';
+        foreach ($brands as $brand) {
+            foreach ($products as $product) {
+                if (!in_array([$product, $brand], $listCommissions)) {
+                    switch ($product) {
+                        case 'BOLETO':
+                            $value = 100 - $user->boleto_tax;
+                            break;
+                        case 'DEBITO A VISTA':
+                            $value = 100 - $user->debit_card_tax;
+                            break;
+                        default:
+                            $value = 100 - $user->credit_card_tax;
+                    }
+
+                    if ((in_array($brand, $brands)) && ($product == 'DEBITO A VISTA' || $product == 'BOLETO')) {
+                        continue;
+                    }
+
+                    if ((in_array($brand, ['MAESTRO', 'VISA ELECTRON', 'ELO DÉBITO']))
+                        && in_array($product, [
+                            'CREDITO A VISTA',
+                            'PARCELADO LOJISTA 3X',
+                            'PARCELADO LOJISTA 6X',
+                            'PARCELADO LOJISTA 9X',
+                            'PARCELADO LOJISTA 12X',
+                            'PARCELADO EMISSOR'
+                        ])) {
+                        continue;
+                    }
+
+                    $listCommissions[] = [
+                        'brand' => $brand,
+                        'product' => $product,
+                        'commission_percentage' => $value,
+                        'payment_plan' => 3,
+                    ];
+                }
+            }
         }
 
-        return [
-            'merchant_id' => $this->getMerchantId(),
-            'subseller_id' => $company->subseller_getnet_id,
-            'legal_document_number' => $company->company_document,
-            'bank_accounts' => [
-                'type_accounts' => 'unique',
-                'unique_account' => [
-                    'bank' => $company->bank,
-                    'agency' => $company->agency,
-                    'account' => $company->account,
-                    'account_type' => $company->present()->getAccountType(),
-                    'account_digit' => $company->account_digit == 'X' || $company->account_digit == 'x' ? 0 : $company->account_digit,
-                ],
-            ],
-            'legal_name' => FoxUtils::removeAccents(FoxUtils::removeSpecialChars($company->fantasy_name)),
-            'trade_name' => FoxUtils::removeAccents(FoxUtils::removeSpecialChars($company->fantasy_name)),
-            'block_payments' => 'N',
-            'block_transactions' => 'N',
-            'business_entity_type' => FoxUtils::onlyNumbers($company->business_entity_type),
-            'economic_activity_classification_code' => FoxUtils::onlyNumbers(
-                $company->economic_activity_classification_code
-            ),
-            'state_fiscal_document_number' => $company->state_fiscal_document_number,
-            'federal_registration_status' => 'active',
-            'email' => $company->support_email,
-            'business_address' => [
-                'street' => FoxUtils::removeAccents(FoxUtils::removeSpecialChars($company->street)),
-                'number' => $company->number ?? '',
-                'district' => FoxUtils::removeAccents(FoxUtils::removeSpecialChars($company->neighborhood)),
-                'city' => FoxUtils::removeAccents(FoxUtils::removeSpecialChars($company->city)),
-                'state' => $company->state,
-                'postal_code' => FoxUtils::onlyNumbers($company->zip_code),
-                'suite' => empty($company->complement) ? '' : FoxUtils::removeAccents(
-                    FoxUtils::removeSpecialChars($company->complement)
-                ),
-                'country' => $country
-            ],
-            'phone' => [
-                'area_code' => $telephone['dd'],
-                'phone_number' => $telephone['number']
-            ],
-
-        ];
+        return $listCommissions;
     }
-
 }
 
