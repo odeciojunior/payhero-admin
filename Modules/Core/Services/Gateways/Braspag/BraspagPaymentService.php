0758cffeb03ebb69d1d9c4528eaf0d168c3989ad
joao lucas <joaolucas@cloudfox.net>
2020-10-01 12:31:25 -0300

    implementação BraspagPaymentService gerando token de autenticação

---
+++ Modules/Core/Services/Gateways/Braspag/BraspagPaymentService.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Modules\Core\Services\Gateways\Braspag;
+
+use Exception;
+use GuzzleHttp\Client;
+use Modules\Core\Entities\Gateway;
+use Modules\Core\Services\FoxUtils;
+
+class BraspagPaymentService
+{
+    private Client $client;
+    private Gateway $gateway;
+    private string $urlApi;
+    private string $urlApiAuth;
+    private string $cloudFoxClientId;
+    private string $cloudFoxClientSecret;
+    private string $accessToken;
+
+    public function __construct()
+    {
+        if (FoxUtils::isProduction()) {
+            $this->gateway = Gateway::where("name", "braspag_production")->first();
+            $this->urlApi = 'https://api.cieloecommerce.cielo.com.br/';
+            $this->urlApiAuth = 'https://auth.braspag.com.br/';
+        } else {
+            $this->gateway = Gateway::where("name", "braspag_sandbox")->first();
+            $this->urlApi = 'https://apisandbox.cieloecommerce.cielo.com.br/';
+            $this->urlApiAuth = 'https://authsandbox.braspag.com.br/';
+        }
+
+        if (empty($this->gateway)) {
+            throw new Exception("Gateway braspag não encontrado no ambiente de ".env('APP_ENV'));
+        }
+
+        $configs = json_decode(FoxUtils::xorEncrypt($this->gateway->json_config, "decrypt"), true);
+
+        $this->cloudFoxClientId = $configs["public_token"];
+        $this->cloudFoxClientSecret = $configs["private_token"];
+
+        $this->client = new Client([]);
+
+        $this->authenticated();
+    }
+
+    private function authenticated()
+    {
+        $url = $this->urlApiAuth.'oauth2/token';
+        $options = [
+            'headers' => [
+                'Authorization' => 'Basic '.base64_encode($this->cloudFoxClientId.':'.$this->cloudFoxClientSecret),
+                'Content-Type' => 'application/x-www-form-urlencoded'
+            ],
+            'form_params' => ['grant_type' => 'client_credentials']
+        ];
+
+        $response = $this->client->post($url, $options);
+        $content = $response->getBody()->getContents();
+        $response = \GuzzleHttp\json_decode($content);
+
+        $this->accessToken = $response->access_token;
+    }
+
+}
