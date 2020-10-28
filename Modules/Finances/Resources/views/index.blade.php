b1686a424b186996d5b16ba2ab2d09f0687a0901
joao lucas <joaolucas@cloudfox.net>
2020-10-28 16:38:19 -0300

    fix cache update version

--- Modules/Finances/Resources/views/index.blade.php
+++ Modules/Finances/Resources/views/index.blade.php
@@ -443,7 +443,7 @@
     @push('scripts')
         <script src="{{ asset('modules/global/js-extra/moment.min.js') }}"></script>
         <script src='{{ asset('modules/global/js/daterangepicker.min.js') }}'></script>
-        <script src="{{ asset('modules/finances/js/index.js?v=1') }}"></script>
+        <script src="{{ asset('modules/finances/js/index.js?v=6') }}"></script>
     @endpush
 
 @endsection
