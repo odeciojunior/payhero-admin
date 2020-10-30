0db124f0b2a79b6245dfd15e7eacc4ff2935cbad
joao lucas <joaolucas@cloudfox.net>
2020-10-30 19:44:10 -0300

    fix message not invite

--- Modules/Invites/Resources/views/index.blade.php
+++ Modules/Invites/Resources/views/index.blade.php
@@ -7,15 +7,18 @@
     @endpush
     <div class="page">
         <div class="page-header container">
-            <button id="store-invite" title='Adicionar convite' type="button" class="btn btn-floating btn-danger" style="position: relative; float: right" {{--data-target='#modal' data-toggle='modal'--}}>
+            <button id="store-invite" title='Adicionar convite' type="button" class="btn btn-floating btn-danger"
+                    style="position: relative; float: right" {{--data-target='#modal' data-toggle='modal'--}}>
                 <i class="icon wb-plus" aria-hidden="true"></i></button>
             <h2 class="page-title">Convites</h2>
-            <p id='text-info' style="margin-top: 12px; display: none;">A cada convite aceito, você vai ganhar 1% de comissão das vendas efetuadas pelos novos usuários que você convidou durante 6 meses.</p>
+            <p id='text-info' style="margin-top: 12px; display: none;">A cada convite aceito, você vai ganhar 1% de
+                comissão das vendas efetuadas pelos novos usuários que você convidou durante 6 meses.</p>
             <div class="card shadow p-20" id='card-invitation-data' style='display:none;'>
                 <div class="row justify-content-center">
                     <div style="width: 20%">
                         <h6 class="text-center orange-gradient">
-                            <i class="material-icons align-middle mr-1 orange-gradient"> group_add </i> Convites enviados
+                            <i class="material-icons align-middle mr-1 orange-gradient"> group_add </i> Convites
+                            enviados
                         </h6>
                         <h4 id='invitations_sent' class="number text-center orange-gradient"></h4>
                     </div>
@@ -55,28 +58,31 @@
                 <div class="tab-pane active" id="tab_convites_enviados" role="tabpanel">
                     <table class="table table-striped unify">
                         <thead class="text-center">
-                            <th class="text-left">Convite</th>
-                            <th class="text-center">Email convidado</th>
-                            <th class="text-center">Empresa Recebedora</th>
-                            <th class="text-center">Status</th>
-                            <th class="text-center">Data cadastro</th>
-                            <th class="text-center">Data expiração</th>
+                        <th class="text-left">Convite</th>
+                        <th class="text-center">Email convidado</th>
+                        <th class="text-center">Empresa Recebedora</th>
+                        <th class="text-center">Status</th>
+                        <th class="text-center">Data cadastro</th>
+                        <th class="text-center">Data expiração</th>
                         </thead>
                         <tbody id='table-body-invites'>
-                            {{-- js invites carrega  --}}
+                        {{-- js invites carrega  --}}
                         </tbody>
                     </table>
                 </div>
             </div>
-            <ul id="pagination-invites" class="pagination-sm margin-chat-pagination" style="margin-top:10px;position:relative;float:right">
+            <ul id="pagination-invites" class="pagination-sm margin-chat-pagination"
+                style="margin-top:10px;position:relative;float:right">
                 {{-- js pagination carrega --}}
             </ul>
-            <div class="modal fade modal-3d-flip-vertical" id="modal-invite" aria-labelledby="exampleModalTitle" role="dialog" tabindex="-1">
+            <div class="modal fade modal-3d-flip-vertical" id="modal-invite" aria-labelledby="exampleModalTitle"
+                 role="dialog" tabindex="-1">
                 <div id='mainModalBody' class="modal-dialog modal-simple">
                     <!-- Tem company -->
                     <div id='modal-then-companies' class='modal-content' style='display:none;'>
                         <div class='modal-header'>
-                            <button type='button' id='btn-close-invite' class='close' data-dismiss='modal' aria-label='Close'>
+                            <button type='button' id='btn-close-invite' class='close' data-dismiss='modal'
+                                    aria-label='Close'>
                                 <span aria-hidden='true'>×</span>
                             </button>
                             <h4 id='modal-reverse-title' class='modal-title' style='width:100%; text-align:center'></h4>
@@ -86,7 +92,8 @@
                                 <div class='row'>
                                     <div class='form-group col-12'>
                                         <label for='email'>Email do convidado</label>
-                                        <input name='email_invited' type='text' class='form-control' id='email' placeholder='Email'>
+                                        <input name='email_invited' type='text' class='form-control' id='email'
+                                               placeholder='Email'>
                                     </div>
                                 </div>
                                 <div class='row'>
@@ -111,7 +118,10 @@
                                 </div>
                                 <div class='row' style='margin-top: 35px'>
                                     <div class='form-group col-12'>
-                                        <input id='btn-send-invite' type='button' class='form-control btn col-sm-12 col-m-3 col-lg-3' value='Enviar Convite' style='color:white; background-image: linear-gradient(to right, #e6774c, #f92278); position: relative; float: right;'>
+                                        <input id='btn-send-invite' type='button'
+                                               class='form-control btn col-sm-12 col-m-3 col-lg-3'
+                                               value='Enviar Convite'
+                                               style='color:white; background-image: linear-gradient(to right, #e6774c, #f92278); position: relative; float: right;'>
                                     </div>
                                 </div>
                             </div>
@@ -150,7 +160,10 @@
                                 <span class='swal2-x-mark-line-right'></span>
                             </span>
                             </div>
-                            <h3 align='center'>Para enviar convites, a transação e todos os documentos precisam estar aprovados!</h3>
+                            <h3 align='center'>
+                                Para enviar convites você precisa ter pelo menos uma empresa aprovada para transacionar
+                                e todos os documentos da empresa e do seu perfil precisam estar aprovados!
+                            </h3>
                         </div>
                         <div style='width:100%; text-align: center; padding-top: 3%;'>
                         <span class='btn btn-danger' data-dismiss='modal' style='font-size: 25px;'>
@@ -165,7 +178,8 @@
                         </div>
                         <div class='modal-body simple-border-bottom' style='padding-bottom: 1%; padding-top: 1%;'>
                             <div></div>
-                            <h3>O limite de convites para a versão beta foi atingindo, aguarde a versão oficial para poder enviar novos convites!</h3>
+                            <h3>O limite de convites para a versão beta foi atingindo, aguarde a versão oficial para
+                                poder enviar novos convites!</h3>
                             <h3></h3>
                         </div>
                         <div style='width: 100%; text-align: center; padding-top: 3%;'>
@@ -177,7 +191,8 @@
                 </div>
             </div>
             <!-- End Modal -->
-            <div id='modal-invite-not' class='modal fade show' aria-labelledby='modal-invite' role='dialog' tabindex='-1' style=' padding-right: 12px;'>
+            <div id='modal-invite-not' class='modal fade show' aria-labelledby='modal-invite' role='dialog'
+                 tabindex='-1' style=' padding-right: 12px;'>
                 <div class='modal-dialog modal-simple modal-center'>
                     <div class='modal-content text-center'>
                         <div class='modal-header text-center'>
@@ -187,10 +202,13 @@
                             <h4 class='modal-title'>Aviso</h4>
                         </div>
                         <div class='modal-body text-center'>
-                            <h3>O limite de convites para a versão beta foi atingido, aguarde a versão oficial para poder enviar novos convites!</h3>
+                            <h3>O limite de convites para a versão beta foi atingido, aguarde a versão oficial para
+                                poder enviar novos convites!</h3>
                         </div>
                         <div class='modal-footer' style='text-align: center; padding-top: 3%;'>
-                            <button class='btn btn-danger' type='button' data-dismiss='modal' style='font-size: 15px;'>Fechar</button>
+                            <button class='btn btn-danger' type='button' data-dismiss='modal' style='font-size: 15px;'>
+                                Fechar
+                            </button>
                         </div>
                     </div>
                 </div>
@@ -199,11 +217,13 @@
         </div>
     </div>
     <!-- Modal padrão para excluir -->
-    <div class="modal fade example-modal-lg modal-3d-flip-vertical" id="modal-delete-invitation" aria-hidden="true" aria-labelledby="exampleModalTitle" role="dialog" tabindex="-1">
+    <div class="modal fade example-modal-lg modal-3d-flip-vertical" id="modal-delete-invitation" aria-hidden="true"
+         aria-labelledby="exampleModalTitle" role="dialog" tabindex="-1">
         <div class="modal-dialog  modal-dialog-centered  modal-simple">
             <div class="modal-content">
                 <div class="modal-header text-center">
-                    <a class="close-card pointer close" role="button" data-dismiss="modal" aria-label="Close" id="fechar_modal_excluir">
+                    <a class="close-card pointer close" role="button" data-dismiss="modal" aria-label="Close"
+                       id="fechar_modal_excluir">
                         <i class="material-icons md-16">close</i>
                     </a>
                 </div>
@@ -215,18 +235,24 @@
                     <p class="gray"> Se você excluir esse registro, não será possível recuperá-lo! </p>
                 </div>
                 <div class="modal-footer d-flex align-items-center justify-content-center">
-                    <button id='btn-cancel-invitation' type="button" class="col-4 btn btn-gray" data-dismiss="modal" style="width: 20%;">Cancelar</button>
-                    <button id="btn-delete-invitation" type="button" class="col-4 btn btn-danger" style="width: 20%;" data-dismiss="modal">Excluir</button>
+                    <button id='btn-cancel-invitation' type="button" class="col-4 btn btn-gray" data-dismiss="modal"
+                            style="width: 20%;">Cancelar
+                    </button>
+                    <button id="btn-delete-invitation" type="button" class="col-4 btn btn-danger" style="width: 20%;"
+                            data-dismiss="modal">Excluir
+                    </button>
                 </div>
             </div>
         </div>
     </div>
     <!-- Modal para reenviar convite -->
-    <div class="modal fade example-modal-lg modal-3d-flip-vertical" id="modal-resend-invitation" aria-hidden="true" aria-labelledby="exampleModalTitle" role="dialog" tabindex="-1">
+    <div class="modal fade example-modal-lg modal-3d-flip-vertical" id="modal-resend-invitation" aria-hidden="true"
+         aria-labelledby="exampleModalTitle" role="dialog" tabindex="-1">
         <div class="modal-dialog  modal-dialog-centered  modal-simple">
             <div class="modal-content">
                 <div class="modal-header text-center">
-                    <a class="close-card pointer close" role="button" data-dismiss="modal" aria-label="Close" id="fechar_modal_excluir">
+                    <a class="close-card pointer close" role="button" data-dismiss="modal" aria-label="Close"
+                       id="fechar_modal_excluir">
                         <i class="material-icons md-16">close</i>
                     </a>
                 </div>
@@ -238,14 +264,18 @@
                     {{--                    <p class="gray"> Se você excluir esse registro, não será possível recuperá-lo! </p>--}}
                 </div>
                 <div class="modal-footer d-flex align-items-center justify-content-center">
-                    <button id='btn-cancel' type="button" class="col-4 btn btn-gray" data-dismiss="modal" style="width: 20%;">Cancelar</button>
-                    <button id="btn-resend-invitation" type="button" class="col-4 btn btn-success" style="width: 20%;" data-dismiss="modal">Enviar</button>
+                    <button id='btn-cancel' type="button" class="col-4 btn btn-gray" data-dismiss="modal"
+                            style="width: 20%;">Cancelar
+                    </button>
+                    <button id="btn-resend-invitation" type="button" class="col-4 btn btn-success" style="width: 20%;"
+                            data-dismiss="modal">Enviar
+                    </button>
                 </div>
             </div>
         </div>
     </div>
     @push('scripts')
-        <script src="{{asset('modules/invites/js/invites.js?v=3') }}"></script>
+        <script src="{{asset('modules/invites/js/invites.js?v=14') }}"></script>
     @endpush
 
 @endsection
