be4037ee827e4819d9dbcb7ba6d7daad5ea08f4b
Eliseu <eliseu.ferreiradesouza@gmail.com>
2020-11-05 20:07:24 -0300

    integration smart funnel

---
+++ Modules/Core/Entities/SmartfunnelSent.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Modules\Core\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Spatie\Activitylog\Models\Activity;
+use App\Traits\LogsActivity;
+use Laracasts\Presenter\PresentableTrait;
+use Modules\Core\Presenters\SmartfunnelSentPresenter;
+
+/**
+ * @property integer $id
+ * @property integer $sale_id
+ * @property integer $smartfunnel_integration_id
+ * @property string $data
+ * @property string $response
+ * @property int $sent_status
+ * @property string $created_at
+ * @property string $updated_at
+ * @property SmartfunnelIntegration $smartfunnelIntegration
+ * @property Sale $sale
+ */
+class SmartfunnelSent extends Model
+{
+    use LogsActivity, PresentableTrait;
+    /**
+     * @var string
+     */
+    protected $presenter = SmartfunnelSentPresenter::class;
+    /**
+     * The table associated with the model.
+     * @var string
+     */
+    protected $table = 'smartfunnel_sent';
+    /**
+     * The "type" of the auto-incrementing ID.
+     * @var string
+     */
+    protected $keyType = 'integer';
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'sale_id',
+        'smartfunnel_integration_id',
+        'data',
+        'response',
+        'sent_status',
+        'event_sale',
+        'created_at',
+        'updated_at',
+    ];
+    /**
+     * @var bool
+     */
+    protected static $logFillable = true;
+    /**
+     * @var bool
+     */
+    protected static $logUnguarded = true;
+    /**
+     * Registra apenas os atributos alterados
+     * @var bool
+     */
+    protected static $logOnlyDirty = true;
+    /**
+     * Impede que o pacote armazene logs vazios
+     * @var bool
+     */
+    protected static $submitEmptyLogs = false;
+
+    /**
+     * @param Activity $activity
+     * @param string $eventName
+     */
+    public function tapActivity(Activity $activity, string $eventName)
+    {
+        if ($eventName == 'deleted') {
+            $activity->description = 'SmartfunnelSent foi deletedo.';
+        } else if ($eventName == 'updated') {
+            $activity->description = 'SmartfunnelSent foi atualizado.';
+        } else if ($eventName == 'created') {
+            $activity->description = 'SmartfunnelSent foi criado.';
+        } else {
+            $activity->description = $eventName;
+        }
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function smartfunnelIntegration()
+    {
+        return $this->belongsTo(SmartfunnelIntegration::class);
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function sale()
+    {
+        return $this->belongsTo(Sale::class);
+    }
+}
---
+++ Modules/Smartfunnel/Http/Controllers/SmartfunnelController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Modules\Smartfunnel\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Routing\Controller;
+
+class SmartfunnelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     * @return Response
+     */
+    public function index()
+    {
+        return view('smartfunnel::index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     * @return Response
+     */
+    public function create()
+    {
+        return view('smartfunnel::create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     * @param Request $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Show the specified resource.
+     * @param int $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        return view('smartfunnel::show');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     * @param int $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        return view('smartfunnel::edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     * @param Request $request
+     * @param int $id
+     * @return Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     * @param int $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
