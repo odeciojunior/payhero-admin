a340ab8e50269b09aabb8cd3ab396f34045f5b4b
Pedro Henrique Rodrigues <pedroh.rodriguees@gmail.com>
2020-11-19 15:52:18 -0300

    [fix] local migration

--- database/migrations/2020_11_16_152355_ prepare_drop_user_information.php
+++ database/migrations/2020_11_16_152355_ prepare_drop_user_information.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
-use Modules\Core\Entities\UserInformation;
+use Modules\Core\Entities\User;
 
 class PrepareDropUserInformation extends Migration
 {
@@ -15,41 +15,45 @@ class PrepareDropUserInformation extends Migration
      */
     public function up()
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->string('sex', '50')->nullable();
-            $table->string('mother_name', '255')->nullable();
+        try {
+            Schema::table('users', function (Blueprint $table) {
+                $table->string('sex', '50')->nullable();
+                $table->string('mother_name', '255')->nullable();
 
-            $table->dropColumn('get_net_status');
-            $table->dropColumn('abroad_transfer_tax');
-            $table->dropColumn('debit_card_tax');
-            $table->dropColumn('debit_card_antecipation_money_days');
-            $table->dropColumn('credit_card_antecipation_money_days');
-            $table->dropColumn('boleto_antecipation_money_days');
-            $table->dropColumn('percentage_antecipable');
-            $table->dropColumn('antecipation_tax');
-            $table->dropColumn('debit_card_release_money_days');
-            $table->dropColumn('percentage_rate');
-            $table->dropColumn('antecipation_enabled_flag');
-        });
+                $table->dropColumn('get_net_status');
+                $table->dropColumn('abroad_transfer_tax');
+                $table->dropColumn('debit_card_tax');
+                $table->dropColumn('debit_card_antecipation_money_days');
+                $table->dropColumn('credit_card_antecipation_money_days');
+                $table->dropColumn('boleto_antecipation_money_days');
+                $table->dropColumn('percentage_antecipable');
+                $table->dropColumn('antecipation_tax');
+                $table->dropColumn('debit_card_release_money_days');
+                $table->dropColumn('percentage_rate');
+                $table->dropColumn('antecipation_enabled_flag');
+            });
 
-        DB::statement("ALTER TABLE users MODIFY COLUMN created_at timestamp AFTER mother_name");
-        DB::statement("ALTER TABLE users MODIFY COLUMN updated_at timestamp AFTER created_at");
-        DB::statement("ALTER TABLE users MODIFY COLUMN deleted_at timestamp AFTER updated_at");
+            DB::statement("ALTER TABLE users MODIFY COLUMN created_at timestamp AFTER mother_name");
+            DB::statement("ALTER TABLE users MODIFY COLUMN updated_at timestamp AFTER created_at");
+            DB::statement("ALTER TABLE users MODIFY COLUMN deleted_at timestamp AFTER updated_at");
 
-        foreach (UserInformation::whereRaw('mother_name is not null or sex is not null')->get() as $userInformation) {
-            $user = $userInformation->user;
+            $usersInformation = DB::select('select * from user_informations where mother_name is not null or sex is not null');
 
-            if (!empty($userInformation->sex)) {
-                $user->update(['sex' => $userInformation->sex]);
+            foreach ($usersInformation as $userInformation) {
+                $user = User::find($userInformation->user_id);
+
+                if (!empty($userInformation->sex)) {
+                    $user->update(['sex' => $userInformation->sex]);
+                }
+                if (!empty($userInformation->mother_name)) {
+                    $user->update(['mother_name' => $userInformation->mother_name]);
+                }
             }
 
-            if (!empty($userInformation->mother_name)) {
-                $user->update(['mother_name' => $userInformation->mother_name]);
-            }
+            Schema::dropIfExists('user_informations');
+        } catch (Exception $e) {
+
         }
-
-        Schema::dropIfExists('user_informations');
-
     }
 
     /**
