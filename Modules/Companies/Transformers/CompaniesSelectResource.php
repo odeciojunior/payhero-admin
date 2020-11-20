35ce179217dddf5f60cb1146002698ab37dee9ec
joao lucas <joaolucas@cloudfox.net>
2020-11-20 11:01:46 -0300

    fix  sales query where Not in on getnet

--- Modules/Companies/Transformers/CompaniesSelectResource.php
+++ Modules/Companies/Transformers/CompaniesSelectResource.php
@@ -14,9 +14,12 @@ class CompaniesSelectResource extends JsonResource
     {
         $companyService = new CompanyService();
         $companyDocumentValidated = $companyService->isDocumentValidated($this->id);
-        $hasSaleCielo = Sale::where('owner_id', auth()->user()->account_owner_id)
-            ->orWhere('affiliate_id', auth()->user()->account_owner)
-            ->whereNotIn('gateway_id',  [14, 15])->exists();
+        $hasSaleBeforeGetnet = Sale::where(
+            function ($q) {
+                $q->where('owner_id', auth()->user()->account_owner_id)
+                    ->orWhere('affiliate_id', auth()->user()->account_owner_id);
+            }
+        )->whereNotIn('gateway_id', [14, 15])->exists();
 
         return [
             'id' => Hashids::encode($this->id),
@@ -24,11 +27,15 @@ class CompaniesSelectResource extends JsonResource
             'name' => $this->company_type == 1 ? 'Pessoa física' : $this->fantasy_name,
             'company_document_status' => ($companyDocumentValidated) ? 'approved' : 'pending',
             'capture_transaction_enabled' => $this->capture_transaction_enabled,
-            'company_has_sales_in_cielo' => $hasSaleCielo,
+            'company_has_sale_before_getnet' => $hasSaleBeforeGetnet,
             'active_flag' => $this->active_flag,
             'company_type' => $this->present()->getCompanyType($this->company_type),
-            'user_address_document_status' => (new User())->present()->getAddressDocumentStatus($this->user->address_document_status),
-            'user_personal_document_status' => (new User())->present()->getAddressDocumentStatus($this->user->personal_document_status),
+            'user_address_document_status' => (new User())->present()->getAddressDocumentStatus(
+                $this->user->address_document_status
+            ),
+            'user_personal_document_status' => (new User())->present()->getAddressDocumentStatus(
+                $this->user->personal_document_status
+            ),
         ];
     }
 }
