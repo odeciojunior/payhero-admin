07d064559860b434a3231cd442c674d1d983af04
Julio Leichtweis <julioleichtweis@gmail.com>
2020-12-07 16:20:24 -0300

    script to update redis-statement database

--- app/Console/Commands/GenericCommand.php
+++ app/Console/Commands/GenericCommand.php
@@ -4,58 +4,29 @@ namespace App\Console\Commands;
 
 use Exception;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Redis;
+use Modules\Core\Entities\Gateway;
 use Modules\Core\Entities\Sale;
 use Modules\Core\Services\ShopifyService;
 
 class GenericCommand extends Command
 {
-    protected $signature = 'generic {user?}';
+    protected $signature = 'generic';
 
     protected $description = 'Command description';
 
     public function handle()
     {
-        $sales = Sale::with('project.shopifyIntegrations')
-            ->where('status', 3)
-            ->where('payment_method', 1)
-            ->whereDate('start_date', '>=', now()->subDays(30)->startOfDay())
-            ->whereNotNull('shopify_order')
-            ->get();
+        $sales = Sale::where('gateway_id', 15)->orderBy('id', 'desc')->get();
 
-        $total = $sales->count();
-        $count = 0;
-
-        $integrations = [];
-        foreach ($sales as $sale) {
-            try {
-                $count++;
-                $this->line("Venda {$count} de {$total}: {$sale->id}");
-
-                if (empty($integrations[$sale->project_id])) {
-                    $integration = $sale->project->shopifyIntegrations->first();
-                    $integrations[$sale->project_id] = new ShopifyService($integration->url_store,
-                        $integration->token,
-                        false);
-                }
-                $shopifyService = $integrations[$sale->project_id];
-
-                $order = $shopifyService->getClient()->getOrderManager()->find($sale->shopify_order);
-
-                $fulfillments = $order->getFulfillments();
-                foreach ($fulfillments as $fulfillment) {
-                    $shopifyService->getClient()->getFulfillmentManager()->cancel($order->getId(),
-                        $fulfillment->getId());
-                }
-                $shopifyService->getClient()->getOrderManager()->cancel($order->getId());
-
-                $sale->shopify_order = null;
-                $sale->save();
-
-                $this->line('Foi!');
-            } catch (Exception $e) {
-                $this->error('ERR: ' . $e->getMessage());
-            }
+        foreach($sales as $sale) {
+            Redis::connection('redis-statement')->set("sale:has:tracking:{$sale->id}", $sale->has_valid_tracking);
         }
+
+        // foreach($sales as $sale) {
+        //     $this->line("Venda {$sale->id} -> " . Redis::connection('redis-statement')->get("sale:has:tracking:{$sale->id}"));
+        // }
+
     }
 }
 
