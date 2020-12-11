8e49f101507f4ce92ba44f971325480c36cdb26d
marcosmarion <marcosmarioncloudfox@cloudfox.net>
2020-12-11 17:28:20 -0300

    Alterando versao

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -129,7 +129,7 @@
 <script src="{{ asset('modules/global/adminremark/assets/examples/js/dashboard/v1.js') }}"></script>
 <script src="{{ asset('modules/global/adminremark/global/vendor/sortable/Sortable.js') }}"></script>
 <script src="{{ asset('modules/global/jquery-imgareaselect/scripts/jquery.imgareaselect.pack.js') }}"></script>
-<script src="{{ asset('modules/global/js/global.js?v=16') }}"></script>
+<script src="{{ asset('modules/global/js/global.js?v=17') }}"></script>
 <script>
     verifyDocumentPending();
 </script>
