abd21dd379a56f4a8cd3f08b928ceac2be610527
marcosmarion <marcosmarioncloudfox@cloudfox.net>
2020-12-11 16:28:37 -0300

    Removendo rotas dos colaboradores

--- Modules/Collaborators/Routes/web.php
+++ Modules/Collaborators/Routes/web.php
@@ -10,10 +10,10 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::group(['middleware' => ['web', 'auth', 'setUserAsLogged']], function() {
-    Route::Resource('collaborators', 'CollaboratorsController')
-         ->only('index')->middleware('role:account_owner|admin');
-});
+//Route::group(['middleware' => ['web', 'auth', 'setUserAsLogged']], function() {
+//    Route::Resource('collaborators', 'CollaboratorsController')
+//         ->only('index')->middleware('role:account_owner|admin');
+//});
 //Route::prefix('collaborators')->group(function() {
 //    Route::get('/', 'CollaboratorsController@index');
 //});
