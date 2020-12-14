f02597731b2866ca2a5a49e61c8df52b6d72a1f9
joao <joaolucas@cloudfox.net>
2020-12-14 15:39:23 -0300

    fix try catch

--- Modules/Core/Observers/SaleObserver.php
+++ Modules/Core/Observers/SaleObserver.php
@@ -2,24 +2,20 @@
 
 namespace Modules\Core\Observers;
 
+use Exception;
 use Modules\Core\Entities\Sale;
 use Redis;
 
 class SaleObserver
 {
-
-    /**
-     * Handle the faq "updating" event.
-     *
-     * @param Sale $sale
-     * @return void
-     */
     public function updating(Sale $sale)
     {
-
-        if ($sale->getOriginal('has_valid_tracking') != $sale->has_valid_tracking) {
-
-            Redis::connection('redis-statement')->set("sale:has:tracking:{$sale->id}", $sale->has_valid_tracking);
+        try {
+            if ($sale->getOriginal('has_valid_tracking') != $sale->has_valid_tracking) {
+                Redis::connection('redis-statement')->set("sale:has:tracking:{$sale->id}", $sale->has_valid_tracking);
+            }
+        } catch (Exception $e) {
+            report($e);
         }
     }
 }
