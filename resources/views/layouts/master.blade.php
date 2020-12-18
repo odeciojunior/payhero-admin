676f564a85a88f659a0d4596dd68c270f94e04ee
Pedro Henrique Rodrigues <pedroh.rodriguees@gmail.com>
2020-12-18 10:17:59 -0300

    [fix] css and js version

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -38,7 +38,7 @@
     <link rel="stylesheet" href="{{ asset('modules/global/adminremark/global/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('modules/global/adminremark/global/css/bootstrap-extend.min.css') }}">
     <link rel="stylesheet" href="{{ asset('modules/global/adminremark/assets/css/site.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('modules/global/css/loading.css?v=2') }}">
+    <link rel="stylesheet" href="{{ asset('modules/global/css/loading.css?v=3') }}">
     <link rel="stylesheet" href="{{ asset('modules/global/css/checkAnimation.css') }}">
     <link rel="stylesheet" href="{{ asset('modules/global/css/ribbon.css') }}">
     <!-- Plugins -->
@@ -59,7 +59,7 @@
     <!-- New CSS -->
     <link rel="stylesheet" href="{{ asset('modules/global/css/new-site.css') }}">
     <link rel="stylesheet" href="{{ asset('modules/global/css/finances.css') }}">
-    <link rel="stylesheet" href="{{ asset('modules/global/css/global.css?v=12') }}">
+    <link rel="stylesheet" href="{{ asset('modules/global/css/global.css?v=13') }}">
     @stack('css')
 
     @if(env('APP_ENV', 'production') == 'production' && getenv('APP_DEBUG') === 'false')
