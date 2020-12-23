97bbf0f1e645c15a436e2e4da5dc9da2c2609867
Pedro Henrique Rodrigues <pedroh.rodriguees@gmail.com>
2020-12-23 17:02:04 -0300

    new whatsapp.svg

--- resources/assets/modules/salesrecovery/js/salesrecovery.js
+++ resources/assets/modules/salesrecovery/js/salesrecovery.js
@@ -48,7 +48,7 @@ $(document).ready(function () {
                         dados += "<td><span class='badge badge-primary'>" + value.recovery_status + "</span></td>";
                     }
                     dados += "<td>" + value.value + "</td>";
-                    dados += "<td><a href='" + value.whatsapp_link + "', '', $client['telephone']); !!}' target='_blank'><img style='height:24px' src='https://logodownload.org/wp-content/uploads/2015/04/whatsapp-logo-4-1.png'></a></td>";
+                    dados += "<td><a href='" + value.whatsapp_link + "', '', $client['telephone']); !!}' target='_blank'><img src='/modules/global/img/svg/whatsapp.svg' style='width: 24px'></a></td>";
                     dados += "<td> <a role='button' class='copy_link' style='cursor:pointer;' link='" + value.link + "'><i class='material-icons gradient'>file_copy</i></a></td>";
                     dados += "<td><a  role='button' class='details-cart-recovery' style='cursor:pointer;' venda='" + value.id + "' data-target='#modal_detalhes' data-toggle='modal'><img src='/modules/global/img/svg/eye.svg' style='width: 24px'></button></td>";
                     dados += '</tr>';
