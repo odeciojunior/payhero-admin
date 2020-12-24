197e26a1cbcff7479e7a1425a0604afec082b7cd
Rafael Rocha <rafaelcosta@cloudfox.net>
2020-12-24 10:55:28 -0300

    remove gradient from btn-primary

--- Modules/Companies/Resources/views/empty.blade.php
+++ Modules/Companies/Resources/views/empty.blade.php
@@ -5,5 +5,5 @@
     <img src="{!! asset('modules/global/img/emptyempresas.svg') !!}" width="250px">
     <h1 class="big gray">Você ainda não tem nenhuma empresa!</h1>
     <p class="desc gray">Vamos cadastrar a primeira empresa? </p>
-    <a href="/companies/create" class="btn btn-primary gradient">Cadastrar empresa</a>
+    <a href="/companies/create" class="btn btn-primary">Cadastrar empresa</a>
 </div>
