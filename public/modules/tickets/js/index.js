558bc122c5abdb3776fd2c9a8f1342bb3e04ecaf
Pedro Henrique Rodrigues <pedroh.rodriguees@gmail.com>
2020-12-28 15:01:42 -0300

    [fix] date lifelong

--- public/modules/tickets/js/index.js
+++ public/modules/tickets/js/index.js
@@ -422,7 +422,7 @@ $(document).ready(function () {
                 'Últimos 30 dias': [moment().subtract(29, 'days'), moment()],
                 'Este mês': [moment().startOf('month'), moment().endOf('month')],
                 'Mês passado': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')],
-                'Vitalício': [moment('2018-01-21 00:00:00'), moment()]
+                'Vitalício': [moment('2018-01-01 00:00:00'), moment()]
             }
         }, function (start, end) {
             startDate = start.format('YYYY-MM-DD');
