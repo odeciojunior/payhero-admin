461a8d0fde924e7a77c6e984a6467b8acf590fbc
joao <joaolucas@cloudfox.net>
2021-01-11 10:13:44 -0300

    fix position pagination view products

--- Modules/Products/Resources/views/index.blade.php
+++ Modules/Products/Resources/views/index.blade.php
@@ -60,7 +60,7 @@
             <div class="page-content container">
                 <div id='data-table-products' class='row'>
                 </div>
-                <div id='pagination-products' class='float-right margin-chat-pagination' style="margin-bottom: 20px"></div>
+                <div id='pagination-products' class='float-right margin-chat-pagination' style="margin-bottom: 20px; margin-right: 100px;"></div>
                 <div class='products-is-empty' style='display:none;'>
                     @push('css')
                         <link rel="stylesheet" href="{!! asset('modules/global/css/empty.css?v=02') !!}">
