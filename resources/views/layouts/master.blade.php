95f0b70baa473963e72d3aaa41c83212d7842660
Pedro Henrique Rodrigues <pedroh.rodriguees@gmail.com>
2021-01-29 09:35:59 -0300

    [fix] master.blade

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -59,7 +59,7 @@
     <link rel="stylesheet" href="{{ asset('modules/global/adminremark/global/fonts/orion-icons/iconfont.css?v=06') }}">
     <!-- New CSS -->
     <link rel="stylesheet" href="{{ asset('modules/global/css/new-site.css?v=64') }}">
-    <link rel="stylesheet" href="{{ asset('modules/global/css/global.css?v=51' }}">
+    <link rel="stylesheet" href="{{ asset('modules/global/css/global.css?v=51') }}">
     <link rel="stylesheet" href="{{ asset('modules/global/css/finances.css?v=31') }}">
     @stack('css')
 
