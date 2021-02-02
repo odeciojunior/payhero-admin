fbd1d58aaf23cb7974d4fa6a2d4da360905af6ba
Jean Castro <jeanvcastro1@gmail.com>
2021-02-02 10:22:39 -0300

    database settings

---
+++ Modules/Core/Entities/OrderBumpRule.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Modules\Core\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property integer $id
+ * @property int $project_id
+ * @property string $description
+ * @property int $discount
+ * @property string $apply_on_plans
+ * @property string $offer_plans
+ * @property boolean $active_flag
+ * @property string $created_at
+ * @property string $updated_at
+ * @property Project $project
+ */
+class OrderBumpRule extends Model
+{
+    protected $fillable = [
+        'project_id',
+        'description',
+        'discount',
+        'apply_on_plans',
+        'offer_plans',
+        'active_flag',
+        'created_at',
+        'updated_at'
+    ];
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
+}
