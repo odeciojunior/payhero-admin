a110e5e65e286859f79fc6578fd644b460005ebf
Pedro Henrique Rodrigues <pedroh.rodriguees@gmail.com>
2021-02-03 10:26:16 -0300

    [fix] css

--- Modules/Finances/Resources/views/index.blade.php
+++ Modules/Finances/Resources/views/index.blade.php
@@ -2,7 +2,7 @@
 
 @push('css')
     <link rel="stylesheet" href="{{ asset('modules/global/css/switch.css') }}">
-    <link rel="stylesheet" href="{{ asset('modules/finances/css/new-finances.css?v=01'.uniqid()) }}">
+    <link rel="stylesheet" href="{{ asset('modules/finances/css/new-finances.css?v=02'.uniqid()) }}">
 @endpush
 
 @section('content')
