e4843827c79c791dfced6304136850f7643d2102
Rupert Lustosa <rupertlustosa@gmail.com>
2021-02-15 15:24:01 -0300

    Fix errors

--- Modules/Transfers/Services/GetNetStatementService.php
+++ Modules/Transfers/Services/GetNetStatementService.php
@@ -128,10 +128,10 @@ class GetNetStatementService
     {
 
         $this->filters = $filters;
-
-        $transactions = array_reverse($data->list_transactions) ?? [];
-        $adjustments = array_reverse($data->adjustments) ?? [];
-        $chargeback = array_reverse($data->chargeback) ?? [];
+    
+        $transactions = array_reverse($data->list_transactions ?? []);
+        $adjustments = array_reverse($data->adjustments ?? []);
+        $chargeback = array_reverse($data->chargeback ?? []);
 
         if (request('debug')) {
 
