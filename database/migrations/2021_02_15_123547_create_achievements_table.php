7c4b238b3039d6ebb83ced490439bc00601c883d
Rafael Rocha <rafaelcosta@cloudfox.net>
2021-03-11 11:31:39 -0300

    achievements migration

--- database/migrations/2021_02_15_123547_create_achievements_table.php
+++ database/migrations/2021_02_15_123547_create_achievements_table.php
@@ -24,18 +24,18 @@ class CreateAchievementsTable extends Migration
         });
 
         DB::statement("INSERT INTO achievements (id, name, description, icon, created_at)
-                             VALUES (1, 'Velocidade da Luz',       'Faça entregas rápidas',                  'https://cloudfox-documents.s3.amazonaws.com/cloudfox/defaults/VelocidadeLuz.png',      now()),
-                                    (2, 'Suporte Meteórico',       'Tenha um atendimento rápido',            'https://cloudfox-documents.s3.amazonaws.com/cloudfox/defaults/SuporteMeteorico.png',   now()),
-                                    (3, 'Colonizador',             'Tenha +10 convites aprovados',           'https://cloudfox-documents.s3.amazonaws.com/cloudfox/defaults/Colonizador.png',        now()),
-                                    (4, 'Comerciante Celeste',     '+1000 vendas aprovadas no cartão',       'https://cloudfox-documents.s3.amazonaws.com/cloudfox/defaults/ComercianteCeleste.png', now()),
-                                    (5, 'Poeira Estelar',          '100 vendas digitais aprovadas',          'https://cloudfox-documents.s3.amazonaws.com/cloudfox/defaults/PoeiraEstelar.png',      now()),
-                                    (6, 'Estrela Cadente',         '10 afiliados ativos',                    'https://cloudfox-documents.s3.amazonaws.com/cloudfox/defaults/estrelaCadente.png',     now()),
-                                    (7, 'Guerra nas Estrelas',     '50% de boletos aprovados',               'https://cloudfox-documents.s3.amazonaws.com/cloudfox/defaults/GuerraNasEstrelas.png',  now()),
-                                    (8, 'Extraterrestre',          'Recupere 6% dos carrinhos abandonados',  'https://cloudfox-documents.s3.amazonaws.com/cloudfox/defaults/Extraterrestre.png',     now()),
-                                    (9, 'Mochileiro das Galáxias', 'Faça vendas em 5 projetos diferentes',   'https://cloudfox-documents.s3.amazonaws.com/cloudfox/defaults/MochileiroGalaxias.png', now()),
-                                    (10, 'Órbita Capitalista',     'Realize 50 saques',                      'https://cloudfox-documents.s3.amazonaws.com/cloudfox/defaults/OrbitaCapitalista.png',  now()),
-                                    (11, 'Lunático',               'Faça login por 21 dias',                 'https://cloudfox-documents.s3.amazonaws.com/cloudfox/defaults/Lunatico.png',           now()),
-                                    (12, 'Ao Infinito e Além',     'Faça 50 vendas com Orderbump ou Upsell', 'https://cloudfox-documents.s3.amazonaws.com/cloudfox/defaults/AoInfinioAlem.png',      now());");
+                             VALUES ( 1, 'Velocidade da Luz',       'Faça entregas rápidas',                  'https://cloudfox-documents.s3.amazonaws.com/cloudfox/defaults/VelocidadeLuz.png',      now()),
+                                    ( 2, 'Suporte Meteórico',       'Tenha um atendimento rápido',            'https://cloudfox-documents.s3.amazonaws.com/cloudfox/defaults/SuporteMeteorico.png',   now()),
+                                    ( 3, 'Colonizador',             'Tenha +10 convites aprovados',           'https://cloudfox-documents.s3.amazonaws.com/cloudfox/defaults/Colonizador.png',        now()),
+                                    ( 4, 'Comerciante Celeste',     '+1000 vendas aprovadas no cartão',       'https://cloudfox-documents.s3.amazonaws.com/cloudfox/defaults/ComercianteCeleste.png', now()),
+                                    ( 5, 'Poeira Estelar',          '100 vendas digitais aprovadas',          'https://cloudfox-documents.s3.amazonaws.com/cloudfox/defaults/PoeiraEstelar.png',      now()),
+                                    ( 6, 'Estrela Cadente',         '10 afiliados ativos',                    'https://cloudfox-documents.s3.amazonaws.com/cloudfox/defaults/estrelaCadente.png',     now()),
+                                    ( 7, 'Guerra nas Estrelas',     '50% de boletos aprovados',               'https://cloudfox-documents.s3.amazonaws.com/cloudfox/defaults/GuerraNasEstrelas.png',  now()),
+                                    ( 8, 'Extraterrestre',          'Recupere 6% dos carrinhos abandonados',  'https://cloudfox-documents.s3.amazonaws.com/cloudfox/defaults/Extraterrestre.png',     now()),
+                                    ( 9, 'Mochileiro das Galáxias', 'Faça vendas em 5 projetos diferentes',   'https://cloudfox-documents.s3.amazonaws.com/cloudfox/defaults/MochileiroGalaxias.png', now()),
+                                    (10, 'Órbita Capitalista',      'Realize 50 saques',                      'https://cloudfox-documents.s3.amazonaws.com/cloudfox/defaults/OrbitaCapitalista.png',  now()),
+                                    (11, 'Lunático',                'Faça login por 21 dias',                 'https://cloudfox-documents.s3.amazonaws.com/cloudfox/defaults/Lunatico.png',           now()),
+                                    (12, 'Ao Infinito e Além',      'Faça 50 vendas com Orderbump ou Upsell', 'https://cloudfox-documents.s3.amazonaws.com/cloudfox/defaults/AoInfinioAlem.png',      now());");
     }
 
     /**
