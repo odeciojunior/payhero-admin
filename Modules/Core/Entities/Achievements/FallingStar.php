c62c79aec5cc8df84e4d45e9a641ddcacbb64ef2
Rafael Rocha <rafaelcosta@cloudfox.net>
2021-03-12 10:08:20 -0300

    FallingStar Achievement

--- Modules/Core/Entities/Achievements/FallingStar.php
+++ Modules/Core/Entities/Achievements/FallingStar.php
@@ -2,7 +2,9 @@
 
 namespace Modules\Core\Entities\Achievements;
 
+use Illuminate\Support\Facades\DB;
 use Modules\Core\Entities\Achievement;
+use Modules\Core\Entities\Sale;
 use Modules\Core\Entities\User;
 use Modules\Core\Interfaces\AchievementCheck;
 
@@ -12,6 +14,26 @@ class FallingStar extends Achievement implements AchievementCheck
 
     public function userAchieved(User $user): bool
     {
-        return false;
+        $sql = 'SELECT u.id, count(*) AS total_affiliates
+                FROM users u
+                         INNER JOIN users_projects up ON u.id = up.user_id
+                         INNER JOIN projects p ON p.id = up.project_id AND p.status_url_affiliates = TRUE
+                         INNER JOIN affiliates a ON a.project_id = p.id AND a.id IN (
+                    SELECT affiliate_id
+                    FROM sales s
+                    WHERE a.id = s.affiliate_id
+                      AND owner_id = u.id
+                      AND s.status IN (' . Sale::STATUS_APPROVED . ',
+                                       ' . Sale::STATUS_CHARGEBACK . ',
+                                       ' . Sale::STATUS_REFUNDED . ',
+                                       ' . Sale::STATUS_IN_DISPUTE . '
+                                       )
+                )
+                WHERE u.id = ?
+                GROUP BY u.id;';
+
+        $queryReturn = DB::select($sql, [$user->id]);
+
+        return count($queryReturn) && $queryReturn[0]['total_affiliates'] >= 10;
     }
 }
