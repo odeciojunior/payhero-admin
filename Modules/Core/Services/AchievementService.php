2254f6c8e6ace8b5516b677556930c655b11cd70
Rafael Rocha <rafaelcosta@cloudfox.net>
2021-03-16 10:59:37 -0300

    returning achievements on dashboard api

--- Modules/Core/Services/AchievementService.php
+++ Modules/Core/Services/AchievementService.php
@@ -3,8 +3,8 @@
 namespace Modules\Core\Services;
 
 use Modules\Core\Entities\Achievement;
-use Modules\Core\Entities\User;
 use Modules\Core\Entities\Achievements;
+use Modules\Core\Entities\User;
 
 class AchievementService
 {
@@ -45,7 +45,7 @@ class AchievementService
             return true;
         }
 
-        if ($achievement-> userAchieved($user)) {
+        if ($achievement->userAchieved($user)) {
             return $this->setUserAchievement($user, $achievement);
         }
 
@@ -63,5 +63,14 @@ class AchievementService
             report($e);
             return false;
         }
+    }
+
+    public function getCurrentUserAchievements(User $user): array
+    {
+        $achievements = (new Achievement())->select('id', 'name', 'description', 'icon', 'storytelling')
+            ->selectRaw('EXISTS(SELECT user_id FROM achievement_user WHERE user_id = ' . $user->id . ' AND achievement_id = achievements.id) AS active')
+            ->get([$user->id])->toArray();
+
+        return $achievements;
     }
 }
