007664a187d2c5adc7ab8336985ee2314089b1ac
Jean Castro <jeanvcastro1@gmail.com>
2021-03-18 11:38:32 -0300

    fix benefits migrations

--- database/migrations/2021_02_12_095528_create_user_benefits_table.php
+++ database/migrations/2021_02_12_095528_create_user_benefits_table.php
@@ -13,14 +13,13 @@ class CreateUserBenefitsTable extends Migration
      */
     public function up()
     {
-        Schema::dropIfExists('benefit_user');
-
         Schema::create('user_benefits', function (Blueprint $table) {
             $table->id();
             $table->unsignedInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
             $table->unsignedBigInteger('benefit_id');
             $table->foreign('benefit_id')->references('id')->on('benefits');
+            $table->boolean('enabled')->default(false);
             $table->timestamps();
         });
     }
