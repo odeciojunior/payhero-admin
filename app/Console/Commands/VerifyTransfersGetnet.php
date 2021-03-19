e48a637208aaa154c4b8f9ab8faf87550741ddc7
joao <joaolucas@cloudfox.net>
2021-03-19 15:06:55 -0300

    fix

--- app/Console/Commands/VerifyTransfersGetnet.php
+++ app/Console/Commands/VerifyTransfersGetnet.php
@@ -41,6 +41,7 @@ class VerifyTransfersGetnet extends Command
 
     public function handle()
     {
+        $start = now();
 
         $companyModel = new Company();
         $transactionModel = new Transaction();
@@ -119,7 +120,6 @@ class VerifyTransfersGetnet extends Command
                 report($e);
             }
         }
-
         $end = now();
 
         report(new CommandMonitorTimeException("command {$this->signature} começou as {$start} e terminou as {$end}"));
