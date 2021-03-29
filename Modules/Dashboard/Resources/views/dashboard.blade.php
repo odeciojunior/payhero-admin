7284ffb4f9b24ab7800a3f0cfa4e5aed931e2418
Pedro Henrique Rodrigues <pedroh.rodriguees@gmail.com>
2021-03-29 15:45:29 -0300

    update: modal onboarding layout

--- Modules/Dashboard/Resources/views/dashboard.blade.php
+++ Modules/Dashboard/Resources/views/dashboard.blade.php
@@ -15,8 +15,8 @@
     <div class="page dashboard">
         @include('dashboard::achievement-details')
         {{--    @include('dashboard::onboarding.presentation')--}}
-        @include('dashboard::onboarding.gamification')
-        @include('dashboard::onboarding.account-health')
+{{--        @include('dashboard::onboarding.gamification')--}}
+{{--        @include('dashboard::onboarding.account-health')--}}
         <div style="display: none" class="page-header container">
             <div class="row align-items-center justify-content-between">
                 <div class="col-lg-8 mb-15">
