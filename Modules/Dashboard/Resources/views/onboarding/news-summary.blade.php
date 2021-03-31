08de4bbe5ca872f2aae6f2217a63522dd30ff292
Pedro Henrique Rodrigues <pedroh.rodriguees@gmail.com>
2021-03-31 01:57:52 -0300

    update: modal finish

--- Modules/Dashboard/Resources/views/onboarding/news-summary.blade.php
+++ Modules/Dashboard/Resources/views/onboarding/news-summary.blade.php
@@ -2,51 +2,49 @@
     <link rel="stylesheet" href="{{ asset('modules/dashboard/css/onboarding-details.css?v=01') }}">
 @endpush
 
-<div class="modal-content">
-    <div class="modal-header flex-wrap">
-        <div id="title-news-onboarding">
-            <img id="icon" src="{{asset('/modules/global/adminremark/assets/images/siriusM.svg')}}" width="40"
-                 alt="Image"> <span>Novidades</span>
+<div class="modal-header flex-wrap">
+    <div id="title-news-onboarding">
+        <img id="icon" src="{{asset('/modules/global/adminremark/assets/images/siriusM.svg')}}" width="40"
+             alt="Image"> <span>Novidades</span>
+    </div>
+    <div class="pages">
+        <span id="page-gamification"></span>
+        <span id="page-account-health"></span>
+        <span id="page-news-summary" class="active"></span>
+    </div>
+</div>
+<div class="modal-body px-50">
+    <div class="summary-title-primary">Talvez você ainda não saiba...</div>
+    <div class="row">
+        <div class="col-12 col-sm-6 my-15">
+            <div class="summary-title">CASHBACK <span class="summary-badge new">NOVO</span></div>
+            <div class="summary-description">O primeiro gateway de pagamentos a oferecer cashback em cada compra.</div>
         </div>
-        <div class="pages">
-            <span id="page-gamification"></span>
-            <span id="page-account-health"></span>
-            <span id="page-news-summary" class="active"></span>
+        <div class="col-12 col-sm-6 my-15">
+            <div class="summary-title">ORDER-BUMP <span class="summary-badge new">NOVO</span></div>
+            <div class="summary-description">Além do upsell, agora você pode utilizar order-bump em nosso checkout.</div>
+        </div>
+
+        <div class="col-12 col-sm-6 my-15">
+            <div class="summary-title">CONTESTAÇÕES <span class="summary-badge new">NOVO</span></div>
+            <div class="summary-description">Uma nova área para cuidar de perto as contestações e chargebacks.</div>
+        </div>
+        <div class="col-12 col-sm-6 my-15">
+            <div class="summary-title">ATENDIMENTO</div>
+            <div class="summary-description">Nova classificação de atendimentos. Melhorias gerais no SAC das lojas.</div>
+        </div>
+
+        <div class="col-12 col-sm-6 my-15">
+            <div class="summary-title">CHECKOUT 2.0 <span class="summary-badge soon">EM BREVE</span></div>
+            <div class="summary-description">O novo checkout do Sirius para aumentar ainda mais a sua conversão.</div>
+        </div>
+        <div class="col-12 col-sm-6 my-15">
+            <div class="summary-title">APP SIRIUS <span class="summary-badge soon">EM BREVE</span></div>
+            <div class="summary-description">Um app prático, rápido e fácil para consultar na tela do seu celular.</div>
         </div>
     </div>
-    <div class="modal-body px-50">
-        <div class="summary-title-primary">Talvez você ainda não saiba...</div>
-        <div class="row">
-            <div class="col-12 col-sm-6 my-15">
-                <div class="summary-title">CASHBACK <span class="summary-badge new">NOVO</span></div>
-                <div class="summary-description">O primeiro gateway de pagamentos a oferecer cashback em cada compra.</div>
-            </div>
-            <div class="col-12 col-sm-6 my-15">
-                <div class="summary-title">ORDER-BUMP <span class="summary-badge new">NOVO</span></div>
-                <div class="summary-description">Além do upsell, agora você pode utilizar order-bump em nosso checkout.</div>
-            </div>
 
-            <div class="col-12 col-sm-6 my-15">
-                <div class="summary-title">CONTESTAÇÕES <span class="summary-badge new">NOVO</span></div>
-                <div class="summary-description">Uma nova área para cuidar de perto as contestações e chargebacks.</div>
-            </div>
-            <div class="col-12 col-sm-6 my-15">
-                <div class="summary-title">ATENDIMENTO</div>
-                <div class="summary-description">Nova classificação de atendimentos. Melhorias gerais no SAC das lojas.</div>
-            </div>
-
-            <div class="col-12 col-sm-6 my-15">
-                <div class="summary-title">CHECKOUT 2.0 <span class="summary-badge soon">EM BREVE</span></div>
-                <div class="summary-description">O novo checkout do Sirius para aumentar ainda mais a sua conversão.</div>
-            </div>
-            <div class="col-12 col-sm-6 my-15">
-                <div class="summary-title">APP SIRIUS <span class="summary-badge soon">EM BREVE</span></div>
-                <div class="summary-description">Um app prático, rápido e fácil para consultar na tela do seu celular.</div>
-            </div>
-        </div>
-
-        <div class="modal-footer justify-content-center">
-            <div id="onboarding-finish" class="btn btn-primary">Finalizar</div>
-        </div>
+    <div class="modal-footer justify-content-center">
+        <div id="onboarding-finish" class="btn btn-primary">Finalizar</div>
     </div>
 </div>
--- Modules/Dashboard/Resources/views/onboarding/presentation.blade.php
+++ Modules/Dashboard/Resources/views/onboarding/presentation.blade.php
@@ -1,34 +1,43 @@
 @push('css')
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css"/>
+
     <link rel="stylesheet" href="{{ asset('modules/dashboard/css/onboarding-details.css?v=01') }}">
 @endpush
 
 <div id="modal-onboarding" class="modal fade modal-fade-in-scale-up show">
     <div class="modal-dialog modal-simple modal-onboarding">
-        <div id="modal-presentation">
             <div class="modal-content">
-                <div id="header-onboarding-1" class="modal-header flex-wrap">
-                    <div class="w-p100 text-center">
-                        <img id="icon" src="{{ asset('modules/global/img/onboarding-presentation.png') }}" alt="Image">
+                <div id="modal-content-onboarding">
+                    <div id="modal-presentation">
+                        <div id="header-onboarding-1" class="modal-header flex-wrap">
+                            <div class="w-p100 d-flex justify-content-center">
+                                <img id="icon" src="{{ asset('modules/global/img/onboarding-presentation.png') }}" alt="Image">
+                            </div>
+                        </div>
+                        <div class="modal-body text-center">
+                            <div id="title-onboarding-1">Temos novidades pra você, <span id="user-name"></span></div>
+                            <div id="description-onboarding-1">Você já deve ter percebido que o Sirius tem passado por diversas melhorias. Além de um novo visual, hoje queremos te apresentar algumas ferramentas e funcionalidades que acabaram de chegar por aqui.</div>
+                            <div id="onboarding-next-presentation" class="btn btn-primary">Vamos lá!</div>
+                        </div>
+                    </div>
+
+                    <div id="modal-gamification">
+                        @include('dashboard::onboarding.gamification')
+                    </div>
+
+                    <div id="modal-account-health">
+                        @include('dashboard::onboarding.account-health')
+                    </div>
+
+                    <div id="modal-news-summary">
+                        @include('dashboard::onboarding.news-summary')
                     </div>
                 </div>
-                <div class="modal-body text-center">
-                    <div id="title-onboarding-1">Temos novidades pra você, Vitor!</div>
-                    <div id="description-onboarding-1">Você já deve ter percebido que o Sirius tem passado por diversas melhorias. Além de um novo visual, hoje queremos te apresentar algumas ferramentas e funcionalidades que acabaram de chegar por aqui.</div>
-                    <div id="onboarding-next-presentation" class="btn btn-primary">Vamos lá!</div>
-                </div>
-            </div>
-        </div>
-
-        <div id="modal-gamification" class="d-none">
-            @include('dashboard::onboarding.gamification')
-        </div>
-
-        <div id="modal-account-health" class="d-none">
-            @include('dashboard::onboarding.account-health')
-        </div>
-
-        <div id="modal-news-summary" class="d-none">
-            @include('dashboard::onboarding.news-summary')
         </div>
     </div>
 </div>
+
+@push('scripts')
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+@endpush
