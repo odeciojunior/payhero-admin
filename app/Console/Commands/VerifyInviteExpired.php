ce5baa5bf12811a7687083db9c2246e66e626e13
joao <joaolucas@cloudfox.net>
2021-04-14 11:01:35 -0300

    fix: expiring invites

--- app/Console/Commands/VerifyInviteExpired.php
+++ app/Console/Commands/VerifyInviteExpired.php
@@ -2,29 +2,17 @@
 
 namespace App\Console\Commands;
 
-use App\Exceptions\CommandMonitorTimeException;
 use Carbon\Carbon;
-use Illuminate\Console\Command;
 use Exception;
+use Illuminate\Console\Command;
 use Modules\Core\Entities\Invitation;
 
 class VerifyInviteExpired extends Command
 {
-    /**
-     * The name and signature of the console command.
-     * @var string
-     */
     protected $signature = 'verify:inviteexpired';
-    /**
-     * The console command description.
-     * @var string
-     */
+
     protected $description = 'Command description';
 
-    /**
-     * Create a new command instance.
-     * @return void
-     */
     public function __construct()
     {
         parent::__construct();
@@ -33,12 +21,11 @@ class VerifyInviteExpired extends Command
     public function handle()
     {
         try {
-
-            $invitationModel = new Invitation();
-
-            $invites = $invitationModel->where('expiration_date', '<=', Carbon::now()->toDateString())->get();
+            $invites = Invitation::where('expiration_date', '<=', Carbon::now())
+                ->where("status", "!=", Invitation::INVITATION_EXPIRED)
+                ->get();
             foreach ($invites as $invite) {
-                $invite->update(['status' => $invitationModel->present()->getStatus('expired')]);
+                $invite->update(['status' => Invitation::INVITATION_EXPIRED]);
             }
         } catch (Exception $e) {
             report($e);
