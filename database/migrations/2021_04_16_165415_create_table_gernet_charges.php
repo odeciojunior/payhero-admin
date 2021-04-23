17c36b23e095863187238c45d589992b6081e589
Edwin Medina <edwingarcia@cloudfox.net>
2021-04-23 15:40:19 -0400

    alter table field pix

--- database/migrations/2021_04_16_165415_create_table_gernet_charges.php
+++ database/migrations/2021_04_16_165415_create_table_gernet_charges.php
@@ -13,20 +13,22 @@ class CreateTableGernetCharges extends Migration
      */
     public function up()
     {
-        Schema::create('gernet_charges', function(Blueprint $table){
+        Schema::create('pix_charges', function(Blueprint $table){
             $table->increments('id');            
-            $table->foreignId('sale_id');//->references('id')->on('sales');
-            $table->foreignId('checkout_id');//->references('id')->on('checkouts');
+            $table->foreignId('sale_id');//->references('id')->on('sales');            
+            $table->foreignId('gateway_id');//->references('id')->on('checkouts');
             $table->string('txid',35);
             $table->integer('location_id')->default(0);
             $table->string('location',100)->nullable();
-            $table->string('status',10);            
+            $table->string('qrcode',200)->nullable();
+            $table->mediumText('qrcode_image')->nullable();
+            $table->string('status',10);
             $table->timestamps();            
         });    
         
-        DB::statement("ALTER TABLE gernet_charges 
-        ADD CONSTRAINT `fk_sale_id` FOREIGN KEY ( `sale_id` ) REFERENCES `sales` ( `id` ),
-        ADD CONSTRAINT `fk_checkout_id` FOREIGN KEY ( `checkout_id` ) REFERENCES `checkouts` ( `id` )  ;");
+        DB::statement("ALTER TABLE pix_charges 
+        ADD CONSTRAINT `fk_sale_id` FOREIGN KEY ( `sale_id` ) REFERENCES `sales` ( `id` ),        
+        ADD CONSTRAINT `fk_gateway_id` FOREIGN KEY ( `gateway_id` ) REFERENCES `gateways` ( `id` );");
     }
 
     /**
@@ -36,6 +38,6 @@ class CreateTableGernetCharges extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('gernet_charges');
+        Schema::dropIfExists('pix_charges');
     }
 }
