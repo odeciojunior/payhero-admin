20b518713f9295f72854b318d21a35ce9ef95224
Pedro Henrique Rodrigues <pedroh.rodriguees@gmail.com>
2021-04-28 17:23:30 -0300

    add: new pix flag in reports

--- Modules/Reports/Resources/views/pending.blade.php
+++ Modules/Reports/Resources/views/pending.blade.php
@@ -55,6 +55,7 @@
                                             <option value="">Boleto e cartão de crédito</option>
                                             <option value="1">Cartão de crédito</option>
                                             <option value="2">Boleto</option>
+                                            <option value="4">PIX</option>
                                         </select>
                                     </div>
                                     <div class="col-sm-6 col-md-6 col-xl-3 col-12">
