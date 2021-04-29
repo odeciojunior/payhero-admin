d385ef4f6dcdbcd0ac09d1a2d1c256abbafe6f18
joao <joaolucas@cloudfox.net>
2021-04-29 18:54:15 -0300

    feat: new file add message return response controllers

---
+++ resources/lang/br/controller.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'error' => [
+        'generic' => 'Ocorreu um erro, tente novamente mais tarde',
+        'create' => 'Ocorreu um erro ao tentar cadastrar',
+    ],
+    'success' => [
+        'create' => 'Atualizado com sucesso'
+    ],
+    'pixel' => [
+        'create' => [
+            'permission' => 'Sem permissão para criar pixel'
+        ],
+        'index' => [
+            'permission' => 'Sem permissão para listar pixels'
+        ],
+        'log' => [
+            'visualization' => 'Visualizou tela todos os pixels para o projeto'
+        ]
+    ]
+
+];
