12ab6a385a1ca41ce2dcc8ec49b01349c91fa97b
Weslley Richard <weslleyrichardc@gmail.com>
2021-05-07 11:41:33 -0300

    fix: finances filter buttons

--- Modules/Finances/Resources/views/index.blade.php
+++ Modules/Finances/Resources/views/index.blade.php
@@ -434,7 +434,7 @@
                                             <div class="row">
                                                 <div class="col-12">
                                                     <div class="row" style="height: 0">
-                                                        <div class="col-6 col-xl-3 offset-xl-6 pr-0">
+                                                        <div class="col-6 col-xl-3 offset-xl-6 pr-0 mt-20">
                                                             <div
                                                                 class="btn btn-light-1 w-p100 bold d-flex justify-content-center align-items-center"
                                                                 data-toggle="collapse"
@@ -447,7 +447,7 @@
                                                             </div>
                                                         </div>
 
-                                                        <div class="col-6 col-xl-3">
+                                                        <div class="col-6 col-xl-3 mt-20">
                                                             <div id="bt_filtro" class="btn btn-primary-1 w-p100 bold d-flex justify-content-center align-items-center">
                                                                 <img class="hidden-xs-down" style="height: 12px; margin-right: 4px" src="{{ asset('/modules/global/img/svg/check-all.svg') }}">
                                                                 Aplicar filtros
