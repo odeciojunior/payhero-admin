b6d4425977497931efe1c9de3708cb570251f51a
Rupert Lustosa <rupertlustosa@gmail.com>
2021-05-10 19:34:10 -0300

    Cleaning code

--- Modules/Withdrawals/Http/Controllers/WithdrawalsApiController.php
+++ Modules/Withdrawals/Http/Controllers/WithdrawalsApiController.php
@@ -288,7 +288,6 @@ class WithdrawalsApiController
                     'date' => $arrayBrand['date'] ?? ' - ',
                 ];
             }
-            
 
             $return = [
                 'id' => $id,
