0bef9a00658371f0a1475a2ba03042a0eaff1e4f
marcosmarion <marcosmarioncloudfox@cloudfox.net>
2021-05-13 10:24:43 -0300

    change column pix to has_pix_key

---
+++ database/migrations/2021_05_13_095332_change_pix_key_column_to_companies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangePixKeyColumnToCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('companies', function (Blueprint $table) {
+            $table->boolean('pix_key')->default(false)->change();
+            $table->renameColumn('pix_key', 'has_pix_key');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('companies', function (Blueprint $table) {
+            $table->string('pix_key')->after('gateway_tax')->nullable()->change();
+            $table->renameColumn('has_pix_key', 'pix_key');
+        });
+    }
+}
