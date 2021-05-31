d2e563201415945344b7d324d16d6613e29002e4
Pedro Henrique Rodrigues <pedroh.rodriguees@gmail.com>
2021-05-31 11:53:25 -0300

    fix(pix): total discount on email pix

--- app/Console/Commands/GenericCommand.php
+++ app/Console/Commands/GenericCommand.php
@@ -3,6 +3,9 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Carbon;
+use Modules\Core\Entities\Sale;
+use Modules\Core\Events\PixExpiredEvent;
 use Modules\Core\Services\CloudFlareService;
 
 class GenericCommand extends Command
@@ -22,7 +25,14 @@ class GenericCommand extends Command
 
     public function handle()
     {
-        //
+        $lastPixSale = Sale::where('payment_method', Sale::PIX_PAYMENT)->get()->last();
+
+        $lastPixSale->update([
+            'created_at' => Carbon::now()->subHours(2)->toDateTimeString(),
+            'status' => Sale::STATUS_PENDING
+                             ]);
+
+        dd(event(new PixExpiredEvent(Sale::latest()->first())));
     }
 }
 
