3be10b6ba548d4b32e6251e1a11870647b754e73
Jean Castro <jeanvcastro1@gmail.com>
2021-06-17 16:03:58 -0300

    fix

--- Modules/Core/Services/TrackingmoreService.php
+++ Modules/Core/Services/TrackingmoreService.php
@@ -163,7 +163,7 @@ class TrackingmoreService
         while ($retry) {
             $result = curl_exec($curl);
             $result = json_decode($result);
-            if (!empty($resul) && !empty($result->meta) && !empty($result->meta->code) && $result->meta->code == 429) {
+            if (!empty($result) && !empty($result->meta) && !empty($result->meta->code) && $result->meta->code == 429) {
                 sleep(1);
             } else {
                 $retry = false;
