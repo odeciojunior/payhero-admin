c83b166f304626b452e11bcf0a05a36c6ca04da7
jonas.ferutcci <jonas.ferutcci@gmail.com>
2021-06-18 09:44:05 -0300

    fix chat over button save new project

--- Modules/Projects/Resources/views/create.blade.php
+++ Modules/Projects/Resources/views/create.blade.php
@@ -53,7 +53,7 @@
                     </div>
                 </div>
 
-                <div class="row pr-15 form-buttons pb-30">
+                <div class="row pr-15 form-buttons pb-20">
                     <a type="button" class="btn btn-cancelar" href="/projects">Cancelar</a>
 
                     <button id="btn-save" type="submit" class="btn btn-primary btn-lg ml-15">
