df703f4904901fa0ed8e1b4d715ed5318ee9c55f
Henrique Brites <henriquebrites@live.com>
2021-06-29 16:29:15 -0300

    bugfix: remove adjust flashing screen

--- Modules/Projects/Resources/views/empty.blade.php
+++ Modules/Projects/Resources/views/empty.blade.php
@@ -1,4 +1,15 @@
-<div id="project-empty" class="content-error text-center d-none">
+{{--<div id="project-empty" class="content-error text-center d-none">--}}
+<div id="project-empty" class="content-error text-center" style="display:none;
+                                                                height: 100%;
+                                                                width: 100%;
+                                                                position: absolute;
+                                                                display: -webkit-flex;
+                                                                display: flex;
+                                                                -webkit-align-items: center;
+                                                                align-items: center;
+                                                                -webkit-justify-content: center;
+                                                                justify-content: center;
+                                                                padding-bottom:116px">
     <img src="{!! asset('modules/global/img/projetos.svg') !!}" width="156px">
     <h1 class="big gray">Você ainda não tem nenhum projeto!</h1>
     <p class="desc gray">Que tal criar um primeiro projeto para começar a vender? </p>
@@ -7,5 +18,5 @@
     </a>
 </div>
 @push('css')
-    <link rel="stylesheet" href="{!! asset('modules/global/css/empty.css?v=03') !!}">
+    <link rel="stylesheet" href="{!! asset('modules/global/css/empty.css?v=06') !!}">
 @endpush
