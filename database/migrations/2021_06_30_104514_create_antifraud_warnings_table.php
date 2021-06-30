afe48e4c0060fca7fa6bd38cf3c29e579624d4ef
Rafael Rocha <rafaelcosta@cloudfox.net>
2021-06-30 13:35:04 -0300

    feat: antifraud warnings migrations

---
+++ database/migrations/2021_06_30_104514_create_antifraud_warnings_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAntifraudWarningsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create(
+            'antifraud_warnings',
+            function (Blueprint $table) {
+                $table->id();
+                $table->unsignedBigInteger("sale_id")->nullable();
+                $table->foreign('sale_id')->references('id')->on('sales');
+                $table->string('column');
+                $table->string('value');
+                $table->string('level', 20);
+                $table->timestamps();
+            }
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table(
+            'antifraud_warnings',
+            function (Blueprint $table) {
+                Schema::dropIfExists('antifraud_warnings');
+            }
+        );
+    }
+}
