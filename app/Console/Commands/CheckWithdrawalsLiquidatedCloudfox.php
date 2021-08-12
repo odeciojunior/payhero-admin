13c23b53b21b0ad4c6603c79f554ec163127ec13
Henrique Brites <henriquebrites@live.com>
2021-08-12 17:47:40 -0300

    hotfix: ajust command released cloudfox checkout

--- app/Console/Commands/CheckWithdrawalsLiquidatedCloudfox.php
+++ app/Console/Commands/CheckWithdrawalsLiquidatedCloudfox.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use Exception;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Modules\Core\Entities\Company;
--- app/Console/Commands/CheckWithdrawalsReleasedCloudfox.php
+++ app/Console/Commands/CheckWithdrawalsReleasedCloudfox.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use Exception;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
