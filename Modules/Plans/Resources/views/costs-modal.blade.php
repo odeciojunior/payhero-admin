471cea1e0a38309c1f98b02d53a6643b11bd0edd
Sheldry Souza <sheldrysouza@cloudfox.net>
2021-08-20 22:42:59 -0300

    html stage 2 - add plan

---
+++ Modules/Plans/Resources/views/costs-modal.blade.php
@@ -0,0 +1,197 @@
+<div class="stage-costs">
+    <div class="row" style="margin-bottom: 24px;">
+        <div class="col-sm-12">
+            <div class="header">
+                <p class="title m-0">Insira os custos de cada produto</p>
+            </div>
+        </div>
+    </div>
+    
+    <div class="row">
+        <div class="col-sm-12">
+            <table>
+                <thead>
+                    <td>Produto</td>
+                    <td>Custo (un)</td>
+                    <td>Quantidade</td>
+                    <td>Moeda</td>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <div class="d-flex align-items-center">
+                                <img src="https://cloudfox-digital-products.s3.amazonaws.com/uploads/public/products/MNZM5z7164NCd3GnSTujQH9DJVSzrtLdKnHs2m4P.png" alt="Image Product">
+                                <h1 class="title">SmartBand Verm...</h1>
+                            </div>
+                        </td>
+                        <td>
+                            <input class="form-control form-control-lg" type="text" placeholder="Valor un.">
+                        </td>
+                        <td>
+                            <input class="form-control form-control-lg" type="text" placeholder="Qtd.">
+                        </td>
+                        <td>
+                            <select class="form-control form-control-lg" type="text" placeholder="Valor un.">
+                                <option>BRL (R$)</option>
+                                <option>BRL (R$)</option>
+                            </select>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <div class="d-flex align-items-center">
+                                <img src="https://cloudfox-digital-products.s3.amazonaws.com/uploads/public/products/MNZM5z7164NCd3GnSTujQH9DJVSzrtLdKnHs2m4P.png" alt="Image Product">
+                                <h1 class="title">SmartBand Verm...</h1>
+                            </div>
+                        </td>
+                        <td>
+                            <input class="form-control form-control-lg" type="text" placeholder="Valor un.">
+                        </td>
+                        <td>
+                            <input class="form-control form-control-lg" type="text" placeholder="Qtd.">
+                        </td>
+                        <td>
+                            <select class="form-control form-control-lg" type="text" placeholder="Valor un.">
+                                <option>BRL (R$)</option>
+                                <option>BRL (R$)</option>
+                            </select>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <div class="d-flex align-items-center">
+                                <img src="https://cloudfox-digital-products.s3.amazonaws.com/uploads/public/products/MNZM5z7164NCd3GnSTujQH9DJVSzrtLdKnHs2m4P.png" alt="Image Product">
+                                <h1 class="title">SmartBand Verm...</h1>
+                            </div>
+                        </td>
+                        <td>
+                            <input class="form-control form-control-lg" type="text" placeholder="Valor un.">
+                        </td>
+                        <td>
+                            <input class="form-control form-control-lg" type="text" placeholder="Qtd.">
+                        </td>
+                        <td>
+                            <select class="form-control form-control-lg" type="text" placeholder="Valor un.">
+                                <option>BRL (R$)</option>
+                                <option>BRL (R$)</option>
+                            </select>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <div class="d-flex align-items-center">
+                                <img src="https://cloudfox-digital-products.s3.amazonaws.com/uploads/public/products/MNZM5z7164NCd3GnSTujQH9DJVSzrtLdKnHs2m4P.png" alt="Image Product">
+                                <h1 class="title">SmartBand Verm...</h1>
+                            </div>
+                        </td>
+                        <td>
+                            <input class="form-control form-control-lg" type="text" placeholder="Valor un.">
+                        </td>
+                        <td>
+                            <input class="form-control form-control-lg" type="text" placeholder="Qtd.">
+                        </td>
+                        <td>
+                            <select class="form-control form-control-lg" type="text" placeholder="Valor un.">
+                                <option>BRL (R$)</option>
+                                <option>BRL (R$)</option>
+                            </select>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-12">        
+            <div class="footer">
+                <div class="d-flex align-items-center">
+                    <div class="checkbox"></div>
+                    <p class="m-0">Todos os produtos têm o mesmo custo</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+    .stage-costs .title {
+        color: #636363;
+    }
+
+    .stage-costs .header p {
+        font-weight: bold;
+        font-size: 16px;
+        line-height: 20px;
+    }
+
+    .stage-costs table {
+        table-layout: fixed;
+        width: 100%;
+    }
+    .stage-costs table thead td {
+        font-weight: normal;
+        font-size: 14px;
+        line-height: 18px;
+        letter-spacing: 0;
+        padding-bottom: 18px;
+    }
+    .stage-costs table td {
+        padding-right: 24px;
+    }
+    .stage-costs table thead td:nth-child(1) {
+        width: calc(272px - 24px);
+    }
+    .stage-costs table thead td:nth-child(2) {
+        width: 126px;
+    }
+    .stage-costs table thead td:nth-child(4) {
+        width: calc(128px - 24px);
+    }
+    .stage-costs table thead td:nth-child(3) {
+        width: calc(134px - 24px);
+    }
+    .stage-costs table td:nth-child(4) {
+        padding-right: 0;
+    }
+    .stage-costs table tbody td {
+        padding-bottom: 18px;
+    }
+    .stage-costs .table-striped > tbody > tr:nth-child(2n+2) > td,
+    .stage-costs .table-striped > tbody > tr:nth-child(2n+2) > th {
+        background-color: #fff !important;
+    }
+    .stage-costs table tbody td img {
+        width: 46px;
+        height: 46px;
+        border-radius: 8px;
+        margin-right: 14px;
+    }
+    .stage-costs table tbody td .title {
+        font-weight: bold;
+        font-size: 16px;
+        line-height: 18px;
+        margin: 0;
+    }
+
+    .stage-costs .footer {
+        margin-top: 24px;
+    }
+    .stage-costs .footer .checkbox {
+        width: 31px;
+        height: 31px;
+        background: #FEFEFE;
+        border: 1px solid #B3B3B3;
+        box-sizing: border-box;
+        border-radius: 8px;
+        margin-right: 10px;
+    }
+    .stage-costs .footer p {
+        font-weight: bold;
+        font-size: 14px;
+        line-height: 18px;
+    }
+</style>
