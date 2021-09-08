080e9aa330789620b45a0da803bac553ed7e92a5
jeanvcastro <jean.castro1@outlook.com>
2021-09-08 11:27:35 -0300

    optimize importShopifyProduct method in ShopifyService

--- app/Jobs/ProcessShopifyPostbackJob.php
+++ app/Jobs/ProcessShopifyPostbackJob.php
@@ -42,7 +42,7 @@ class ProcessShopifyPostbackJob implements ShouldQueue
 
                 $integration = ShopifyIntegration::where('project_id', $project->id)->first();
 
-                if (!empty($integration)) {
+                if (!empty($user) && !empty($project) && !empty($integration)) {
                     $shopifyService = new ShopifyService($integration->url_store, $integration->token, false);
 
                     if (!empty($postback['variants']) && count($postback['variants']) > 0) {
