eef3cd821eb60f306d7e8b7a54fbd59f264748dd
Edwin Medina <edwingarcia@cloudfox.net>
2021-09-22 09:30:08 -0400

    clean generic command

--- app/Console/Commands/GenericCommand.php
+++ app/Console/Commands/GenericCommand.php
@@ -14,32 +14,6 @@ class GenericCommand extends Command
 
     public function handle()
     {
-        $this->rollbackPermissionAdmin();
-    }
-
-    public function setPermissionsAdmin(){
-        $users = User::role('account_owner')->get();
-        foreach($users as $us){
-            $us->syncRoles(['account_owner','admin']);
-        }
-    }
-   
-    public function rollbackPermissionAdmin(){
-        $users = DB::select("SELECT COUNT(model_id) as total, model_id FROM model_has_roles WHERE role_id = 4 OR role_id = 5 GROUP BY model_id;");
-        
-        $total = count($users);
-        $bar = $this->output->createProgressBar($total);
-        $bar->start();
-
-        foreach($users as $us){
-            if($us->total >= 2){
-                $user = User::find($us->model_id);
-                $user->syncRoles(['account_owner']);
-                $this->line('User id: ' .  $us->model_id );
-            }
-            $bar->advance();
-        }
-        $bar->finish();
-        
+       
     }
 }
