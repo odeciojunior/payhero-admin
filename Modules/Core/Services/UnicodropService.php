b2401123f158c4e46b5a45fde8cc97568daa14f5
Julio Leichtweis <julioleichtweis@gmail.com>
2021-10-07 14:55:53 -0300

    unicodrop integration

--- Modules/Core/Services/UnicodropService.php
+++ Modules/Core/Services/UnicodropService.php
@@ -2,132 +2,121 @@
 
 namespace Modules\Core\Services;
 
-use Exception;
-use Modules\Core\Entities\PlanSale;
 use Modules\Core\Entities\Sale;
-use Vinkla\Hashids\Facades\Hashids;
-use Carbon\Carbon;
-use Modules\Core\Entities\ReportanaSent;
-use Modules\Core\Entities\ReportanaIntegration;
+use Exception;
+use Modules\Core\Entities\IntegrationLog;
+use Modules\Core\Entities\UnicodropIntegration;
 
-/**
- * Class UnicodropService
- * @package Modules\Core\Services
- */
 class UnicodropService
 {
-    /**
-     * @var
-     */
-    public $token;
-    /**
-     * @var
-     */
-    private $integrationId;
-    /**
-     * @var
-     */
-    private $link;
+    const API_ENDPOINT = 'https://www.unicodrop.com.br/integracoes/cloudfox/default.asp?t=1&u=';
 
-    /**
-     * ReportanaService constructor.
-     * @param $token
-     * @param $integrationId
-     */
-    function __construct($token, $integrationId)
+    public UnicodropIntegration $integration;
+
+    function __construct(UnicodropIntegration $integration)
     {
-        $this->token = $token;
-
-        $this->integrationId = $integrationId;
-
-        $this->link = 'https://www.unicodrop.com.br/integracoes/cloudfox99134z84x5/';
+        $this->integration = $integration;
     }
 
-    /**
-     * @param Sale $sale
-     */
     function boletoPaid(Sale $sale)
     {
-        $dataProducts = [];
-        $dataVariants = [];
-        foreach ($sale->plansSales as $planSale) {
-            foreach ($planSale->plan->products as $product) {
-                $dataProducts[] = [
-                    'produto_id'             => Hashids::encode($product->id),
-                    'produto_nome'           => $product->name,
-                    'produto_imagempequena'  => $product->photo ?? '',
-                    'produto_imagemgrande'   => $product->photo ?? '',
-                    'produto_data_criacao'   => Carbon::parse($product->created_at)->format('Y-m-d h:i:s'),
-                    'produto_data_alteracao' => Carbon::parse($product->updated_at)->format('Y-m-d h:i:s'),
-                    'produto_preco'          => $product->price ?? '',
-                    'produto_sku'            => $product->sku ?? '',
-                ];
-                $dataVariants[] = [
-                    'produto_id'  => Hashids::encode($product->id),
-                    'variante_id' => $product->shopify_variant_id ?? '',
-                ];
-            }
+        try{
+            $data = [
+                'transaction_id' => hashids_encode($sale->id, 'sale_id'),
+                'payment_method' => 'billet',
+                'status' => 'paid',
+                'name' => $sale->customer->name,
+                'phone' => str_replace('+55', '', $sale->customer->telephone),
+                'email' => $sale->customer->present()->getEmail(),
+                'address' => $sale->delivery->street,
+                'address_number' => $sale->delivery->number,
+                'address_district' => $sale->delivery->neighborhood,
+                'address_zip_code' => $sale->delivery->zip_code,
+                'address_city' => $sale->delivery->city,
+                'address_state' => $sale->delivery->state,
+                'address_country' => 'BR',
+                'document' => $sale->customer->document,
+                'total_price' => $sale->total_paid_value,
+                'billet_url' => $sale->boleto_link,
+                'products' => $this->getPlansList($sale),
+            ];
+            $this->sendPost($data);
         }
-        $data = [
-            'token'     => $this->token,
-            'produtos'  => $dataProducts,
-            'variantes' => $dataVariants,
-            'pedido'    => [
-                'pedido_id'                      => Hashids::encode($sale->id),
-                'pedido_idshopify'               => $sale->shopify_order ?? '',
-                'pedido_data_criacao'            => Carbon::parse($sale->created_at)->format('Y-m-d h:i:s'),
-                'pedido_data_aprovacaopagamento' => Carbon::parse($sale->end_date)->format('Y-m-d h:i:s'),
-
-                'pedido_status'            => $sale->present()->getStatus(),
-                'pedido_formadepagamento'  => $sale->present()->getPaymentType(),
-                'pedido_gateway_pagamento' => '',
-                'pedido_linkboleto'        => $sale->boleto_link,
-                'pedido_valor_frete'       => $sale->shipping->value,
-                'pedido_valor_desconto'    => $sale->shopify_discount,
-                'pedido_valor_totalpedido' => $sale->total_paid_value,
-
-                'cliente_id'           => Hashids::encode($sale->customer->id),
-                'cliente_primeironome' => $sale->customer->present()->getFirstName(),
-                'cliente_ultimonome'   => $sale->customer->present()->getLastName(),
-                'cliente_email'        => $sale->customer->email,
-                'cliente_telefone'     => $sale->customer->telephone,
-
-                'entrega_endereco1' => $sale->delivery->street,
-                'entrega_cidade'    => $sale->delivery->city,
-                'entrega_cep'       => $sale->delivery->zip_code,
-
-                'frete_nome'    => $sale->shipping->name,
-                'desconto_nome' => $sale->shopify_discount ?? '',
-
-                'transaction_id' => Hashids::connection('sale_id')->encode($sale->id),
-            ],
-        ];
-        self::sendPost($data);
+        catch(Exception $e) {
+            report($e);
+        }
     }
 
-    /**
-     * @param $data
-     */
+    function pixExpired(Sale $sale)
+    {
+        try{
+            $data = [
+                'transaction_id' => hashids_encode($sale->id, 'sale_id'),
+                'payment_method' => 'pix',
+                'status' => 'expired',
+                'name' => $sale->customer->name,
+                'phone' => str_replace('+55', '', $sale->customer->telephone),
+                'email' => $sale->customer->present()->getEmail(),
+                'address' => $sale->delivery->street,
+                'address_number' => $sale->delivery->number,
+                'address_district' => $sale->delivery->neighborhood,
+                'address_zip_code' => $sale->delivery->zip_code,
+                'address_city' => $sale->delivery->city,
+                'address_state' => $sale->delivery->state,
+                'address_country' => 'BR',
+                'document' => $sale->customer->document,
+                'total_price' => $sale->total_paid_value,
+                'billet_url' => $sale->boleto_link,
+                'products' => $this->getPlansList($sale),
+            ];
+            $this->sendPost($data);
+        }
+        catch(Exception $e) {
+            report($e);
+        }
+    }
+
     private function sendPost($data)
     {
-
         $curl = curl_init();
+        $url = self::API_ENDPOINT . $this->integration->token;
+
+        // $url = 'https://www.unicodrop.com.br/integracoes/cloudfox/default.asp?t=1&u=6F8DD99B-BAC4-4436-9EC1-E0E285987B0B';
 
         curl_setopt_array($curl,
                           [
-                              CURLOPT_URL            => $this->link,
+                              CURLOPT_URL            => $url,
                               CURLOPT_RETURNTRANSFER => true,
                               CURLOPT_CUSTOMREQUEST  => "POST",
                               CURLOPT_POSTFIELDS     => json_encode($data),
-                              CURLOPT_HTTPHEADER     =>
-                                  [
-                                      'Content-Type: application/json',
-                                  ],
+                              CURLOPT_HTTPHEADER     => ['Content-Type: application/json'],
                           ]
         );
 
         $response = curl_exec($curl);
-
         curl_close($curl);
+
+        IntegrationLog::create([
+            'source_table' => 'sale',
+            'source_id' => '0',
+            'request' => json_encode(['url' => $url, 'data' => $data]),
+            'response' => $response,
+            'api' => 'UnicoDrop',
+        ]);
+    }
+
+    public function getPlansList(Sale $sale)
+    {
+        $plans = [];
+
+        foreach ($sale->plansSales as $planSale) {
+            $plans[] = [
+                "price" => $planSale->plan()->first()->price,
+                "quantity" => $planSale->amount,
+                "product_name" => $planSale->plan()->first()->name . ' - ' . $planSale->plan()->first()->description,
+            ];
+        }
+
+        return $plans;
     }
 }
