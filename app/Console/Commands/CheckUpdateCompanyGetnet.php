464541c009eabeba675ecd98c13b2b3884c14730
Henrique Brites <henriquebrites@live.com>
2021-10-25 09:56:15 -0400

    hotfix: adjusrment command CheckUpdateCompanyGetnet

--- app/Console/Commands/CheckUpdateCompanyGetnet.php
+++ app/Console/Commands/CheckUpdateCompanyGetnet.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\Exceptions\CommandMonitorTimeException;
 use Exception;
 use Illuminate\Console\Command;
+use Modules\Core\Entities\Company;
 use Modules\Core\Entities\Gateway;
 use Modules\Core\Entities\GatewaysCompaniesCredential;
 use Modules\Core\Services\CompanyService;
@@ -39,42 +40,50 @@ class CheckUpdateCompanyGetnet extends Command
     public function handle()
     {
         try {
-            $companiesCredencial = GatewaysCompaniesCredential::where('gateway_id',Gateway::GETNET_PRODUCTION_ID)->where('gateway_status',2)->get();            
+            $companiesCredencial = GatewaysCompaniesCredential::where('gateway_id',Gateway::GETNET_PRODUCTION_ID)->where('gateway_status', GatewaysCompaniesCredential::GATEWAY_STATUS_REVIEW)->get();
             $getnet = new GetnetBackOfficeService();
             $companyService = new CompanyService();
 
             foreach ($companiesCredencial as $credential) {
                 $company = $credential->company;
 
-                if ($company->company_type == 1) { // physical person
-                    $result = $getnet->checkPfCompanyRegister($company->document, $company->id);
-                } else { // 'juridical person'
-                    $result = $getnet->checkPjCompanyRegister($company->document, $company->id);
-                }
+                if (!foxutils()->isEmpty($company)) {
+                    if ($company->company_type == Company::PHYSICAL_PERSON) { // physical person
+                        $result = $getnet->checkPfCompanyRegister($company->document, $company->id);
+                    } else { // 'juridical person'
+                        $result = $getnet->checkPjCompanyRegister($company->document, $company->id);
+                    }
 
-                $result = json_decode($result);
+                    $result = json_decode($result);
 
-                if (!empty($result->subseller_id) && $credential->gateway_subseller_id == $result->subseller_id) {
-                    switch($result->status){
-                        case 'Aprovado Transacionar':
+                    if (!empty($result->subseller_id) && $credential->gateway_subseller_id == $result->subseller_id) {
+                        switch ($result->status) {
+                            case 'Aprovado Transacionar':
                             case 'Aprovado Transacionar e Antecipar':
                                 if ($result->capture_payments_enabled == 'S') {
-                                    $credential->update([
-                                        'gateway_status' => GatewaysCompaniesCredential::GATEWAY_STATUS_APPROVED // approved
-                                    ]);
-            
+                                    $credential->update(
+                                        [
+                                            'gateway_status' => GatewaysCompaniesCredential::GATEWAY_STATUS_APPROVED
+                                            // approved
+                                        ]
+                                    );
+
                                     $companyService->updateCaptureTransactionEnabled($company);
                                 }
-                        break;
-                        case 'Reprovado':
+                                break;
+                            case 'Reprovado':
                             case 'Rejeitado':
                                 if ($result->capture_payments_enabled == 'N') {
-                                    $credential->update([
-                                        'gateway_status' => GatewaysCompaniesCredential::GATEWAY_STATUS_REPROVED // reproved
-                                    ]);
+                                    $credential->update(
+                                        [
+                                            'gateway_status' => GatewaysCompaniesCredential::GATEWAY_STATUS_REPROVED
+                                            // reproved
+                                        ]
+                                    );
                                 }
-                        break;
-                    }                   
+                                break;
+                        }
+                    }
                 }
             }
         } catch (Exception $e) {
