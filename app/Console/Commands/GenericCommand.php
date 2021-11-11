a500e535c80769973cf7d895025dfdef4e106e40
Edwin Medina <edwingarcia@cloudfox.net>
2021-11-11 11:38:31 -0400

    get total balance asaas

--- app/Console/Commands/GenericCommand.php
+++ app/Console/Commands/GenericCommand.php
@@ -4,7 +4,9 @@ namespace App\Console\Commands;
 
 use Vinkla\Hashids\Facades\Hashids;
 use Illuminate\Console\Command;
+use Modules\Core\Entities\Gateway;
 use Modules\Core\Entities\User;
+use Modules\Core\Services\Gateways\CheckoutGateway;
 
 class GenericCommand extends Command
 {
@@ -14,19 +16,8 @@ class GenericCommand extends Command
 
     public function handle()
     {
-        $userExist = User::whereHas(
-            'roles',
-            function ($query) {
-                $query->whereIn('name', ['admin']);
-            }
-        )
-        ->where('email', 'luccas332@gmail.com')
-        ->orWhere(function($qr){
-            $qr->where('email', 'assdds')
-            ->whereNull('account_owner_id');
-        })->exists();
-
-            dd($userExist);
+        $service = new CheckoutGateway(Gateway::ASAAS_PRODUCTION_ID);
+        dd($service->getCurrentBalance(3442));
     }
 
 }
