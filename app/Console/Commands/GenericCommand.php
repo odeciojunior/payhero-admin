f63370c3fc0d1b2a40c0bf9673473f61f95fa3d1
Henrique Brites <henriquebrites@live.com>
2021-11-12 11:45:33 -0400

    hotfix: adjustment command cenceled pix

--- app/Console/Commands/GenericCommand.php
+++ app/Console/Commands/GenericCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use Modules\Core\Entities\Sale;
 use Vinkla\Hashids\Facades\Hashids;
 use Illuminate\Console\Command;
 
@@ -13,7 +14,21 @@ class GenericCommand extends Command
 
     public function handle()
     {
-        //
+        $sale = Sale::find(1348648);
+        //1156391
+        $saleModel = Sale::where(
+            [
+                ['payment_method', '=', Sale::PIX_PAYMENT],
+                ['status', '=', Sale::STATUS_APPROVED],
+                //['customer_id', $sale->customer->id]
+            ]
+        )
+            ->whereHas('customer', function($q) use($sale){
+                $q->where('document', $sale->customer->document);
+            })
+            ->whereDate('start_date', \Carbon\Carbon::parse($sale->start_date)->format("Y-m-d"))->first();
+
+        dd($saleModel);
     }
 
 }
