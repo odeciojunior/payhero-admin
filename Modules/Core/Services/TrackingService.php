df69d53c2da8666f3d878cab109d3df31ec9bdea
Edwin Medina <edwingarcia@cloudfox.net>
2021-11-17 16:25:41 -0400

    tracking filter status transaction

--- Modules/Core/Services/TrackingService.php
+++ Modules/Core/Services/TrackingService.php
@@ -279,24 +279,42 @@ class TrackingService
             //filtro transactions
             if (!empty($filters['transaction_status'])) {
                 $filterTransaction = explode(',', $filters['transaction_status']);
-                $query->whereHas('transactions', function ($queryTransaction) use ($filterTransaction) {
-                    
-                    if (!empty($filterTransaction)) {
-                        $queryTransaction->whereIn('status', $filterTransaction);
-
-                    } else {
-                        $queryTransaction->where(function ($query) {
-                            $query->where('transactions.release_date', '>', '2020-05-25') //data que começou a bloquear
-                            ->orWhereHas('sale', function ($query) {
-                                $query->where('is_chargeback_recovered', true);
-                            });
-                        })->where('transactions.release_date', '<=', Carbon::now()->format('Y-m-d'))
-                        ->where('tracking_required', true);
+              
+                $query->whereHas('transactions', function ($qrTransaction) use ($filterTransaction) 
+                {
+                    $transactionPresenter = (new Transaction())->present();
+                    $statusEnum = [];
+                    foreach($filterTransaction as $item){
+                        if($item <> 'blocked'){
+                            $statusEnum[] = $transactionPresenter->getStatusEnum($item);
+                        }
                     }
-                    $queryTransaction->where('type', Transaction::TYPE_PRODUCER)
-                        ->whereNull('invitation_id')
-                        ->where('is_waiting_withdrawal', 0)
-                        ->whereNull('withdrawal_id');
+
+                    if(in_array('blocked',$filterTransaction))
+                    {
+                        $qrTransaction->where(function ($qr) use ($statusEnum) {
+                            $qr->where(function ($query) {
+                                $query->where('transactions.release_date', '>', '2020-05-25') //data que começou a bloquear
+                                ->orWhereHas('sale', function ($query) {
+                                    $query->where('is_chargeback_recovered', true);
+                                });
+                            })->where('transactions.release_date', '<=', Carbon::now()->format('Y-m-d'))                                                   
+                            ->where('tracking_required', true);  
+                            
+                            if(count($statusEnum)>0){
+                                $qr->orWhereIn('status_enum', $statusEnum);
+                            }
+                        });
+                        
+                    }else{
+                        $qrTransaction->whereIn('status_enum', $statusEnum);
+                    }
+
+                    $qrTransaction->where('type', Transaction::TYPE_PRODUCER)
+                    ->whereNull('invitation_id')
+                    ->where('is_waiting_withdrawal', 0)
+                    ->whereNull('withdrawal_id');
+                    
                 });
             }
         });
