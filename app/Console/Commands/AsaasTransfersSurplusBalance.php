22ef1d01f6ce38e7a4c8ec28f35d4292ee8a6cc0
Edwin Medina <edwingarcia@cloudfox.net>
2021-12-16 09:53:20 -0400

    command asaas transfers-surplus-balance

--- app/Console/Commands/AsaasTransfersSurplusBalance.php
+++ app/Console/Commands/AsaasTransfersSurplusBalance.php
@@ -3,10 +3,16 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
 use Modules\Core\Entities\Company;
 use Modules\Core\Entities\Gateway;
+use Modules\Core\Entities\Transfer;
+use Modules\Core\Entities\Withdrawal;
 use Modules\Core\Services\Gateways\AsaasService;
 use Modules\Core\Services\Gateways\CheckoutGateway;
+use PhpParser\Node\Stmt\While_;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Output\ConsoleOutput;
 
 class AsaasTransfersSurplusBalance extends Command
 {
@@ -44,13 +50,13 @@ class AsaasTransfersSurplusBalance extends Command
      */
     public function handle()
     {
-        $companies = Company::where('bank_document_status',Company::DOCUMENT_STATUS_APPROVED)
-        ->where('address_document_status',Company::DOCUMENT_STATUS_APPROVED)
-        ->where('contract_document_status',Company::DOCUMENT_STATUS_APPROVED)
+        $companies = Company::whereHas('transactions', function($q) {
+            $q->where('gateway_id' ,Gateway::ASAAS_PRODUCTION_ID)
+            ->where('created_at', '>' , '2021-09');
+        })
+        ->with('user')
         ->get();
-
-        dd(count($companies));
-
+        
         $checkoutGateway = new CheckoutGateway($this->gatewayId);
         $asaasService = new AsaasService();
 
@@ -59,16 +65,22 @@ class AsaasTransfersSurplusBalance extends Command
         $amountTransfer = 0;
         $status = '';
 
-        $this->line(
+        Log::info(
             str_pad("Company",15,' ',STR_PAD_RIGHT).
             str_pad("Asaas Bal.",15,' ',STR_PAD_RIGHT).
             str_pad("Company Bal.",15,' ',STR_PAD_RIGHT).
             str_pad("Balance",15,' ',STR_PAD_RIGHT).
-            'Status'
+            str_pad('Status',15,' ',STR_PAD_RIGHT).
+            "Usuário"
         ); 
-        
+        $this->comment('processando...');
+        $output = new ConsoleOutput();
+        $progress = new ProgressBar($output, count($companies));
+        $progress->start();
+    
         foreach($companies as $company)
         {
+            $progress->advance();
             $asaasBalance = 0;
             $companyBalance = 0;
             $amountTransfer = 0;
@@ -79,12 +91,26 @@ class AsaasTransfersSurplusBalance extends Command
                 $asaasBalance = $response->total_balance*100;
             }
 
+            $pendingChargebacks = Transfer::whereDoesntHave('asaasTransfer',function($qr){
+                $qr->where('status','DONE');
+            })
+            ->where('reason','chargedback')
+            ->where('gateway_id',$this->gatewayId)
+            ->where('company_id', $company->id)
+            ->sum('value');
+
+            $asaasBalance -= $pendingChargebacks;
+
             $asaasService->setCompany($company);
             $companyBalance = $asaasService->getAvailableBalance() + $asaasService->getPendingBalance();
 
+            $pendingWtihdrawals = Withdrawal::where('company_id',$company->id)
+            ->whereIn('status',[Withdrawal::STATUS_PENDING,Withdrawal::STATUS_IN_REVIEW])->sum('value');
+            $companyBalance+= $pendingWtihdrawals;          
+
             if($asaasBalance > 0 && $asaasBalance > $companyBalance)
             {
-                $amountTransfer = $companyBalance > 0 ? $asaasBalance - $companyBalance : $asaasBalance;
+                $amountTransfer = intval($companyBalance > 0 ? $asaasBalance - $companyBalance : $asaasBalance);
                 
                 $response = $checkoutGateway->transferSubSellerToSeller(
                     $company->id, $amountTransfer
@@ -96,13 +122,16 @@ class AsaasTransfersSurplusBalance extends Command
                 }
             }  
 
-            $this->line(
+            Log::info(
                 str_pad($company->id,15,' ',STR_PAD_RIGHT).
                 str_pad($asaasBalance,15,' ',STR_PAD_RIGHT).
                 str_pad($companyBalance,15,' ',STR_PAD_RIGHT).
                 str_pad($amountTransfer,15,' ',STR_PAD_RIGHT).
-                $status
+                str_pad($status ,15,' ',STR_PAD_RIGHT).
+                $company->user->name 
             );    
         }
+
+        $progress->finish();
     }
 }
