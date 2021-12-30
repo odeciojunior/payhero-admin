022d9023815661343b80ca5c46b28382349870a2
sheldrysouzacloudfox <sheldrysouza@cloudfox.net>
2021-12-30 18:14:51 -0300

    adjustments errors

--- Modules/Products/Transformers/ProductsSelectResource.php
+++ Modules/Products/Transformers/ProductsSelectResource.php
@@ -16,16 +16,21 @@ class ProductsSelectResource extends JsonResource
      */
     public function toArray($request)
     {
+        $cost = $this->currency_type_enum == 1 ? 'R$ ' : '$ ' .number_format(($this->cost ?? 0) / 100, 2, '.', ',');
+        if (count($this->productsPlans) > 0) {
+            $cost = $this->productsPlans->first()->currency_type_enum == 1 ? 'R$ ' : '$ ' .number_format(($this->productsPlans->first()->cost ?? 0) / 100, 2, '.', ',');
+        }
+
         return [
             'id'                    => Hashids::encode($this->id),
             'name'                  => $this->name,
             'name_short'            => Str::limit($this->name, 14),
             'name_short_flag'       => mb_strwidth($this->name, 'UTF-8') <= 14 ? false : true,
             'description'           => Str::limit($this->description, 28),
-            'currency_type_enum'    => $this->productsPlans->first()->currency_type_enum,
+            'currency_type_enum'    => $this->productsPlans->first()->currency_type_enum ?? $this->currency_type_enum,
             'type_enum'             => $this->type_enum,
             'status_enum'           => $this->status_enum,
-            'cost'                  => $this->productsPlans->first()->currency_type_enum == 1 ? 'R$ ' : '$ ' .number_format(($this->productsPlans->first()->cost ?? 0) / 100, 2, '.', ','),
+            'cost'                  => $cost,
             'photo'                 => !empty($this->photo) ? $this->photo : 'https://cloudfox-digital-products.s3.amazonaws.com/public/global/img/produto.svg',
         ];
     }
