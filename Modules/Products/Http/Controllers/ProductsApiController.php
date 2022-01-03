2c105f1e1447a97088804eb80a2254e61e63a86f
sheldrysouzacloudfox <sheldrysouza@cloudfox.net>
2022-01-03 15:17:08 -0300

    adjustments in products list

--- Modules/Products/Http/Controllers/ProductsApiController.php
+++ Modules/Products/Http/Controllers/ProductsApiController.php
@@ -79,8 +79,6 @@ class ProductsApiController extends Controller
 
             if (isset($filters['shopify']) && $filters['shopify'] == 1) {
                 $productsSearch->where('shopify', $filters['shopify']);
-            } else {
-                $productsSearch->whereNull('shopify_variant_id');
             }
 
             if (isset($filters['name'])) {
