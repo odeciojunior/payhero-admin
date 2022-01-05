232e067cb9051dd39f145d529b6b2c39d8fc9a59
sheldrysouzacloudfox <sheldrysouza@cloudfox.net>
2022-01-05 18:08:58 -0300

    adjusted plan tax calculation

--- Modules/Plans/Resources/views/stages/stage1-edit.blade.php
+++ Modules/Plans/Resources/views/stages/stage1-edit.blade.php
@@ -87,7 +87,7 @@
             </div>
         </div>
         <div class="text-center description-tax">
-            <p class="m-0" style="line-height: 14px; font-size: 11px;">Simulação considerando compras à vista com taxa de <span></span> % (30D).</p>
+            <p class="m-0" style="line-height: 14px; font-size: 11px;">Simulação considerando compras à vista com taxa de <span></span> % + 1,00 (30D).</p>
             <p class="font-weight-bold m-0" style="line-height: 14px; font-size: 11px;">Valor estimado sujeito à mudanças de acordo com as condições de pagamento.</p>
         </div>
     </div>
