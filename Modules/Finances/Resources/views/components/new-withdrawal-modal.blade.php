5cfae9a6134848b60354104deb65cb70bd67792d
Pedro Henrique Rodrigues <pedrohenrique@cloudfox.net>
2022-01-19 17:30:32 -0300

    fix: modal

--- Modules/Finances/Resources/views/components/new-withdrawal-modal.blade.php
+++ Modules/Finances/Resources/views/components/new-withdrawal-modal.blade.php
@@ -1,5 +1,5 @@
 <div id="modal-withdrawal-custom" class="modal fade modal-3d-flip-vertical p-0" role="dialog" tabindex="-1" data-keyboard="false" data-backdrop="static">
-    <div class="modal-dialog modal-dialog-centered modal-simple" style="max-width: 500px;">
+    <div class="modal-dialog modal-dialog-centered modal-simple" style="max-width: 500px; margin: 10px auto">
         <div class="modal-content modal-content-style">
             <div class="modal-header header-modal simple-border-bottom modal-title-withdrawal" style="height: 60px;">
                 <h3 id="modal-title-withdrawal-custom" class="modal-title"></h3>
