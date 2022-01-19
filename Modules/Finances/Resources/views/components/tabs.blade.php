e8b38c2577f2c125a17e6d9619e2b214ce4f2294
Pedro Henrique Rodrigues <pedrohenrique@cloudfox.net>
2022-01-19 16:37:29 -0300

    fix: margin

--- Modules/Finances/Resources/views/components/tabs.blade.php
+++ Modules/Finances/Resources/views/components/tabs.blade.php
@@ -1,7 +1,7 @@
 <nav id="menu-tabs-view">
     <div class="nav-tabs-horizontal">
         <div class="nav nav-tabs nav-tabs-line flex-nowrap" id="nav-tab" role="tablist">
-            <a class="nav-item nav-link active nav-link-finances-hide-export" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;" id="nav-transfers-tab" data-toggle="tab" href="#nav-transfers" role="tab" aria-controls="nav-home" aria-selected="true">
+            <a class="nav-item nav-link active nav-link-finances-hide-export mr-5 mr-md-0" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;" id="nav-transfers-tab" data-toggle="tab" href="#nav-transfers" role="tab" aria-controls="nav-home" aria-selected="true">
                 Transferências
             </a>
             <a class="nav-item nav-link nav-link-finances-show-export" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;" data-toggle="tab" href="#nav-statement" role="tab" aria-controls="nav-profile" aria-selected="true">
