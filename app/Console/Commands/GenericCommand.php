2a499ec18b3f2b098d0893bbea24927e073bbff7
jeanvcastro <jean.castro1@outlook.com>
2022-01-25 12:54:24 -0300

    generic command

--- app/Console/Commands/GenericCommand.php
+++ app/Console/Commands/GenericCommand.php
@@ -16,16 +16,19 @@ class GenericCommand extends Command
     public function handle()
     {
         $this->info("APROVANDO RASTREIOS");
-        $trackings = Tracking::select(DB::raw('trackings.*'))
+        $trackingsQuery = Tracking::select(DB::raw('trackings.*'))
                             ->join('sales as s', 'trackings.sale_id', '=', 's.id')
-                            ->where('s.project_id', 5496)
-                            ->get();
+                            ->where('s.project_id', 5496);
+
+        $trackings = $trackingsQuery->get();
+
+        $trackingsQuery->update([
+            'system_status_enum' => Tracking::SYSTEM_STATUS_CHECKED_MANUALLY
+        ]);
+
         $bar = $this->getOutput()->createProgressBar($trackings->count());
         $bar->start();
         foreach ($trackings as $tracking) {
-            $tracking->update([
-                'system_status_enum' => Tracking::SYSTEM_STATUS_CHECKED_MANUALLY
-            ]);
             event(new CheckSaleHasValidTrackingEvent($tracking->sale_id));
             $bar->advance();
         }
