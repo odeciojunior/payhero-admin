bddaa148ce4bfd0ba540d1e67d6a8ff00e06b8b8
Andre Silva <dresch666@gmail.com>
2022-01-26 11:23:06 -0300

    Add the class menu-active, when the user accesses the reports screen.

--- Modules/Reports/Resources/views/index.blade.php
+++ Modules/Reports/Resources/views/index.blade.php
@@ -11,17 +11,100 @@
     @endpush
 
     <div class="page mb-0">
-        <div style="display: none" class="page-header container">
+        <!-- <div style="display: none" class="page-header container">
             <div class="row">
                 <div class="col-12">
                     <h1 class="page-title">Relatório de Vendas</h1>
                     <span type="hidden" class="error-data"></span>
                 </div>
             </div>
+        </div> -->
+        <div style="display: none" class="page-header container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="row">
+                        <div class="col-lg-4 col-sm-12 col-xs-12">
+                            <h1 class="page-title new-title grey">
+                                <span class="box-title ico-finance">Vendas</span>
+                                Vendas
+                            </h1>
+                            <span type="hidden" class="error-data"></span>
+                        </div>
+                        <div class="col-lg-8 col-sm-12 col-xs-12">
+                            <div class="row">
+                                <div class="col-sm-6 col-m-3 col-lg-4">
+                                    <div>
+                                        <select id='select_projects' class="form-control input-pad">
+                                            {{-- JS carrega.. --}}
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-sm-6 col-m-3 col-lg-5">
+                                    <div class="row align-items-center form-icons box-select">
+                                        <i style="right:10%;" class="form-control-icon form-control-icon-right o-agenda-1 font-size-18"></i>
+                                        <input id="date-filter" type="text" name="daterange" class="input-pad text-center pr-30 font-size-14 ml-5" style="width: 92%" value="" readonly>
+                                    </div>
+                                </div>
+                                <div class="box-export col-lg-3">
+                                    <div class="inner-reports">
+                                        <a href="" class="grey lk-export">
+                                            <i class="o-download-cloud-1 mr-2"></i>
+                                            Exportar relatórios
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="line-reports row">
+                <div class="modal-reports">
+                    <header class="head-modal">
+                        <h3>Exportar relatórios</h3>
+                        <a href="" title="Fechar" class="reports-remove">
+                            <i class="icon wb-close"></i>
+                        </a>
+                    </header>
+                    <section class="modal-report-content">
+                        <h6>E-mail</h6>
+                        <form action="">
+                            <fieldset>
+                                <div class="form-group form-icons">
+                                    <i class="form-control-icon form-control-icon-right o-agenda-1 mt-5 font-size-18"></i>
+                                    <input 
+                                        type="text" 
+                                        placeholder="E-mail para enviar os relatórios"
+                                        class="modal-email"
+                                    >
+                                </div>
+
+                                <h6>Selecione quais:</h6>
+                                <div class="d-flex">
+                                    <div class=""><input type="checkbox"><label for="">Vendas</label></div>
+                                    <div class=""><input type="checkbox"><label for="">Financeiro</label></div>
+                                    <div class=""><input type="checkbox"><label for="">Marketing</label></div>
+                                </div>
+
+                                <h6>Escolha o formato que irá receber</h6>
+                                <div class="d-flex">
+                                    <div><input type="radio" name="format" value="csv"><label for="">.csv</label></div>
+                                    <div><input type="radio" name="format" value="xls"><label for="">.xls</label></div>
+                                </div>
+
+                                <div class="d-flex modal-buttons">
+                                    <input type="reset" value="Cancelar" class="reset">
+                                    <input type="submit" value="Enviar para o e-mail" class="send">
+                                </div>
+                            </fieldset>
+                        </form>
+                    </section>
+                </div>
+            </div>
         </div>
         <div id="project-not-empty" style="display: none">
-            <div id="reports-content" class="page-content container">
-                <div class="row align-items-center justify-content-between">
+            <div id="reports-content" class="page-content container box-reports">
+                <!-- <div class="row align-items-center justify-content-between" style="border: 1px solid red">
                     <div class="col-sm-6 col-m-3 col-lg-3">
                         <div class="">
                             <select id='select_projects' class="form-control input-pad">
@@ -35,8 +118,8 @@
                             <input id="date-filter" type="text" name="daterange" class="input-pad text-center pr-30 font-size-14 ml-5" style="width: 92%" value="" readonly>
                         </div>
                     </div>
-                </div>
-                <div class='container col-sm-12 mt-20 d-lg-block'>
+                </div> -->
+                <!-- <div class='container col-sm-12 mt-20 d-lg-block'>
                     <div class='row'>
                         <div class="col-md-3 col-sm-6 col-xs-12 card">
                             <div class="card-body">
@@ -88,7 +171,51 @@
                             </div>
                         </div>
                     </div>
-                </div>
+                </div> -->
+                <div class="row">
+					<div class="col-12 box-items-finance">
+                        <div class="row mb-20">
+                            <div class="fianance-items col-md-3 col-6 pr-5 pr-md-15">
+                                <div class="finance-card border blue mb-10">
+                                    <span class="title">N de transações</span>
+                                    <div class="d-flex">
+                                        <strong class="number">74.860</strong>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="fianance-items col-md-3 col-6 pr-5 pr-md-15">
+                                <div class="finance-card border orange mb-10">
+                                    <span class="title">Ticket Médio</span>
+                                    <div class="d-flex">
+                                        <span class="detail">R$</span>
+                                        <strong class="number">1.440,20</strong>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="fianance-items col-md-3 col-6 pr-5 pr-md-15">
+                                <div class="finance-card border green mb-10">
+                                    <span class="title">Comissão total</span>
+                                    <div class="d-flex">
+                                        <span class="detail">R$</span>
+                                        <strong class="number">1.457.080,55</strong>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="fianance-items col-md-3 col-6 pr-5 pr-md-15">
+                                <div class="finance-card border pink mb-10">
+                                    <span class="title">Total em Chargebacks</span>
+                                    <div class="d-flex">
+                                        <span class="detail">R$</span>
+                                        <strong class="number">24.120,50</strong>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+					</div>
+				</div>
                 <div class="row justify-content-between mt-20">
                     {{-- <div class="col-lg-12">
                         <div class="card shadow">
