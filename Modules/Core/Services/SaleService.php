ba39a5185725697e00236a37654da3c6ee631280
Edwin Medina <edwingarcia@cloudfox.net>
2022-01-27 17:12:48 -0400

    adjustment blocked balance report

--- Modules/Core/Services/SaleService.php
+++ Modules/Core/Services/SaleService.php
@@ -1067,6 +1067,7 @@ class SaleService
             ',',
             [
                 $transactionModel->present()->getStatusEnum('transfered'),
+                $transactionModel->present()->getStatusEnum('paid'),
                 $transactionModel->present()->getStatusEnum('pending'),
             ]
         );
