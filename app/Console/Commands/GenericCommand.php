cd9643f0806737620029bc5a0b4c68c386327e0f
Edwin Medina <edwingarcia@cloudfox.net>
2022-01-31 09:17:48 -0400

    fix asaas update available balance

--- app/Console/Commands/GenericCommand.php
+++ app/Console/Commands/GenericCommand.php
@@ -14,34 +14,6 @@ class GenericCommand extends Command
 
     public function handle()
     {
-        $transactions = Transaction::with('company')
-        ->where('created_at', '>', '2021-09-15')
-        ->where('release_date', '<=', Carbon::now()->format('Y-m-d'))
-        ->where('status_enum', Transaction::STATUS_PAID)
-        ->whereIn('gateway_id', [8])
-        ->whereNotNull('company_id')
-        ->where(function ($where) {
-            $where->where('tracking_required', false)
-                ->orWhereHas('sale', function ($query) {
-                    $query->where(function ($q) {
-                        $q->where('has_valid_tracking', true)
-                            ->orWhereNull('delivery_id');
-                    });
-                });
-        });
-
-        if (!empty($saleId)) {
-            $transactions->where('sale_id', $saleId);
-        }
-
-        foreach ($transactions->cursor() as $transaction) {
-            $company = $transaction->company;
-            $this->line('');
-            $this->line('asaas_balance '. $company->asaas_balance??0);
-            $this->line('transaction value '. intval($transaction->value));
-            $this->line('somando '. $company->asaas_balance .'+'. $transaction->value);
-            $this->line(intval($company->asaas_balance) + intval($transaction->value));
-            
-        }
+        
     }
 }
