effa40b7f9a9acbee065c3f93a4755dd1dd23394
Pedro Henrique Rodrigues <pedrohenrique@cloudfox.net>
2022-02-11 17:04:33 -0300

    fix: select null when no have shopify projects

--- Modules/Products/Resources/views/index.blade.php
+++ Modules/Products/Resources/views/index.blade.php
@@ -141,7 +141,7 @@
 </div>
 
 @push('scripts')
-<script src="{{ asset('/modules/products/js/index.js?v='.uniqid()) }}"></script>
+    <script src="{{ asset('/modules/products/js/index.js?v='.versionsFile()) }}"></script>
 @endpush
 
 @endsection
