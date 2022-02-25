b1a07b988cf500ab5fdbb03b035f9548bca0ff53
Henrique Brites <henriquebrites@live.com>
2022-02-25 15:03:25 -0400

    hotfix: removing unimportant files

--- Modules/Companies/Http/Controllers/CompaniesApiController.php
+++ Modules/Companies/Http/Controllers/CompaniesApiController.php
@@ -515,29 +515,6 @@ class CompaniesApiController extends Controller
         }
     }
 
-    public function checkBraspagCompany(): JsonResponse
-    {
-        try {
-            $user = auth()->user();
-            $companyModel = new Company();
-//            $columnName = FoxUtils::isProduction() ? 'braspag_merchant_id' : 'braspag_merchant_homolog_id';
-//            $hasMerchantId = $companyModel->whereNotNull($columnName)
-//                ->where('user_id', $user->account_owner_id)->exists();
-
-            return response()->json(
-                [
-//                    'has_merchant_id' => $hasMerchantId,
-//                    'env' => env("APP_ENV", "local"),
-                ],
-                Response::HTTP_UNAUTHORIZED
-            );
-        } catch (Exception $e) {
-            report($e);
-
-            return response()->json(['message' => 'Erro ao verificar empresas'], 400);
-        }
-    }
-
     public function checkStatementAvailable(): JsonResponse
     {
         try {
