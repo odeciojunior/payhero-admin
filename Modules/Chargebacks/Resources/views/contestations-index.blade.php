4287a277ec4b2c6238adca5b0bc0005e34838632
Jonas <jonas.feruttci@gmail.com>
2022-03-03 10:20:10 -0300

    adjust calendar on contestation

--- Modules/Chargebacks/Resources/views/contestations-index.blade.php
+++ Modules/Chargebacks/Resources/views/contestations-index.blade.php
@@ -271,8 +271,8 @@
         @push('scripts')
             <script src="{{ asset('/modules/chargebacks/js/contestations-index.js?v='. random_int(100, 10000)) }}"></script>
             <script src="{{ asset('modules/global/js-extra/moment.min.js') }}"></script>
-            {{-- <script src="{{ asset('modules/global/jquery-daterangepicker/daterangepicker.min.js?v=' . versionsFile()) }}"></script> --}}
-            <script src="{{ asset('modules/global/js/daterangepicker.min.js') }}"></script>
+            <script src="{{ asset('modules/global/jquery-daterangepicker/daterangepicker.min.js?v=' . versionsFile()) }}"></script>
+            {{-- <script src="{{ asset('modules/global/js/daterangepicker.min.js') }}"></script> --}}
             <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.10/js/select2.min.js"></script>
     @endpush
 
