574c32c93b984cce389eb4fb0db54feab54c6a8b
jeanvcastro <jean.castro1@outlook.com>
2022-03-14 18:13:10 -0300

    change boleto_due_date column

---
+++ database/migrations/2022_03_14_172946_alter_table_sales_change_boleto_due_date.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class AlterTableSalesChangeBoletoDueDate extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement("update sales set boleto_due_date = SUBSTRING(boleto_due_date, 1, 10)
+                                where boleto_due_date is not null
+                                and length(boleto_due_date) > 10");
+
+        DB::statement("alter table sales modify boleto_due_date timestamp default null null");
+
+        Schema::table('sales', function(Blueprint $table) {
+            $table->index('boleto_due_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
