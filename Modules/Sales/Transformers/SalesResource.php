a9caad18c902a143e8952eedf77cd1f6f90ed425
Paulo Queiroz <paulophp@gmail.com>
2022-03-15 10:50:31 -0300

    Discounts cards

--- Modules/Sales/Transformers/SalesResource.php
+++ Modules/Sales/Transformers/SalesResource.php
@@ -46,7 +46,10 @@ class SalesResource extends JsonResource
         if ($this->payment_method == 4 && $this->status <> Sale::STATUS_APPROVED) {
             $thankLabelText = 'Link página de Qrcode:';
         }
-
+        if($this->progressive_discount){
+            $total = (FoxUtils::formatMoney( (FoxUtils::onlyNumbers($this->details->total) - $this->progressive_discount) / 100) );
+            $this->details->total = $total;
+        }
         $data = [
             'id' => hashids_encode($this->id, 'sale_id'),
             'upsell' => hashids_encode($this->upsell_id, 'sale_id'),
