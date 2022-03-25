ac1b301c2ba0cbb2fb0f466086011e0a1be344d8
Paulo Queiroz <paulophp@gmail.com>
2022-03-25 10:51:50 -0300

    Avoid log permission errors

--- config/logging.php
+++ config/logging.php
@@ -46,7 +46,7 @@ return [
 
         'daily' => [
             'driver' => 'daily',
-            'path' => storage_path('logs/laravel.log'),
+            'path' => storage_path('logs/' . php_sapi_name() . '/laravel.log'),
             'level' => 'debug',
             'days' => 200,
             'permission' => 0664,
