1477c9191eb4115d75135cd85679c79b2513fbef
lucianno <luciannonascimento@cloudfox.net>
2022-04-11 16:09:30 -0300

    adjusting receipt layout

--- Modules/Sales/Resources/views/refund_receipt_vega.blade.php
+++ Modules/Sales/Resources/views/refund_receipt_vega.blade.php
@@ -5,23 +5,30 @@
     <style>
         body {margin: 0; font-family: sans-serif; font-size: 14pt;}
         table {margin: 0 auto;}
-        td{padding: 20px; text-align: left}
+        td{padding: 0 20px; text-align: left}
         div{margin: 10px 0}
 
         .title{font-size: 16px; font-weight: 700; letter-spacing: 0em; text-align: left; color: #002F66;}
 
-        .box{border-radius: 8px; padding: 24px; border: 1px solid #EBEBEB}
+        .box{border-radius: 8px; padding: 10px; border: 1px solid #EBEBEB}
+        .box table{width:100%}
+        .box-icon{width:32px}
         .box td{padding: 0; margin: 0;}
         .box td:nth-child(2){padding-left: 10px;}
-        .box .head{font-size: 14px; font-weight: 400; letter-spacing: 0em; text-align: left; color: #3D4456; margin:0; padding: 0}
-        .box .content{font-size: 12px; font-weight: 700; letter-spacing: 0em; text-align: left; color: #3D4456; margin:0; padding: 0}
+        .box .tit{font-size: 12px; font-weight: 400; letter-spacing: 0em; text-align: left; color: #3D4456; margin:0; padding: 0}
+        .box .val{font-size: 14px; font-weight: 700; letter-spacing: 0em; text-align: left; color: #3D4456; margin:0; padding: 0}
 
         .head{font-size: 15px;font-weight: 400; letter-spacing: 0em; text-align: left; color: #0050AF;}
+        .head-top{margin-top:26px}
         .content{font-size: 13px; font-weight: 400; letter-spacing: 0em; text-align: left; color: #3D4456; margin:0; padding: 0}
         .content .bold{font-weight: 700}
 
-        .bg-azul{background-color: #F4F6FB}
+        .bg-azul{background-color: #F4F6FB; height: 400px;}
         .bg-azul div{margin: 20px 0}
+        #linha1{margin-top: 80px; margin-bottom:20px}
+        #linha2{margin-bottom:20px; color: #0050AF; font-weight: 700}
+        #linha2 span{color:#000}
+        #linha3 {margin-bottom:20px}
         .icon{padding:0; margin:0; width: 32px}
 
     </style>
@@ -43,12 +50,12 @@
             <div class="box">
                 <table>
                     <tr>
-                        <td>
+                        <td class="box-icon">
                             <img src="build/global/img/estorno-money.svg" class="icon" alt="icon money">
                         </td>
                         <td>
-                            <div class="head">Valor estornado</div>
-                            <div class="content">{{\Modules\Core\Services\FoxUtils::formatMoney($sale->sale->original_total_paid_value/100)}}</div>
+                            <div class="tit">Valor estornado</div>
+                            <div class="val">{{\Modules\Core\Services\FoxUtils::formatMoney($sale->sale->original_total_paid_value/100)}}</div>
                         </td>
                     </tr>
                 </table>
@@ -58,12 +65,12 @@
             <div class="box">
                 <table>
                     <tr>
-                        <td>
+                        <td class="box-icon">
                             <img src="build/global/img/estorno-calendar.svg" class="icon" alt="icon calendar">
                         </td>
                         <td>
-                            <div class="head">Realizado em</div>
-                            <div class="content">{{\Illuminate\Support\Carbon::parse($sale->sale->date_refunded)->format('d/m/Y H:i')}}</div>
+                            <div class="tit">Realizado em</div>
+                            <div class="val">{{\Illuminate\Support\Carbon::parse($sale->sale->date_refunded)->format('d/m/Y')}} às {{\Illuminate\Support\Carbon::parse($sale->sale->date_refunded)->format('H:i')}}</div>
                         </td>
                     </tr>
                 </table>
@@ -80,35 +87,34 @@
                 <span class="bold">CNPJ:</span> {{\Modules\Core\Services\FoxUtils::getDocument($company->document)}}
             </div>
 
-            <div class="head">
+            <div class="head head-top">
                 Sua compra
             </div>
             <div class="content">
-                Código: #{{hashids_encode($sale->sale_id, 'sale_id')}}<br>&nbsp;<br>
+                Código: #{{hashids_encode($sale->sale_id, 'sale_id')}}<br>
                 <span class="bold">
                     {{count($plans_sales)}} Produto(s):<br>
                 </span>
                 @foreach ($plans_sales as $plan)
                     - {{\Illuminate\Support\Str::limit($plan->name, 20)}}
                 @endforeach
-
             </div>
 
-            <div class="head">
+            <div class="head head-top">
                 Cliente
             </div>
             <div class="content">
                 {{$sale_info->customer_name}}
             </div>
 
-            <div class="head">
+            <div class="head head-top">
                 Prazo de recebimento
             </div>
             <div class="content">
                 Até 30 dias após o recebimento desta confirmação (normalmente é efetuado dentro de 1 dia útil).
             </div>
 
-            <div class="head">
+            <div class="head head-top">
                 Recebimento
             </div>
             <div class="content">
@@ -117,25 +123,27 @@
                     Cartão de crédito
                     @elseif ($sale->sale->payment_method == 2)
                     Boleto
-                    @elseif ($sale->sale->payment_method == 3)
+                    @elseif ($sale->sale->payment_method == 4)
                     PIX
                     @endif
 
-                    @if ($sale->flag)
+                    @if ($sale->sale->payment_method == 1 && $sale->flag)
                         {{$sale->flag}}
                     @endif
                 </span>
                 <br />
-                Final {{$sale_info->last_four_digits}}
+                @if ($sale->sale->payment_method == 1 && $sale_info->last_four_digits)
+                    Final {{$sale_info->last_four_digits}}
+                @endif
             </div>
 
             </td>
         </td>
         <td>
-            <div class="box bg-azul" style="height: 400px;">
-                <div style="margin-top: 80px; margin-bottom:20px"><img src="build/global/img/estorno-shape.svg" alt="icon estorno"></div>
-                <div class="head" style="margin-bottom:20px">Sua compra foi estornada, <span style="color:#000">{{ $sale_info->firstname }}</span></div>
-                <div class="head" style="margin-bottom:20px">Esperamos que seu problema tenha sido solucionado e pedimos desculpas por qualquer transtorno.</div>
+            <div class="box bg-azul">
+                <div id="linha1"><img src="build/global/img/estorno-shape.svg" alt="icon estorno"></div>
+                <div id="linha2" class="head">Sua compra foi estornada, <span>{{ $sale_info->firstname }}</span></div>
+                <div id="linha3" class="head">Esperamos que seu problema tenha sido solucionado e pedimos desculpas por qualquer transtorno.</div>
                 <div class="head">Lembrando que você sempre pode voltar a conversar conosco através do <a href="https://sac.cloudfox.net/login">https://sac.cloudfox.net</a></div>
             </div>
         </td>
@@ -143,7 +151,7 @@
     <tr>
         <td colspan="2" style="text-align: right">
             <br>
-            <div style="font-size: 14px">Com a tecnologia <img src="build/global/img/vega-logo.png" alt="Vega Logo" style="vertical-align: middle;"></div>
+            <div style="font-size: 14px;">Com a tecnologia <img src="build/global/img/vega-logo.png" alt="Vega Logo" style="vertical-align: text-bottom;"></div>
         </td>
     </tr>
 </table>
