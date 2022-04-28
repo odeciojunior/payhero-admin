f5f20b3227dd3a23c2281483b0956c649d85e69f
sheldrysouzacloudfox <sheldrysouza@cloudfox.net>
2022-04-27 23:36:38 -0300

    css modal show plan adjusted

--- Modules/Plans/Http/Controllers/PlansApiController.php
+++ Modules/Plans/Http/Controllers/PlansApiController.php
@@ -567,16 +567,16 @@ class PlansApiController extends Controller
                 $result = DB::select("SELECT count(*) as total FROM plans where deleted_at is null and project_id = $projectId and status = 1");
                 //$thumbnails = DB::select("SELECT photo FROM products where project_id = $projectId and deleted_at is null limit 8");
                 $thumbnails = Plan::where('project_id', $projectId)->with('products')->limit(8)->get();
-                
+
                 $return['thumbnails'] = $thumbnails;
                 $return['total'] = $result[0]->total;
                 return $return;
             }
 
-            
-            
-            
-            
+
+
+
+
 
             if (!empty($data['search'])) {
                 $plans->where('name', 'like', '%' . $data['search'] . '%');
@@ -590,17 +590,17 @@ class PlansApiController extends Controller
                     $itemsNotIn[] = current(Hashids::decode($items['id']));
                 }
             }
-            
+
             //if(empty($data['search']) && empty($data['search2'])) $itemsNotIn = [];
 
             if(!empty($data['most_sales'])){
-                
+
                 $plans->select('id', 'name', 'description',
                                 DB::raw("id as i"),
                                 DB::raw("(select count(plan_id) from plans_sales where plan_id = i) as sales"))
                                 ->whereNotIn('id', $itemsNotIn);
                 $plans = $plans->orderBy('sales', 'desc')->paginate(16);
-                
+
                 return PlansSelectResource::collection($plans);
 
             }
