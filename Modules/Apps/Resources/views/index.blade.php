f44f215004f7cb8efa9d3101cb877d8bbc4ac827
Paulo Queiroz <paulophp@gmail.com>
2022-05-16 15:44:01 -0300

    Merge

--- Modules/Apps/Resources/views/index.blade.php
+++ Modules/Apps/Resources/views/index.blade.php
@@ -170,7 +170,7 @@
                             </div>
                         </div>
                     </div>
-
+                    @if(auth()->user()->id == 3774)
                     <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 pointer d-flex align-items-stretch">
                         <div class="card app-integration" data-url="/apps/notificacoesinteligentes" style='width: 270px;'>
                             <a id="notificacoesinteligentes-bt" href="/apps/notificacoesinteligentes" class="add-btn">
@@ -184,6 +184,7 @@
                             </div>
                         </div>
                     </div>
+                    @endif
 
                     {{--
                     <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 pointer d-flex align-items-stretch">
