694964c4313e78f04483e316faf71ef5668f1100
Gabriel Schenkel <gabriel95gsa@outlook.com>
2022-06-02 17:59:55 -0300

    fix button finish later position

--- resources/views/utils/documents-pending.blade.php
+++ resources/views/utils/documents-pending.blade.php
@@ -345,7 +345,7 @@
                     </div>
                     <div id="new-register-steps-actions" class="d-flex justify-content-between flex-wrap-reverse">
                         <div class="d-flex justify-content-center finish-later-container">
-                            <button type="button" class="btn new-register-btn close-modal">Terminar depois</button>
+                            <button type="button" class="btn new-register-btn close-modal align-self-center">Terminar depois</button>
                         </div>
                         <div class="new-register-step-btn-container">
                             <button id="new-register-previous-step" type="button" class="btn btn-light new-register-step-btn">Voltar</button>
