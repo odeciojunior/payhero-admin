f1b750e1f7feb1ee55a96aaca3933ebabe0a0729
Carlos Lima <carloslima@cloudfox.net>
2022-06-03 17:46:01 -0300

    changed google tag manager ID

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -43,27 +43,9 @@
                     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-TDM6SV5');</script>
+            })(window,document,'script','dataLayer','GTM-KDD46QX');</script>
         <!-- End Google Tag Manager -->
         <script src="{{ mix('build/layouts/master/master.min.js') }}"></script>
-
-        <!-- Meta Pixel Code -->
-        <script>
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                n.queue=[];t=b.createElement(e);t.async=!0;
-                t.src=v;s=b.getElementsByTagName(e)[0];
-                s.parentNode.insertBefore(t,s)}(window, document,'script',
-                'https://connect.facebook.net/en_US/fbevents.js%27);
-            fbq('init', '568190524313935');
-            fbq('track', 'PageView');
-        </script>
-        <noscript><img height="1" width="1" style="display:none"
-                    src="https://www.facebook.com/tr?id=568190524313935&ev=PageView&noscript=1"
-        /></noscript>
-
         <script>
             Breakpoints();
         </script>
