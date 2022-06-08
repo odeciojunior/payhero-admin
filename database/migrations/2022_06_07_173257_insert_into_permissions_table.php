8c32086c7e09fbf87383195602ca2f265eadbeb3
Mateus Rodrigues de Souza <mateussouza@cloudfox.net>
2022-06-08 12:31:25 -0300

    assigning permission to Manager users

--- database/migrations/2022_06_07_173257_insert_into_permissions_table.php
+++ database/migrations/2022_06_07_173257_insert_into_permissions_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Modules\Core\Entities\User;
 use Spatie\Permission\Models\Permission;
 
 class InsertIntoPermissionsTable extends Migration
@@ -14,11 +15,23 @@ class InsertIntoPermissionsTable extends Migration
      */
     public function up()
     {
+        /**
+         * Insert the permission into the database
+         */
         Permission::create([
             'name' => 'login_sirius_by_manager',
             'title' => 'Acessar o Sirius pelo Manager',
             'guard' => 'web'
         ]);
+
+        /**
+         * Assign permission to all Manager users
+         */
+        $managerUsers = User::where('email', 'like', '%@cloudfox.net')->get();
+
+        foreach($managerUsers as $user) {
+            $user->givePermissionTo('login_sirius_by_manager');
+        }
     }
 
     /**
