634d9b2738ebca44acb6ab0e4fe85ecfd9a7221b
Andre Silva <dresch666@gmail.com>
2022-06-17 15:50:24 -0300

    Adjustments fields in pending template.

--- Modules/Reports/Resources/views/pending.blade.php
+++ Modules/Reports/Resources/views/pending.blade.php
@@ -90,27 +90,19 @@
                         <form id='filter_form'>
                             <div id="" class="card shadow p-20">
                                 <div class="row align-items-baseline">
-                                    <!-- <div class="col-sm-6 col-md-6 col-xl-3 col-12">
+                                    <div class="col-sm-6 col-md-6 col-xl-3 col-12">
                                         <label for="company">Empresa</label>
                                         <select name='select_company' id="company" class="sirius-select">
                                             <option value="0">Todas as empresas</option>
                                         </select>
-                                    </div> -->
+                                    </div>
                                     <div class="col-sm-6 col-md-6 col-xl-3 col-12">
-                                        <label for="project">Projeto</label>
+                                        <label for="project">Lojas</label>
                                         <select name='select_project' id="project" class="sirius-select">
-                                            <option value="0">Todos os projetos</option>
+                                            <option value="0">Todas as lojas</option>
                                         </select>
                                     </div>
-                                    <div class="col-sm-8 col-md-6 col-xl-3">
-                                            <label for="payment_method">Forma de pagamento</label>
-                                            <select name='select_payment_method' id="payment_method" class="sirius-select">
-                                                <option value="">Boleto e cartão de crédito</option>
-                                                <option value="1">Cartão de crédito</option>
-                                                <option value="2">Boleto</option>
-                                                <option value="4">PIX</option>
-                                            </select>
-                                        </div>
+                                    
                                     <div class="col-sm-8 col-md-6 col-xl-3">
                                         <label for="client">Nome do cliente</label>
                                         <input name='client' id="client" class="input-pad" placeholder="Digite o nome">
@@ -128,7 +120,7 @@
                                 </div>
                                 <div class="collapse pt-20" id="bt_collapse">
                                     <div class="row">
-                                        <!-- <div class="col-sm-6 col-md-3">
+                                        <div class="col-sm-6 col-md-3">
                                             <label for="payment_method">Forma de pagamento</label>
                                             <select name='select_payment_method' id="payment_method" class="sirius-select">
                                                 <option value="">Boleto e cartão de crédito</option>
@@ -136,23 +128,20 @@
                                                 <option value="2">Boleto</option>
                                                 <option value="4">PIX</option>
                                             </select>
-                                        </div> -->
+                                        </div>
                                         <div class="col-sm-6 col-md-3">
                                             <label for="sale_code">Transação</label>
                                             <input type="text" id="sale_code" placeholder="Digite ID da transação" class="input-pad">
                                         </div>
                                         <div class="col-sm-6 col-md-3">
-                                            <label for="acquirer">Adquirente</label>
-                                            <select name='select_acquirer' id="acquirer" class="sirius-select">
-                                                <option value="0">Todas os adquirentes</option>
-                                            </select>
-                                        </div>
-                                        <div class="col-sm-6 col-md-3">
                                             <label for="date_type">Data</label>
-                                            <!-- <select name='date_type' id="date_type" class="sirius-select">
+                                            <select name='date_type' id="date_type" class="sirius-select">
                                                 <option value="start_date">Data do pedido</option>
                                                 <option value="end_date">Data do pagamento</option>
-                                            </select> -->
+                                            </select>
+                                        </div>
+                                        <div class="col-sm-6 col-md-3 form-icons">
+                                            <label for="date_range">‏‏‎ ‎</label>
                                             <div class="col-12 mb-10 date-report">
                                                 <div class="row align-items-center form-icons box-select">
                                                     <input id="date-filter" type="text" name="daterange" class="font-size-14" value="" readonly>
@@ -166,13 +155,7 @@
                                             <input name='date_range' id="date_range" class="input-pad"
                                             placeholder="Clique para editar..." readonly>
                                         </div> -->
-                                        <div class='col-md-3 pt-20 col-sm-6 d-flex align-items-center'>
-                                            <label class="switch mr-2">
-                                                <input type="checkbox" id='is-security-reserve' name="cashback" class='check' value='0'>
-                                                <span class="slider round"></span>
-                                            </label>
-                                            <span class="switch-text pl-5"> Reserva de Segurança </span>
-                                        </div>
+                                        
                                     </div>
                                     <div class="row">
                                         <div class="col-sm-6 col-md-3 pt-20" style="display:none">
@@ -187,19 +170,19 @@
                                                 </select>
                                             </div>
                                         </div>
-                                        <!-- <div class="col-sm-6 col-md-6 col-xl-3 col-12  pt-20">
+                                        <div class="col-sm-6 col-md-6 col-xl-3 col-12  pt-20">
                                             <label for="acquirer">Adquirente</label>
                                             <select name='select_acquirer' id="acquirer" class="sirius-select">
                                                 <option value="0">Todas os adquirentes</option>
                                             </select>
-                                        </div> -->
-                                        <!-- <div class='col-md-3 pt-30 d-flex align-items-center'>
+                                        </div>
+                                        <div class='col-md-3 pt-30 d-flex align-items-center'>
                                             <label class="switch mr-2">
                                                 <input type="checkbox" id='is-security-reserve' name="cashback" class='check' value='0'>
                                                 <span class="slider round"></span>
                                             </label>
                                             <span class="switch-text w-100"> Reserva de Segurança </span>
-                                        </div> -->
+                                        </div>
                                     </div>
                                 </div>
                                 <div class="row" style="height: 30px">
