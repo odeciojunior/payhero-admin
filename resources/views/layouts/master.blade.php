9f7ebed379c9134940f013ecd9cb33b8741248b7
Gabriel Schenkel <gabriel95gsa@outlook.com>
2022-06-20 09:56:42 -0300

    fix navbar new register link

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -81,20 +81,7 @@
         <input type="hidden" id="accountStatus">
 
         @if(!auth()->user()->account_is_approved)
-            <div class="new-register-page-open-modal-container">
-                <div class="page-header container">
-                    <div class="row align-items-center justify-content-between">
-                        <div class="col-lg-6">
-                            <div class="new-register-page-open-modal">
-                                <!--
-                                <span style="color: #5B5B5B;">Você está logado em uma conta demonstrativa.</span>
-                                -->
-                                <span class="new-register-open-modal-btn">Clique aqui para começar</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            @include('utils.new-register-link');
         @endif
 
         @yield('content')
---
+++ resources/views/utils/new-register-link.blade.php
@@ -0,0 +1,14 @@
+<div class="new-register-page-open-modal-container">
+    <div class="page-header container">
+        <div class="row align-items-center justify-content-between">
+            <div class="col-lg-6">
+                <div class="new-register-page-open-modal">
+                    <!--
+                    <span style="color: #5B5B5B;">Você está logado em uma conta demonstrativa.</span>
+                    -->
+                    <span class="new-register-open-modal-btn">Clique aqui para começar</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
