4cdedd11ff48057efc3abe280667cb0a4961ef9e
jeanvcastro <jean.castro1@outlook.com>
2022-06-24 16:13:48 -0300

    add reportana expired and tracking events

---
+++ database/migrations/2022_06_24_125214_add_tracking_and_expired_columns_to_reportana_integrations.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddTrackingAndExpiredColumnsToReportanaIntegrations extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('reportana_integrations', function (Blueprint $table) {
+            $table->boolean('billet_expired')
+                ->default(true)
+                ->after('billet_paid');
+            $table->boolean('pix_expired')
+                ->default(true)
+                ->after('pix_paid');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('reportana_integrations', function (Blueprint $table) {
+            $table->dropColumn(['billet_expired', 'pix_expired']);
+        });
+    }
+}
