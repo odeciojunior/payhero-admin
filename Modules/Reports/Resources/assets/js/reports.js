59943b9a61396dff9083c2d4f6915fbe82328140
Andre Silva <dresch666@gmail.com>
2022-07-13 15:28:16 -0300

    Changed access regions graph.

--- Modules/Reports/Resources/assets/js/reports.js
+++ Modules/Reports/Resources/assets/js/reports.js
@@ -809,13 +809,14 @@ function getRegions() {
                 });
 
                 for(let i = 0; i < regionArr.length; i++) {
-                    conversionArr.push(regionArr[i].conversion_conversion);
+                    conversionArr.push(regionArr[i].conversion);
                     accessArr.push(regionArr[i].access);
                     statesArr.push(regionArr[i].region);
 
                     $(".conversion-colors").append(`<li>${regionArr[i].percentage_conversion}%</li>`);
                 }
 
+                accessArr = new Array(statesArr.length).fill(100);
                 $(".new-graph-regions").height($('.conversion-colors').height());
                 $(".states").height($('.conversion-colors').height());
                 $('.new-graph-regions').html('<canvas id="regionsChart"></canvas>');
@@ -1458,6 +1459,7 @@ function graphComission(series, labels) {
 }
 
 function graphRegions(labels, conversion, access) {
+    console.log(access);
     Chart.register(ChartDataLabels);
     const ctx = document.getElementById('regionsChart').getContext('2d');
     const myChart = new Chart(ctx, {
