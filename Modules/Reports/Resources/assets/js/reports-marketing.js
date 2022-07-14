e6beb053a832940fbf9f8d6db61adf8e452b2f1d
sheldrysouzacloudfox <sheldrysouza@cloudfox.net>
2022-07-14 18:01:30 -0300

    fix: block filter in load requests

--- Modules/Reports/Resources/assets/js/reports-marketing.js
+++ Modules/Reports/Resources/assets/js/reports-marketing.js
@@ -31,21 +31,7 @@ function changeMap() {
         $('#brazil-map-filter').addClass('disabled');
         $('#brazil-map-filter').find('input').attr('disabled', 'disabled');
 
-        Promise.all([
-            loadBrazilMap()
-        ])
-        .then(() => {
-            $('.sirius-select-container').removeClass('disabled');
-            $('input[name="daterange"]').removeAttr('disabled', 'disabled');
-            $('#brazil-map-filter').removeClass('disabled');
-            $('#brazil-map-filter').find('input').removeAttr('disabled', 'disabled');
-        })
-        .catch(() => {
-            $('.sirius-select-container').removeClass('disabled');
-            $('input[name="daterange"]').removeAttr('disabled', 'disabled');
-            $('#brazil-map-filter').removeClass('disabled');
-            $('#brazil-map-filter').find('input').removeAttr('disabled', 'disabled');
-        });
+        loadBrazilMap();
     });
 }
 
@@ -86,6 +72,11 @@ function loadOrigins(link = null) {
             Accept: "application/json",
         },
         error: function error(response) {
+            $('.sirius-select-container').removeClass('disabled');
+            $('input[name="daterange"]').removeAttr('disabled');
+            $('#brazil-map-filter').removeClass('disabled');
+            $('#brazil-map-filter').find('input').removeAttr('disabled');
+
             $("#block-origins").html(td);
             $('#card-origin').css('height', 'auto');
 
@@ -93,6 +84,11 @@ function loadOrigins(link = null) {
         },
         success: function success(response) {
             $('#card-origin').css('height', 'auto');
+
+            $('.sirius-select-container').removeClass('disabled');
+            $('input[name="daterange"]').removeAttr('disabled');
+            $('#brazil-map-filter').removeClass('disabled');
+            $('#brazil-map-filter').find('input').removeAttr('disabled');
 
             if (response.data.length == 0) {
                 $('.table-vendas').height('100%');
@@ -456,29 +452,16 @@ function changeCompany() {
 function changeOrigin() {
     $("#origin").on("change", function () {
         if (origin !== $(this).val()) {
+            origin = $(this).val();
+
+            $("#origin").val($(this).val());
+
             $('.sirius-select-container').addClass('disabled');
             $('input[name="daterange"]').attr('disabled', 'disabled');
             $('#brazil-map-filter').addClass('disabled');
             $('#brazil-map-filter').find('input').attr('disabled', 'disabled');
 
-            origin = $(this).val();
-
-            $("#origin").val($(this).val());
-            Promise.all([
-                loadOrigins()
-            ])
-            .then(() => {
-                $('.sirius-select-container').removeClass('disabled');
-                $('input[name="daterange"]').removeAttr('disabled');
-                $('#brazil-map-filter').removeClass('disabled');
-                $('#brazil-map-filter').find('input').removeAttr('disabled');
-            })
-            .catch(() => {
-                $('.sirius-select-container').removeClass('disabled');
-                $('input[name="daterange"]').removeAttr('disabled');
-                $('#brazil-map-filter').removeClass('disabled');
-                $('#brazil-map-filter').find('input').removeAttr('disabled');
-            });
+            loadOrigins();
         }
     });
 }
@@ -530,7 +513,7 @@ function loadDevices() {
                                 <div class="box-payment-option option">
                                     <div class="col-payment">
                                         <div class="box-payment center">
-                                            <span class="silver">${mobile.percentage_total}</span>
+                                            <span class="silver">${mobile.percentage_approved}</span>
                                         </div>
                                     </div>
                                     <div class="col-payment">
@@ -556,7 +539,7 @@ function loadDevices() {
                                 <div class="box-payment-option option">
                                     <div class="col-payment">
                                         <div class="box-payment center">
-                                            <span class="silver">${desktop.percentage_total}</span>
+                                            <span class="silver">${desktop.percentage_approved}</span>
                                         </div>
                                     </div>
                                     <div class="col-payment">
@@ -576,7 +559,7 @@ function loadDevices() {
                         <div>
                             <span class="ico-coin mkt">
                                 <svg width="12" height="20" viewBox="0 0 12 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M4.5 15.7143C4.08579 15.7143 3.75 16.0341 3.75 16.4286C3.75 16.8231 4.08579 17.1429 4.5 17.1429H7.5C7.91421 17.1429 8.25 16.8231 8.25 16.4286C8.25 16.0341 7.91421 15.7143 7.5 15.7143H4.5ZM2.625 0C1.17525 0 0 1.11929 0 2.5V17.5C0 18.8807 1.17525 20 2.625 20H9.375C10.8247 20 12 18.8807 12 17.5V2.5C12 1.11929 10.8247 0 9.375 0H2.625ZM1.5 2.5C1.5 1.90827 2.00368 1.42857 2.625 1.42857H9.375C9.99632 1.42857 10.5 1.90827 10.5 2.5V17.5C10.5 18.0917 9.99632 18.5714 9.375 18.5714H2.625C2.00368 18.5714 1.5 18.0917 1.5 17.5V2.5Z" fill="#E8EAEB"/>
+                                    <path d="M4.5 15.7143C4.08579 15.7143 3.75 16.0341 3.75 16.4286C3.75 16.8231 4.08579 17.1429 4.5 17.1429H7.5C7.91421 17.1429 8.25 16.8231 8.25 16.4286C8.25 16.0341 7.91421 15.7143 7.5 15.7143H4.5ZM2.625 0C1.17525 0 0 1.11929 0 2.5V17.5C0 18.8807 1.17525 20 2.625 20H9.375C10.8247 20 12 18.8807 12 17.5V2.5C12 1.11929 10.8247 0 9.375 0H2.625ZM1.5 2.5C1.5 1.90827 2.00368 1.42857 2.625 1.42857H9.375C9.99632 1.42857 10.5 1.90827 10.5 2.5V17.5C10.5 18.0917 9.99632 18.5714 9.375 18.5714H2.625C2.00368 18.5714 1.5 18.0917 1.5 17.5V2.5Z" fill="#2E85EC"/>
                                 </svg>
                             </span>
                         </div>
@@ -591,7 +574,7 @@ function loadDevices() {
                         <div>
                             <span class="ico-coin mkt">
                                 <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M0 2.83333C0 1.26853 1.26853 0 2.83333 0H14.1667C15.7315 0 17 1.26853 17 2.83333V11.3333C17 12.8981 15.7315 14.1667 14.1667 14.1667H11.3333V14.875C11.3333 15.2662 11.6505 15.5833 12.0417 15.5833H12.75C13.1412 15.5833 13.4583 15.9005 13.4583 16.2917C13.4583 16.6829 13.1412 17 12.75 17H4.25C3.8588 17 3.54167 16.6829 3.54167 16.2917C3.54167 15.9005 3.8588 15.5833 4.25 15.5833H4.95833C5.34953 15.5833 5.66667 15.2662 5.66667 14.875V14.1667H2.83333C1.26853 14.1667 0 12.8981 0 11.3333V2.83333ZM10.0376 15.5833C9.95928 15.3618 9.91667 15.1234 9.91667 14.875V14.1667H7.08333V14.875C7.08333 15.1234 7.04072 15.3618 6.96242 15.5833H10.0376ZM14.1667 12.75C14.9491 12.75 15.5833 12.1157 15.5833 11.3333H1.41667C1.41667 12.1157 2.05093 12.75 2.83333 12.75H14.1667ZM15.5833 2.83333C15.5833 2.05093 14.9491 1.41667 14.1667 1.41667H2.83333C2.05093 1.41667 1.41667 2.05093 1.41667 2.83333V9.91667H15.5833V2.83333Z" fill="#E8EAEB" />
+                                    <path d="M0 2.83333C0 1.26853 1.26853 0 2.83333 0H14.1667C15.7315 0 17 1.26853 17 2.83333V11.3333C17 12.8981 15.7315 14.1667 14.1667 14.1667H11.3333V14.875C11.3333 15.2662 11.6505 15.5833 12.0417 15.5833H12.75C13.1412 15.5833 13.4583 15.9005 13.4583 16.2917C13.4583 16.6829 13.1412 17 12.75 17H4.25C3.8588 17 3.54167 16.6829 3.54167 16.2917C3.54167 15.9005 3.8588 15.5833 4.25 15.5833H4.95833C5.34953 15.5833 5.66667 15.2662 5.66667 14.875V14.1667H2.83333C1.26853 14.1667 0 12.8981 0 11.3333V2.83333ZM10.0376 15.5833C9.95928 15.3618 9.91667 15.1234 9.91667 14.875V14.1667H7.08333V14.875C7.08333 15.1234 7.04072 15.3618 6.96242 15.5833H10.0376ZM14.1667 12.75C14.9491 12.75 15.5833 12.1157 15.5833 11.3333H1.41667C1.41667 12.1157 2.05093 12.75 2.83333 12.75H14.1667ZM15.5833 2.83333C15.5833 2.05093 14.9491 1.41667 14.1667 1.41667H2.83333C2.05093 1.41667 1.41667 2.05093 1.41667 2.83333V9.91667H15.5833V2.83333Z" fill="#2E85EC" />
                                 </svg>
                             </span>
                         </div>
@@ -606,20 +589,22 @@ function loadDevices() {
                         <div>
                             <span class="ico-coin mkt">
                                 <svg width="12" height="20" viewBox="0 0 12 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M4.5 15.7143C4.08579 15.7143 3.75 16.0341 3.75 16.4286C3.75 16.8231 4.08579 17.1429 4.5 17.1429H7.5C7.91421 17.1429 8.25 16.8231 8.25 16.4286C8.25 16.0341 7.91421 15.7143 7.5 15.7143H4.5ZM2.625 0C1.17525 0 0 1.11929 0 2.5V17.5C0 18.8807 1.17525 20 2.625 20H9.375C10.8247 20 12 18.8807 12 17.5V2.5C12 1.11929 10.8247 0 9.375 0H2.625ZM1.5 2.5C1.5 1.90827 2.00368 1.42857 2.625 1.42857H9.375C9.99632 1.42857 10.5 1.90827 10.5 2.5V17.5C10.5 18.0917 9.99632 18.5714 9.375 18.5714H2.625C2.00368 18.5714 1.5 18.0917 1.5 17.5V2.5Z" fill="#E8EAEB"/>
+                                    <path d="M4.5 15.7143C4.08579 15.7143 3.75 16.0341 3.75 16.4286C3.75 16.8231 4.08579 17.1429 4.5 17.1429H7.5C7.91421 17.1429 8.25 16.8231 8.25 16.4286C8.25 16.0341 7.91421 15.7143 7.5 15.7143H4.5ZM2.625 0C1.17525 0 0 1.11929 0 2.5V17.5C0 18.8807 1.17525 20 2.625 20H9.375C10.8247 20 12 18.8807 12 17.5V2.5C12 1.11929 10.8247 0 9.375 0H2.625ZM1.5 2.5C1.5 1.90827 2.00368 1.42857 2.625 1.42857H9.375C9.99632 1.42857 10.5 1.90827 10.5 2.5V17.5C10.5 18.0917 9.99632 18.5714 9.375 18.5714H2.625C2.00368 18.5714 1.5 18.0917 1.5 17.5V2.5Z" fill="#2E85EC"/>
                                 </svg>
                             </span>
                         </div>
                         <div>
                             <span class="mkt-msg-conversion">
-                                São iguais ${desktop.percentage_approved}
+                                São iguais ${desktop.percentage_conversion}
                             </span>
                         </div>
                         `;
                 }
-            }
 
-            if (response.data == null) {
+                $('#card-info-conversion').show();
+            } else {
+                deviceInfoBlock = '';
+
                 $('#card-info-conversion').hide();
             }
 
@@ -933,6 +918,11 @@ function loadBrazilMap() {
             Accept: "application/json",
         },
         error: function error(response) {
+            $('.sirius-select-container').removeClass('disabled');
+            $('input[name="daterange"]').removeAttr('disabled', 'disabled');
+            $('#brazil-map-filter').removeClass('disabled');
+            $('#brazil-map-filter').find('input').removeAttr('disabled', 'disabled');
+
             $("#list-states").html(noData);
 
             errorAjaxResponse(response);
@@ -940,6 +930,11 @@ function loadBrazilMap() {
         success: function success(response) {
             $(".state").removeClass('skeleton');
             $("#list-states").html('');
+
+            $('.sirius-select-container').removeClass('disabled');
+            $('input[name="daterange"]').removeAttr('disabled', 'disabled');
+            $('#brazil-map-filter').removeClass('disabled');
+            $('#brazil-map-filter').find('input').removeAttr('disabled', 'disabled');
 
             if(response.data.length == 0) {
                 let noData = `
