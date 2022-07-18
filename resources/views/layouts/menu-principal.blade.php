c8e6c59f1da3f8fdeb9649235f8d044091fce85c
lucianno <luciannonascimento@cloudfox.net>
2022-07-18 10:11:34 -0300

    fixed margins of anonounces alert at topbar

--- resources/views/layouts/menu-principal.blade.php
+++ resources/views/layouts/menu-principal.blade.php
@@ -91,8 +91,8 @@
                 </div>
 
                 <!-- MODAL DE NOVIDADE ANNOUCEKIT -->
-                <div id="my-iframe" class="announcekit-widget d-none d-sm-flex align-items-center justify-content-center">
-                    <span class="nav-link navbar-avatar" data-toggle="dropdown" title="Novidades" id='notification' aria-expanded="false" data-animation="scale-up" role="button" style='padding-right: 0rem'><!--  style='cursor:pointer; padding-right: .0rem' -->
+                <div id="my-iframe" class="announcekit-widget d-none d-sm-flex align-items-center justify-content-center" style="padding-right: 10px">
+                    <span class="nav-link navbar-avatar" data-toggle="dropdown" title="Novidades" id='notification' aria-expanded="false" data-animation="scale-up" role="button" style='padding-right: 10px'><!--  style='cursor:pointer; padding-right: .0rem' -->
                         <img class="svg-menu" src="{{ mix('build/global/img/svg/notificacao.svg') }}" alt="Novidades">
                     </span>
                 </div>
@@ -104,7 +104,7 @@
                     <li class="nav-item dropdown">
 
                         <!-- FOTO DO USUARIO -->
-                        <a class="nav-link navbar-avatar pr-10 pr-sm-25" data-toggle="dropdown" href="#" aria-expanded="false" data-animation="scale-up" role="button" style='padding-left: 1.8rem'>
+                        <a class="nav-link navbar-avatar pr-10 pr-sm-25" data-toggle="dropdown" href="#" aria-expanded="false" data-animation="scale-up" role="button" style="margin-left: -6px;">
                             <span class="avatar avatar-online">
                                 <img class='img-user-menu-principal' src="{!! \Auth::user()->photo ? \Auth::user()->photo : 'https://cloudfox-documents.s3.amazonaws.com/cloudfox/defaults/user-default.png' !!}" onerror="this.onerror=null; this.src='https://cloudfox-documents.s3.amazonaws.com/cloudfox/defaults/user-default.png'" alt="">
                                 <i></i>
