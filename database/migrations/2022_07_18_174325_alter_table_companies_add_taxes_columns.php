262264160b9b3bd1daff96480daf5d7b158f6fda
Mateus Rodrigues de Souza <mateussouza@cloudfox.net>
2022-07-19 10:01:05 -0300

    adding fields in the Company template

---
+++ database/migrations/2022_07_18_174325_alter_table_companies_add_taxes_columns.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterTableCompaniesAddTaxesColumns extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('companies',function(Blueprint $table){
+            $table->string('boleto_rule')->nullable()->after('gateway_tax');
+            $table->string('boleto_tax')->nullable()->after('gateway_tax');
+            $table->string('pix_rule')->nullable()->after('gateway_tax');
+            $table->string('pix_tax')->nullable()->after('gateway_tax');
+            $table->string('credit_card_rule')->nullable()->after('gateway_tax');
+            $table->string('credit_card_tax')->nullable()->after('gateway_tax');
+            $table->boolean('tax_default')->default(true)->after('gateway_tax');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('companies',function(Blueprint $table){
+            $table->dropColumn(['boleto_rule']);
+            $table->dropColumn(['boleto_tax']);
+            $table->dropColumn(['pix_rule']);
+            $table->dropColumn(['pix_tax']);
+            $table->dropColumn(['credit_card_rule']);
+            $table->dropColumn(['credit_card_tax']);
+            $table->dropColumn(['tax_default']);
+        });
+    }
+}
