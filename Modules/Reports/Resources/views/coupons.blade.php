50191a7ea838d2538389c2d33bb33c25235eb20f
sheldrysouzacloudfox <sheldrysouza@cloudfox.net>
2022-07-26 12:39:04 -0300

    fix: view subpages reports to collaborators

--- Modules/Reports/Resources/views/coupons.blade.php
+++ Modules/Reports/Resources/views/coupons.blade.php
@@ -9,6 +9,7 @@
 
         <div class="page">
             <div style="display: none" class="page-header container inner-header">
+                @can('report_sales')
                 <header class="top-system">
                     <a href="{!! route('reports.marketing') !!}" class="back">
                         <svg style="margin-right: 10px;" width="27" height="16" viewBox="0 0 27 16" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -17,6 +18,7 @@
                         Voltar para Marketing
                     </a>
                 </header>
+                @endcan
 
                 <div class="row align-items-center justify-content-between top-inner-reports">
                     <div class="col-8">
