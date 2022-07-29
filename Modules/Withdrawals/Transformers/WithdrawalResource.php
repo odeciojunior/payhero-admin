124433c47f96f764f602b1f6697b9dd4cc0e4dad
Mateus Rodrigues de Souza <mateussouza@cloudfox.net>
2022-07-29 17:32:13 -0300

    including fields in withdrawals resource

--- Modules/Withdrawals/Transformers/WithdrawalResource.php
+++ Modules/Withdrawals/Transformers/WithdrawalResource.php
@@ -22,7 +22,7 @@ class WithdrawalResource extends JsonResource
             $realeaseTime = $this->release_date->format('H:i');
         }
 
-        return [
+        $data = [
             'id' => Hashids::encode($this->id),
             'account_information_bank' => $bankName,
             'account_information' => $accountInformation,
@@ -39,30 +39,36 @@ class WithdrawalResource extends JsonResource
             'tax_value' => $this->tax,
             'debt_pending_value' => 'R$' . number_format(intval($this->debt_pending_value) / 100, 2, ',', '.')
         ];
+
+        if (request('is_mobile')) {
+            $data['request_datetime'] = $this->created_at->format('Y-m-d H:i:s');
+            $data['release_datetime'] = $this->release_date->format('Y-m-d H:i:s');
+        }
+
+        return $data;
     }
 
     private function accountInformation(): string
     {
         $bankAccount =  $this->company->getDefaultBankAccount();
-        if(!empty($bankAccount)){
-            switch($bankAccount->transfer_type){
+        if (!empty($bankAccount)) {
+            switch ($bankAccount->transfer_type) {
                 case 'PIX':
-                    return $bankAccount->transfer_type.': '.$bankAccount->key_pix;                    
+                    return $bankAccount->transfer_type . ': ' . $bankAccount->key_pix;
                 case 'TED':
                     $agency = "Ag: $bankAccount->agency";
                     if ($bankAccount->agency_digit) {
                         $agency .= "-$bankAccount->agency_digit";
                     }
-            
+
                     $account = "Conta: $bankAccount->account";
                     if ($bankAccount->account_digit) {
                         $account .= "-$bankAccount->account_digit";
                     }
-            
+
                     return "$agency - $account";
-            }            
+            }
         }
         return '';
     }
 }
-
