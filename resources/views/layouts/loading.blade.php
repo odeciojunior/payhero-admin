9c467e4d22fbf69de24891410b8f6ddf0dd99c6c
lucianno <luciannonascimento@cloudfox.net>
2022-07-29 12:59:07 -0300

    fix sales hashid bug and loading screen logo position

--- resources/views/layouts/loading.blade.php
+++ resources/views/layouts/loading.blade.php
@@ -1,5 +1,5 @@
 <div id='loadingOnScreen' style='height:100%; width:100%; position:absolute'>
-    <div class="sirius-loading">
+    <div class="sirius-loading" style="padding-left:64px;">
         <img style="height: 125px; width: 125px" src="/build/global/img/logos/2021/svg/icon-secundary.svg"
              class="img-responsive"/>
     </div>
