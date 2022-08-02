d3a5099706b439aa596b1e0b3838fd5515dec17c
lucianno <luciannonascimento@cloudfox.net>
2022-08-02 14:45:09 -0300

    fix margins of select company at mobile

--- resources/views/layouts/company-select.blade.php
+++ resources/views/layouts/company-select.blade.php
@@ -1,7 +1,7 @@
 <!-- EMPRESAS  -->
 <div class="<?php
 
-echo (!empty($version) && $version=='mobile') ? 'p-20 pb-0' : 'pr-20';
+echo (!empty($version) && $version=='mobile') ? 'p-10 pb-0' : 'pr-20';
 
 ?>" id="company-select<?php
 
