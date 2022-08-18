41413756c3cc98b38202edc622810c7e6e9c8e62
Julio Leichtweis <julioleichtweis@gmail.com>
2022-08-18 12:18:49 -0300

    improving tracking shopify postback endpoint performance

--- Modules/PostBack/Http/Controllers/PostBackShopifyController.php
+++ Modules/PostBack/Http/Controllers/PostBackShopifyController.php
@@ -5,18 +5,9 @@ namespace Modules\PostBack\Http\Controllers;
 use App\Jobs\ProcessShopifyPostbackJob;
 use App\Jobs\ProcessShopifyTrackingPostbackJob;
 use Exception;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
-use Illuminate\Support\Facades\Log;
-use Modules\Core\Entities\PostbackLog;
-use Modules\Core\Entities\Project;
-use Vinkla\Hashids\Facades\Hashids;
 
-/**
- * Class PostBackShopifyController
- * @package Modules\PostBack\Http\Controllers
- */
 class PostBackShopifyController extends Controller
 {
 
@@ -24,7 +15,7 @@ class PostBackShopifyController extends Controller
     {
         try {
             $requestData = $request->all();
-            $projectId = current(Hashids::decode($request->project_id));
+            $projectId = hashids_decode($request->project_id);
 
             ProcessShopifyTrackingPostbackJob::dispatch($projectId, $requestData)->onQueue(
                 "postback-shopify-tracking"
@@ -40,30 +31,14 @@ class PostBackShopifyController extends Controller
     public function postBackListener(Request $request)
     {
         try {
-            $postBackLogModel = new PostbackLog();
-            $projectModel = new Project();
-
             $requestData = $request->all();
 
-            $postBackLogModel->create([
-                "origin" => 3,
-                "data" => json_encode($requestData),
-                "description" => "shopify",
-            ]);
+            $projectId = hashids_decode($request->project_id);
+            ProcessShopifyPostbackJob::dispatch($projectId, $requestData)->onQueue("low");
 
-            $projectId = current(Hashids::decode($request->project_id));
-            $project = $projectModel->find($projectId);
-
-            if (!empty($project)) {
-                ProcessShopifyPostbackJob::dispatch($projectId, $requestData)->onQueue("low");
-
-                return response()->json(["message" => "success"]);
-            } else {
-                Log::warning("Shopify postback - projeto não encontrado");
-                //projeto nao existe
-                return response()->json(["message" => "project not found"]);
-            }
-        } catch (\Exception $e) {
+            return response()->json(["message" => "success"]);
+        } catch (Exception $e) {
+            report($e);
             return response()->json(["message" => "error processing postback"]);
         }
     }
