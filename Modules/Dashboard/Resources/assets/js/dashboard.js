ad6d66515a6efe25ce84542c073d6bd38f75ca60
Edwin Medina <edwingarcia@cloudfox.net>
2022-08-26 15:53:39 -0400

    fix loading in dashboard account health

--- Modules/Dashboard/Resources/assets/js/dashboard.js
+++ Modules/Dashboard/Resources/assets/js/dashboard.js
@@ -20,7 +20,7 @@ $('.company-navbar').change(function () {
 
         }
     });
-    loadingOnAccountsHealth('.sirius-performance > .card','150px');
+    loadingOnAccountsHealth('.sirius-performance > .card','60px');
     loadingOnAccountsHealth('.sirius-account > .card');
     $(".sirius-cashback > .card").addClass("d-none");
     loadingOnChart('#chart-loading');
