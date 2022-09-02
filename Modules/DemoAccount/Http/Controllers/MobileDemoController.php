fd4aba211fbf074414ee15507ac9ea32ebe698c2
Edwin Medina <edwingarcia@cloudfox.net>
2022-09-02 15:21:15 -0400

    feat: demo account for mobile

---
+++ Modules/DemoAccount/Http/Controllers/MobileDemoController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\DemoAccount\Http\Controllers;
+
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Modules\Mobile\Http\Controllers\MobileController;
+
+class MobileDemoController extends MobileController
+{
+
+}
