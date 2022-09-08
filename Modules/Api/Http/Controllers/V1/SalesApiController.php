d5e4f218df8b1e2b25a22870c9d89ae2fe38c7fa
sheldrysouzacloudfox <sheldrysouza@cloudfox.net>
2022-09-08 10:34:42 -0300

    fix: adjusts get sales

--- Modules/Api/Http/Controllers/V1/SalesApiController.php
+++ Modules/Api/Http/Controllers/V1/SalesApiController.php
@@ -8,20 +8,15 @@ use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Validator;
 use Modules\Api\Http\Requests\V1\SalesApiRequest;
 use Modules\Api\Transformers\V1\SalesApiResource;
-use Modules\Core\Entities\Company;
-use Modules\Core\Entities\Sale;
-use Modules\Core\Entities\Transaction;
-use Modules\Core\Entities\User;
-use Modules\Core\Services\Api\SaleApiService;
-use Modules\Sales\Transformers\TransactionResource;
-use Vinkla\Hashids\Facades\Hashids;
+use Modules\Core\Services\Api\V1\SalesApiService;
 
 class SalesApiController extends Controller
 {
-    public function index(Request $request)
+    public function getSales(Request $request)
     {
         try {
             $data = $request->all();
+
             $verifyRequest = new SalesApiRequest();
             $validator = Validator::make(
                 $data,
@@ -33,62 +28,9 @@ class SalesApiController extends Controller
                 return response()->json($validator->errors()->toArray());
             }
 
-            $saleModel = new Sale();
-            $transactionModel = new Transaction();
-            $transactions = $transactionModel->join("sales", "sales.id", "transactions.sale_id");
+            $sales = SalesApiService::getSalesQueryBuilder($data);
 
-            if (!empty($data["transaction"])) {
-                $transaction_id = current(Hashids::connection('sale_id')->decode($data["transaction"]));
-
-                $transactions->where("sales.id", $transaction_id);
-            }
-
-            if (!empty($data["company"])) {
-                $companies = Hashids::decode($data["company"]);
-            } else {
-                $companies = Company::where("user_id", request()->user_id)->pluck("id")->toArray();
-            }
-
-            $transactions->whereIn("transactions.company_id", $companies);
-
-            if (!empty($data["user"])) {
-                $user_id = current(Hashids::decode($data["user"]));
-                $subsellers = User::where("subseller_owner_id", request()->user_id)->where("id", $user_id)->pluck("id")->toArray();
-            } else {
-                $subsellers = User::where("subseller_owner_id", request()->user_id)->pluck("id")->toArray();
-                array_push($subsellers, request()->user_id);
-            }
-
-            $transactions->whereIn("sales.owner_id", $subsellers);
-
-            if (!empty($data["status"])) {
-                $transactions->where("sales.status", $saleModel->present()->getStatus($data["status"]));
-            } else {
-                $transactions->whereIn("sales.status", [
-                    Sale::STATUS_APPROVED,
-                    Sale::STATUS_PENDING,
-                    Sale::STATUS_CHARGEBACK,
-                    Sale::STATUS_REFUNDED,
-                    Sale::STATUS_PARTIAL_REFUNDED,
-                    Sale::STATUS_IN_REVIEW,
-                    Sale::STATUS_CANCELED_ANTIFRAUD,
-                    Sale::STATUS_IN_DISPUTE,
-                ]);
-            }
-
-            if (!empty($data['date_type']) && !empty($data['date_range'])) {
-                $dateType = $data["date_type"];
-                $dateRange = foxutils()->validateDateRange($data["date_range"]);
-
-                $transactions->whereBetween("sales.".$dateType, [
-                    $dateRange[0] . " 00:00:00",
-                    $dateRange[1] . " 23:59:59",
-                ]);
-            }
-
-            $query = $transactions->orderByDesc("sales.start_date")->simplePaginate(10);
-
-            return SalesApiResource::collection($query);
+            return SalesApiResource::collection($sales->simplePaginate(10));
         } catch (Exception $e) {
             report($e);
             return response()->json(["message" => "Erro ao carregar vendas"], 400);
