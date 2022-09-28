91a0cc179fe2e3906562a0dbc76fa624818e45ba
lucianno <luciannonascimento@cloudfox.net>
2022-09-28 16:06:29 -0300

    fix bug in chrome: position of more info button

--- Modules/Trackings/Resources/views/index.blade.php
+++ Modules/Trackings/Resources/views/index.blade.php
@@ -10,7 +10,7 @@
     <div class="page pb-0 "
          style="margin-bottom: 0px !important;">
 
-    @include('layouts.company-select',['version'=>'mobile'])
+        @include('layouts.company-select', ['version' => 'mobile'])
 
         <div style="display: none"
              class="page-header container">
@@ -282,13 +282,14 @@
                             </div>
                             <div class="col-sm-6 col-md-6 col-xl-3 col-12 mb-15 mb-sm-0 form-icons">
                                 <label for="date_updated">Data de aprovação venda</label>
-                                <i style="right: 30px; margin-top: 13px;"
+                                <i style="right: 30px; margin-top: -9px;"
                                    class="form-control-icon form-control-icon-right o-agenda-1 font-size-25"></i>
                                 <input name='date_updated'
                                        id="date_updated"
                                        class="input-pad"
                                        placeholder="Clique para editar..."
-                                       readonly>
+                                       readonly
+                                       style="margin-bottom: 44px">
                             </div>
                             <div class="col-sm-6 col-md-6 col-xl-3 col-12 d-flex flex-column justify-content-center">
                                 <label for="tracking_problem"
