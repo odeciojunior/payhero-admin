4d3753ebed76ba159294e5a9f4ab6a8c7f21d3e2
Julio Leichtweis <julioleichtweis@gmail.com>
2022-09-30 12:52:10 -0300

    fix check account approved

--- Modules/Core/Transformers/CompaniesSelectResource.php
+++ Modules/Core/Transformers/CompaniesSelectResource.php
@@ -24,12 +24,9 @@ class CompaniesSelectResource extends JsonResource
 
         $companyDocumentStatus = $companyDocumentValidated ? "approved" : "pending";
 
-        $userAddressDocumentStatus = (new User())->present()->getAddressDocumentStatus(
-            $this->user->address_document_status
-        );
-        $userPersonalDocumentStatus = (new User())->present()->getAddressDocumentStatus(
-            $this->user->personal_document_status
-        );
+        $userAddressDocumentStatus = (new User())
+            ->present()
+            ->getAddressDocumentStatus($this->user->address_document_status);
 
         $bankAccount = CompanyBankAccount::where("company_id", $this->id)
             ->where("is_default", true)
@@ -40,82 +37,101 @@ class CompaniesSelectResource extends JsonResource
         if (
             $companyDocumentStatus == "approved" &&
             $userAddressDocumentStatus == "approved" &&
-            $userPersonalDocumentStatus == "approved"
+            $this->user->biometry_status == User::BIOMETRY_STATUS_APPROVED
         ) {
             $companyIsApproved = true;
         }
 
         $activeFlag = false;
-        if (
-            $this->active_flag &&
-            $this->user->account_is_approved
-        ) {
+        if ($this->active_flag && $this->user->account_is_approved) {
             $activeFlag = true;
         }
 
         // SEARCH AFFILIATED PROJECTS
-        $affiliates = Affiliate::select('affiliates.project_id as id', 'projects.name', 'users_projects.order_priority as order_p', 'projects.status',DB::Raw("'' as prefix"))
-            ->join('projects','projects.id','=','affiliates.project_id')
-            ->join('users_projects', 'users_projects.project_id', '=', 'projects.id')
-            ->where('affiliates.company_id',$this->id);
+        $affiliates = Affiliate::select(
+            "affiliates.project_id as id",
+            "projects.name",
+            "users_projects.order_priority as order_p",
+            "projects.status",
+            DB::Raw("'' as prefix")
+        )
+            ->join("projects", "projects.id", "=", "affiliates.project_id")
+            ->join("users_projects", "users_projects.project_id", "=", "projects.id")
+            ->where("affiliates.company_id", $this->id);
 
-        if(auth()->user()->deleted_project_filter)
-            $affiliates = $affiliates->whereIn('projects.status', [1,2]);
-        else
-            $affiliates = $affiliates->where('projects.status',1);
+        if (auth()->user()->deleted_project_filter) {
+            $affiliates = $affiliates->whereIn("projects.status", [1, 2]);
+        } else {
+            $affiliates = $affiliates->where("projects.status", 1);
+        }
 
-        $affiliates = $affiliates->orderBy('projects.status')
-            ->orderBy('order_p')
-            ->orderBy('projects.id', 'DESC');
+        $affiliates = $affiliates
+            ->orderBy("projects.status")
+            ->orderBy("order_p")
+            ->orderBy("projects.id", "DESC");
 
         // SEARCH OWN PROJECTS
-        $projects = CheckoutConfig::select('checkout_configs.project_id as id','projects.name', 'users_projects.order_priority as order_p', 'projects.status',DB::Raw("'' as prefix"))
-            ->join('projects','projects.id','=','checkout_configs.project_id')
-            ->join('users_projects', 'users_projects.project_id', '=', 'projects.id')
-            ->where('checkout_configs.company_id',$this->id);
+        $projects = CheckoutConfig::select(
+            "checkout_configs.project_id as id",
+            "projects.name",
+            "users_projects.order_priority as order_p",
+            "projects.status",
+            DB::Raw("'' as prefix")
+        )
+            ->join("projects", "projects.id", "=", "checkout_configs.project_id")
+            ->join("users_projects", "users_projects.project_id", "=", "projects.id")
+            ->where("checkout_configs.company_id", $this->id);
 
-        if(auth()->user()->deleted_project_filter)
-            $projects = $projects->whereIn('projects.status', [1,2]);
-        else
-            $projects = $projects->where('projects.status',1);
+        if (auth()->user()->deleted_project_filter) {
+            $projects = $projects->whereIn("projects.status", [1, 2]);
+        } else {
+            $projects = $projects->where("projects.status", 1);
+        }
 
-        $tokens = DB::table('api_tokens')->select('id as project_id','description as name',DB::Raw("'1' as order_p"),DB::Raw("'1' as status"),DB::Raw("'TOKEN-' as prefix"))
-            ->where('user_id',$this->user->id)
-            ->whereIn('integration_type_enum',[4,5])
-            ->whereNull('deleted_at')
-            ->where('company_id',$this->id);
+        $tokens = DB::table("api_tokens")
+            ->select(
+                "id as project_id",
+                "description as name",
+                DB::Raw("'1' as order_p"),
+                DB::Raw("'1' as status"),
+                DB::Raw("'TOKEN-' as prefix")
+            )
+            ->where("user_id", $this->user->id)
+            ->whereIn("integration_type_enum", [4, 5])
+            ->whereNull("deleted_at")
+            ->where("company_id", $this->id);
 
-        $projects = $projects->orderBy('projects.status')
-            ->orderBy('order_p')
-            ->orderBy('projects.id', 'DESC')
+        $projects = $projects
+            ->orderBy("projects.status")
+            ->orderBy("order_p")
+            ->orderBy("projects.id", "DESC")
             ->union($affiliates)
             ->union($tokens)
             ->get()
             ->map(function ($project) {
-                return (object)[
-                    'id' => $project->prefix.hashids_encode($project->id),
-                    'name'=>$project->name,
-                    'order_p'=>$project->order_p,
-                    'status'=>$project->status,
+                return (object) [
+                    "id" => $project->prefix . hashids_encode($project->id),
+                    "name" => $project->name,
+                    "order_p" => $project->order_p,
+                    "status" => $project->status,
                 ];
             });
 
-
         return [
-            'id' => Hashids::encode($this->id),
-            'country' => $this->country,
-            'name' => $this->company_type == 1 ? 'Pessoa física' : $this->fantasy_name,
-            'document' => foxutils()->getDocument($this->document),
-            'company_document_status' => $companyDocumentStatus,
-            'company_has_sale_before_getnet' => auth()->user()->has_sale_before_getnet,
-            'active_flag' => $activeFlag,
-            'has_pix_key' => !empty($bankAccount) && $bankAccount->transfer_type=='PIX',
-            'company_type' => $this->present()->getCompanyType($this->company_type),
-            'user_address_document_status' => $userAddressDocumentStatus,
-            'user_personal_document_status' => $userPersonalDocumentStatus,
-            'company_is_approved' => $companyIsApproved,
-            'order_priority' => $this->order_priority,
-            'projects' => $projects
+            "id" => Hashids::encode($this->id),
+            "country" => $this->country,
+            "name" => $this->company_type == 1 ? "Pessoa física" : $this->fantasy_name,
+            "document" => foxutils()->getDocument($this->document),
+            "company_document_status" => $companyDocumentStatus,
+            "company_has_sale_before_getnet" => auth()->user()->has_sale_before_getnet,
+            "active_flag" => $activeFlag,
+            "has_pix_key" => !empty($bankAccount) && $bankAccount->transfer_type == "PIX",
+            "company_type" => $this->present()->getCompanyType($this->company_type),
+            "user_address_document_status" => $userAddressDocumentStatus,
+            "user_personal_document_status" => $this->user->biometry_status == User::BIOMETRY_STATUS_APPROVED,
+            "company_is_approved" => $companyIsApproved,
+            "order_priority" => $this->order_priority,
+            "projects" => $projects,
         ];
     }
 }
