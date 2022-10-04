78d7f5f35f9b4b8e477b85850bdfdfedf88140e3
Henrique Brites <henriquebrites@live.com>
2022-10-04 16:34:03 -0400

    hotfix: remove command

--- app/Console/Commands/GenericCommand.php
+++ app/Console/Commands/GenericCommand.php
@@ -4,8 +4,6 @@ namespace App\Console\Commands;
 
 use Exception;
 use Illuminate\Console\Command;
-use Modules\Core\Entities\CompanyBankAccount;
-use Illuminate\Support\Str;
 
 class GenericCommand extends Command
 {
@@ -40,37 +38,5 @@ class GenericCommand extends Command
      */
     public function handle()
     {
-        try {
-            
-            foreach (CompanyBankAccount::cursor() as $companieBankAccount) {
-
-                if ($companieBankAccount->transfer_type == "PIX") {
-
-                    if (str_contains($companieBankAccount->key_pix, "@")) {
-                        $companieBankAccount->type_key_pix = "EMAIL";
-                    } elseif (str_contains($companieBankAccount->key_pix, "-") and (strlen($companieBankAccount->key_pix) == 36)) {
-                        $companieBankAccount->type_key_pix = "CHAVE_ALEATORIA";
-                    } elseif (str_contains($companieBankAccount->key_pix, "/") and (strlen($companieBankAccount->key_pix) == 18)) {
-                        $companieBankAccount->type_key_pix = "CNPJ";
-                    } elseif (str_contains($companieBankAccount->key_pix, ".") and str_contains($companieBankAccount->key_pix, "-") and (strlen($companieBankAccount->key_pix) == 14)) {
-                        $companieBankAccount->type_key_pix = "CNPJ";
-                    } elseif (strlen($companieBankAccount->key_pix) == strlen(foxutils()->onlyNumbers($companieBankAccount->key_pix))) {
-                        $companieBankAccount->type_key_pix = "TELEFONE";
-                    } else {
-                        $this->line("id: " . $companieBankAccount->id . " - " . $companieBankAccount->key_pix);
-                    }
-
-                    $companieBankAccount->save();
-
-                    if (isset($companieBankAccount->type_key_pix) && in_array($companieBankAccount->type_key_pix, ['TELEFONE', 'CPF', 'CNPJ'])) {
-                        $companieBankAccount->key_pix = foxutils()->onlyNumbers($companieBankAccount->key_pix);
-                        //$this->line("id: " . $companieBankAccount->id . " - " . $companieBankAccount->key_pix);
-                        $companieBankAccount->save();
-                    }
-                }
-            }
-        } catch (Exception $ex) {
-            report($ex->getMessage());
-        }
     }
 }
