01e782f88108605efeab5d926e9ff17e59799fd7
Edwin Medina <edwingarcia@cloudfox.net>
2022-10-06 16:26:25 -0400

    fix get cashbacks in reports

--- app/Console/Commands/GenericCommand.php
+++ app/Console/Commands/GenericCommand.php
@@ -2,12 +2,7 @@
 
 namespace App\Console\Commands;
 
-use Exception;
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\DB;
-use Modules\Core\Entities\Gateway;
-use Modules\Core\Entities\GatewayFlagTax;
-use Modules\Core\Services\FoxUtils;
 
 class GenericCommand extends Command
 {
@@ -43,42 +38,5 @@ class GenericCommand extends Command
     public function handle()
     {
 
-            for ($i = 1; $i <= 12; $i++) {
-                // $installmentTax =2.25;
-                // if($i > 6){
-                //     $installmentTax =3.16;
-                // }elseif($i>1){
-                //     $installmentTax =2.73;
-                // }
-
-                $installmentTax = ($i * 2.05);
-                $this->line($i.'=>'.$installmentTax);
-            }
-    }
-
-    public function updateTaxes(){
-        $gatewayFlags = DB::table("gateway_flags")->whereIn('gateway_id',[Gateway::IUGU_PRODUCTION_ID,Gateway::IUGU_SANDBOX_ID])->get();
-
-        foreach ($gatewayFlags as $flag)
-        {
-            for ($i = 1; $i <= 12; $i++) {
-                $installmentTax =2.25;
-                if($i > 6){
-                    $installmentTax =3.16;
-                }elseif($i>1){
-                    $installmentTax =2.73;
-                }
-
-                $installmentTax+= $i==1? ($i * 2.05) : ($i * 2.05)/2;
-                $this->line($i.'=>'.$installmentTax);
-
-                $taxFlag = GatewayFlagTax::where('gateway_flag_id',$flag->id)->where('installments',$i)->where('type_enum',1)->first();
-                if(!empty($taxFlag)){
-                    $taxFlag->update([
-                        "percent" => round($installmentTax,2)
-                    ]);
-                }
-            }
-        }
     }
 }
