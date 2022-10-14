d4d03b44ba6f1626b316749959e9d6b83ecfef3f
Fabio Rosa <fabiorosa@impa.br>
2022-10-14 14:51:13 -0300

    add column in company balance

--- database/migrations/2022_10_13_100255_add_column_blocked_balance_in_company_balances.php
+++ database/migrations/2022_10_13_100255_add_column_blocked_balance_in_company_balances.php
@@ -21,6 +21,19 @@ return new class extends Migration {
                 ->integer("vega_total_balance")
                 ->default(0)
                 ->after("vega_blocked_balance");
+            $table
+                ->integer("vega_available_balance_with_blocked")
+                ->default(0)
+                ->after("vega_total_balance");
+            $table
+                ->integer("vega_pending_balance_with_blocked")
+                ->default(0)
+                ->after("vega_available_balance_with_blocked");
+            $table
+                ->integer("vega_total_balance_with_blocked")
+                ->default(0)
+                ->after("vega_pending_balance_with_blocked");
+
 
             $table
                 ->integer("asaas_blocked_balance")
@@ -31,6 +44,20 @@ return new class extends Migration {
                 ->default(0)
                 ->after("asaas_blocked_balance");
             $table
+                ->integer("asaas_available_balance_with_blocked")
+                ->default(0)
+                ->after("asaas_total_balance");
+            $table
+                ->integer("asaas_pending_balance_with_blocked")
+                ->default(0)
+                ->after("asaas_available_balance_with_blocked");
+            $table
+                ->integer("asaas_total_balance_with_blocked")
+                ->default(0)
+                ->after("asaas_pending_balance_with_blocked");
+
+
+            $table
                 ->integer("cielo_blocked_balance")
                 ->default(0)
                 ->after("cielo_pending_balance");
@@ -38,6 +65,20 @@ return new class extends Migration {
                 ->integer("cielo_total_balance")
                 ->default(0)
                 ->after("cielo_blocked_balance");
+            $table
+                ->integer("cielo_available_balance_with_blocked")
+                ->default(0)
+                ->after("cielo_total_balance");
+            $table
+                ->integer("cielo_pending_balance_with_blocked")
+                ->default(0)
+                ->after("cielo_available_balance_with_blocked");
+            $table
+                ->integer("cielo_total_balance_with_blocked")
+                ->default(0)
+                ->after("cielo_pending_balance_with_blocked");
+
+
             $table
                 ->integer("getnet_blocked_balance")
                 ->default(0)
@@ -47,6 +88,20 @@ return new class extends Migration {
                 ->default(0)
                 ->after("getnet_blocked_balance");
             $table
+                ->integer("getnet_available_balance_with_blocked")
+                ->default(0)
+                ->after("getnet_total_balance");
+            $table
+                ->integer("getnet_pending_balance_with_blocked")
+                ->default(0)
+                ->after("getnet_available_balance_with_blocked");
+            $table
+                ->integer("getnet_total_balance_with_blocked")
+                ->default(0)
+                ->after("getnet_pending_balance_with_blocked");
+
+
+            $table
                 ->integer("gerencianet_blocked_balance")
                 ->default(0)
                 ->after("gerencianet_pending_balance");
@@ -55,9 +110,27 @@ return new class extends Migration {
                 ->default(0)
                 ->after("gerencianet_blocked_balance");
             $table
+                ->integer("gerencianet_available_balance_with_blocked")
+                ->default(0)
+                ->after("gerencianet_total_balance");
+            $table
+                ->integer("gerencianet_pending_balance_with_blocked")
+                ->default(0)
+                ->after("gerencianet_available_balance_with_blocked");
+            $table
+                ->integer("gerencianet_total_balance_with_blocked")
+                ->default(0)
+                ->after("gerencianet_pending_balance_with_blocked");
+
+
+            $table
                 ->integer("total_balance")
                 ->default(0)
                 ->after("gerencianet_total_balance");
+            $table
+                ->integer("total_balance_with_blocked")
+                ->default(0)
+                ->after("total_balance");
         });
     }
 
