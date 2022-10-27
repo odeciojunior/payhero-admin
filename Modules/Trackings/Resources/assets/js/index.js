0dde35eaf8bc78f696f6c5819ce345c0717399fb
Jonas Ferutcci <jonas.ferutcci@gmail.com>
2022-10-27 17:13:30 -0300

    fix pagination

--- Modules/Trackings/Resources/assets/js/index.js
+++ Modules/Trackings/Resources/assets/js/index.js
@@ -111,6 +111,7 @@ $(() => {
     });
 
     $(document).on("click", ".tracking-close", function (event) {
+        $(event.target).parent().prev().addClass("col-5");
 
         let row = $(this).parent().parent().parent();
         row.find(".input-tracking-code")
@@ -496,7 +497,6 @@ $(() => {
                 $("#pagination-trackings").html("");
 
                 if (isEmpty(response.data)) {
-                    $("#container-pagination-trackings").removeClass("d-flex").addClass("d-none")
                     $("#dados_tabela").html(`
                     <tr class="text-center">
                       <td colspan="6" style="vertical-align: middle;height:257px;">
@@ -520,17 +520,17 @@ $(() => {
                     dados += "<tr>";
 
                     dados += `${lastSale !== tracking.sale
-                        ? `<td class="detalhes_venda pointer table-title col-sm-1" venda="${tracking.sale}">
+                        ? `<td class="detalhes_venda pointer table-title col-sm-1" venda="${tracking.sale}" style="padding-right:4px">
                                     #${tracking.sale}
                                 </td>`
                         : `<td></td>`
                         }`;
 
                     dados += `<td>
-                                    <span style="max-width: 330px; display:block; margin: 0px 0px 0px 0px;">
+                                    <div class="ellipsis-text" style="max-width: 240px; display:block; margin: 0px 0px 0px 0px;">
                                     ${tracking.product.amount}x ${tracking.product.name}
                                     ${tracking.product.description ? "(" + tracking.product.description + ")" : ""}
-                                    </span>
+                                    </div>
                                 </td>`;
 
                     dados += `<td class="col-sm-1">${tracking.approved_date}</td>`;
@@ -542,7 +542,10 @@ $(() => {
                                 </td>`;
 
                     dados += `<td style="width: 2%;padding: 0px !important;">
-                                    ${systemStatus[tracking.system_status_enum]}
+                                    ${systemStatus[tracking.system_status_enum] != undefined
+                            ? systemStatus[tracking.system_status_enum]
+                            : ""
+                        }
                                     ${tracking.is_chargeback_recovered
                             ? `<img class="orange-gradient ml-10" width="20px" src="/build/global/img/svg/chargeback.svg" title="Chargeback recuperado">`
                             : ``
@@ -554,7 +557,7 @@ $(() => {
 
                     let save = `<div class="save-close buttons d-flex px-0" style="max-height: 35px;">
                             <a id='pencil' class='o-checkmark-1 text-white tracking-save pointer mr-10 text-center default-buttons' title="Salvar" pps='${tracking.pps_id}'style="display:none; height:34px"></a>
-                            <div class='tracking-close pointer' data-code='${tracking.tracking_code}' title="Fechar" style="display:none; padding: 0px 7px 0px 9px; height:34px">
+                            <div class='tracking-close pointer' data-code='${tracking.tracking_code}' title="Fechar" style="display:none; padding: 0px 7px 0px 9px !important; height:34px">
                                 &#x2715
                             </div>
                         </div>`;
@@ -569,7 +572,9 @@ $(() => {
                         : ``
                         }`;
 
-                    dados += `${tracking.tracking_status_enum && tracking.tracking_status_enum != 3
+                    dados += `${tracking.tracking_status_enum &&
+                        (tracking.tracking_status_enum != 3 ||
+                            (tracking.tracking_status_enum == 3 && tracking.system_status_enum == 5))
                         ? `<div class="col-7" >
                             <input maxlength="18" minlength="10" class="mr-10 form-control font-weight-bold input-tracking-code" readonly placeholder="Informe o código de rastreio" style="border-radius: 8px;" value="${tracking.tracking_code}">
                             </div>
@@ -586,7 +591,9 @@ $(() => {
                         : ``
                         }`;
 
-                    dados += `${tracking.tracking_status_enum && tracking.tracking_status_enum == 3
+                    dados += `${tracking.tracking_status_enum &&
+                        tracking.tracking_status_enum == 3 &&
+                        tracking.system_status_enum != 5
                         ? `<div class="col-7">${tracking.tracking_code}</div>
                         <div class="edit-detail" style="margin-top:-5px; text-align:right; margin-left: 62px;">
                             <a class='tracking-detail pointer col-5' title="Visualizar" tracking='${tracking.id}' style="margin-right: 0;">
@@ -605,8 +612,6 @@ $(() => {
                     lastSale = tracking.sale;
                 });
                 pagination(response, "trackings", index);
-                $("#container-pagination-trackings").removeClass("d-none").addClass("d-flex")
-
             },
             complete: (response) => {
                 unlockSearch($("#bt_filter"));
