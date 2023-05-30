6c41d602130b5195a428894a64203c3eefc4911e
Edwin Medina <edwingarcia@kirvano.com>
2023-05-29 21:59:25 -0400

    fix: update gateway model

--- Modules/Core/Entities/Gateway.php
+++ Modules/Core/Entities/Gateway.php
@@ -55,18 +55,6 @@ class Gateway extends Model
     public const IUGU_SANDBOX_ID = 8;
 
     // disabled
-    // public const PAGARME_PRODUCTION_ID = 1;
-    // public const PAGARME_SANDBOX_ID = 2;
-    // public const ZOOP_PRODUCTION_ID = 3;
-    // public const ZOOP_SANDBOX_ID = 4;
-    public const CIELO_PRODUCTION_ID = 5;
-    public const CIELO_SANDBOX_ID = 6;
-    public const GETNET_PRODUCTION_ID = 15;
-    public const GETNET_SANDBOX_ID = 14;
-    public const GERENCIANET_PRODUCTION_ID = 18;
-    public const GERENCIANET_SANDBOX_ID = 19;
-    public const ASAAS_PRODUCTION_ID = 8;
-    public const ASAAS_SANDBOX_ID = 20;
 
     public const PAYMENT_STATUS_CONFIRMED = "CONFIRMED";
 
@@ -107,22 +95,6 @@ class Gateway extends Model
     public static function getServiceById($gatewayId)
     {
         switch ($gatewayId) {
-            case self::ASAAS_PRODUCTION_ID:
-            case self::ASAAS_SANDBOX_ID:
-                return new AsaasService();
-
-            case self::GERENCIANET_PRODUCTION_ID:
-            case self::GERENCIANET_SANDBOX_ID:
-                return new GerencianetService();
-
-            case self::GETNET_PRODUCTION_ID:
-            case self::GETNET_SANDBOX_ID:
-                return new GetnetService();
-
-            case self::CIELO_PRODUCTION_ID:
-            case self::CIELO_SANDBOX_ID:
-                return new CieloService();
-
             case self::SAFE2PAY_PRODUCTION_ID:
             case self::SAFE2PAY_SANDBOX_ID:
                 return new Safe2PayService();
