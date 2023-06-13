71a8cc1904f3f1d03970f035c2ccc0622c5c68c3
Sheldr Souza <sheldrysouza@cloudfox.net>
2023-06-13 13:20:20 -0300

    fix: route to create discount coupons recovery removed

--- routes/web.php
+++ routes/web.php
@@ -25,34 +25,6 @@ Route::get("/up", function () {
     return 'System is up';
 });
 
-Route::get("/discount-coupons-recovery", function () {
-    $projects = Project::where("status", Project::STATUS_ACTIVE)->get();
-
-    foreach ($projects as $project) {
-        DiscountCoupon::create([
-            "project_id"            => $project->id,
-            "name"                  => "Desconto 10%",
-            "type"                  => 0,
-            "value"                 => 10,
-            "code"                  => "NEXX10",
-            "status"                => 1,
-            "rule_value"            => 0,
-            "recovery_flag"         => true,
-        ]);
-
-        DiscountCoupon::create([
-            "project_id"            => $project->id,
-            "name"                  => "Desconto 20%",
-            "type"                  => 0,
-            "value"                 => 20,
-            "code"                  => "NEXX20",
-            "status"                => 1,
-            "rule_value"            => 0,
-            "recovery_flag"         => true,
-        ]);
-    }
-});
-
 Route::get("/termos", function () {
     return response()->file(public_path("terms-of-use.pdf"));
 });
