671c54cdfdc0b6c8b444f0da30cde8d09d60c667
Edwin Medina <edwingarcia@kirvano.com>
2023-11-16 15:26:29 -0400

    fix: vega service

--- Modules/Core/Services/Gateways/VegaService.php
+++ Modules/Core/Services/Gateways/VegaService.php
@@ -10,18 +10,23 @@ class VegaService extends GatewayServicesAbstract
     public function __construct()
     {
         $this->gatewayIds = [
-            Gateway::SAFE2PAY_PRODUCTION_ID, Gateway::SAFE2PAY_SANDBOX_ID,
-            Gateway::IUGU_PRODUCTION_ID, Gateway::IUGU_SANDBOX_ID,
-            Gateway::VEGA_PRODUCTION_ID, Gateway::VEGA_SANDBOX_ID
+            Gateway::SAFE2PAY_PRODUCTION_ID,
+            Gateway::SAFE2PAY_SANDBOX_ID,
+            Gateway::IUGU_PRODUCTION_ID,
+            Gateway::IUGU_SANDBOX_ID,
+            Gateway::ABMEX_PRODUCTION_ID,
+            Gateway::ABMEX_SANDBOX_ID,
+            Gateway::VEGA_PRODUCTION_ID,
+            Gateway::VEGA_SANDBOX_ID,
         ];
 
-        $this->gatewayName = 'Vega';
+        $this->gatewayName = "Vega";
 
-        $this->companyColumnBalance = 'vega_balance';
+        $this->companyColumnBalance = "vega_balance";
     }
 
-    public function getGatewayId():int
+    public function getGatewayId(): int
     {
-        return foxutils()->isProduction()?Gateway::VEGA_PRODUCTION_ID:Gateway::VEGA_SANDBOX_ID;
+        return foxutils()->isProduction() ? Gateway::VEGA_PRODUCTION_ID : Gateway::VEGA_SANDBOX_ID;
     }
 }
