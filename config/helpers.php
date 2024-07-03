f5d4240117c7a6d0a2388df50c7f7023c2a4ea07
jeanvcastro <jean.castro1@outlook.com>
2024-07-02 22:18:47 -0300

    feat: adjust utmify

--- config/helpers.php
+++ config/helpers.php
@@ -74,7 +74,7 @@ if (!function_exists("getRegionByIp")) {
 if (!function_exists("versionsFile")) {
     function versionsFile()
     {
-        return md5(418);
+        return md5(419);
     }
 }
 
