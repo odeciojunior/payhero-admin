078b5c752763c6a1b7a2d4f727f384e9348226dc
jeanvcastro <jean.castro1@outlook.com>
2024-07-10 02:33:51 -0300

    fix: sale details

--- Modules/Core/Services/SalesRecoveryService.php
+++ Modules/Core/Services/SalesRecoveryService.php
@@ -84,7 +84,7 @@ class SalesRecoveryService
             "checkout.id as checkout_id",
             "checkout.id_log_session",
             DB::raw(
-                "(ifnull(products_sales_api, (plan_sale.amount * plan_sale.plan_value), (products_sales_api.price * products_sales_api.quantity)) as value",
+                "(ifnull(products_sales_api.id, (plan_sale.amount * plan_sale.plan_value), (products_sales_api.price * products_sales_api.quantity))) as value",
             ),
         )
 
