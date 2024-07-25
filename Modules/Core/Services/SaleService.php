7ba3f2e15737d9b67e8fb13e42b4437cc584a824
Odécio Machado <odecio@azcend.com.br>
2024-07-24 23:08:29 -0300

    refactor: update SaleService to fix calculation of total paid value

--- Modules/Core/Services/SaleService.php
+++ Modules/Core/Services/SaleService.php
@@ -282,7 +282,7 @@ class SaleService
                 DB::raw(
                     "count(sales.id) as total_sales,
                               sum(if(transactions.status_enum in ({$transactionStatus}) && sales.status <> {$statusDispute}, transactions.value, 0)) / 100 as commission,
-                              sum(sales.total_paid_value / 100) as total",
+                              sum(sales.total_paid_value) as total",
                 ),
             )
             ->first()
