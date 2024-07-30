d45e2466cbb5f8ee2d5f61de095751918240fcd1
Odécio Machado <odecio@azcend.com.br>
2024-07-30 16:42:51 -0300

    refactor: company-select layout

--- resources/views/layouts/company-select.blade.php
+++ resources/views/layouts/company-select.blade.php
@@ -15,19 +15,10 @@
 @endif
 
 <!-- EMPRESAS  -->
-<div class="<?php
-
-//echo (!empty($version) && $version=='mobile') ? 'p-10 pb-0' : 'pr-30';
-echo 'pr-30';
-
-?>" id="company-select<?php
-
-    //echo (!empty($version) && $version=='mobile') ? '-'.$version : '';
-
-    ?>" style="display:none"><!--d-sm-flex -->
+<div class="pr-10" id="company-select" style="display:none"><!--d-sm-flex -->
     <div class="d-lg-flex align-items-center justify-content-end">
         <div>
-            <select class="sirius-select company-navbar" title="Escolha uma opção"> </select>
+            <select class="sirius-select company-navbar"> </select>
         </div>
     </div>
 </div>
