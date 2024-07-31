d07155c6f22caac0f303a14fe52ebcd3db17a6e0
Odécio Machado <odecio@azcend.com.br>
2024-07-31 08:21:42 -0300

    Revert "fix: company-select erro"
    This reverts commit bee01e2f7c2e1cd8206a74ebe09e6db3abacd829.

--- resources/views/layouts/company-select.blade.php
+++ resources/views/layouts/company-select.blade.php
@@ -15,7 +15,15 @@
 @endif
 
 <!-- EMPRESAS  -->
-<div class="pr-10" id="company-select" style="display:none"><!--d-sm-flex -->
+<div class="<?php
+
+echo (!empty($version) && $version=='mobile') ? 'p-10 pb-0' : 'pr-30';
+
+?>" id="company-select<?php
+
+    echo (!empty($version) && $version=='mobile') ? '-'.$version : '';
+
+    ?>" style="display:none"><!--d-sm-flex -->
     <div class="d-lg-flex align-items-center justify-content-end">
         <div>
             <select class="sirius-select company-navbar"> </select>
