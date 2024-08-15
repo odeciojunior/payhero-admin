8a8cbd34b451ca9dc75a84df37d6eace672e8e07
jeanvcastro <jean.castro1@outlook.com>
2024-08-15 13:17:07 -0300

    refactor: check company

--- app/Console/Commands/CheckCompany.php
+++ app/Console/Commands/CheckCompany.php
@@ -3,8 +3,8 @@
 namespace App\Console\Commands;
 
 use Carbon\Carbon;
-use Illuminate\Console\Command;
 use Exception;
+use Illuminate\Console\Command;
 use Modules\Core\Entities\Company;
 use Modules\Core\Services\CompanyService;
 
@@ -42,79 +42,68 @@ class CheckCompany extends Command
     public function handle()
     {
         try {
-
             $companyService = new CompanyService();
 
             $subThreeDays = Carbon::now()
                 ->subDays(5)
                 ->format("Y-m-d");
 
-            $companies = Company::where('company_type', Company::JURIDICAL_PERSON)
-            ->where("situation->situation_enum", 1)
-            ->where("situation->date_check_situation", "<", $subThreeDays);
+            $companies = Company::where("company_type", Company::JURIDICAL_PERSON)
+                ->where("situation->situation_enum", 1)
+                ->where("situation->date_check_situation", "<", $subThreeDays);
 
             $total = $companies->count();
             $bar = $this->output->createProgressBar($total);
             $bar->start();
 
-            $auxCount = 0;
-            $auxService = 0;
-            foreach ($companies->cursor() as $key=>$company) {
-                $bar->advance();
+            $companies->chunk(100, function ($companies) use ($companyService, $bar) {
+                foreach ($companies as $company) {
+                    $bar->advance();
 
-                if ($auxService == 100) {
-                    $companyService = new CompanyService();
-                    $auxService = 0;
-                }
+                    $cleanDocument = foxutils()->onlyNumbers($company->document);
+                    if (strlen($cleanDocument) == 14) {
+                        $getCompany = $companyService->getCompanyByApiCNPJ($company->document);
 
-                if(strlen(foxutils()->onlyNumbers($company->document)) == 14 ) {
-
-                    $getCompany = $companyService->getCompanyByApiCNPJ($company->document);
-
-                    if($getCompany) {
-
-                        if(isset($getCompany['status']) and $getCompany['status'] == "ERROR"){
-
-                            if((!strtolower(foxutils()->removeAccents($getCompany['message'])) ==  "cnpj invalido")) {
-
-                                $mesage = 'returnSearch: ' . json_encode($getCompany);
-                                $this->warn($mesage);
-                                report(new Exception($mesage));
+                        if ($getCompany) {
+                            if (
+                                !strtolower(foxutils()->removeAccents($getCompany["message"])) ==
+                                "cnpj $cleanDocument invalido"
+                            ) {
+                                $message = "returnSearch: " . json_encode($getCompany);
+                                $this->warn($message);
+                                report(new Exception($message));
                                 continue;
-
                             }
-                            $getCompany['situacao'] = 'invalido';
+
+                            $situation = isset($getCompany["situacaoCadastral"])
+                                ? $companyService->getSituation($getCompany["situacaoCadastral"]["codigo"])
+                                : null;
+
+                            if ($situation) {
+                                $company->update([
+                                    "situation" => [
+                                        "situation" => $situation["situation"],
+                                        "situation_enum" => $situation["situation_enum"],
+                                        "date_check_situation" => Carbon::now()->format("Y-m-d H:i:s"),
+                                        "company_data" => $getCompany,
+                                    ],
+                                ]);
+                            }
                         }
-
-                        $situation = $companyService->getSituation($getCompany['situacaoCadastral']['codigo']);
-
-                        if($situation) {
-
-                            $company->update([
-                                'situation' => [
-                                    'situation' => $situation['situation'],
-                                    'situation_enum' => $situation['situation_enum'],
-                                    'date_check_situation' => Carbon::now()->format("Y-m-d H:i:s"),
-                                    "company_data" => $getCompany,
-                                ]
-                            ]);
-                        }
+                        sleep(21);
+                    } else {
+                        $situation = $companyService->getSituation("invalido ");
+                        $company->update([
+                            "situation" => [
+                                "situation" => $situation["situation"],
+                                "situation_enum" => $situation["situation_enum"],
+                                "date_check_situation" => Carbon::now()->format("Y-m-d H:i:s"),
+                            ],
+                        ]);
                     }
-                    sleep(21);
-                } else {
-                    $situation = $companyService->getSituation('invalido ');
-                    $company->update([
-                        'situation' => [
-                            'situation' => $situation['situation'],
-                            'situation_enum' => $situation['situation_enum'],
-                            'date_check_situation' => Carbon::now()->format("Y-m-d H:i:s"),
-                        ]
-                    ]);
                 }
+            });
 
-                $auxCount ++;
-                $auxService ++;
-            }
             $bar->finish();
         } catch (Exception $e) {
             report($e);
