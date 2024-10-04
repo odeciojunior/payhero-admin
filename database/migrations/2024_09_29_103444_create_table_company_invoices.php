ca9f9e41d8af49e671a06aa876745219301dfdf5
Thyago Hoffman <hoffmandev@outlook.com>
2024-10-04 14:22:07 -0300

    Cria tabela company_invoices

---
+++ database/migrations/2024_09_29_103444_create_table_company_invoices.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('company_invoices', function (Blueprint $table) {
+            $table->bigIncrements("id");
+            $table->unsignedInteger("company_id")
+                    ->nullable()
+                    ->foreign("company_id")
+                    ->references("id")
+                    ->on("companies");
+            $table->string("document")
+                    ->nullable()
+                    ->foreign("document")
+                    ->references("document")
+                    ->on("companies");
+            // $table->string("document", 100)->nullable();
+            $table->string("invoice_id", 100)->nullable();
+            $table->string("value");
+            $table->string("description")->nullable();
+            $table->string("tax_iss");
+            $table->string("invoice_date");
+            $table->timestamps();
+            $table->timestamp("deleted_at");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('company_invoices');
+    }
+};
