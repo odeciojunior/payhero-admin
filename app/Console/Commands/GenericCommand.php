d14eeec1b0305325d3008f159bfff0caa1c41eb7
Edwin Medina <edwingarcia@kirvano.com>
2024-10-09 22:11:08 -0400

    feat: new abmex credentials

--- app/Console/Commands/GenericCommand.php
+++ app/Console/Commands/GenericCommand.php
@@ -3,10 +3,11 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Modules\Core\Services\FoxUtils;
 
 class GenericCommand extends Command
 {
-    protected $signature = "generic:get-encrypted-config {client_id} {client_secret} {auth_basic}";
+    protected $signature = "generic";
     protected $description = "Get encrypted config";
     public function __construct()
     {
@@ -15,5 +16,12 @@ class GenericCommand extends Command
 
     public function handle()
     {
+        dd(
+            FoxUtils::xorEncrypt(
+                json_encode([
+                    "api_key" => "1736|eXAeMSylNF7QsbdNq7wYMtwZ7KMmX4pMKVW7zqvW79c796e9",
+                ])
+            )
+        );
     }
 }
