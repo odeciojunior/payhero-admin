ab821b117874c03160b896e0b6867fb7e72c4c8c
Odécio Machado <odecio@azcend.com.br>
2024-10-30 02:25:46 -0300

    refactor: Update default security reserve taxes migration

--- database/migrations/2024_10_30_003350_alter_security_reserve_taxes.php
+++ database/migrations/2024_10_30_003350_alter_security_reserve_taxes.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AlterDefaultsSecurityTaxAndDays extends Migration
+class AlterDefaultsSecurityTaxAndDays202410 extends Migration
 {
     /**
      * Run the migrations.
