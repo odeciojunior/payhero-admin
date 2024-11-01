fe2cbd1f6582eebe22920319ebe53fb10db02331
Odécio Machado <odecio@azcend.com.br>
2024-10-31 21:57:29 -0300

    Create migration for new table company_invoices

---
+++ database/migrations/2024_10_31_215536_new_table_company_invoices.php
@@ -0,0 +1,65 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Deleta a tabela se ela já existir
+        if (Schema::hasTable('company_invoices')) {
+            Schema::drop('company_invoices');
+        }
+
+        // Cria a tabela
+        Schema::create('company_invoices', function (Blueprint $table) {
+            $table->bigIncrements("id");
+
+            // Definindo a coluna e chave estrangeira para company_id
+            $table->unsignedInteger("company_id")->nullable();
+            $table->foreign("company_id")
+                  ->references("id")
+                  ->on("companies")
+                  ->onDelete("set null");
+
+            // Definindo a coluna e chave estrangeira para document
+            $table->string("document")->nullable();
+            $table->foreign("document")
+                  ->references("document")
+                  ->on("companies")
+                  ->onDelete("set null");
+
+            // Demais colunas
+            $table->string("invoice_id", 100)->nullable();
+            $table->string("value")->nullable();
+            $table->string("description")->nullable();
+            $table->string("tax_iss")->nullable();
+            $table->string("invoice_date")->nullable();
+            $table->integer('status')->nullable();
+            $table->string("notazz_id")->nullable();
+
+            // Timestamps e Soft Deletes
+            $table->timestamps();
+            $table->softDeletes(); // Corrige o campo deleted_at para soft delete
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        if (Schema::hasTable('company_invoices')) {
+            Schema::dropIfExists('company_invoices');
+        }
+    }
+};
