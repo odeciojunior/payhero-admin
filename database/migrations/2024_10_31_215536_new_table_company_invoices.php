cb872c4b39b66ca0b0f52aa120b9e78cc1117974
Odécio Machado <odecio@azcend.com.br>
2024-11-02 14:46:54 -0300

    Refactor: Adjust migration for company_invoices table structure

--- database/migrations/2024_10_31_215536_new_table_company_invoices.php
+++ database/migrations/2024_10_31_215536_new_table_company_invoices.php
@@ -17,43 +17,43 @@ return new class extends Migration
         // Deleta a tabela se ela já existir e estiver vazia
         if (Schema::hasTable('company_invoices') && DB::table('company_invoices')->count() == 0) {
             Schema::drop('company_invoices');
+
+            Schema::table('companies', function (Blueprint $table) {
+                $table->index('document');
+            });
+    
+            // Cria a tabela
+            Schema::create('company_invoices', function (Blueprint $table) {
+                $table->bigIncrements("id");
+    
+                // Definindo a coluna e chave estrangeira para company_id
+                $table->unsignedInteger("company_id")->nullable();
+                $table->foreign("company_id")
+                      ->references("id")
+                      ->on("companies")
+                      ->onDelete("set null");
+    
+                // Definindo a coluna e chave estrangeira para document
+                $table->string("document")->nullable();
+                $table->foreign("document")
+                      ->references("document")
+                      ->on("companies")
+                      ->onDelete("set null");
+    
+                // Demais colunas
+                $table->string("invoice_id", 100)->nullable();
+                $table->string("value")->nullable();
+                $table->string("description")->nullable();
+                $table->string("tax_iss")->nullable();
+                $table->string("invoice_date")->nullable();
+                $table->integer('status')->nullable();
+                $table->string("notazz_id")->nullable();
+    
+                // Timestamps e Soft Deletes
+                $table->timestamps();
+                $table->softDeletes(); // Corrige o campo deleted_at para soft delete
+            });
         }
-       
-        Schema::table('companies', function (Blueprint $table) {
-            $table->index('document');
-        });
-
-        // Cria a tabela
-        Schema::create('company_invoices', function (Blueprint $table) {
-            $table->bigIncrements("id");
-
-            // Definindo a coluna e chave estrangeira para company_id
-            $table->unsignedInteger("company_id")->nullable();
-            $table->foreign("company_id")
-                  ->references("id")
-                  ->on("companies")
-                  ->onDelete("set null");
-
-            // Definindo a coluna e chave estrangeira para document
-            $table->string("document")->nullable();
-            $table->foreign("document")
-                  ->references("document")
-                  ->on("companies")
-                  ->onDelete("set null");
-
-            // Demais colunas
-            $table->string("invoice_id", 100)->nullable();
-            $table->string("value")->nullable();
-            $table->string("description")->nullable();
-            $table->string("tax_iss")->nullable();
-            $table->string("invoice_date")->nullable();
-            $table->integer('status')->nullable();
-            $table->string("notazz_id")->nullable();
-
-            // Timestamps e Soft Deletes
-            $table->timestamps();
-            $table->softDeletes(); // Corrige o campo deleted_at para soft delete
-        });
     }
 
     /**
