4b9293d332ea22acbb99ae005871be041f4ae99a
jeanvcastro <jean.castro1@outlook.com>
2024-12-06 00:54:14 -0300

    fix: nuvemshop event

--- Modules/Core/Events/ImportNuvemshopProductsEvent.php
+++ Modules/Core/Events/ImportNuvemshopProductsEvent.php
@@ -2,10 +2,15 @@
 
 namespace Modules\Core\Events;
 
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
 use Modules\Core\Entities\NuvemshopIntegration;
 
 class ImportNuvemshopProductsEvent
 {
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
     public $integration;
 
     public function __construct(NuvemshopIntegration $integration)
