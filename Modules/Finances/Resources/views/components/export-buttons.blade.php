a2c5d95a5790b2d2a759ee57538dc61953f5aa85
Odécio Machado <odecio@azcend.com.br>
2024-12-18 15:40:21 -0300

    Remove unused export button code and clean up related markup

--- Modules/Finances/Resources/views/components/export-buttons.blade.php
+++ Modules/Finances/Resources/views/components/export-buttons.blade.php
@@ -3,17 +3,17 @@
          id="export-excel">
         <div class="p-2 d-flex justify-content-end align-items-center">
             <span class="o-download-cloud-1 mr-2"></span>
-            <div class="btn-group"
-                 role="group">
+            <!-- <div class="btn-group"
+                 role="group"> -->
                 <!-- <button id="bt_get_xls"
                         type="button"
                         class="btn btn-round btn-default btn-outline btn-pill-left">.XLS
                 </button> -->
-                <button id="bt_get_csv"
+                <!-- <button id="bt_get_csv"
                         type="button"
                         class="btn btn-round btn-default btn-outline btn-pill">.CSV
-                </button>
-            </div>
+                </button> -->
+            <!-- </div> -->
         </div>
     </div>
 </div>
--- Modules/SalesRecovery/Resources/views/index.blade.php
+++ Modules/SalesRecovery/Resources/views/index.blade.php
@@ -18,13 +18,13 @@
             @can('recovery')
             <div class="col-6 text-right">
                 <div class="justify-content-end align-items-center" id="export-excel">
-                    <div class="p-2 d-flex justify-content-end align-items-center">
+                    <!-- <div class="p-2 d-flex justify-content-end align-items-center">
                         <span class="o-download-cloud-1 mr-2"></span>
-                        <div class="btn-group" role="group">
+                        <div class="btn-group" role="group"> -->
                             <!-- <button id="bt_get_xls" type="button" class="btn btn-round btn-default btn-outline btn-pill-left">.XLS</button> -->
-                            <button id="bt_get_csv" type="button" class="btn btn-round btn-default btn-outline btn-pill">.CSV</button>
+                            <!-- <button id="bt_get_csv" type="button" class="btn btn-round btn-default btn-outline btn-pill">.CSV</button>
                         </div>
-                    </div>
+                    </div> -->
                 </div>
             </div>
             @endcan
